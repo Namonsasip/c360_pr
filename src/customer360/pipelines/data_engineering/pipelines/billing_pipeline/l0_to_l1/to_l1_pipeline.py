593bc4e0866adc4ae42aa353cfa32e2064cd98a7
shashank219 <shashank_mishra@mckinsey.com>
2020-01-14 12:20:33 +0700

    billing features top up numbers and volume

--- src/customer360/pipelines/data_engineering/pipelines/billing_pipeline/l0_to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/billing_pipeline/l0_to_l1/to_l1_pipeline.py
@@ -3,14 +3,14 @@ from kedro.pipeline import Pipeline, node
 from src.customer360.utilities.config_parser import node_from_config
 
 
-def create_pipeline(**kwargs):
+def billing_to_l1_pipeline(**kwargs):
     return Pipeline(
         [
             node(
                 node_from_config,
                 ["l0_billing_and_payments_rt_t_recharge_daily",
                  "params:l0_billing_and_payment_feature_top_up_and_count"],
-                "l4_billing_and_payments_topup_and_volume"
-            )
+                "l1_billing_and_payments_topup_and_volume"
+            ),
         ]
     )
