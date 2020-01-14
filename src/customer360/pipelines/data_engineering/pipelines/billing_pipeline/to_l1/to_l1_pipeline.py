de599f733a3aa990942d6b0a8f539710dd1ab65d
shashank219 <shashank_mishra@mckinsey.com>
2020-01-14 17:16:12 +0700

    billing and payments topup time difference feature

--- src/customer360/pipelines/data_engineering/pipelines/billing_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/billing_pipeline/to_l1/to_l1_pipeline.py
@@ -12,5 +12,11 @@ def billing_to_l1_pipeline(**kwargs):
                  "params:l0_billing_and_payment_feature_top_up_and_count"],
                 "l1_billing_and_payments_topup_and_volume"
             ),
+            node(
+                node_from_config,
+                ["l0_billing_and_payments_rt_t_recharge_daily",
+                 "params:l0_billing_and_payment_feature_topup_time_diff"],
+                "l1_billing_and_payments_topup_time_diff"
+            ),
         ]
     )
