33c13045811a789c345a5f5a4fa7bbcb5e80f4bc
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-01-23 21:50:50 +0700

    some bug fixes AND l4 layer

--- src/customer360/pipelines/data_engineering/pipelines/usage_pipeline/to_l4/to_l4_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/usage_pipeline/to_l4/to_l4_pipeline.py
@@ -39,11 +39,24 @@ from src.customer360.utilities.config_parser import l4_rolling_window
 def usage_to_l4_pipeline(**kwargs):
     return Pipeline(
         [
+            # node(
+            #     l4_rolling_window,
+            #     ["l2_usage_call_relation_sum_weekly",
+            #      "params:l4_usage_call_relation_features"],
+            #     "l4_usage_call_relation_features"
+            # ),
+            # node(
+            #     l4_rolling_window,
+            #     ["l2_usage_call_relation_sum_ir_weekly",
+            #      "params:l4_usage_call_relation_ir_features"],
+            #     "l4_usage_call_relation_ir_features"
+            # ),
             node(
                 l4_rolling_window,
-                ["l2_usage_call_relation_sum_weekly",
-                 "params:l4_usage_call_relation_features"],
-                "l4_usage_call_relation_features"
-            )
+                ["l2_usage_data_prepaid_postpaid_weekly",
+                 "params:l4_usage_data_prepaid_postpaid_features"],
+                "l4_usage_data_prepaid_postpaid_features"
+            ),
+
         ], name="usage_to_l4_pipeline"
     )
