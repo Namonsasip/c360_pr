bc9ae29b2c1ce2ec7c87ccfb6257de65438cb7ce
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-01-23 17:56:31 +0700

    Add source data

--- src/customer360/run.py
+++ src/customer360/run.py
@@ -102,8 +102,8 @@ def run_package():
     # entry point for running pip-install projects
     # using `<project_package>` command
     project_context = load_context(Path.cwd())
-    # project_context.run(pipeline_name="usage_to_l1_pipeline")
-    #project_context.run(pipeline_name="usage_to_l2_pipeline")
+    project_context.run(pipeline_name="usage_to_l1_pipeline")
+    project_context.run(pipeline_name="usage_to_l2_pipeline")
     project_context.run(pipeline_name="usage_to_l4_pipeline")
 
 
