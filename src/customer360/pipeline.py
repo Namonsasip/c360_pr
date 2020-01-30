defde4fa3ef212e2582f8d92907f995f6688c9bc
joshuahendinata <joshua.hendinata@quantumblack.com>
2020-01-30 18:07:24 +0700

    added streaming L2 features

--- src/customer360/pipeline.py
+++ src/customer360/pipeline.py
@@ -62,6 +62,7 @@ from src.customer360.pipelines.data_engineering.pipelines.billing_pipeline.to_l4
 from .pipelines.data_engineering.pipelines.usage_pipeline.to_l2 import usage_to_l2_pipeline
 from .pipelines.data_engineering.pipelines.usage_pipeline.to_l4.to_l4_pipeline import usage_to_l4_pipeline
 from .pipelines.data_engineering.pipelines.stream_pipeline.to_l1.to_l1_pipeline import streaming_to_l1_pipeline
+from .pipelines.data_engineering.pipelines.stream_pipeline.to_l2.to_l2_pipeline import streaming_to_l2_pipeline
 
 
 def create_pipelines(**kwargs) -> Dict[str, Pipeline]:
@@ -103,6 +104,7 @@ def create_pipelines(**kwargs) -> Dict[str, Pipeline]:
         'billing_to_l3_pipeline': billing_to_l3_pipeline(),
         'billing_to_l2_pipeline': billing_to_l2_pipeline(),
         'billing_to_l4_pipeline': billing_to_l4_pipeline(),
-        "streaming_to_l1_pipeline": streaming_to_l1_pipeline()
+        "streaming_to_l1_pipeline": streaming_to_l1_pipeline(),
+        "streaming_to_l2_pipeline": streaming_to_l2_pipeline(),
         # "de": data_engineering_pipeline,
     }
