b8f0caa886e11481391c54afa2e7f3907abc4ec7
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-01-31 18:43:45 +0700

    prepaid-post revenue features

--- src/customer360/run.py
+++ src/customer360/run.py
@@ -103,8 +103,8 @@ def run_package():
     project_context = load_context(Path.cwd())
     # project_context.run(pipeline_name="usage_to_l1_pipeline")
     # project_context.run(pipeline_name="usage_to_l2_pipeline")
-    # project_context.run(pipeline_name="revenue_to_l3_pipeline")
-    project_context.run(pipeline_name="revenue_to_l4_pipeline")
+    project_context.run(pipeline_name="revenue_to_l3_pipeline")
+    # project_context.run(pipeline_name="revenue_to_l4_pipeline")
 
 
     # Replace line above with below to run on databricks cluster
