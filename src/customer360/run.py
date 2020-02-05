4a159983ec6a5bb916c534954c2a0eb42c5b9d73
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-02-05 11:12:19 +0700

    run commit

--- src/customer360/run.py
+++ src/customer360/run.py
@@ -102,9 +102,9 @@ def run_package():
     # using `<project_package>` command
     project_context = load_context(Path.cwd())
     # project_context.run()
-    project_context.run(pipeline_name="usage_to_l1_pipeline")
+    # project_context.run(pipeline_name="usage_to_l1_pipeline")
     # project_context.run(pipeline_name="usage_to_l2_pipeline")
-    # project_context.run(pipeline_name="usage_to_l4_pipeline")
+    project_context.run(pipeline_name="usage_to_l4_pipeline")
     # project_context.run(pipeline_name="revenue_to_l3_pipeline")
     # project_context.run(pipeline_name="revenue_to_l4_pipeline")
 
