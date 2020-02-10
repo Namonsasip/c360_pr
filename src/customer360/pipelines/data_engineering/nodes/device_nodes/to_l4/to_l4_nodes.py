30ffc13d0ab17f3374f6d63a62dc0f7c6bb9461c
shashank219 <shashank_mishra@mckinsey.com>
2020-02-10 22:27:35 +0530

    Changed output variable names and added config to write data inot L4 from L2/L3

---
+++ src/customer360/pipelines/data_engineering/nodes/device_nodes/to_l4/to_l4_nodes.py
@@ -0,0 +1,2 @@
+def read_df(input_df):
+    return input_df
--- src/customer360/pipelines/data_engineering/pipelines/device_pipeline/to_l4/to_l4_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/device_pipeline/to_l4/to_l4_pipeline.py
@@ -1,6 +1,7 @@
 from kedro.pipeline import Pipeline, node
 
 from src.customer360.utilities.config_parser import *
+from src.customer360.pipelines.data_engineering.nodes.device_nodes.to_l4.to_l4_nodes import *
 
 
 def device_to_l4_pipeline(**kwargs):
@@ -25,6 +26,28 @@ def device_to_l4_pipeline(**kwargs):
                 ["l4_device_rolling_window_most_used_2",
                  "params:l4_device_most_used"],
                 "l4_device_rolling_window_most_used"
+            ),
+
+            node(
+                read_df,
+                ["l2_device_handset_summary_with_configuration_weekly"],
+                "l4_device_handset_summary_with_configuration_weekly"
+            ),
+            node(
+                read_df,
+                ["l3_device_handset_summary_with_configuration_monthly"],
+                "l4_device_handset_summary_with_configuration_monthly"
+            ),
+
+            node(
+                read_df,
+                ["l2_previous_device_handset_summary_with_configuration_weekly"],
+                "l4_previous_device_handset_summary_with_configuration_weekly"
+            ),
+            node(
+                read_df,
+                ["l3_previous_device_handset_summary_with_configuration_monthly"],
+                "l4_previous_device_handset_summary_with_configuration_monthly"
             ),
         ]
     )
