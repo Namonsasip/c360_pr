00dde9a7503f7cfc5e2f1e001ed1cf9b9f7ce3c4
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-02-12 20:18:34 +0700

    revenue feature enhancements...

---
+++ src/customer360/pipelines/data_engineering/nodes/revenue_nodes/to_l3/to_l3_nodes.py
@@ -0,0 +1,43 @@
+from pyspark.sql import DataFrame
+
+
+def merge_with_customer_prepaid_df(source_df: DataFrame,
+                                   cust_df: DataFrame) -> DataFrame:
+    """
+
+    :param source_df:
+    :param cust_df:
+    :return:
+    """
+    # This code will populate a subscriber id to the data set.
+    cust_df_cols = ['access_method_num', 'start_of_month', 'crm_sub_id']
+    join_key = ['access_method_num', 'start_of_month']
+
+    cust_df = cust_df.select(cust_df_cols) \
+        .withColumnRenamed("crm_sub_id", "subscription_identifier")
+
+    final_df = source_df.join(cust_df, join_key)
+
+    return final_df
+
+
+def merge_with_customer_postpaid_df(source_df: DataFrame,
+                                    cust_df: DataFrame) -> DataFrame:
+    """
+
+    :param source_df:
+    :param cust_df:
+    :return:
+    """
+    # This code will populate a subscriber id to the data set.
+    cust_df_cols = ['start_of_month', 'crm_sub_id']
+    join_key = ['subscription_identifier', 'start_of_month']
+
+    cust_df = cust_df.select(cust_df_cols) \
+        .withColumnRenamed("crm_sub_id", "subscription_identifier")
+
+    source_df = source_df.withColumnRenamed("sub_id", "subscription_identifier")
+
+    final_df = source_df.join(cust_df, join_key)
+
+    return final_df
