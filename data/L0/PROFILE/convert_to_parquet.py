18d5bb4b865edd50351c8ba5f9bf344e751d63ac
joshuahendinata <joshua.hendinata@quantumblack.com>
2020-02-13 20:26:57 +0700

    removed wrong files

--- data/L0/PROFILE/convert_to_parquet.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-
-df = pd.read_csv("profile_drm_t_active_profile_customer_journey_monthly.csv")
-df.to_parquet(os.path.join(os.getcwd(),
-                           'mod_profile_drm_t_active_profile_customer_journey_monthly'),
-              partition_cols=["partition_month"])
