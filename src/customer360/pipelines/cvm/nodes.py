c8b3e2e3e3482685f3f8f2b41ee37dcdc1e1f570
jtyrek <jakub_tyrek@mckinsey.com>
2020-02-17 12:53:49 +0700

    l5_cvm_users_table creation ndoe added

--- src/customer360/pipelines/cvm/nodes.py
+++ src/customer360/pipelines/cvm/nodes.py
@@ -26,84 +26,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Example code for the nodes in the example pipeline. This code is meant
-just for illustrating basic Kedro features.
-
-PLEASE DELETE THIS FILE ONCE YOU START WORKING ON YOUR OWN PROJECT!
-"""
-# pylint: disable=invalid-name
-
-import logging
 from typing import Any, Dict
-
-import numpy as np
-import pandas as pd
+from pyspark.sql import DataFrame
 
 
-def train_model(
-    train_x: pd.DataFrame, train_y: pd.DataFrame, parameters: Dict[str, Any]
-) -> np.ndarray:
-    """Node for training a simple multi-class logistic regression model. The
-    number of training iterations as well as the learning rate are taken from
-    conf/project/parameters.yml. All of the data as well as the parameters
-    will be provided to this function at the time of execution.
+def create_l5_cvm_users_table(
+        profile: DataFrame,
+        main_packs: DataFrame,
+        parameters: Dict[str, Any]
+) -> DataFrame:
+    """Create l5_cvm_users_table - monthly table of users used for training and
+    validating.
+
+    Args:
+        profile: monthly customer profiles.
+        main_packs: pre-paid main packages description.
     """
-    num_iter = parameters["example_num_train_iter"]
-    lr = parameters["example_learning_rate"]
-    X = train_x.values
-    Y = train_y.values
 
-    # Add bias to the features
-    bias = np.ones((X.shape[0], 1))
-    X = np.concatenate((bias, X), axis=1)
+    min_date = parameters["l5_cvm_users_table"]["min_date"]
+    users = profile.filter("month_id >= '{}'".format(min_date))
+    users = users.filter("charge_type == 'Pre-paid' AND mobile_status == 'SA'")
+    users = users.filter("service_month >= 4")
+    users = users.filter("norms_net_revenue > 0")
 
-    weights = []
-    # Train one model for each class in Y
-    for k in range(Y.shape[1]):
-        # Initialise weights
-        theta = np.zeros(X.shape[1])
-        y = Y[:, k]
-        for _ in range(num_iter):
-            z = np.dot(X, theta)
-            h = _sigmoid(z)
-            gradient = np.dot(X.T, (h - y)) / y.size
-            theta -= lr * gradient
-        # Save the weights for each model
-        weights.append(theta)
+    main_packs = main_packs.filter("promotion_group_tariff not in ('SIM 2 Fly',\
+     'SIM NET MARATHON', 'Net SIM', 'Traveller SIM', 'Foreigner SIM')")
+    main_packs = main_packs.select('package_id').\
+        withColumnRenamed('package_id', 'main_promo_id')
+    users = users.join(main_packs, ['main_promo_id'], 'inner')
+    columns_to_pick = ['month_id', 'register_date', 'customer_id']
+    users = users.select(columns_to_pick)
 
-    # Return a joint multi-class model with weights for all classes
-    return np.vstack(weights).transpose()
-
-
-def predict(model: np.ndarray, test_x: pd.DataFrame) -> np.ndarray:
-    """Node for making predictions given a pre-trained model and a test set.
-    """
-    X = test_x.values
-
-    # Add bias to the features
-    bias = np.ones((X.shape[0], 1))
-    X = np.concatenate((bias, X), axis=1)
-
-    # Predict "probabilities" for each class
-    result = _sigmoid(np.dot(X, model))
-
-    # Return the index of the class with max probability for all samples
-    return np.argmax(result, axis=1)
-
-
-def report_accuracy(predictions: np.ndarray, test_y: pd.DataFrame) -> None:
-    """Node for reporting the accuracy of the predictions performed by the
-    previous node. Notice that this function has no outputs, except logging.
-    """
-    # Get true class index
-    target = np.argmax(test_y.values, axis=1)
-    # Calculate accuracy of predictions
-    accuracy = np.sum(predictions == target) / target.shape[0]
-    # Log the accuracy of the model
-    log = logging.getLogger(__name__)
-    log.info("Model accuracy on test set: %0.2f%%", accuracy * 100)
-
-
-def _sigmoid(z):
-    """A helper sigmoid function used by the training and the scoring nodes."""
-    return 1 / (1 + np.exp(-z))
+    return users
