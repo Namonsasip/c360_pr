ef71141173bf8b5651bc6c19bd9e3f34b200ce16
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-02-17 20:52:34 +0700

    revenue fix

--- src/customer360/pipelines/data_engineering/nodes/revenue_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/revenue_nodes/to_l3/to_l3_nodes.py
@@ -13,6 +13,8 @@ def merge_with_customer_prepaid_df(source_df: DataFrame,
     cust_df_cols = ['access_method_num', 'partition_month', 'subscription_identifier']
     join_key = ['access_method_num', 'start_of_month']
 
+    cust_df = cust_df.where("charge_type = 'Pre-paid'")
+
     cust_df = cust_df.select(cust_df_cols).withColumnRenamed("partition_month", "start_of_month")
 
     final_df = source_df.join(cust_df, join_key)
@@ -31,6 +33,8 @@ def merge_with_customer_postpaid_df(source_df: DataFrame,
     # This code will populate a subscriber id to the data set.
     cust_df_cols = ['partition_month', 'subscription_identifier']
     join_key = ['subscription_identifier', 'start_of_month']
+
+    cust_df = cust_df.where("charge_type = 'Post-paid'")
 
     cust_df = cust_df.select(cust_df_cols).withColumnRenamed("partition_month", "start_of_month")
 
