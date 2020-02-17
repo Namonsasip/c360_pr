34a3efda29f34fe5096eabdb116b9e2f32d5b2fb
jtyrek <jakub_tyrek@mckinsey.com>
2020-02-17 18:23:27 +0700

    Added possibility run only chosen node

--- src/customer360/run.py
+++ src/customer360/run.py
@@ -98,34 +98,11 @@ class ProjectContext(KedroContext):
         return catalog
 
 
-def run_package(pipelines=['usage_to_l4_daily_pipeline']):
+def run_package(node_names):
     # entry point for running pip-install projects
     # using `<project_package>` command
-    project_context = load_context(Path.cwd(), env=conf)
-
-
-
-    from pyspark.sql import SparkSession
-    spark = SparkSession.builder.getOrCreate()
-
-    # Dont delete this line. This allow spark to only overwrite the partition
-    # saved to parquet instead of entire table folder
-    spark.conf.set("spark.sql.sources.partitionOverwriteMode", "DYNAMIC")
-
-    if pipelines is not None:
-        for each_pipeline in pipelines:
-            project_context.run(pipeline_name=each_pipeline)
-        return
-
-    #project_context.run(pipeline_name='usage_to_l1_pipeline')
-    # project_context.run(pipeline_name='customer_profile_to_l3_pipeline')
-
-    # Replace line above with below to run on databricks cluster
-    # and Dont forget to clear state for every git pull in notebook
-    # (change the pipeline name to your pipeline name)
-    #
-    # project_context = load_context(Path.cwd(), env='base')
-    # project_context.run(pipeline_name="customer_profile_to_l4_pipeline")
+    project_context = load_context(Path.cwd())
+    project_context.run(node_names=node_names)
 
 
 if __name__ == "__main__":
