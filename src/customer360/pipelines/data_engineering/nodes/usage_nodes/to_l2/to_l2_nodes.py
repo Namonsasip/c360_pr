26b1ff93ebd1af1f90ba346ad0aba58d243f4d37
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-02-21 01:15:02 +0530

    revenue fix

--- src/customer360/pipelines/data_engineering/nodes/usage_nodes/to_l2/to_l2_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/usage_nodes/to_l2/to_l2_nodes.py
@@ -43,12 +43,10 @@ def build_usage_l2_layer(data_frame: DataFrame, dict_obj: dict) -> DataFrame:
     mvv_array = [row[0] for row in dates_list if row[0] != "SAMPLING"]
     logging.info("Dates to run for {0}".format(str(mvv_array)))
 
-    mvv_new = list(divide_chunks(mvv_array, 2))
+    mvv_new = list(divide_chunks(mvv_array, 5))
     add_list = mvv_new
 
     first_item = add_list[0]
-
-    data_frame = data_frame.repartition(200, 'start_of_week')
 
     add_list.remove(first_item)
     for curr_item in add_list:
