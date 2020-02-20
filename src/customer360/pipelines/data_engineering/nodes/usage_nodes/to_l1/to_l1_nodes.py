5a97a4041f5bfc325ddb349de20708f81c0d1f29
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-02-20 16:49:20 +0530

    revenue fix

--- src/customer360/pipelines/data_engineering/nodes/usage_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/usage_nodes/to_l1/to_l1_nodes.py
@@ -188,6 +188,7 @@ def merge_all_dataset_to_one_table(l1_usage_outgoing_call_relation_sum_daily_stg
     first_item = add_list[0]
     add_list.remove(first_item)
     for curr_item in add_list:
+        logging.info("running for dates {0}".format(str(curr_item)))
         small_df = data_frame.filter(F.col("event_partition_date").isin(*[curr_item]))
         output_df = execute_sql(data_frame=small_df, table_name='roaming_incoming_outgoing_data', sql_str=final_df_str)
         cust_df = l1_customer_profile_union_daily_feature.filter(F.col("event_partition_date").isin(*[curr_item])) \
@@ -195,6 +196,7 @@ def merge_all_dataset_to_one_table(l1_usage_outgoing_call_relation_sum_daily_stg
         output_df = cust_df.join(output_df, join_cols, how="left")
         CNTX.catalog.save("l1_usage_postpaid_prepaid_daily", output_df.drop(*drop_cols))
 
+    logging.info("running for dates {0}".format(str(first_item)))
     return_df = data_frame.filter(F.col("event_partition_date").isin(*[first_item]))
     cust_df = l1_customer_profile_union_daily_feature.filter(F.col("event_partition_date").isin(*[first_item])) \
         .select(sel_cols)
