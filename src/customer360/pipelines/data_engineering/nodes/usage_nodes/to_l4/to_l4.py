fdb885c43f3e0aaf005873be5740c92cfc7c2718
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-02-21 08:27:05 +0530

    revenue fix

---
+++ src/customer360/pipelines/data_engineering/nodes/usage_nodes/to_l4/to_l4.py
@@ -0,0 +1,16 @@
+from pyspark.sql import DataFrame
+
+
+def merge_all_usage_outputs(df1: DataFrame, df2: DataFrame, df3: DataFrame) -> DataFrame:
+    """
+    :param df1:
+    :param df2:
+    :param df3:
+    :return:
+    """
+    join_key = ["subscriber_identifier", "start_of_week"]
+
+    final_df = df1.join(df2, join_key)
+    final_df = final_df.join(df3, join_key)
+
+    return final_df
