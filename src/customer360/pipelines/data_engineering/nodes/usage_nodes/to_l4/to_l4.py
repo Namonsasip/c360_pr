ad5d9022c6e33775448d70ab6f157299b061ce6f
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-02-21 16:46:43 +0530

    revenue fix

--- src/customer360/pipelines/data_engineering/nodes/usage_nodes/to_l4/to_l4.py
+++ src/customer360/pipelines/data_engineering/nodes/usage_nodes/to_l4/to_l4.py
@@ -1,5 +1,8 @@
 from pyspark.sql import DataFrame
-
+from kedro.context import load_context
+import logging
+from pathlib import Path
+from pyspark.sql import functions as F
 
 def merge_all_usage_outputs(df1: DataFrame, df2: DataFrame, df3: DataFrame, df4: DataFrame) -> DataFrame:
     """
@@ -8,10 +11,45 @@ def merge_all_usage_outputs(df1: DataFrame, df2: DataFrame, df3: DataFrame, df4:
     :param df3:
     :return:
     """
+    def divide_chunks(l, n):
+
+        # looping till length l
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
+
+    CNTX = load_context(Path.cwd(), env='base')
+    dates_list = df1.select('start_of_week').distinct().collect()
+    mvv_array = [row[0] for row in dates_list if row[0] != "SAMPLING"]
+    logging.info("Dates to run for {0}".format(str(mvv_array)))
     join_key = ["subscription_identifier", "start_of_week"]
 
-    final_df = df1.join(df2, join_key)
-    final_df = final_df.join(df3, join_key)
-    final_df = final_df.join(df4, join_key)
+    mvv_new = list(divide_chunks(mvv_array, 2))
+    add_list = mvv_new
 
-    return final_df
+    first_item = add_list[0]
+
+    add_list.remove(first_item)
+    for curr_item in add_list:
+        logging.info("running for dates {0}".format(str(curr_item)))
+        first_df = df1.filter(F.col("start_of_week").isin(*[curr_item]))
+        second_df = df2.filter(F.col("start_of_week").isin(*[curr_item]))
+        third_df = df3.filter(F.col("start_of_week").isin(*[curr_item]))
+        fourth_df = df4.filter(F.col("start_of_week").isin(*[curr_item]))
+
+        final_df = first_df.join(second_df, join_key)
+        final_df = final_df.join(third_df, join_key)
+        final_df = final_df.join(fourth_df, join_key)
+
+        CNTX.catalog.save("l4_usage_postpaid_prepaid_weekly_features", final_df)
+
+    logging.info("Final date to run for {0}".format(str(first_item)))
+    first_df = df1.filter(F.col("start_of_week").isin(*[first_item]))
+    second_df = df2.filter(F.col("start_of_week").isin(*[first_item]))
+    third_df = df3.filter(F.col("start_of_week").isin(*[first_item]))
+    fourth_df = df4.filter(F.col("start_of_week").isin(*[first_item]))
+
+    return_df = first_df.join(second_df, join_key)
+    return_df = return_df.join(third_df, join_key)
+    return_df = return_df.join(fourth_df, join_key)
+
+    return return_df
