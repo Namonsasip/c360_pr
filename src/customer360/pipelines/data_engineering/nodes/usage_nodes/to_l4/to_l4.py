9200113a8aa901fc0ca6ca38af2b256f45ddd807
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-02-21 14:30:43 +0530

    revenue fix

--- src/customer360/pipelines/data_engineering/nodes/usage_nodes/to_l4/to_l4.py
+++ src/customer360/pipelines/data_engineering/nodes/usage_nodes/to_l4/to_l4.py
@@ -8,7 +8,7 @@ def merge_all_usage_outputs(df1: DataFrame, df2: DataFrame, df3: DataFrame, df4:
     :param df3:
     :return:
     """
-    join_key = ["subscriber_identifier", "start_of_week"]
+    join_key = ["subscription_identifier", "start_of_week"]
 
     final_df = df1.join(df2, join_key)
     final_df = final_df.join(df3, join_key)
