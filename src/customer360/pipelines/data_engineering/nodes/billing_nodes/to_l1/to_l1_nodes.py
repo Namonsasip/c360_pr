150f4b1e646b8d3dc6fa8959a5967ee40b4b2cad
shashank219 <shashank_mishra@mckinsey.com>
2020-02-24 12:48:01 +0530

    Added filter for pre-paid and post-paid customers in L1 layer

--- src/customer360/pipelines/data_engineering/nodes/billing_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/billing_nodes/to_l1/to_l1_nodes.py
@@ -6,7 +6,7 @@ from kedro.context.context import load_context
 from pathlib import Path
 import logging
 
-def massive_processing(input_df, customer_prof_input_df, join_function,sql,partition_date, cust_partition_date, output_df_catalog):
+def massive_processing(input_df, customer_prof_input_df, join_function,sql,partition_date, cust_partition_date, cust_type, output_df_catalog):
     """
     :return:
     """
@@ -18,7 +18,7 @@ def massive_processing(input_df, customer_prof_input_df, join_function,sql,parti
 
     CNTX = load_context(Path.cwd(), env='base')
     data_frame = input_df
-    cust_data_frame = customer_prof_input_df
+    cust_data_frame = customer_prof_input_df.where("charge_type = '"+cust_type+"'")
     dates_list = data_frame.select(f.to_date(partition_date).alias(partition_date)).distinct().collect()
     mvv_array = [row[0] for row in dates_list if row[0] != "SAMPLING"]
     logging.info("Dates to run for {0}".format(str(mvv_array)))
@@ -49,61 +49,66 @@ def billing_topup_count_and_volume_node(input_df, customer_prof, sql) -> DataFra
     """
     :return:
     """
-    return_df = massive_processing(input_df, customer_prof, daily_recharge_data_with_customer_profile, sql,'recharge_date', 'event_partition_date', "l1_billing_and_payments_daily_topup_and_volume")
+    return_df = massive_processing(input_df, customer_prof, daily_recharge_data_with_customer_profile, sql,'recharge_date', 'event_partition_date', "Pre-paid","l1_billing_and_payments_daily_topup_and_volume")
     return return_df
 
 def billing_daily_rpu_roaming(input_df, customer_prof, sql) -> DataFrame:
     """
     :return:
     """
-    return_df = massive_processing(input_df, customer_prof, daily_roaming_data_with_customer_profile, sql,'date_id','event_partition_date', "l1_billing_and_payments_daily_rpu_roaming")
+    return_df = massive_processing(input_df, customer_prof, daily_roaming_data_with_customer_profile, sql,'date_id','event_partition_date', "Post-paid","l1_billing_and_payments_daily_rpu_roaming")
     return return_df
 
 def billing_before_topup_balance(input_df, customer_prof, sql) -> DataFrame:
     """
     :return:
     """
-    return_df = massive_processing(input_df, customer_prof, daily_sa_account_data_with_customer_profile, sql,'recharge_date', 'event_partition_date',"l1_billing_and_payments_daily_before_top_up_balance")
+    return_df = massive_processing(input_df, customer_prof, daily_sa_account_data_with_customer_profile, sql,'recharge_date', 'event_partition_date',"Pre-paid", "l1_billing_and_payments_daily_before_top_up_balance")
     return return_df
 
 def billing_topup_channels(input_df, customer_prof, sql) -> DataFrame:
     """
     :return:
     """
-    return_df = massive_processing(input_df, customer_prof, daily_recharge_data_with_customer_profile, sql,'recharge_date', 'event_partition_date',"l1_billing_and_payments_daily_top_up_channels")
+    return_df = massive_processing(input_df, customer_prof, daily_recharge_data_with_customer_profile, sql,'recharge_date', 'event_partition_date',"Pre-paid", "l1_billing_and_payments_daily_top_up_channels")
     return return_df
 
 def billing_most_popular_topup_channel(input_df, customer_prof, sql) -> DataFrame:
     """
     :return:
     """
-    return_df = massive_processing(input_df, customer_prof, daily_recharge_data_with_customer_profile, sql,'recharge_date', 'event_partition_date',"l1_billing_and_payments_daily_most_popular_top_up_channel")
+    return_df = massive_processing(input_df, customer_prof, daily_recharge_data_with_customer_profile, sql,'recharge_date', 'event_partition_date',"Pre-paid","l1_billing_and_payments_daily_most_popular_top_up_channel")
     return return_df
 
 def billing_popular_topup_day_hour(input_df, customer_prof, sql) -> DataFrame:
     """
     :return:
     """
-    return_df = massive_processing(input_df, customer_prof, daily_recharge_data_with_customer_profile, sql,'recharge_date', 'event_partition_date',"l1_billing_and_payments_daily_popular_topup_day")
+    return_df = massive_processing(input_df, customer_prof, daily_recharge_data_with_customer_profile, sql,'recharge_date', 'event_partition_date',"Pre-paid","l1_billing_and_payments_daily_popular_topup_day")
     return return_df
 
 def billing_time_since_last_topup(input_df, customer_prof, sql) -> DataFrame:
     """
     :return:
     """
-    return_df = massive_processing(input_df, customer_prof, daily_recharge_data_with_customer_profile, sql,'recharge_date', 'event_partition_date',"l1_billing_and_payments_daily_time_since_last_top_up")
+    return_df = massive_processing(input_df, customer_prof, daily_recharge_data_with_customer_profile, sql,'recharge_date', 'event_partition_date',"Pre-paid","l1_billing_and_payments_daily_time_since_last_top_up")
     return return_df
 
 
-def daily_recharge_data_with_customer_profile(customer_prof,recharge_data):
-
+def derives_in_customer_profile(customer_prof):
     customer_prof = customer_prof.select("access_method_num",
                                          "subscription_identifier",
                                          f.to_date("register_date").alias("register_date"),
-                                         "event_partition_date")
+                                         "event_partition_date",
+                                         "charge_type")
 
     customer_prof = customer_prof.withColumn("start_of_month",f.to_date(f.date_trunc('month',customer_prof.event_partition_date)))\
         .withColumn("start_of_week",f.to_date(f.date_trunc('week',customer_prof.event_partition_date)))
+
+    return customer_prof
+def daily_recharge_data_with_customer_profile(customer_prof,recharge_data):
+
+    customer_prof = derives_in_customer_profile(customer_prof)
 
     output_df = customer_prof.join(recharge_data,(customer_prof.access_method_num == recharge_data.access_method_num) &
                                    (customer_prof.register_date.eqNullSafe(f.to_date(recharge_data.register_date))) &
@@ -116,13 +121,7 @@ def daily_recharge_data_with_customer_profile(customer_prof,recharge_data):
 
 def daily_roaming_data_with_customer_profile(customer_prof,roaming_data):
 
-    customer_prof = customer_prof.select("access_method_num",
-                                         "subscription_identifier",
-                                         f.to_date("register_date").alias("register_date"),
-                                         "event_partition_date")
-
-    customer_prof = customer_prof.withColumn("start_of_month",f.to_date(f.date_trunc('month',customer_prof.event_partition_date)))\
-        .withColumn("start_of_week",f.to_date(f.date_trunc('week',customer_prof.event_partition_date)))
+    customer_prof = derives_in_customer_profile(customer_prof)
 
     output_df = customer_prof.join(roaming_data,(customer_prof.access_method_num == roaming_data.access_method_number) &
                                    (customer_prof.register_date.eqNullSafe(f.to_date(roaming_data.mobile_register_date))) &
@@ -133,13 +132,7 @@ def daily_roaming_data_with_customer_profile(customer_prof,roaming_data):
 
 def daily_sa_account_data_with_customer_profile(customer_prof,sa_account_data):
 
-    customer_prof = customer_prof.select("access_method_num",
-                                         "subscription_identifier",
-                                         f.to_date("register_date").alias("register_date"),
-                                         "event_partition_date")
-
-    customer_prof = customer_prof.withColumn("start_of_month",f.to_date(f.date_trunc('month',customer_prof.event_partition_date)))\
-        .withColumn("start_of_week",f.to_date(f.date_trunc('week',customer_prof.event_partition_date)))
+    customer_prof = derives_in_customer_profile(customer_prof)
 
     output_df = customer_prof.join(sa_account_data,(customer_prof.access_method_num == sa_account_data.access_method_num) &
                                    (customer_prof.event_partition_date == f.to_date(sa_account_data.recharge_date)),'left')
--- src/customer360/pipelines/data_engineering/pipelines/billing_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/billing_pipeline/to_l1/to_l1_pipeline.py
@@ -8,7 +8,7 @@ def billing_to_l1_pipeline(**kwargs):
     return Pipeline(
         [
 
-            # Top up count and top up volume feature
+            # Top up count and top up volume feature pre-paid
             node(
                 billing_topup_count_and_volume_node,
                 ["l0_billing_and_payments_rt_t_recharge_daily","l1_customer_profile_union_daily_feature",
@@ -16,7 +16,7 @@ def billing_to_l1_pipeline(**kwargs):
                 "l1_billing_and_payments_daily_topup_and_volume"
             ),
 
-            # Average revenue for roaming feature
+            # Average revenue for roaming feature post-paid
             node(
                 billing_daily_rpu_roaming,
                 ["l0_billing_ir_a_usg_daily","l1_customer_profile_union_daily_feature",
@@ -24,7 +24,7 @@ def billing_to_l1_pipeline(**kwargs):
                 "l1_billing_and_payments_daily_rpu_roaming"
             ),
 
-            # Balance before top up feature
+            # Balance before top up feature pre-paid
             node(
                 billing_before_topup_balance,
                 ["l0_billing_sa_t_account_recharge_daily","l1_customer_profile_union_daily_feature",
@@ -32,7 +32,7 @@ def billing_to_l1_pipeline(**kwargs):
                 "l1_billing_and_payments_daily_before_top_up_balance"
             ),
 
-            # Top up channels feature
+            # Top up channels feature pre-paid
             node(
                 billing_topup_channels,
                 ["l0_billing_and_payments_rt_t_recharge_daily","l1_customer_profile_union_daily_feature",
@@ -40,7 +40,7 @@ def billing_to_l1_pipeline(**kwargs):
                 "l1_billing_and_payments_daily_top_up_channels"
             ),
 
-            # Most popular top up channel feature
+            # Most popular top up channel feature pre-paid
             node(
                 billing_most_popular_topup_channel,
                 ["l0_billing_and_payments_rt_t_recharge_daily","l1_customer_profile_union_daily_feature",
@@ -48,7 +48,7 @@ def billing_to_l1_pipeline(**kwargs):
                 "l1_billing_and_payments_daily_most_popular_top_up_channel"
             ),
 
-            # Popular top up day and hour feature
+            # Popular top up day and hour feature pre-paid
             node(
                 billing_popular_topup_day_hour,
                 ["l0_billing_and_payments_rt_t_recharge_daily","l1_customer_profile_union_daily_feature",
@@ -56,7 +56,7 @@ def billing_to_l1_pipeline(**kwargs):
                 "l1_billing_and_payments_daily_popular_topup_day"
             ),
 
-            # Time since last top up feature
+            # Time since last top up feature pre-paid
             node(
                 billing_time_since_last_topup,
                 ["l0_billing_and_payments_rt_t_recharge_daily","l1_customer_profile_union_daily_feature",
