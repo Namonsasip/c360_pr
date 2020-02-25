d232ccd6b73d59e8524104c0855de2673a5d31d1
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-02-25 11:09:27 +0530

    revenue fix

--- src/customer360/pipelines/data_engineering/nodes/campaign_nodes/to_l1/to_l1.py
+++ src/customer360/pipelines/data_engineering/nodes/campaign_nodes/to_l1/to_l1.py
@@ -2,28 +2,33 @@ from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
 from pyspark.sql import SparkSession
 from customer360.utilities.config_parser import node_from_config
+from customer360.utilities.re_usable_functions import union_dataframes_with_missing_cols
 
 
 def get_spark_session():
     return SparkSession.builder.getOrCreate()
 
 
-def cam_post_channel_with_highest_conversion(data_frame: DataFrame, dictionary_obj) -> [DataFrame, DataFrame]:
+def cam_post_channel_with_highest_conversion(postpaid: DataFrame,
+                                             prepaid: DataFrame,
+                                             dictionary_obj,
+                                             dictionary_obj_2) -> [DataFrame, DataFrame]:
     """
-    :param data_frame:
+    :param postpaid:
+    :param prepaid:
+    :param dictionary_obj:
+    :param dictionary_obj_2:
     :return:
     """
-    data_frame = data_frame.withColumn("start_of_week", F.to_date(F.date_trunc('week', F.to_date("contact_date")))) \
-        .withColumn("event_partition_date", F.to_date("contact_date"))
+    data_frame = union_dataframes_with_missing_cols(postpaid, prepaid)
 
-    all_count_grp_cols = ['subscription_identifier', "event_partition_date", "start_of_week",
+    all_count_grp_cols = ['subscription_identifier', "contact_date",
                           'campaign_type', 'campaign_channel', 'response']
 
     all_count_df = data_frame.groupBy(all_count_grp_cols).agg(F.count("subscription_identifier").alias("base_count"))
 
     # calculating at campaign type
-    total_camp_by_camp_type = ['subscription_identifier', "event_partition_date",
-                               "start_of_week", 'campaign_type', 'campaign_channel']
+    total_camp_by_camp_type = ['subscription_identifier', "contact_date", 'campaign_type', 'campaign_channel']
 
     total_cam_by_cam_type = all_count_df.groupBy(total_camp_by_camp_type) \
         .agg(F.sum("base_count").alias("campaign_total_campaign_by_campaign_type"))
@@ -39,8 +44,7 @@ def cam_post_channel_with_highest_conversion(data_frame: DataFrame, dictionary_o
 
     # calculating at campaign channel
 
-    total_camp_by_camp_chnl_cols = ['subscription_identifier', "event_partition_date",
-                                    "start_of_week", 'campaign_channel']
+    total_camp_by_camp_chnl_cols = ['subscription_identifier', "contact_date", 'campaign_channel']
 
     total_cam_by_cam_chnl = all_count_df.groupBy(total_camp_by_camp_chnl_cols) \
         .agg(F.sum(F.col("base_count")).alias("campaign_total_campaign_by_campaign_channel"))
@@ -67,7 +71,7 @@ def cam_post_channel_with_highest_conversion(data_frame: DataFrame, dictionary_o
         final_df = final_df.withColumn(col, F.coalesce(col, F.lit(0)))
 
     campaign_channel_top_df = final_df.filter(F.col("campaign_channel").isNotNull()). \
-        groupBy(["subscription_identifier", "campaign_channel", "event_partition_date", "start_of_week"]) \
+        groupBy(["subscription_identifier", "campaign_channel", "contact_date"]) \
         .agg(F.sum("campaign_total_campaign_by_campaign_channel_y_n").alias("campaign_total_campaign"),
              F.sum("campaign_total_campaign_by_campaign_channel_y").alias("success_channel_camp"))
 
@@ -77,13 +81,14 @@ def cam_post_channel_with_highest_conversion(data_frame: DataFrame, dictionary_o
                                                                      "campaign_total_campaign")) \
         .drop("total_campaign", "success_channel_camp")
 
-    total_campaign = all_count_df.groupBy(["subscription_identifier", "event_partition_date", "start_of_week"]) \
+    total_campaign = all_count_df.groupBy(["subscription_identifier", "contact_date"]) \
         .agg(F.sum(F.col("base_count")).alias("campaign_overall_count")
-             , F.max(F.col("event_partition_date")).alias("campaign_last_communication_date"))
+             , F.max(F.col("contact_date")).alias("campaign_last_communication_date"))
 
-    final_df = final_df.join(total_campaign, ["subscription_identifier", "event_partition_date", "start_of_week"],
+    final_df = final_df.join(total_campaign, ["subscription_identifier", "contact_date"],
                              how="left")
 
     final_df = node_from_config(final_df, dictionary_obj)
+    campaign_channel_top_df = node_from_config(campaign_channel_top_df, dictionary_obj_2)
 
     return [final_df, campaign_channel_top_df]
