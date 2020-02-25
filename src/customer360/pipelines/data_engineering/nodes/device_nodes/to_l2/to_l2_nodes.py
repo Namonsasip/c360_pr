aa5f524592cc7be7084b83f95fefafc7cb981dd7
shashank219 <shashank_mishra@mckinsey.com>
2020-02-25 14:42:08 +0530

    Deduped customers from weekly level

--- src/customer360/pipelines/data_engineering/nodes/device_nodes/to_l2/to_l2_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/device_nodes/to_l2/to_l2_nodes.py
@@ -96,6 +96,8 @@ def derive_customer_profile(customer_prof):
     customer_prof = customer_prof.withColumn("start_of_month",f.to_date(f.date_trunc('month',customer_prof.event_partition_date)))\
         .withColumn("start_of_week",f.to_date(f.date_trunc('week',customer_prof.event_partition_date)))
 
+    customer_prof = customer_prof.dropDuplicates(["start_of_month","start_of_week","access_method_num","register_date","subscription_identifier"])
+
     return customer_prof
 
 def join_with_customer_profile(customer_prof,hs_summary):
--- src/customer360/pipelines/data_engineering/pipelines/device_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/device_pipeline/to_l3/to_l3_pipeline.py
@@ -8,67 +8,52 @@ def device_to_l3_pipeline(**kwargs):
 
     return Pipeline(
         [
-            # # Monthly handset configuration related features
-            # node(
-            #     device_summary_with_customer_profile,
-            #     ["l1_customer_profile_union_daily_feature",
-            #      "l0_devices_summary_customer_handset"],
-            #     "device_handset_summary_with_customer_profile"
-            # ),
-            #
-            # node(
-            #     device_summary_with_config,
-            #     ["device_handset_summary_with_customer_profile",
-            #      "l0_devices_handset_configurations"],
-            #     "l3_device_handset_summary_with_configuration_monthly_1"
-            # ),
-            #
-            # node(
-            #     node_from_config,
-            #     ["l3_device_handset_summary_with_configuration_monthly_1",
-            #      "params:l3_device_handset_summary_with_configuration"],
-            #     "l3_device_handset_summary_with_configuration_monthly_2"
-            # ),
-            # node(
-            #     filter,
-            #     ["l3_device_handset_summary_with_configuration_monthly_2"],
-            #     "l3_device_handset_summary_with_configuration_monthly"
-            # ),
-            #
-            # # Monthly number of phone updates
-            # node(
-            #     device_number_of_phone_updates_monthly,
-            #     ["l3_device_handset_summary_with_configuration_monthly_1",
-            #      "params:l3_device_number_of_phone_updates"],
-            #     "l3_device_number_of_phone_updates_monthly"
-            # ),
-            #
-            # # Monthly previous configurations features
-            # node(
-            #     node_from_config,
-            #     ["device_handset_summary_with_customer_profile",
-            #      "params:l3_previous_device_features_with_config_ranked"],
-            #     "l3_previous_device_handset_summary_with_configuration_monthly_2"
-            # ),
-            # node(
-            #     device_previous_configurations_monthly,
-            #     ["l3_previous_device_handset_summary_with_configuration_monthly_2",
-            #      "params:l3_previous_device_features_with_config"],
-            #     "l3_previous_device_handset_summary_with_configuration_monthly"
-            # ),
-            #
-            # # Monthly most used device
-            # node(
-            #     node_from_config,
-            #     ["device_handset_summary_with_customer_profile",
-            #      "params:l3_device_most_used_initial"],
-            #     "l3_device_most_used_intermediate_monthly"
-            # ),
-            # node(
-            #     device_most_used_monthly,
-            #     ["l3_device_most_used_intermediate_monthly",
-            #      "params:l3_device_most_used"],
-            #     "l3_device_most_used_monthly"
-            # ),
+
+            node(
+                device_summary_with_configuration,
+                ["l0_devices_summary_customer_handset",
+                 "l0_devices_handset_configurations"],
+                "device_summary_with_config"
+            ),
+
+            # Monthly current device configs
+            node(
+                device_current_configurations_monthly,
+                ["l2_device_handset_summary_with_configuration_weekly",
+                 "params:l3_device_handset_summary_with_configuration"],
+                "l3_device_handset_summary_with_configuration_monthly"
+            ),
+
+            # Monthly number of phone updates
+            node(
+                device_number_of_phone_updates_monthly,
+                ["l2_device_number_of_phone_updates_weekly",
+                 "params:l3_device_number_of_phone_updates"],
+                "l3_device_number_of_phone_updates_monthly"
+            ),
+
+            # Monthly previous configurations features
+            node(
+                node_from_config,
+                ["device_summary_with_config",
+                 "params:l3_previous_device_features_with_config_ranked"],
+                "l3_previous_device_handset_summary_with_configuration_monthly_1"
+            ),
+            node(
+                device_previous_configurations_monthly,
+                ["l3_customer_profile_include_1mo_non_active",
+                 "l3_previous_device_handset_summary_with_configuration_monthly_1",
+                 "params:l3_previous_device_features_with_config"],
+                "l3_previous_device_handset_summary_with_configuration_monthly"
+            ),
+
+            # Monthly most used device
+            node(
+                device_most_used_monthly,
+                ["l3_customer_profile_include_1mo_non_active",
+                 "device_summary_with_config",
+                 "params:l3_device_most_used"],
+                "l3_device_most_used_monthly"
+            ),
         ]
     )
