2e9b902783fe50b6c3df6721c5f1f594336f5821
jtyrek <jakub_tyrek@mckinsey.com>
2020-02-26 22:05:23 +0700

    Added dev node

---
+++ src/customer360/pipelines/cvm/src/dev/nodes.py
@@ -0,0 +1,67 @@
+# Copyright 2018-2019 QuantumBlack Visual Analytics Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND
+# NONINFRINGEMENT. IN NO EVENT WILL THE LICENSOR OR OTHER CONTRIBUTORS
+# BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF, OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# The QuantumBlack Visual Analytics Limited ("QuantumBlack") name and logo
+# (either separately or in combination, "QuantumBlack Trademarks") are
+# trademarks of QuantumBlack. The License does not grant you any right or
+# license to the QuantumBlack Trademarks. You may not use the QuantumBlack
+# Trademarks or any confusingly similar mark as a trademark for your product,
+#     or use the QuantumBlack Trademarks in any other manner that might cause
+# confusion in the marketplace, including but not limited to in advertising,
+# on websites, or on software.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pyspark.sql import DataFrame
+from typing import Dict, Any
+
+
+def create_dev_version(
+        df: DataFrame,
+        parameters: Dict[str, Any],
+) -> DataFrame:
+    """ Create dev sample of given table. Dev sample is super small sample.
+
+    Args:
+        parameters: parameters defined in parameters.yml.
+        df: given table.
+    Returns:
+        Dev sample of table.
+    """
+
+    chosen_date = parameters["dev_parameters"]["chosen_date"]
+
+    dates_cols = ["partition_month", "event_partition_date", "start_of_month"]
+    subs_cols = ["subscription_identifier"]
+
+    dates_intersect = set(dates_cols) & set(df.columns)
+    subs_intersect = set(subs_cols) & set(df.columns)
+
+    dates_col = dates_intersect.pop()
+    subs_col = subs_intersect.pop()
+
+    dates_filter = "{} == {}".format(subs_col, chosen_date)
+    df = df.withColumn(
+        "subscription_identifier_last_letter",
+        df.subscription_identifier.substr(-2, 2))
+    subs_filter = "subscription_identifier_last_letter == {}".format()
+    dates_filter = "{} == '{}".format(dates_col, chosen_date)
+
+    df = df.filter(subs_filter).filter(dates_filter).drop(
+        "subscription_identifier_last_letter")
+
+    return df
