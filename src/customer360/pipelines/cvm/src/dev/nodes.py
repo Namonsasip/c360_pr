d401af3ffede2c03454919bd738e8cd17613c1ad
jtyrek <jakub_tyrek@mckinsey.com>
2020-02-26 22:44:09 +0700

    Add missing quote

--- src/customer360/pipelines/cvm/src/dev/nodes.py
+++ src/customer360/pipelines/cvm/src/dev/nodes.py
@@ -56,7 +56,7 @@ def create_dev_version(
         df.subscription_identifier.substr(-2, 2))
     subs_filter = "subscription_identifier_last_letter == '{}'".format(
         subscription_id_suffix)
-    dates_filter = "{} == '{}".format(dates_col, chosen_date)
+    dates_filter = "{} == '{}'".format(dates_col, chosen_date)
 
     df = df.filter(subs_filter).filter(dates_filter).drop(
         "subscription_identifier_last_letter")
