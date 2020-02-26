8ea8fcca10f8ace31d0b4ea10627acb1cdc62dde
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-02-27 00:46:41 +0530

    few bug fixes on campaigns

--- src/customer360/pipelines/data_engineering/pipelines/campaign_pipeline/to_l4/to_l4_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/campaign_pipeline/to_l4/to_l4_pipeline.py
@@ -50,6 +50,11 @@ def campaign_to_l4_pipeline(**kwargs):
             node(
                 add_relative_time_features, ['l4_campaign_postpaid_prepaid_fbb_int'],
                 'l4_campaign_postpaid_prepaid_fbb_features'
+            ),
+            node(
+                l4_rolling_ranked_window, ["l2_campaign_top_channel_weekly",
+                                           "params:l4_campaign_top_channel_features"],
+                "l4_campaign_top_channel_features"
             )
 
         ], name="campaign_to_l4_pipeline"
