862fc5d1ee2082fb307bbe14308e26be8a76a9b2
jtyrek <jakub_tyrek@mckinsey.com>
2020-02-27 16:06:17 +0700

    Format file

--- src/customer360/pipeline.py
+++ src/customer360/pipeline.py
@@ -67,7 +67,8 @@ from .pipelines.data_engineering.pipelines.revenue_pipeline.to_l4 import \
     revenue_to_l4_pipeline
 from .pipelines.data_engineering.pipelines.campaign_pipeline.to_l1 import \
     campaign_to_l1_pipeline
-from customer360.pipelines.cvm.data_prep.pipeline import create_cvm_prepare_data_pipeline, \
+from customer360.pipelines.cvm.data_prep.pipeline import \
+    create_cvm_prepare_data_pipeline, \
     create_cvm_prepare_sample_data_pipeline
 from customer360.pipelines.cvm.dev.pipeline import \
     create_cvm_dev_inputs_pipeline, \
