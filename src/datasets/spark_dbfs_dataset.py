718495f6ef7807a54308d3747554073143e0bc9b
Miguel Rodriguez Gutierrez <miguel_rodriguez_gutierrez@mckinsey.com>
2020-03-08 20:13:46 +0700

    Fixed catalog to work in Windows

---
+++ src/datasets/spark_dbfs_dataset.py
@@ -0,0 +1,28 @@
+from pathlib import PurePosixPath, WindowsPath
+from typing import Any, Dict, List, Tuple
+
+from kedro.contrib.io.pyspark import SparkDataSet
+from kedro.io import Version
+
+
+class SparkDbfsDataSet(SparkDataSet):
+    """
+    Fixes bugs from SparkDataSet
+    """
+
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        filepath: str,
+        file_format: str = "parquet",
+        load_args: Dict[str, Any] = None,
+        save_args: Dict[str, Any] = None,
+        version: Version = None,
+        credentials: Dict[str, Any] = None,
+    ) -> None:
+        super().__init__(
+            filepath, file_format, load_args, save_args, version, credentials,
+        )
+
+        # Fixes paths in Windows
+        if isinstance(self._filepath, WindowsPath):
+            self._filepath = PurePosixPath(str(self._filepath).replace("\\", "/"))
