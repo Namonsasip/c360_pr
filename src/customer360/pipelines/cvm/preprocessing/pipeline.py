282a2b64e68d4955fffa2aaf7faa8abeedef13af
Kankawee K <kankawee.kiat@gmail.com>
2020-03-09 11:37:05 +0700

    fix node names

--- src/customer360/pipelines/cvm/preprocessing/pipeline.py
+++ src/customer360/pipelines/cvm/preprocessing/pipeline.py
@@ -32,7 +32,7 @@ from customer360.pipelines.cvm.src.utils.get_suffix import get_suffix
 from src.customer360.pipelines.cvm.preprocessing.nodes import (
     pipeline1_fit,
     pipeline1_transform,
-    feature_selection,
+    feature_selection_all_target,
 )
 
 
@@ -65,7 +65,7 @@ def create_cvm_preprocessing(sample_type: str = None) -> Pipeline:
                 name="create_l5_cvm_one_day_test_preprocessed" + suffix,
             ),
             node(
-                feature_selection,
+                feature_selection_all_target,
                 ["l5_cvm_one_day_train_preprocessed" + suffix, "parameters"],
                 ["important_columns", "rfe_plot"],
                 name="feature_selection_l5_cvm_one_day_train_preprocessed" + suffix,
