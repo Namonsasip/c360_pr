4df52ae3e81668be0661bfb335bbd0ed731882b3
Kankawee K <kankawee.kiat@gmail.com>
2020-03-09 10:55:15 +0700

    add new node to pipelines

--- src/customer360/pipelines/cvm/preprocessing/pipeline.py
+++ src/customer360/pipelines/cvm/preprocessing/pipeline.py
@@ -32,6 +32,7 @@ from customer360.pipelines.cvm.src.utils.get_suffix import get_suffix
 from src.customer360.pipelines.cvm.preprocessing.nodes import (
     pipeline1_fit,
     pipeline1_transform,
+    feature_selection,
 )
 
 
@@ -62,6 +63,12 @@ def create_cvm_preprocessing(sample_type: str = None) -> Pipeline:
                 ["l5_cvm_one_day_test" + suffix, "parameters"],
                 "l5_cvm_one_day_test_preprocessed" + suffix,
                 name="create_l5_cvm_one_day_test_preprocessed" + suffix,
+            ),
+            node(
+                feature_selection,
+                ["l5_cvm_one_day_train_preprocessed" + suffix, "parameters"],
+                ["important_columns", "rfe_plot"],
+                name="feature_selection_l5_cvm_one_day_train_preprocessed" + suffix,
             ),
         ]
     )
