d24ccdee2559160f1f17722f5565c68b6e53daa2
jtyrek <jakub_tyrek@mckinsey.com>
2020-03-09 22:35:07 +0700

    Try installing instead of importing xgboost for pyspark udf

--- src/customer360/pipelines/cvm/src/models/predict.py
+++ src/customer360/pipelines/cvm/src/models/predict.py
@@ -36,6 +36,7 @@ import xgboost
 
 from customer360.pipelines.cvm.src.utils.list_operations import list_sub
 from customer360.pipelines.cvm.src.utils.list_targets import list_targets
+import os
 
 
 def pandas_predict_xgb(
@@ -82,7 +83,7 @@ def pyspark_predict_xgb(
             returnType=DoubleType(), functionType=func.PandasUDFType.SCALAR
         )
         def _pandas_predict(*cols):
-            import xgboost
+            os.system("pip install xgboost")
 
             chosen_xgb_model = xgb_models[target_chosen]
             pd_df = pd.concat(cols, axis=1)
