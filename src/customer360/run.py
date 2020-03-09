b4a127bbc74e28f29ff2be705d726d10dac2f9a3
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-03-09 12:16:20 +0530

    added updated catalog

--- src/customer360/run.py
+++ src/customer360/run.py
@@ -105,7 +105,7 @@ class ProjectContext(KedroContext):
         return catalog
 
 
-def run_package(pipelines=['campaign_to_l4_pipeline']):
+def run_package(pipelines=['campaign_to_l1_pipeline']):
     # entry point for running pip-install projects
     # using `<project_package>` command
     project_context = load_context(Path.cwd(), env=conf)
