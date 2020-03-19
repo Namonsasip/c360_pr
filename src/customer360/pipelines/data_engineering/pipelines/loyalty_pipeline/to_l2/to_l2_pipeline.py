48656c3d46e479a36716bcec6f42faccbd78edc9
saurabh.verma <saurabh.verma@quantumblack.com>
2020-03-19 18:24:44 +0530

    loyalty features

--- src/customer360/pipelines/data_engineering/pipelines/loyalty_pipeline/to_l2/to_l2_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/loyalty_pipeline/to_l2/to_l2_pipeline.py
@@ -15,21 +15,21 @@ def loyalty_to_l2_pipeline(**kwargs):
                  "l2_loyalty_number_of_services"
             ),
 
-            # Number of rewards for each category
-            node(
-                node_from_config,
-                ["l1_loyalty_number_of_rewards",
-                 "params:l2_loyalty_number_of_rewards_weekly"],
-                "l2_loyalty_number_of_rewards"
-            ),
-
-            # Number of points spend for each category
-            node(
-                node_from_config,
-                ["l1_loyalty_number_of_points_spend",
-                 "params:l2_loyalty_number_of_points_spend_weekly"],
-                "l2_loyalty_number_of_points_spend"
-            ),
+            # # Number of rewards for each category
+            # node(
+            #     node_from_config,
+            #     ["l1_loyalty_number_of_rewards",
+            #      "params:l2_loyalty_number_of_rewards_weekly"],
+            #     "l2_loyalty_number_of_rewards"
+            # ),
+            #
+            # # Number of points spend for each category
+            # node(
+            #     node_from_config,
+            #     ["l1_loyalty_number_of_points_spend",
+            #      "params:l2_loyalty_number_of_points_spend_weekly"],
+            #     "l2_loyalty_number_of_points_spend"
+            # ),
 
             #Serenade class weekly
             node(
