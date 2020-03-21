8b036e962c6907f0103497a5008b467f7f0b08ae
jtyrek <jakub_tyrek@mckinsey.com>
2020-03-21 14:57:25 +0100

    Fix taking max date

--- src/customer360/pipelines/cvm/src/utils/incremental_manipulation.py
+++ src/customer360/pipelines/cvm/src/utils/incremental_manipulation.py
@@ -47,8 +47,8 @@ def get_latest_date(df: DataFrame, maximum_date: str = None) -> str:
 
     if maximum_date is not None:
         df = df.filter("key_date <= '{}'".filter(maximum_date))
-
-    return df.select("key_date").collect()[0][0]
+    max_date = df.select("key_date").agg("max").collect()[0][0]
+    return max_date.strftime("%Y-%m-%d")
 
 
 def filter_latest_date(df: DataFrame) -> DataFrame:
