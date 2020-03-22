40641ab59f02bb19d0858ddf605b15263d06d932
saurabh.verma <saurabh.verma@quantumblack.com>
2020-03-23 00:42:58 +0530

    increment_load_implementation_for_revenue_domain

--- src/customer360/pipelines/data_engineering/nodes/revenue_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/revenue_nodes/to_l3/to_l3_nodes.py
@@ -55,3 +55,7 @@ def merge_with_customer_postpaid_df(source_df: DataFrame,
     final_df = final_df.drop_duplicates(subset=["subscription_identifier", "start_of_month"])
 
     return final_df
+
+
+def copy_df(df: DataFrame):
+    return df
