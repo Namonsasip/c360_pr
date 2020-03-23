9ee52b6bd17d82e24fd0723d43e2d0972ebb2217
saurabh.verma <saurabh.verma@quantumblack.com>
2020-03-24 00:26:09 +0530

    fix number of points spend and reward feature

--- src/customer360/pipelines/data_engineering/nodes/loyalty_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/loyalty_nodes/to_l1/to_l1_nodes.py
@@ -129,6 +129,25 @@ def daily_privilege_or_aunjai_data_with_customer_profile(customer_prof,privilege
 
     return output_df
 
+
+def daily_privilege_or_aunjai_data_with_customer_profile_reward_and_points_spend(customer_prof,privilege_or_aunjai_data,priv_project):
+
+
+    output_df_1 = customer_prof.join(privilege_or_aunjai_data,(customer_prof.access_method_num == privilege_or_aunjai_data.mobile_no) &
+                                   (customer_prof.register_date.eqNullSafe(f.to_date(privilege_or_aunjai_data.register_date))) &
+                                   (customer_prof.event_partition_date == f.to_date(privilege_or_aunjai_data.response_date)))
+
+    output_df_1 = output_df_1.drop(privilege_or_aunjai_data.register_date)
+
+    output_df_2 = output_df_1.join(priv_project,
+                               output_df_1.project_id.cast(StringType()) == priv_project.project_id.cast(StringType()))
+
+    output_df = output_df_2.drop(output_df_1.project_id)\
+        .drop(output_df_1.category)
+
+    return output_df
+
+
 def daily_privilege_or_aunjai_data_with_priv_points(privilege_or_aunjai_data_with_customer_profile,priv_points_raw):
 
     privilege_or_aunjai_data = privilege_or_aunjai_data_with_customer_profile.select("access_method_num",
@@ -186,6 +205,7 @@ def loyalty_number_of_rewards_for_each_category(customer_prof,input_df,priv_proj
     """
 
     priv_project = priv_project_ranked(priv_project)
+    priv_project = priv_project.where("project_type_id = 6 and project_subtype like 'REDEEM%'")
 
     customer_prof = customer_prof.select("access_method_num",
                                          "subscription_identifier",
@@ -194,7 +214,11 @@ def loyalty_number_of_rewards_for_each_category(customer_prof,input_df,priv_proj
                                          "start_of_week",
                                          "start_of_month")
 
-    return_df = massive_processing(input_df, customer_prof, priv_project, daily_privilege_or_aunjai_data_with_customer_profile,
+    customer_prof = customer_prof.where("event_partition_date is not null")
+
+    input_df = input_df.where("msg_event_id = 13")
+
+    return_df = massive_processing(input_df, customer_prof, priv_project, daily_privilege_or_aunjai_data_with_customer_profile_reward_and_points_spend,
                                    sql,'response_date', 'event_partition_date',"l1_loyalty_number_of_rewards")
     return return_df
 
@@ -204,6 +228,7 @@ def loyalty_number_of_points_spend_for_each_category(customer_prof,input_df,priv
     """
 
     priv_project = priv_project_ranked(priv_project)
+    priv_project = priv_project.where("project_type_id = 6 and project_subtype like 'REDEEM%'")
 
     customer_prof = customer_prof.select("access_method_num",
                                          "subscription_identifier",
@@ -211,7 +236,10 @@ def loyalty_number_of_points_spend_for_each_category(customer_prof,input_df,priv
                                          "event_partition_date",
                                          "start_of_week",
                                          "start_of_month")
+    customer_prof = customer_prof.where("event_partition_date is not null")
 
-    return_df = customize_massive_processing(input_df, customer_prof, priv_project, priv_points_raw, daily_privilege_or_aunjai_data_with_customer_profile,
+    input_df = input_df.where("msg_event_id = 13")
+
+    return_df = customize_massive_processing(input_df, customer_prof, priv_project, priv_points_raw, daily_privilege_or_aunjai_data_with_customer_profile_reward_and_points_spend,
                                    sql,'response_date','event_partition_date',"l1_loyalty_number_of_points_spend")
     return return_df
