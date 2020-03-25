e829a69fc5e8a04323e52cdc4b5485340921dc2f
jtyrek <jakub_tyrek@mckinsey.com>
2020-03-25 17:51:27 +0100

    Fix import

--- src/cvm/modelling/nodes.py
+++ src/cvm/modelling/nodes.py
@@ -31,7 +31,7 @@ from typing import Dict, Any
 from sklearn.ensemble import RandomForestClassifier
 import xgboost
 import logging
-from cvm.src.models import get_pandas_train_test_sample
+from cvm.src.models.get_pandas_train_test_sample import get_pandas_train_test_sample
 from cvm.src.models.predict import (
     pyspark_predict_rf,
     pyspark_predict_xgb,
