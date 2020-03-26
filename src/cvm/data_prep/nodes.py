b4e5f3b7efa0f0ebb0f37949cc9a140b213876b7
jtyrek <jakub_tyrek@mckinsey.com>
2020-03-26 17:14:06 +0100

    Removed logging from joining

--- src/cvm/data_prep/nodes.py
+++ src/cvm/data_prep/nodes.py
@@ -25,7 +25,6 @@
 #
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from typing import Any, Dict, List
 from pyspark.sql import DataFrame
 import functools
