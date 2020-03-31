ce0ca90aac0e24c8f5a710fa11a86e0783923fca
Miguel Rodriguez Gutierrez <miguel_rodriguez_gutierrez@mckinsey.com>
2020-03-31 16:45:50 +0200

    Added functions to train models in a distributed way using pandas udf

--- src/nba/models/models_pipeline.py
+++ src/nba/models/models_pipeline.py
@@ -1,16 +1,21 @@
+import getpass
 from functools import partial
 
 from kedro.pipeline import Pipeline, node
 
-from nba.models.models_nodes import train_single_binary_model
+from customer360.utilities.datetime_utils import get_local_datetime
+from nba.models.models_nodes import (
+    train_multiple_binary_models,
+    create_binary_model_function,
+)
 
 
 def create_nba_models_pipeline() -> Pipeline:
     return Pipeline(
         [
             node(
-                partial(
-                    train_single_binary_model,
+                lambda pdf_master_chunk, pdf_extra_pai_metrics: create_binary_model_function(
+                    as_pandas_udf=False,
                     group_column="campaign_child_code",
                     explanatory_features=[
                         "min_campaign_total_by_sms_sum_weekly_last_week",
@@ -34,15 +39,45 @@ def create_nba_models_pipeline() -> Pipeline:
                         "importance_type": "gain",
                     },
                     min_obs_per_class_for_model=100,
+                    pai_run_prefix=(
+                        f"{get_local_datetime().strftime('%Y%m%d_%H%M%S')}_"
+                        f"{getpass.getuser()[0:6]}_"
+                        "debug_"
+                    ),
                     pai_storage_path=None,
+                    pdf_extra_pai_metrics=pdf_extra_pai_metrics,
+                )(
+                    pdf_master_chunk
                 ),
                 inputs={
                     "pdf_master_chunk": "l5_nba_master_table_chunk_debug",
                     "pdf_extra_pai_metrics": "master_table_chunk_debug_extra_pai_metrics",
                 },
-                outputs=None,
+                outputs="unused_memory_dataset_debug_model_training",
                 name="debug_model_training",
                 tags=["debug_model_training"],
+            ),
+            node(
+                partial(
+                    train_multiple_binary_models,
+                    pai_run_prefix=(
+                        f"{get_local_datetime().strftime('%Y%m%d_%H%M%S')}_"
+                        f"{getpass.getuser()[0:6]}_"
+                    ),
+                ),
+                inputs={
+                    "df_master": "l5_nba_master_table",
+                    "group_column": "params:nba_model_group_column",
+                    "explanatory_features": "params:nba_model_explanatory_features",
+                    "target_column": "params:nba_model_target_column",
+                    "train_sampling_ratio": "params:nba_model_train_sampling_ratio",
+                    "model_params": "params:nba_model_model_params",
+                    "min_obs_per_class_for_model": "params:nba_model_min_obs_per_class_for_model",
+                    "pai_storage_path": "params:nba_model_pai_storage_path",
+                },
+                outputs="unused_memory_dataset_nba_models_training",
+                name="nba_models_training",
+                tags=["nba_models_training"],
             ),
         ],
         tags="nba_models",
