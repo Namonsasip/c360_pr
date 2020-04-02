aaf5158a9d0ef1229e5cc6662829f6215cb4c284
petch <nummpetch@gmail.com>
2020-04-02 12:26:28 +0700

    update test serenade weekly

--- src/tests/test_unit_tests/test_unit_loyalty_features.py
+++ src/tests/test_unit_tests/test_unit_loyalty_features.py
@@ -20,6 +20,7 @@ class TestUnitLoyalty:
         random_type = [545,565,116,124,10,11,17,27,125,30,97,100,100041,2,15,16,26,29,118,119,120,121,122,585,13,28,117,126,127,129,130,131,132,133,134,501,505,14,25,101,123,128,525]
         # print(len(random_type))
         # exit(0)
+        serenade = ['Standard','Classic','Emerald','Gold']
         msisdn_type = ['ank5bkxpOWxOZStrY2FFaVpzeEd6UXl4MDNkanZnMlR0dGdjaGRyUERDUmI0aEx2S21QMVV3eHVhTGdUS2MxNA==','']
         date1 = '2019-01-01'
         date2 = '2019-04-01'
@@ -28,13 +29,14 @@ class TestUnitLoyalty:
         my_dates = [iTemp.date().strftime("%d-%m-%Y") for iTemp in my_dates_list]
         my_dates = my_dates * 3
         random.seed(100)
+        serenade_list =  [serenade[random.randint(0, 3)] for iTemp in range(0, len(my_dates))]
         random_list = [random_type[random.randint(0, 42)] for iTemp in range(0, len(my_dates))]
         msisdn_list = [msisdn_type[random.randint(0, 1)] for iTemp in range(0, len(my_dates))]
         random.seed(100)
         random_list2 = [random.randint(1, 10) * 100 for iTemp in range(0, len(my_dates))]
         point_list = [random.randint(1, 10) for iTemp in range(0, len(my_dates))]
-        df = spark.createDataFrame(zip(random_list, my_dates, random_list2, my_dates,msisdn_list,point_list),
-                                      schema=['category', 'temp', 'face_value','register_date','msisdn','loyalty_points_spend']) \
+        df = spark.createDataFrame(zip(random_list, my_dates, random_list2, my_dates,msisdn_list,point_list,serenade_list),
+                                      schema=['category', 'temp', 'face_value','register_date','msisdn','loyalty_points_spend','ma_segment']) \
             .withColumn("access_method_num", F.lit(1)) \
             .withColumn("recharge_date", F.to_date(F.lit('2019-01-01'), 'yyyy-MM-dd')) \
             .withColumn("event_partition_date", F.to_date('temp', 'dd-MM-yyyy')) \
@@ -43,6 +45,7 @@ class TestUnitLoyalty:
             .withColumn("subscription_identifier", F.lit(123)) \
             .withColumn("mobile_no", F.lit("TEc5cUU1dXRnbDFDRCtwMTJtVlhsZ1p3NW5TOUgxcjdaWEhoY2VJMStYOGFWaTFWZUE3bmV1czZBKy5qQlE2Yg==")) \
             .withColumn("project_id", F.lit('ODA4NDY=')) \
+            .withColumn("last_update", F.lit('2019-08-01T11:25:55.000+0000')) \
             .withColumn("project_type_id", F.lit(1)) \
             .withColumn("project_subtype", F.lit(1)) 
         
@@ -50,6 +53,24 @@ class TestUnitLoyalty:
             .withColumn("start_of_week", F.to_date(F.date_trunc('week', df.event_partition_date)))
 
         return df
+
+    def test_serenade(self,project_context):
+        var_project_context = project_context['ProjectContext']
+        df0 = TestUnitLoyalty().gen_data(project_context)
+
+        df = node_from_config(df0,var_project_context.catalog.load(
+            'params:l2_loyalty_serenade_class_weekly'))
+  
+        assert \
+            df.where("loyalty_serenade_class='Emerald'").select("loyalty_is_emerald").collect()[0][
+                0] == 'Y'
+        assert \
+            df.where("loyalty_serenade_class='Classic'").select("loyalty_is_classic").collect()[0][
+                0] == 'Y'
+        assert \
+            df.where("loyalty_serenade_class='Gold'").select("loyalty_is_gold").collect()[0][
+                0] == 'Y'
+
 
     def test_number_of_rewards(self,project_context):
         var_project_context = project_context['ProjectContext']
