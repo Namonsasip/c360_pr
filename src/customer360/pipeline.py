66a0c6d86ff4a8e221c9c2099bea67ef10494403
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-04-15 16:24:47 +0530

    Adding pipeline

--- src/customer360/pipeline.py
+++ src/customer360/pipeline.py
@@ -100,11 +100,8 @@ from .pipelines.data_engineering.pipelines.complaints_pipeline.to_l4.to_l4_pipel
 # from .pipelines.data_engineering.pipelines.device_pipeline.to_l3.to_l3_pipeline import (
 #     device_to_l3_pipeline,
 # )
-from .pipelines.data_engineering.pipelines.revenue_pipeline.to_l3 import (
-    revenue_to_l3_pipeline,
-)
-from .pipelines.data_engineering.pipelines.revenue_pipeline.to_l4 import (
-    revenue_to_l4_pipeline,
+from .pipelines.data_engineering.pipelines.revenue_pipeline import (
+    revenue_to_l1_pipeline, revenue_to_l4_daily_pipeline, revenue_to_l3_pipeline, revenue_to_l4_pipeline,
 )
 from .pipelines.data_engineering.pipelines.stream_pipeline.to_l1.to_l1_pipeline import (
     streaming_to_l1_pipeline,
@@ -223,6 +220,8 @@ def create_c360_pipeline(**kwargs) -> Dict[str, Pipeline]:
         "streaming_to_l2_pipeline": streaming_to_l2_pipeline(),
         "streaming_to_l3_pipeline": streaming_to_l3_pipeline(),
         "streaming_to_l4_pipeline": streaming_to_l4_pipeline(),
+        "revenue_to_l1_pipeline": revenue_to_l1_pipeline(),
+        "revenue_to_l4_daily_pipelin": revenue_to_l4_daily_pipeline(),
         "revenue_to_l3_pipeline": revenue_to_l3_pipeline(),
         "revenue_to_l4_pipeline": revenue_to_l4_pipeline(),
         "complaints_to_l1_pipeline": complaints_to_l1_pipeline(),
---
+++ src/customer360/pipelines/data_engineering/pipelines/revenue_pipeline/__init__.py
@@ -0,0 +1,4 @@
+from .to_l1.to_l1_pipeline import revenue_to_l1_pipeline
+from .to_l4.to_l4_daily_pipeline import revenue_to_l4_daily_pipeline
+from .to_l3.to_l3_pipeline import revenue_to_l3_pipeline
+from .to_l4.to_l4_pipeline import revenue_to_l4_pipeline
