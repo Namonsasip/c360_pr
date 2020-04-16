8256de8f2bedadfd6a600e063d90804297eede09
jtyrek <jakub_tyrek@mckinsey.com>
2020-04-16 16:56:02 +0200

    Changed parameters of rfe

--- src/cvm/src/utils/feature_selection.py
+++ src/cvm/src/utils/feature_selection.py
@@ -41,12 +41,20 @@ def feature_selection(
     if target_type == "class":
         rfc = RandomForestClassifier(random_state=101, n_estimators=20)
         rfecv = RFECV(
-            estimator=rfc, step=step_size, cv=StratifiedKFold(5), scoring="roc_auc"
+            estimator=rfc,
+            step=step_size,
+            cv=StratifiedKFold(5),
+            scoring="roc_auc",
+            min_features_to_select=20,
         )
     else:
         lr = LinearRegression(normalize=True)
         rfecv = RFECV(
-            estimator=lr, step=step_size, cv=StratifiedKFold(5), scoring="roc_auc"
+            estimator=lr,
+            step=step_size,
+            cv=StratifiedKFold(5),
+            scoring="roc_auc",
+            min_features_to_select=20,
         )
     rfecv.fit(features, target)
 
