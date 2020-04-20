d3cc02c0f266304eb31bb5b2721af363a1726b0f
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-04-20 07:58:51 +0530

    bug fix

--- src/customer360/pipelines/data_engineering/nodes/revenue_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/revenue_nodes/to_l1/to_l1_nodes.py
@@ -47,14 +47,16 @@ def massive_processing_with_customer(input_df: DataFrame
     add_list.remove(first_item)
     for curr_item in add_list:
         logging.info("running for dates {0}".format(str(curr_item)))
-        small_df = data_frame.filter(F.col("partition_date").isin(*[curr_item]))
+        small_df = data_frame.filter(F.col("partition_date").isin(*[curr_item]))\
+            .drop_duplicates(subset=["access_method_num", "partition_date"])
         small_cus_df = customer_df.filter(F.col("event_partition_date").isin(*[curr_item]))
         output_df = node_from_config(small_df, sql)
         output_df = small_cus_df.join(output_df, ["access_method_num", "event_partition_date", "start_of_week"], "left")
         CNTX.catalog.save("l1_revenue_prepaid_pru_f_usage_multi_daily", output_df)
 
     logging.info("Final date to run for {0}".format(str(first_item)))
-    return_df = data_frame.filter(F.col("partition_date").isin(*[first_item]))
+    return_df = data_frame.filter(F.col("partition_date").isin(*[first_item])) \
+                .drop_duplicates(subset=["access_method_num", "partition_date"])
     return_df = node_from_config(return_df, sql)
     small_cus_df = customer_df.filter(F.col("event_partition_date").isin(*[first_item]))
     return_df = small_cus_df.join(return_df, ["access_method_num", "event_partition_date", "start_of_week"], "left")
