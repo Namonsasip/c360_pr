3b258942d57d677389844a46224d627718b51f18
saurabh.verma <saurabh.verma@quantumblack.com>
2020-04-28 14:52:27 +0530

    added data availability changes for stream L2 layer

--- src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l1/to_l1_nodes.py
@@ -1,6 +1,5 @@
 import os
 from src.customer360.utilities.spark_util import get_spark_empty_df
-from pyspark.sql.types import *
 from customer360.utilities.re_usable_functions import union_dataframes_with_missing_cols, check_empty_dfs, \
     data_non_availability_and_missing_check
 from pyspark.sql import functions as f, DataFrame
--- src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l2/to_l2_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l2/to_l2_nodes.py
@@ -1,7 +1,10 @@
 import os
 
-from customer360.utilities.spark_util import get_spark_session
+from customer360.utilities.spark_util import get_spark_session, get_spark_empty_df
 from pathlib import Path
+from customer360.utilities.re_usable_functions import union_dataframes_with_missing_cols, check_empty_dfs, \
+    data_non_availability_and_missing_check
+from pyspark.sql import functions as f, DataFrame
 
 conf = os.getenv("CONF", None)
 
@@ -28,3 +31,38 @@ def generate_l2_fav_streaming_day(input_df, app_list):
                          .format(each_app), df)
 
     return None
+
+
+def dac_for_streaming_to_l2_pipeline_from_l1(input_df: DataFrame, target_table_name: str):
+
+    ################################# Start Implementing Data availability checks #############################
+    if check_empty_dfs([input_df]):
+        return get_spark_empty_df()
+
+    input_df = data_non_availability_and_missing_check(df=input_df, grouping="weekly", par_col="event_partition_date",
+                                                       target_table_name=target_table_name,
+                                                       missing_data_check_flg='Y')
+
+    if check_empty_dfs([input_df]):
+        return get_spark_empty_df()
+
+    ################################# End Implementing Data availability checks ###############################
+
+    return input_df
+
+
+def dac_for_streaming_to_l2_pipeline_from_l2(input_df: DataFrame, target_table_name: str):
+
+    ################################# Start Implementing Data availability checks #############################
+    if check_empty_dfs([input_df]):
+        return get_spark_empty_df()
+
+    input_df = data_non_availability_and_missing_check(df=input_df, grouping="weekly", par_col="start_of_week",
+                                                       target_table_name=target_table_name)
+
+    if check_empty_dfs([input_df]):
+        return get_spark_empty_df()
+
+    ################################# End Implementing Data availability checks ###############################
+
+    return input_df
