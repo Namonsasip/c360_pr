8d576ca58bc0720e50d46e605ddcef321b8dbac1
jtyrek <jakub_tyrek@mckinsey.com>
2020-04-30 21:07:30 +0200

    Added missing quote

--- src/cvm/src/treatments/treatments_build.py
+++ src/cvm/src/treatments/treatments_build.py
@@ -189,7 +189,7 @@ def get_recently_contacted(
     recent_past_date = add_days(today, -parameters["treatment_cadence"])
     recent_users = (
         treatments_history.filter(
-            f"key_date >= '{recent_past_date}' and key_date < '{today}"
+            f"key_date >= '{recent_past_date}' and key_date < '{today}'"
         )
         .select("subscription_identifier")
         .distinct()
