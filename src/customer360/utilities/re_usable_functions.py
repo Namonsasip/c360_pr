d3c44e43a35b634c9b3a40556cefe433350d2c59
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-05-04 07:24:01 +0530

    fix

--- src/customer360/utilities/re_usable_functions.py
+++ src/customer360/utilities/re_usable_functions.py
@@ -352,9 +352,10 @@ def data_non_availability_and_missing_check(df, grouping, par_col, target_table_
     :return:
     """
     logging.info("Entering data_non_availability_and_missing_check")
-    if (run_mode.upper() == 'UNIT_TEST') | (run_mode.upper() == 'LOCAL_FS'):
-        logging.info("Exiting data_non_availability_and_missing_check as OS ENV RUN_MODE  = {}".format(run_mode))
-        return df
+    if run_mode is not None:
+        if (run_mode.upper() == 'UNIT_TEST') | (run_mode.upper() == 'LOCAL_FS'):
+            logging.info("Exiting data_non_availability_and_missing_check as OS ENV RUN_MODE  = {}".format(run_mode))
+            return df
     logging.info("Executing data_non_availability_and_missing_check IMP: OS ENV RUN_MODE NO UNIT TEST CASE SELECTED")
     spark = get_spark_session()
     mtdt_tbl = spark.read.parquet('/mnt/customer360-blob-output/C360/metadata_table/')
