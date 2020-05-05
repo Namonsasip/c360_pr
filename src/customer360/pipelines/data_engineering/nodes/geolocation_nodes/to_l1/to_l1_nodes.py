8c33c9d96ab5f2b578c238a626a64863e40b14d8
sboonchusanong <litty_135@hotmail.com>
2020-05-05 14:19:07 +0700

    master table creation (voice+data)

--- src/customer360/pipelines/data_engineering/nodes/geolocation_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/geolocation_nodes/to_l1/to_l1_nodes.py
@@ -8,8 +8,9 @@ from pathlib import Path
 import logging
 import os
 import statistics
+from pyspark.sql import Window
 
-from customer360.utilities.re_usable_functions import add_start_of_week_and_month
+from customer360.utilities.re_usable_functions import add_start_of_week_and_month, union_dataframes_with_missing_cols
 
 
 def l1_int_number_of_bs_used(input_df):
@@ -216,80 +217,93 @@ def l1_geo_data_distance_weekend_daily(df, sql):
 
     return df
 
-# def l1_geo_data_frequent_cell_weekday_daily(df, sql):  # in progress weekday 434,437,443,449,452,455,458,461,464
-#     df = df.where('day_of_week in (1,2,3,4,5)')
-#     ranked = df.selectExpr('*', 'row_number() over(partition by mobile_no,start_of_week order by sum_call DESC) as rank')
-#     # ranked = df.withColumn('rank_1', F.when(df.rank == 1, df.sum_call)).withColumn('rank_2',
-#     #                                                                                            F.when(df.rank == 2,
-#     #                                                                                                   df.sum_call)).withColumn(
-#     #     'rank_3', F.when(df.rank == 3, df.sum_call))
-#     # ranked = ranked.withColumn('rank_1', F.when(ranked.rank == 1, ranked.sum_call)).withColumn('rank_2',
-#     #                                                                                            F.when(ranked.rank == 2,
-#     #                                                                                                   ranked.sum_call)).withColumn(
-#     #     'rank_3', F.when(ranked.rank == 3, ranked.sum_call))
-#     # agg = ranked.groupBy('mobile_no', 'start_of_week').agg(F.sum('rank_1').alias('sum_no_of_call_rank_1'),
-#     #                                                        F.sum('rank_2').alias('sum_no_of_call_rank_2'),
-#     #                                                        F.sum('rank_3').alias('sum_no_of_call_rank_3'),
-#     #                                                        F.count(F.lit(1)).alias("Number_of_Unique_Cells"))
-#
-#     ranked = ranked.withColumn('sum_rank_1', F.when(ranked.rank == 1, ranked.sum_call)).withColumn('sum_rank_2', F.when(
-#         ranked.rank == 2, ranked.sum_call)).withColumn('sum_rank_3',
-#                                                        F.when(ranked.rank == 3, ranked.sum_call)).withColumn(
-#         'lac_rank_1', F.when(ranked.rank == 1, ranked.lac)).withColumn('ci_rank_1',
-#                                                                        F.when(ranked.rank == 1, ranked.ci)).withColumn(
-#         'lac_rank_2', F.when(ranked.rank == 1, ranked.lac)).withColumn('ci_rank_2', F.when(ranked.rank == 1, ranked.ci))
-#     df = node_from_config(ranked, sql)
-#     agg = ranked.groupBy('mobile_no', 'start_of_week').agg(F.sum('sum_rank_1').alias('sum_no_of_call_rank_1'),
-#                                                            F.sum('sum_rank_2').alias('sum_no_of_call_rank_2'),
-#                                                            F.sum('sum_rank_3').alias('sum_no_of_call_rank_3'),
-#                                                            F.count(F.lit(1)).alias("Number_of_Unique_Cells"),
-#                                                            F.concat_ws("", F.collect_list(ranked.lac_rank_1)).alias(
-#                                                                'lac_rank_1'),
-#                                                            F.concat_ws("", F.collect_list(ranked.ci_rank_1)).alias(
-#                                                                'ci_rank_1'),
-#                                                            F.concat_ws("", F.collect_list(ranked.lac_rank_2)).alias(
-#                                                                'lac_rank_2'),
-#                                                            F.concat_ws("", F.collect_list(ranked.ci_rank_2)).alias(
-#                                                                'ci_rank_2'))
-#
-#     #434 = groupby lac+ci count
-#     #437 lac + ci with most  (first most)
-#     #443  second most
-#     #449  count cell with first most
-#     #452  count cell with second most
-#     #455 count third
-#     #458 count fourth
-#     #461 count fifth
-#     #464 count all
-#
-#     return df
-#
-#
-# def l1_geo_data_frequent_cell_weekend_daily(df, sql):  # in progress weekend 435,439,445,450,453,456,459,462,465
-#     df = df.where('day_of_week in (6,7)')
-#     df = node_from_config(df, sql)
-#     return df
-#
-#
-# def l1_geo_data_frequent_cell_daily(df, sql):  # in progress all 436, 441,447,451,454,457,460,463
-#
-#     df = node_from_config(df, sql)
-#     return df
-#
-#
-# def l1_geo_data_frequent_cell_4g_weekday_daily(df, sql):  # in progress 4g_weekday 438,444
-#     df = df.where('day_of_week in (1,2,3,4,5)').where('gprs_type="4GLTE"')
-#     df = node_from_config(df, sql)
-#     return df
-#
-#
-# def l1_geo_data_frequent_cell_4g_weekend_daily(df, sql):  # in progress 4g_weekend 440,446
-#     df = df.where('day_of_week in (6,7)').where('gprs_type="4GLTE"')
-#     df = node_from_config(df, sql)
-#     return df
-#
-#
-# def l1_geo_data_frequent_cell_4g_daily(df, sql):  # in progress 4g_all 442,448
-#
-#     df = node_from_config(df, sql)
-#     return df
+
+def l1_geo_favorite_cell_master_table(usage_sum_voice_location_daily, usage_sum_data_location_daily):
+    '''
+    Ans
+       - GGSN: 2G
+       - 3GGSN: 3G
+       - 4GLTE: 4G
+       - 0,4: International outbound roaming (IR) . It can not map to geo-master.
+       - 8: NB-IoT (currently can use 4G cell only)
+    '''
+
+    #test
+    usage_sum_voice_location_daily = usage_sum_voice_location_daily.limit(100000)
+    usage_sum_data_location_daily = usage_sum_data_location_daily.limit(100000)
+
+
+
+    usage_sum_voice_location_daily = usage_sum_voice_location_daily.where('service_type in ("VOICE","VOLTE")')
+    usage_sum_voice_location_daily = usage_sum_voice_location_daily.selectExpr("*",
+                                                                               "CASE WHEN service_type in ('VOICE') "
+                                                                               "THEN  CONCAT(LPAD(lac, 5, '0'),"
+                                                                               "LPAD(ci, 5, '0')) WHEN service_type "
+                                                                               "in ('VOLTE') THEN  CONCAT(LPAD(ci, 9, "
+                                                                               "'0')) END AS cgi_partial")
+    # voice_location contains column no_of_call and no_of_inc
+    usage_sum_voice_location_daily = usage_sum_voice_location_daily.selectExpr('*',
+                                                                               'no_of_call+no_of_inc as no_of_call_new')
+    usage_sum_voice_location_daily = usage_sum_voice_location_daily.drop('no_of_call').withColumnRenamed(
+        "no_of_call_new", "no_of_call")
+
+    usage_sum_data_location_daily = usage_sum_data_location_daily.where('gprs_type in ("GGSN","3GGSN","4GLTE")')
+    usage_sum_data_location_daily = usage_sum_data_location_daily.selectExpr("*",
+                                                                             "CASE WHEN gprs_type in ('GGSN','3GGSN') "
+                                                                             "THEN  CONCAT(LPAD(lac, 5, '0'),LPAD(ci, "
+                                                                             "5, '0')) WHEN gprs_type in ('4GLTE') "
+                                                                             "THEN  CONCAT(LPAD(ci, 9, '0')) END AS "
+                                                                             "cgi_partial")
+
+    merged = union_dataframes_with_missing_cols([usage_sum_voice_location_daily.select("hour_id",
+                                                                                       "access_method_num",
+                                                                                       "cgi_partial", "no_of_call",
+                                                                                       "date_id"),
+                                                 usage_sum_data_location_daily.select("hour_id",
+                                                                                      F.col("mobile_no").alias(
+                                                                                          "access_method_num"),
+                                                                                      "cgi_partial", "no_of_call",
+                                                                                      "date_id")])
+    merged = merged.withColumn("day_type", F.when(
+        (F.date_format(F.col('date_id'), 'EEEE') != 'Saturday') & (F.date_format(F.col('date_id'), 'EEEE') != 'Sunday'),
+        F.lit('Weekday')).otherwise(F.lit("Weekend")))
+    merged = merged.withColumn("day_night",
+                               F.when(((F.col('hour_id') >= 20) & (F.col('hour_id') <= 8)), F.lit('Night')).otherwise(
+                                   F.lit("Day")))
+    win = Window.partitionBy("access_method_num").orderBy(F.col("no_of_call").desc())
+    home = merged.where("day_night = 'Night' or day_type = 'Weekend'").groupBy("access_method_num", "cgi_partial").agg(
+        F.sum(F.col("no_of_call")).alias("no_of_call"))
+    office = merged.where("day_night = 'Day' and day_type = 'Weekday'").groupBy("access_method_num", "cgi_partial").agg(
+        F.sum(F.col("no_of_call")).alias("no_of_call"))
+    temp_home = home
+
+    home = home.withColumn("rnk", F.row_number().over(win)).where("rnk = 1").select("access_method_num", "cgi_partial")
+    office = office.withColumn("rnk", F.row_number().over(win)).where("rnk = 1").select("access_method_num",
+                                                                                        "cgi_partial")
+
+    ## use the above dataset to start building features at L1
+    union_home_office = home.union(office)
+    union_home_office = union_home_office.withColumnRenamed('cgi_partial', 'cgi_partial_2').withColumnRenamed(
+        'access_method_num', 'access_method_num_2')
+    home = home.withColumnRenamed('cgi_partial', 'cgi_partial_home')
+    office = office.withColumnRenamed('cgi_partial', 'cgi_partial_office')
+    home_office = home.join(office, ["access_method_num"], how='outer')
+
+    all = merged.groupBy("access_method_num", "cgi_partial").agg(F.sum(F.col("no_of_call")).alias("no_of_call"))
+    ## Now find the top cell_ds similar to the above but make sure we do not have above cell_ids from home and office
+    top_other = all.join(union_home_office, (all.access_method_num == union_home_office.access_method_num_2) & (
+            all.cgi_partial == union_home_office.cgi_partial_2), how='left')
+    top_other = top_other.filter(F.col("cgi_partial_2").isNull())
+    ranked = top_other.withColumn("rnk", F.row_number().over(win)).select("access_method_num", "cgi_partial")
+
+    ## Top Rank 1 apart from home/office take source as merged
+    other_rank1 = ranked.where("rnk = 1").selectExpr("access_method_num", "cgi_partial as cgi_partial_other_rank_1")
+
+    ## Top Rank 2 apart from home/office take source as merged
+    other_rank2 = ranked.where("rnk = 2").selectExpr("access_method_num", "cgi_partial as cgi_partial_other_rank_2")
+    ## Now it is upto your implementation if you want to merge it or not
+    master_fav_cell = home_office.join(other_rank1, ['access_method_num'], how='left').join(other_rank2,
+                                                                                            ['access_method_num'],
+                                                                                            how='left')
+
+    return master_fav_cell
