e47f289309ecf2c1f192cebf2eac728f3fb37624
poom-js <53038303+poom-js@users.noreply.github.com>
2020-05-05 15:35:03 +0700

    Update to_l2_nodes.py
    edit from pyspark.sql import DataFrame in to to_l2_node.py

--- src/customer360/pipelines/data_engineering/nodes/device_nodes/to_l2/to_l2_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/device_nodes/to_l2/to_l2_nodes.py
@@ -1,5 +1,6 @@
 import pyspark.sql.functions as f
 from pyspark.sql import functions as F
+from pyspark.sql import DataFrame
 from customer360.utilities.config_parser import node_from_config
 from kedro.context.context import load_context
 from pathlib import Path
