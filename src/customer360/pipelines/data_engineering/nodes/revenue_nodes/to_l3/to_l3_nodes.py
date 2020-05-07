d6ab73d382cee00eec39e6899590f65e5c71c8a5
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-05-07 10:32:55 +0530

    changes for prepaid data

--- src/customer360/pipelines/data_engineering/nodes/revenue_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/revenue_nodes/to_l3/to_l3_nodes.py
@@ -1,7 +1,8 @@
 from pyspark.sql import DataFrame
+from pyspark.sql import functions as F
 
 from customer360.utilities.re_usable_functions import check_empty_dfs, \
-    data_non_availability_and_missing_check
+    data_non_availability_and_missing_check, union_dataframes_with_missing_cols
 
 
 def merge_with_customer_prepaid_df(source_df: DataFrame,
@@ -14,32 +15,49 @@ def merge_with_customer_prepaid_df(source_df: DataFrame,
     """
 
     ################################# Start Implementing Data availability checks ###############################
-    if check_empty_dfs([source_df]):
+    if check_empty_dfs([source_df, cust_df]):
         return source_df
 
-    source_df = data_non_availability_and_missing_check(df=source_df, grouping="monthly",
-                                                        par_col="start_of_month",
-                                                        target_table_name="l3_revenue_prepaid_ru_f_sum_revenue_by_service_monthly",
-                                                        missing_data_check_flg='N')
+    source_df = data_non_availability_and_missing_check(
+        df=source_df, grouping="monthly",
+        par_col="start_of_month",
+        target_table_name="l3_revenue_prepaid_ru_f_sum_revenue_by_service_monthly",
+        missing_data_check_flg='N')
 
-    if check_empty_dfs([source_df]):
+    cust_df = data_non_availability_and_missing_check(
+        df=cust_df, grouping="monthly",
+        par_col="start_of_month",
+        target_table_name="l3_revenue_prepaid_ru_f_sum_revenue_by_service_monthly",
+        missing_data_check_flg='Y')
+
+    # new section to handle data latency
+    min_value = union_dataframes_with_missing_cols(
+        [
+            source_df.select(
+                F.max(F.col("start_of_month")).alias("max_date")),
+            cust_df.select(
+                F.max(F.col("start_of_month")).alias("max_date"))
+        ]
+    ).select(F.min(F.col("max_date")).alias("min_date")).collect()[0].min_date
+
+    source_df = source_df.filter(F.col("start_of_month") <= min_value)
+    cust_df = cust_df.filter(F.col("start_of_month") <= min_value)
+
+    if check_empty_dfs([source_df, cust_df]):
         return source_df
 
     ################################# End Implementing Data availability checks ###############################
 
     # This code will populate a subscriber id to the data set.
-    cust_df_cols = ['access_method_num', 'partition_month', 'subscription_identifier']
+    cust_df_cols = ['access_method_num', 'start_of_month', 'subscription_identifier']
     join_key = ['access_method_num', 'start_of_month']
 
-    cust_df = cust_df.where("charge_type = 'Pre-paid'")
-
-    cust_df = cust_df.select(cust_df_cols).withColumnRenamed("partition_month", "start_of_month")
+    cust_df = cust_df.where("charge_type = 'Pre-paid'").select(cust_df_cols) \
+        .drop_duplicates(subset=["subscription_identifier", "access_method_num", "start_of_month"])
 
     final_df = source_df.join(cust_df, join_key)
 
-    final_df = final_df.where("subscription_identifier is not null")
-
-    final_df = final_df.where("start_of_month is not null")
+    final_df = final_df.where("subscription_identifier is not null and start_of_month is not null")
 
     final_df = final_df.drop_duplicates(subset=["subscription_identifier", "start_of_month"])
 
@@ -68,8 +86,7 @@ def process_revenue_postpaid_monthly(source_df: DataFrame) -> DataFrame:
     ################################# End Implementing Data availability checks ###############################
 
     # This code will populate a subscriber id to the data set.
-    source_df = source_df.withColumnRenamed("sub_id", "subscription_identifier")\
+    source_df = source_df.withColumnRenamed("sub_id", "subscription_identifier") \
         .where("subscription_identifier is not null and start_of_month is not null")
 
     return source_df
-
