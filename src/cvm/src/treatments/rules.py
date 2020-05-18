825169838587df2c50b173350c51f9dcd5459c86
jtyrek <jakub_tyrek@mckinsey.com>
2020-05-18 19:54:50 +0200

    [adv rules] Add caching

--- src/cvm/src/treatments/rules.py
+++ src/cvm/src/treatments/rules.py
@@ -193,7 +193,8 @@ class Rule:
         df = self._add_user_applicable_column(df, variant_chosen)
         df = self._add_row_number_on_order_policy(df)
         df = self._mark_campaign_for_top_users(df, rule_group_size)
-        return df.drop("policy_row_number", "user_applicable", "sort_on_col")
+        df = df.drop("policy_row_number", "user_applicable", "sort_on_col").cache()
+        return df
 
 
 class Treatment:
