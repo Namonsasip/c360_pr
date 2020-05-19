319d5ee8845806b945bc296965f56c6169e59a5b
ankitkansal <c0b36a7fc79ad25c713746ef069df4094bbe436a>
2020-05-19 09:44:01 +0530

    Added changes for monthly

--- src/customer360/pipelines/data_engineering/pipelines/revenue_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/revenue_pipeline/to_l3/to_l3_pipeline.py
@@ -46,7 +46,10 @@ def revenue_to_l3_pipeline(**kwargs):
                  "params:l3_revenue_postpaid_ru_f_sum_revenue_by_service_monthly"],
                 "l3_revenue_postpaid_ru_f_sum_revenue_by_service_monthly_stg"
             ),
-            node(merge_with_customer_postpaid_df, ['l3_revenue_postpaid_ru_f_sum_revenue_by_service_monthly_stg'],
+            node(merge_with_customer_postpaid_df,
+                 ['l3_revenue_postpaid_ru_f_sum_revenue_by_service_monthly_stg',
+                  'l3_customer_profile_union_monthly_feature_for_l3_revenue_postpaid_ru_f_sum_revenue_by_service_monthly'
+                  ],
                  'l3_revenue_postpaid_ru_f_sum_revenue_by_service_monthly'
                  ),
 
