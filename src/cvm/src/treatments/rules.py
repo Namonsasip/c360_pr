5ea8e4be3e8423e36cfd487c26d7702543a27c1f
jtyrek <jakub_tyrek@mckinsey.com>
2020-05-20 14:13:22 +0200

    [adv rules] Add treatment truncating to variant application

--- src/cvm/src/treatments/rules.py
+++ src/cvm/src/treatments/rules.py
@@ -293,7 +293,8 @@ class Treatment:
         if variant_chosen is not None:
             logging.info("Applying treatments for variant {}".format(variant_chosen))
         rules = self._get_rules_for_variant(variant_chosen)
-        return self._apply_rules(df, rules, self.treatment_size, variant_chosen)
+        variant_applied = self._apply_rules(df, rules, variant_chosen)
+        return self._truncate_assigned_campaigns(variant_applied)
 
     def _apply_all_variants(self, df: DataFrame) -> DataFrame:
         """Apply all variants possible, assumes df has column variant, ie assigning
