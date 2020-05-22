19d4e55c4afe59a1bf97660177bfd30c5f9763ac
jtyrek <jakub_tyrek@mckinsey.com>
2020-05-22 22:01:07 +0200

    Increase allowed results size

--- src/customer360/utilities/spark_util.py
+++ src/customer360/utilities/spark_util.py
@@ -17,6 +17,9 @@ def get_spark_session() -> SparkSession:
     # saved to parquet instead of entire table folder
     spark.conf.set("spark.sql.sources.partitionOverwriteMode", "DYNAMIC")
 
+    # Increase allow size
+    spark.conf.set("spark.driver.maxResultSize", "8g")
+
     spark.conf.set("spark.sql.parquet.mergeSchema", "true")
 
     return spark
