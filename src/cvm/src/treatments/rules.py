c90b4b63852e7a21d4ecc594e2ee4d34beaa4dcb
jtyrek <jakub_tyrek@mckinsey.com>
2020-05-23 09:10:35 +0200

    Add checks

--- src/cvm/src/treatments/rules.py
+++ src/cvm/src/treatments/rules.py
@@ -126,7 +126,11 @@ class Rule:
         if variant_chosen is not None:
             applicable_str += " and (variant == '{}')".format(variant_chosen)
         case_then = "case when " + applicable_str + " then 1 else 0 end"
-        case_then = "case when " + "campaign_code is null" + " then 1 else 0 end"
+        case_then = (
+            "case when "
+            + "(variant == '{}')".format(variant_chosen)
+            + " then 1 else 0 end"
+        )  # TODO drop
         logging.info("case then gen: {}".format(case_then))  # TODO drop
         return df.selectExpr("*", "{} as user_applicable".format(case_then))
 
