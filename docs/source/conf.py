f9271db00a6c9283f279f07012d54bfa0711f1c8
jtyrek <jakub_tyrek@mckinsey.com>
2020-05-26 12:04:04 +0200

    [docs] Drop building to confluence

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,13 +84,7 @@ extensions = [
     "nbsphinx",
     "recommonmark",
     "sphinx_copybutton",
-    "sphinxcontrib.confluencebuilder",
 ]
-
-# confluence builder configuration
-confluence_publish = True
-confluence_space_name = "CU"
-confluence_parent_page = "Documentation"
 
 # enable autosummary plugin (table of contents for modules/classes/class
 # methods)
