45615d88331c3e9c90a57b93f4e1f0ea1f53266e
jtyrek <jakub_tyrek@mckinsey.com>
2020-05-28 19:02:45 +0200

    Add replacement decorator

--- src/cvm/src/temporary_fixes/sub_id_replace.py
+++ src/cvm/src/temporary_fixes/sub_id_replace.py
@@ -2,16 +2,16 @@
 import logging
 
 import pyspark.sql.functions as func
-from pyspark.sql import Window
+from pyspark.sql import DataFrame, Window
 
 from src.customer360.utilities.spark_util import get_spark_session
 
 
 class SubIdReplacer:
-    def __init__(self, parameters):
+    def __init__(self, profile_path):
         logging.info("Creating `subscription_identifier` replacement dictionary")
         spark = get_spark_session()
-        profile = spark.read.parquet(parameters["id_replace_dict_path"])
+        profile = spark.read.parquet(profile_path)
         window_latest = Window.partitionBy("old_subscription_identifier").orderBy(
             func.col("event_partition_date").desc()
         )
@@ -46,3 +46,22 @@ class SubIdReplacer:
         else:
             logging.info("Replacement not needed")
             return df
+
+
+SubIdReplace = SubIdReplacer(
+    "/mnt/customer360-blob-output/C360/PROFILE/"
+    + "l1_features/l1_customer_profile_union_daily_feature/"
+)
+
+
+def replace_sub_ids(fun):
+    def replace_if_applicable(obj):
+        if isinstance(obj, DataFrame) and "subscription_identifier" in obj.columns:
+            return SubIdReplace.replace_if_needed(obj)
+        else:
+            return obj
+
+    def fun_new_sub_id(*args):
+        return fun(*[replace_if_applicable(arg) for arg in args])
+
+    return fun_new_sub_id
