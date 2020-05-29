533e3293792fdb73b5a831feec476be82b407624
sboonchusanong <litty_135@hotmail.com>
2020-05-29 15:02:15 +0700

    add example pipeline

--- src/customer360/pipelines/data_engineering/nodes/geolocation_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/geolocation_nodes/to_l1/to_l1_nodes.py
@@ -14,5 +14,27 @@ from pyspark.sql import Window
 
 from customer360.utilities.re_usable_functions import add_start_of_week_and_month, union_dataframes_with_missing_cols, \
     execute_sql
+from customer360.utilities.spark_util import get_spark_session
 
 
+def l1_geo_test(shape,masterplan):
+    masterplan.createOrReplaceTempView("mst_cell_masterplan")
+    shape.createOrReplaceTempView("mst_poi_shape")
+    stmt ="""
+    	SELECT A.LANDMARK_SUB_NAME_EN,
+	B.LOCATION_ID,B.LOCATION_NAME,
+	CAST((ACOS(COS(RADIANS(90-A.LANDMARK_LATITUDE))*COS(RADIANS(90-B.LATITUDE))+SIN(RADIANS(90-A.LANDMARK_LATITUDE))*SIN(RADIANS(90-B.LATITUDE))*COS(RADIANS(A.LANDMARK_LONGITUDE - B.LONGITUDE)))*6371) AS DECIMAL(13,2)) AS DISTANCE_KM
+	FROM mst_poi_shape A,
+	mst_cell_masterplan B
+	WHERE  A.landmark_cat_name_en IN ('AIS')
+	AND CAST((ACOS(COS(RADIANS(90-A.LANDMARK_LATITUDE))*COS(RADIANS(90-B.LATITUDE))+SIN(RADIANS(90-A.LANDMARK_LATITUDE))*SIN(RADIANS(90-B.LATITUDE))*COS(RADIANS(A.LANDMARK_LONGITUDE - B.LONGITUDE)))*6371) AS DECIMAL(13,2)) <= (0.5)
+	GROUP BY 1,2,3,4
+    """
+
+    ss = get_spark_session()
+    df=ss.sql(stmt)
+    print('testsetset')
+    df.show()
+
+
+    return df
--- src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l1/to_l1_pipeline.py
@@ -1,6 +1,6 @@
 from kedro.pipeline import Pipeline, node
 
-from customer360.pipelines.data_engineering.nodes.geolocation_nodes_backup.to_l1.to_l1_nodes import *
+from customer360.pipelines.data_engineering.nodes.geolocation_nodes.to_l1.to_l1_nodes import *
 
 
 def geo_to_l1_intermediate_pipeline(**kwargs):
@@ -16,7 +16,13 @@ def geo_to_l1_intermediate_pipeline(**kwargs):
 def geo_to_l1_pipeline(**kwargs):
     return Pipeline(
         [
-
+            node(
+                l1_geo_test,
+                ["l0_mst_poi_shape",
+                 "l0_mst_cell_masterplan"
+                 ],
+                "l1_test_pipeline"
+            ),
 
         ], name="geo_to_l1_pipeline"
     )
