2d895490b615c85937e6bc1d9dad7e26a12bb260
jtyrek <jakub_tyrek@mckinsey.com>
2020-06-01 20:30:33 +0200

    [temp fix] Modify mapping sub ids

--- src/cvm/src/temporary_fixes/sub_id_replace.py
+++ src/cvm/src/temporary_fixes/sub_id_replace.py
@@ -2,27 +2,12 @@
 import logging
 
 import pyspark.sql.functions as func
-from pyspark.sql import DataFrame, Window
-
-from src.customer360.utilities.spark_util import get_spark_session
+from pyspark.sql import DataFrame
 
 
 class SubIdReplacer:
-    def __init__(self, profile_path):
-        logging.getLogger(__name__).info(
-            "Creating `subscription_identifier` replacement dictionary"
-        )
-        spark = get_spark_session()
-        profile = spark.read.parquet(profile_path)
-        window_latest = Window.partitionBy("old_subscription_identifier").orderBy(
-            func.col("event_partition_date").desc()
-        )
-        self.replacement_dictionary = (
-            profile.filter("charge_type == 'Pre-paid'")
-            .withColumn("date_lp", func.row_number().over(window_latest))
-            .filter("date_lp == 1")
-            .select(["old_subscription_identifier", "subscription_identifier"])
-        )
+    def __init__(self, replacement_dictionary):
+        self.replacement_dictionary = replacement_dictionary
 
     def replace_old_sub_id(self, df):
         logging.getLogger(__name__).info(
@@ -56,20 +41,13 @@ class SubIdReplacer:
             return df
 
 
-SubIdReplace = SubIdReplacer(
-    "/mnt/customer360-blob-output/C360/PROFILE/"
-    + "l1_features/l1_customer_profile_union_daily_feature/"
-)
+def replace_sub_id_if_needed(
+    df: DataFrame, replacement_dictionary: DataFrame
+) -> DataFrame:
+    """ Replace `subscription_identifier` in `df` only if needed.
 
-
-def replace_sub_ids(fun):
-    def replace_if_applicable(obj):
-        if isinstance(obj, DataFrame) and "subscription_identifier" in obj.columns:
-            return SubIdReplace.replace_if_needed(obj)
-        else:
-            return obj
-
-    def fun_new_sub_id(*args):
-        return fun(*[replace_if_applicable(arg) for arg in args])
-
-    return fun_new_sub_id
+    Args:
+        df: table to replace `subscription_identifier`
+        replacement_dictionary: table with sub id mapping
+    """
+    return SubIdReplacer(replacement_dictionary).replace_if_needed(df)
