9622adfdee5543c493af57bad880755f31041dba
KorawichAnuttra <64677410+KorawichAnuttra@users.noreply.github.com>
2020-06-08 21:25:36 +0700

    Fix l1 pipeline

--- src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l1/to_l1_pipeline.py
@@ -53,27 +53,7 @@ def geo_to_l1_pipeline(**kwargs):
             #      "params:l1_geo_top_visit_exclude_homework"
             #      ],
             #     "l1_geo_top_visit_exclude_homework"
-            # ),
-            
-            node(
-                l1_geo_area_from_ais_store_daily,
-                ["l0_mst_poi_shape_for_l1_geo_area_from_ais_store_daily",
-                 "l0_mst_cell_masterplan_for_l1_geo_area_from_ais_store_daily",
-                 "l0_geo_cust_cell_visit_time_for_l1_geo_area_from_ais_store_daily",
-                 "params:l1_area_from_ais_store_daily"
-                 ],
-                "l1_geo_area_from_ais_store_daily"
-            ),
-
-            node(
-                l1_geo_area_from_competitor_store_daily,
-                ["l0_mst_poi_shape_for_l1_geo_area_from_competitor_store_daily",
-                 "l0_mst_cell_masterplan_for_l1_geo_area_from_competitor_store_daily",
-                 "l0_geo_cust_cell_visit_time_for_l1_geo_area_from_competitor_store_daily",
-                 "params:l1_area_from_competitor_store_daily"
-                 ],
-                "l1_geo_area_from_competitor_store_daily"
-            ),
+            # )
 
         ], name="geo_to_l1_pipeline"
     )
