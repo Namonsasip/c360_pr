d25d6798d0b51e3ef26df9bbb4920b729681b41d
saurabh.verma <saurabh.verma@quantumblack.com>
2020-06-12 09:58:47 +0530

    changed the customer data pull logic to pick only single entry per partition

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
@@ -1,4 +1,5 @@
 import pyspark.sql.functions as f
+from pyspark.sql.functions import expr
 from pyspark.sql import DataFrame, Window
 from pyspark.sql.types import StringType
 
@@ -66,6 +67,10 @@ def build_digital_l3_monthly_features(cxense_user_profile: DataFrame,
     join_key = ['access_method_num', 'start_of_month']
 
     cust_df = cust_df.select(cust_df_cols)
+    cust_df = cust_df.withColumn("rn", expr(
+        "row_number() over(partition by start_of_month,access_method_num order by start_of_month desc)"))
+    cust_df = cust_df.where("rn = 1")
+    cust_df = cust_df.drop("rn")
 
     final_df = return_df.join(cust_df, join_key)
 
