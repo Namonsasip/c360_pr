1cd58541a7abc067b6f605016ddd18e9077449fe
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-06-15 21:01:54 +0530

    making changes to device

--- src/customer360/pipelines/data_engineering/nodes/device_nodes/to_l4/to_l4_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/device_nodes/to_l4/to_l4_nodes.py
@@ -1,14 +1,10 @@
 from customer360.utilities.spark_util import get_spark_empty_df
-from customer360.utilities.re_usable_functions import check_empty_dfs, data_non_availability_and_missing_check, \
-    get_spark_session, union_dataframes_with_missing_cols
+from customer360.utilities.re_usable_functions import check_empty_dfs, \
+    get_spark_session, union_dataframes_with_missing_cols, gen_max_sql, execute_sql
 from customer360.utilities.config_parser import l4_rolling_window
-from customer360.utilities.config_parser import node_from_config
-from pyspark.sql import DataFrame, functions as f
-import os
-from pathlib import Path
-from kedro.context.context import load_context
 
-conf = os.getenv("CONF", None)
+from pyspark.sql import DataFrame
+import os
 
 
 def device_l4_rolling_window(input_df: DataFrame,
@@ -24,13 +20,14 @@ def device_l4_rolling_window(input_df: DataFrame,
     if check_empty_dfs([input_df]):
         return get_spark_empty_df()
 
-    # CNTX = load_context(Path.cwd(), env=conf)
-    join_key = ["national_id_card", "access_method_num", "subscription_identifier", "start_of_week"]
+    group_cols = ["access_method_num", "subscription_identifier", "national_id_card", "start_of_week"]
 
     rolling_df_first = l4_rolling_window(input_df, rolling_window_dict_first)
 
     rolling_df_second = l4_rolling_window(input_df, rolling_window_dict_second)
 
-    merged_df = rolling_df_first.join(rolling_df_second, join_key)
+    union_df = union_dataframes_with_missing_cols([rolling_df_first, rolling_df_second])
+    final_df_str = gen_max_sql(union_df, 'tmp_table_name', group_cols)
+    merged_df = execute_sql(union_df, 'tmp_table_name', final_df_str)
 
     return merged_df
