42309617314896ace8e680a2ed84bf1bde8cff5c
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-06-15 19:43:27 +0530

    device changes

--- src/customer360/pipelines/data_engineering/nodes/sales_nodes/to_l4/to_l4_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/sales_nodes/to_l4/to_l4_nodes.py
@@ -1,12 +1,9 @@
 from customer360.utilities.spark_util import get_spark_empty_df
-from customer360.utilities.re_usable_functions import check_empty_dfs, data_non_availability_and_missing_check, \
-    get_spark_session, union_dataframes_with_missing_cols
+from customer360.utilities.re_usable_functions import check_empty_dfs, \
+     union_dataframes_with_missing_cols, gen_max_sql, execute_sql
 from customer360.utilities.config_parser import l4_rolling_window
-from customer360.utilities.config_parser import node_from_config
 from pyspark.sql import DataFrame, functions as f
 import os
-from pathlib import Path
-from kedro.context.context import load_context
 
 conf = os.getenv("CONF", None)
 
@@ -27,7 +24,8 @@ def sales_l4_rolling_window(input_df: DataFrame,
         return get_spark_empty_df()
 
     # CNTX = load_context(Path.cwd(), env=conf)
-    join_key = ["national_id_card", "access_method_num", "subscription_identifier", "start_of_week"]
+    group_cols = ["national_id_card", "access_method_num", "subscription_identifier", "start_of_week"]
+
     import logging
     logging.info("SALES L4 Input Count -->> {0}".format(str(input_df.count())))
     # metadata = CNTX.catalog.load("util_audit_metadata_table")
@@ -55,8 +53,14 @@ def sales_l4_rolling_window(input_df: DataFrame,
     # rolling_df_second = CNTX.catalog.load("l4_sales_number_and_volume_transaction_weekly_second")
     # rolling_df_third = CNTX.catalog.load("l4_sales_number_and_volume_transaction_weekly_third")
 
-    merged_df = rolling_df_first.join(rolling_df_second, join_key) \
-        .join(rolling_df_third, join_key)
+    # merged_df = rolling_df_first.join(rolling_df_second, on=join_key, how='outer')\
+    #     .join(rolling_df_third, on=join_key, how='outer')
+    union_df = union_dataframes_with_missing_cols([rolling_df_first, rolling_df_second, rolling_df_third])
+
+    group_cols = ['access_method_num', 'event_partition_date', 'start_of_week', 'start_of_month']
+    final_df_str = gen_max_sql(union_df, 'tmp_table_name', group_cols)
+    merged_df = execute_sql(union_df, 'tmp_table_name', final_df_str)
+
     logging.info("SALES L4 merged_df Count -->> {0}".format(str(merged_df.count())))
 
     return merged_df
