7b68b9de1fe9fb7a8504516df981f4f13a30c5af
KorawichAnuttra <64677410+KorawichAnuttra@users.noreply.github.com>
2020-06-16 12:50:21 +0700

    comment for test run pipeline

--- src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l4/to_l4_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l4/to_l4_pipeline.py
@@ -9,40 +9,40 @@ def geo_to_l4_pipeline(**kwargs):
     return Pipeline(
         [
             
-            node(
-                l4_rolling_window,
-                ["l2_geo_time_spent_by_location_weekly_for_l4_geo_time_spent_by_location",
-                 "params:l4_geo_time_spent_by_location"
-                 ],
-                "l4_geo_time_spent_by_location"
-            ),
-
-
-            node(
-                l4_rolling_window,
-                ["l2_geo_area_from_ais_store_weekly_for_l4_geo_area_from_ais_store",
-                 "params:l4_area_from_ais_store"
-                 ],
-                "l4_geo_area_from_ais_store"
-            ),
-
-            node(
-                l4_rolling_window,
-                ["l2_geo_area_from_competitor_store_weekly_for_l4_geo_area_from_competitor_store",
-                 "params:l4_area_from_competitor_store"
-                 ],
-                "l4_geo_area_from_competitor_store"
-            ),
-
-            node(
-                l4_geo_top_visit_exclude_homework,
-                ["l3_geo_time_spent_by_location_monthly_for_l4_geo_top_visit_exclude_homework",
-                 "l4_geo_home_work_location_id",
-                 # "params:l4_geo_top_visit_exclude_homework"
-                 ],
-                "l4_geo_top_visit_exclude_homework"
-            ),
-            
+            # node(
+            #     l4_rolling_window,
+            #     ["l2_geo_time_spent_by_location_weekly_for_l4_geo_time_spent_by_location",
+            #      "params:l4_geo_time_spent_by_location"
+            #      ],
+            #     "l4_geo_time_spent_by_location"
+            # ),
+            #
+            #
+            # node(
+            #     l4_rolling_window,
+            #     ["l2_geo_area_from_ais_store_weekly_for_l4_geo_area_from_ais_store",
+            #      "params:l4_area_from_ais_store"
+            #      ],
+            #     "l4_geo_area_from_ais_store"
+            # ),
+            #
+            # node(
+            #     l4_rolling_window,
+            #     ["l2_geo_area_from_competitor_store_weekly_for_l4_geo_area_from_competitor_store",
+            #      "params:l4_area_from_competitor_store"
+            #      ],
+            #     "l4_geo_area_from_competitor_store"
+            # ),
+            #
+            # node(
+            #     l4_geo_top_visit_exclude_homework,
+            #     ["l3_geo_time_spent_by_location_monthly_for_l4_geo_top_visit_exclude_homework",
+            #      "l4_geo_home_work_location_id",
+            #      # "params:l4_geo_top_visit_exclude_homework"
+            #      ],
+            #     "l4_geo_top_visit_exclude_homework"
+            # ),
+            #
             # node(
             #     l4_geo_home_work_location_id,
             #     ["l0_geo_cust_cell_visit_time_for_l4_geo_home_work_location_id",
@@ -50,15 +50,15 @@ def geo_to_l4_pipeline(**kwargs):
             #      ],
             #     "l4_geo_home_work_location_id"
             # ),
-
-            node(
-                l4_geo_home_weekday_city_citizens,
-                ["l4_geo_home_work_location_id_for_l4_geo_home_weekday_city_citizens",
-                 "l0_mst_cell_masterplan_for_l4_geo_home_weekday_city_citizens",
-                 "params:l4_geo_home_weekday_city_citizens"
-                 ],
-                "l4_geo_home_weekday_city_citizens"
-            ),
+            #
+            # node(
+            #     l4_geo_home_weekday_city_citizens,
+            #     ["l4_geo_home_work_location_id_for_l4_geo_home_weekday_city_citizens",
+            #      "l0_mst_cell_masterplan_for_l4_geo_home_weekday_city_citizens",
+            #      "params:l4_geo_home_weekday_city_citizens"
+            #      ],
+            #     "l4_geo_home_weekday_city_citizens"
+            # ),
 
             node(
                 l4_rolling_window,
