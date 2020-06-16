05dc3ef80d6d5f1ca6bb0b5efa242ad41c18a1a7
KorawichAnuttra <64677410+KorawichAnuttra@users.noreply.github.com>
2020-06-16 13:11:07 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l1/to_l1_pipeline.py
@@ -3,14 +3,14 @@ from kedro.pipeline import Pipeline, node
 from customer360.pipelines.data_engineering.nodes.geolocation_nodes.to_l1.to_l1_nodes import *
 
 
-def geo_to_l1_intermediate_pipeline(**kwargs):
-    return Pipeline(
-        [
-
-
-        ],
-        name="geo_to_l1_intermediate_pipeline"
-    )
+# def geo_to_l1_intermediate_pipeline(**kwargs):
+#     return Pipeline(
+#         [
+#
+#
+#         ],
+#         name="geo_to_l1_intermediate_pipeline"
+#     )
 
 
 def geo_to_l1_pipeline(**kwargs):
@@ -55,7 +55,6 @@ def geo_to_l1_pipeline(**kwargs):
             #     "l1_geo_top_visit_exclude_homework"
             # ),
 
-
             node(
                 l1_geo_cust_subseqently_distance,
                 ["l0_geo_cust_cell_visit_time_for_l1_geo_cust_subseqently_distance_daily",
@@ -68,10 +67,10 @@ def geo_to_l1_pipeline(**kwargs):
     )
 
 
-def geo_to_l1_union_pipeline(**kwargs):
-    return Pipeline(
-        [
-
-
-        ], name="geo_to_l1_union_pipeline"
-    )
+# def geo_to_l1_union_pipeline(**kwargs):
+#     return Pipeline(
+#         [
+#
+#
+#         ], name="geo_to_l1_union_pipeline"
+#     )
