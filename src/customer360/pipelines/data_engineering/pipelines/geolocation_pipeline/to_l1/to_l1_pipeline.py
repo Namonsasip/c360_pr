b957c82d6bb9d1d647da8ce732e2070d42db6d5d
KorawichAnuttra <64677410+KorawichAnuttra@users.noreply.github.com>
2020-06-16 13:12:33 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l1/to_l1_pipeline.py
@@ -3,14 +3,14 @@ from kedro.pipeline import Pipeline, node
 from customer360.pipelines.data_engineering.nodes.geolocation_nodes.to_l1.to_l1_nodes import *
 
 
-# def geo_to_l1_intermediate_pipeline(**kwargs):
-#     return Pipeline(
-#         [
-#
-#
-#         ],
-#         name="geo_to_l1_intermediate_pipeline"
-#     )
+def geo_to_l1_intermediate_pipeline(**kwargs):
+    return Pipeline(
+        [
+
+
+        ],
+        name="geo_to_l1_intermediate_pipeline"
+    )
 
 
 def geo_to_l1_pipeline(**kwargs):
