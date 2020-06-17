9829ed69bb8bac091625d60b1c3251627430a0e1
jtyrek <jakub_tyrek@mckinsey.com>
2020-06-17 17:50:22 +0200

    [report] Drop duplicate columns

--- src/cvm/report/nodes.py
+++ src/cvm/report/nodes.py
@@ -122,6 +122,8 @@ def build_daily_kpis(
     """
     logging.info("Building daily kpis")
     report_parameters = parameters["build_report"]
+    if "key_date" in users_report.columns:
+        users_report = users_report.drop("key_date")
     df = add_arpus(users_report, reve, report_parameters["min_date"])
     df = add_status(df, profile_table)
     df = df.join(microsegments, on="subscription_identifier")
