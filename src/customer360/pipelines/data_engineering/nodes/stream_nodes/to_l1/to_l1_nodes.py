fa2d6924a28ef37172edcc6463a8f618f0ab4530
Sohum Sachathamakul <sohum2002@hotmail.com>
2020-06-18 11:58:42 +0700

    fix: patch session duration

--- src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l1/to_l1_nodes.py
@@ -9,50 +9,36 @@ from pyspark.sql.types import *
 conf = os.getenv("CONF", None)
 
 
-def dac_for_streaming_to_l1_intermediate_pipeline(
-        input_df: DataFrame,
-        streaming_content_type_features_tbl: str,
-        streaming_tv_channel_features_tbl: str,
-        cust_df: DataFrame,
-):
+def dac_for_streaming_to_l1_intermediate_pipeline(input_df: DataFrame, cust_df: DataFrame, target_table_name: str):
 
-    ################################# Start Implementing Data availability checks #############################
-    if check_empty_dfs([input_df, cust_df]):
-        return [get_spark_empty_df(), get_spark_empty_df(), get_spark_empty_df()]
+    # ################################# Start Implementing Data availability checks #############################
+    # if check_empty_dfs([input_df, cust_df]):
+    #     return [get_spark_empty_df(), get_spark_empty_df()]
+    #
+    # input_df = data_non_availability_and_missing_check(df=input_df, grouping="daily", par_col="partition_date",
+    #                                                    target_table_name=target_table_name)
+    #
+    # cust_df = data_non_availability_and_missing_check(df=cust_df, grouping="daily", par_col="event_partition_date",
+    #                                                    target_table_name=target_table_name)
+    #
+    # if check_empty_dfs([input_df, cust_df]):
+    #     return [get_spark_empty_df(), get_spark_empty_df()]
+    #
+    # min_value = union_dataframes_with_missing_cols(
+    #     [
+    #         input_df.select(
+    #             f.max(f.to_date((f.col("partition_date")).cast(StringType()), 'yyyyMMdd')).alias("max_date")),
+    #         cust_df.select(
+    #             f.max(f.col("event_partition_date")).alias("max_date")),
+    #     ]
+    # ).select(f.min(f.col("max_date")).alias("min_date")).collect()[0].min_date
+    #
+    # input_df = input_df.filter(f.to_date((f.col("partition_date")).cast(StringType()), 'yyyyMMdd') <= min_value)
+    # cust_df = cust_df.filter(f.col("event_partition_date") <= min_value)
+    #
+    # ################################# End Implementing Data availability checks ###############################
 
-    input_content_type_df = data_non_availability_and_missing_check(
-        df=input_df, grouping="daily", par_col="partition_date", target_table_name=streaming_content_type_features_tbl)
-
-    input_tv_channel_df = data_non_availability_and_missing_check(
-        df=input_df, grouping="daily", par_col="partition_date", target_table_name=streaming_tv_channel_features_tbl)
-
-    cust_df = data_non_availability_and_missing_check(df=cust_df, grouping="daily", par_col="event_partition_date",
-                                                       target_table_name=streaming_content_type_features_tbl)
-
-    cust_df = data_non_availability_and_missing_check(df=cust_df, grouping="daily", par_col="event_partition_date",
-                                                       target_table_name=streaming_tv_channel_features_tbl)
-
-    if check_empty_dfs([input_content_type_df, input_tv_channel_df, cust_df]):
-        return [get_spark_empty_df(), get_spark_empty_df(), get_spark_empty_df()]
-
-    min_value = union_dataframes_with_missing_cols(
-        [
-            input_content_type_df.select(
-                f.max(f.to_date((f.col("partition_date")).cast(StringType()), 'yyyyMMdd')).alias("max_date")),
-            input_tv_channel_df.select(
-                f.max(f.to_date((f.col("partition_date")).cast(StringType()), 'yyyyMMdd')).alias("max_date")),
-            cust_df.select(
-                f.max(f.col("event_partition_date")).alias("max_date")),
-        ]
-    ).select(f.min(f.col("max_date")).alias("min_date")).collect()[0].min_date
-
-    input_content_type_df = input_content_type_df.filter(f.to_date((f.col("partition_date")).cast(StringType()), 'yyyyMMdd') <= min_value)
-    input_tv_channel_df = input_tv_channel_df.filter(f.to_date((f.col("partition_date")).cast(StringType()), 'yyyyMMdd') <= min_value)
-    cust_df = cust_df.filter(f.col("event_partition_date") <= min_value)
-
-    ################################# End Implementing Data availability checks ###############################
-
-    return [input_content_type_df, input_tv_channel_df, cust_df]
+    return [input_df, cust_df]
 
 
 def dac_for_streaming_to_l1_pipeline(input_df: DataFrame, target_table_name: str):
