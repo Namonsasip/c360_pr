4f213676dd147e15e9c63f0c62df6f8b23b55bbf
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-06-22 18:51:00 +0530

    loyalty changes

--- src/customer360/pipelines/data_engineering/nodes/loyalty_nodes/to_l4/to_l4_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/loyalty_nodes/to_l4/to_l4_nodes.py
@@ -31,3 +31,24 @@ def loyalty_build_weekly_features(input_df: DataFrame,
     final_df_str = gen_max_sql(union_df, 'tmp_table_name', group_cols)
     merged_df = execute_sql(union_df, 'tmp_table_name', final_df_str)
     return merged_df
+
+
+def l4_loyalty_point_balance_statuses_features(input_df: DataFrame,
+                                               first_dict: dict,
+                                               second_dict: dict) -> DataFrame:
+    """
+    :param input_df:
+    :param first_dict:
+    :param second_dict:
+    :return:
+    """
+    first_df = l4_rolling_window(input_df, first_dict)
+    second_df = l4_rolling_window(input_df, second_dict)
+
+    group_cols = ["subscription_identifier", "access_method_num", "national_id_card", "start_of_month"]
+
+    union_df = union_dataframes_with_missing_cols([first_df, second_df])
+
+    final_df_str = gen_max_sql(union_df, 'tmp_table_name', group_cols)
+    merged_df = execute_sql(union_df, 'tmp_table_name', final_df_str)
+    return merged_df
