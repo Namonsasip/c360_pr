6005da28cfb613a5d3a805f44190d6dcdace45e8
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-06-22 14:26:11 +0530

    revenue fix

--- src/customer360/pipelines/data_engineering/nodes/loyalty_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/loyalty_nodes/to_l1/to_l1_nodes.py
@@ -94,9 +94,28 @@ def loyalty_number_of_rewards_redeemed_for_each_category(customer_prof: DataFram
     input_df = data_non_availability_and_missing_check(df=input_df, grouping="daily", par_col="partition_date",
                                                        target_table_name="l1_loyalty_number_of_rewards_daily")
 
+    input_df = add_event_week_and_month_from_yyyymmdd(input_df=input_df
+                                                      , column="partition_date")
+
     customer_prof = data_non_availability_and_missing_check(df=customer_prof, grouping="daily",
                                                             par_col="event_partition_date",
                                                             target_table_name="l1_loyalty_number_of_rewards_daily")
+
+    min_value = union_dataframes_with_missing_cols(
+        [
+            input_df.select(
+                f.max(f.col("event_partition_date")).alias("max_date")),
+            customer_prof.select(
+                f.max(f.col("event_partition_date")).alias("max_date")),
+        ]
+    ).select(f.min(f.col("max_date")).alias("min_date")).collect()[0].min_date
+
+    drop_cols = ["event_partition_date", "start_of_week", "start_of_month"]
+
+    input_df = input_df.filter(f.col("event_partition_date") <= min_value).drop(*drop_cols) \
+               .filter(f.col("response_date").isNotNull())
+
+    customer_prof = customer_prof.filter(f.col("event_partition_date") <= min_value)
 
     if check_empty_dfs([input_df, customer_prof]):
         return get_spark_empty_df()
@@ -109,7 +128,7 @@ def loyalty_number_of_rewards_redeemed_for_each_category(customer_prof: DataFram
     input_df = input_df.where("msg_event_id = 13") \
         .select(f.col("mobile_no").alias("access_method_num"), "project_id", "response_date")
     input_df = add_start_of_week_and_month(input_df, "response_date") \
-        .withColumnRenamed("response_date", "loyalty_rewards_registered_date")\
+        .withColumnRenamed("response_date", "loyalty_rewards_registered_date") \
         .withColumn("flag_with_orignal_data", f.lit(1))
 
     return_df = customer_prof.join(input_df, join_key, 'left')
@@ -142,6 +161,20 @@ def loyalty_number_of_points_spend_for_each_category(customer_prof: DataFrame,
                                                             par_col="event_partition_date",
                                                             target_table_name="l1_loyalty_number_of_points_spend_daily")
 
+    min_value = union_dataframes_with_missing_cols(
+        [
+            input_df.select(
+                f.max(f.col("filtered_date")).alias("max_date")),
+            customer_prof.select(
+                f.max(f.col("event_partition_date")).alias("max_date")),
+        ]
+    ).select(f.min(f.col("max_date")).alias("min_date")).collect()[0].min_date
+
+    drop_cols = ["event_partition_date", "start_of_week", "start_of_month"]
+
+    input_df = input_df.filter(f.col("filtered_date") <= min_value).drop(*drop_cols)
+    customer_prof = customer_prof.filter(f.col("event_partition_date") <= min_value)
+
     if check_empty_dfs([input_df, customer_prof]):
         return get_spark_empty_df()
     ################################# End Implementing Data availability checks ###############################
@@ -158,7 +191,7 @@ def loyalty_number_of_points_spend_for_each_category(customer_prof: DataFrame,
 
     grouped_df = date_curated_df.groupBy(["access_method_num", "event_partition_date", "start_of_week",
                                           "start_of_month", "project_id"]) \
-        .agg(f.sum("points").alias("loyalty_points_spend"))\
+        .agg(f.sum("points").alias("loyalty_points_spend")) \
         .withColumn("flag_with_orignal_data", f.lit(1))
 
     return_df = grouped_df.join(customer_prof, join_key, 'left')
