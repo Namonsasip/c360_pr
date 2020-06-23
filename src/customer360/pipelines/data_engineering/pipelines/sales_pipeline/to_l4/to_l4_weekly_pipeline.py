30cc9db026d3b2d25e1984c49d9b88fb91edcfe2
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-06-23 19:39:37 +0530

    changed lot of things in streaming

--- src/customer360/pipelines/data_engineering/pipelines/sales_pipeline/to_l4/to_l4_weekly_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/sales_pipeline/to_l4/to_l4_weekly_pipeline.py
@@ -45,7 +45,7 @@ def sales_to_l4_pipeline(**kwargs):
                  "params:l4_sales_on_top_transaction_second",
                  "params:l4_sales_on_top_transaction_third",
                  "params:l4_sales_on_top_transaction_fourth",
-                 "parmas:l4_sales_number_and_volume_on_top_transaction_weekly"
+                 "params:l4_sales_number_and_volume_on_top_transaction_weekly"
                  ],
                 "l4_sales_number_and_volume_on_top_transaction_weekly"
             ),
@@ -56,7 +56,7 @@ def sales_to_l4_pipeline(**kwargs):
                  "params:l4_sales_main_transaction_second",
                  "params:l4_sales_main_transaction_third",
                  "params:l4_sales_main_transaction_fourth",
-                 "parmas:l4_sales_number_and_volume_main_transaction_weekly"
+                 "params:l4_sales_number_and_volume_main_transaction_weekly"
                  ],
                 "l4_sales_number_and_volume_main_transaction_weekly"
 
