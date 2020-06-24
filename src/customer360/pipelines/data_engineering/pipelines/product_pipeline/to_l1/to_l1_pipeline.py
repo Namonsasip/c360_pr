1f48e7c0d6e2a119e1d28a20a79bb409337109f2
Sohum Sachathamakul <sohum2002@hotmail.com>
2020-06-24 10:07:48 +0700

    comment out other nodes for test

--- src/customer360/pipelines/data_engineering/pipelines/product_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/product_pipeline/to_l1/to_l1_pipeline.py
@@ -44,26 +44,26 @@ def product_to_l1_pipeline(**kwargs):
                 "int_l1_product_active_customer_promotion_features_temp"
             ),
 
-            node(
-                join_with_master_package,
-                ["int_l1_product_active_customer_promotion_features_temp",
-                 "l0_product_pru_m_package_master_group_for_daily",
-                 "l0_product_pru_m_ontop_master_for_weekly",
-                 "l0_product_ru_m_main_promotion_cvm_proj_for_daily",
-                 "l0_product_ru_m_ontop_promotion_cvm_proj_for_daily"],
-                "l1_product_active_customer_promotion_features_daily"
-            ),
-            node(
-                dac_product_fbb_a_customer_promotion_current_for_daily,
-                "l0_product_fbb_a_customer_promotion_current_for_daily",
-                "int_l1_product_active_fbb_customer_features_daily"
-            ),
-            node(
-                l1_massive_processing,
-                ["int_l1_product_active_fbb_customer_features_daily",
-                 "params:l1_product_active_fbb_customer_features",
-                 "l1_customer_profile_union_daily_feature_for_l1_product_active_fbb_customer_features_daily"],
-                "l1_product_active_fbb_customer_features_daily"
-            )
+            # node(
+            #     join_with_master_package,
+            #     ["int_l1_product_active_customer_promotion_features_temp",
+            #      "l0_product_pru_m_package_master_group_for_daily",
+            #      "l0_product_pru_m_ontop_master_for_weekly",
+            #      "l0_product_ru_m_main_promotion_cvm_proj_for_daily",
+            #      "l0_product_ru_m_ontop_promotion_cvm_proj_for_daily"],
+            #     "l1_product_active_customer_promotion_features_daily"
+            # ),
+            # node(
+            #     dac_product_fbb_a_customer_promotion_current_for_daily,
+            #     "l0_product_fbb_a_customer_promotion_current_for_daily",
+            #     "int_l1_product_active_fbb_customer_features_daily"
+            # ),
+            # node(
+            #     l1_massive_processing,
+            #     ["int_l1_product_active_fbb_customer_features_daily",
+            #      "params:l1_product_active_fbb_customer_features",
+            #      "l1_customer_profile_union_daily_feature_for_l1_product_active_fbb_customer_features_daily"],
+            #     "l1_product_active_fbb_customer_features_daily"
+            # )
         ]
     )
