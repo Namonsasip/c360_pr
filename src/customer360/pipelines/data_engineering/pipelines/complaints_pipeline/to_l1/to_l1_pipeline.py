9551f4e33e943cdafe9ddb7dc4e892cb5580565c
saurabh.verma <saurabh.verma@quantumblack.com>
2020-06-25 09:34:13 +0530

    added exception partition list for complaints call to compititiors

--- src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
@@ -44,7 +44,8 @@ def complaints_to_l1_pipeline(**kwargs):
                 dac_for_complaints_to_l1_pipeline,
                 ["l0_usage_call_relation_sum_daily_for_l1_complaints_call_to_competitor_features",
                  "l1_customer_profile_union_daily_feature_for_l1_complaints_call_to_competitor_features",
-                 "params:l1_complaints_call_to_competitor_features_tbl"],
+                 "params:l1_complaints_call_to_competitor_features_tbl",
+                 "params:exception_partition_list_for_l0_usage_call_relation_sum_daily"],
                 ["int_l0_usage_call_relation_sum_daily_for_l1_complaints_call_to_competitor_features",
                  "int_l1_customer_profile_union_daily_feature_for_l1_complaints_call_to_competitor_features"]
             ),
@@ -56,45 +57,47 @@ def complaints_to_l1_pipeline(**kwargs):
                 "l1_complaints_call_to_competitor_features"
             ),
 
-            node(
-                dac_for_complaints_to_l1_pipeline,
-                ["l0_complaints_acc_atsr_outbound_daily",
-                 "l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_call",
-                 "params:l1_complaints_nps_after_call_tbl"],
-                ["int_l0_complaints_acc_atsr_outbound_daily",
-                 "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_call"]
-            ),
-            node(
-                l1_massive_processing,
-                ["int_l0_complaints_acc_atsr_outbound_daily",
-                 "params:l1_complaints_nps_after_call",
-                 "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_call"],
-                "l1_complaints_nps_after_call"
-            ),
-
-
-            node(
-                dac_for_complaints_to_l1_pipeline,
-                ["l0_complaints_acc_ai_chatbot_survey_daily",
-                 "l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_chatbot",
-                 "params:l1_complaints_nps_after_chatbot_tbl"],
-                ["int_l0_complaints_acc_ai_chatbot_survey_daily",
-                 "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_chatbot"]
-            ),
-            node(
-                l1_massive_processing,
-                ["int_l0_complaints_acc_ai_chatbot_survey_daily",
-                 "params:l1_complaints_nps_after_chatbot",
-                 "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_chatbot"],
-                "l1_complaints_nps_after_chatbot"
-            ),
-
-
+            # node(
+            #     dac_for_complaints_to_l1_pipeline,
+            #     ["l0_complaints_acc_atsr_outbound_daily",
+            #      "l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_call",
+            #      "params:l1_complaints_nps_after_call_tbl",
+            #      "params:exception_partition_list_for_l0_complaints_acc_atsr_outbound_daily"],
+            #     ["int_l0_complaints_acc_atsr_outbound_daily",
+            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_call"]
+            # ),
+            # node(
+            #     l1_massive_processing,
+            #     ["int_l0_complaints_acc_atsr_outbound_daily",
+            #      "params:l1_complaints_nps_after_call",
+            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_call"],
+            #     "l1_complaints_nps_after_call"
+            # ),
+            #
+            #
+            # node(
+            #     dac_for_complaints_to_l1_pipeline,
+            #     ["l0_complaints_acc_ai_chatbot_survey_daily",
+            #      "l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_chatbot",
+            #      "params:l1_complaints_nps_after_chatbot_tbl",
+            #      "params:exception_partition_list_for_l0_complaints_acc_ai_chatbot_survey_daily"],
+            #     ["int_l0_complaints_acc_ai_chatbot_survey_daily",
+            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_chatbot"]
+            # ),
+            # node(
+            #     l1_massive_processing,
+            #     ["int_l0_complaints_acc_ai_chatbot_survey_daily",
+            #      "params:l1_complaints_nps_after_chatbot",
+            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_chatbot"],
+            #     "l1_complaints_nps_after_chatbot"
+            # ),
+            #
             # node(
             #     dac_for_complaints_to_l1_pipeline,
             #     ["l0_complaints_acc_qmt_csi_daily",
             #      "l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_store_visit",
-            #      "params:l1_complaints_nps_after_store_visit_tbl"],
+            #      "params:l1_complaints_nps_after_store_visit_tbl",
+            #      "params:exception_partition_list_for_l0_complaints_acc_qmt_csi_daily"],
             #     ["int_l0_complaints_acc_qmt_csi_daily",
             #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_store_visit"]
             # ),
