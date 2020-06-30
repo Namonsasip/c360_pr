41a9007bf3a4493b919e3ca055122ed32ec37820
jtyrek <jakub_tyrek@mckinsey.com>
2020-06-30 11:58:37 +0200

    [data_prep] Add macrosegment only pipeline

--- src/cvm/data_prep/pipeline.py
+++ src/cvm/data_prep/pipeline.py
@@ -29,14 +29,11 @@
 from cvm.data_prep.nodes import (
     add_ard_targets,
     add_churn_targets,
-    add_macrosegments,
     create_pred_sample,
     feature_selection_all_target,
     get_macrosegments,
     get_micro_macrosegments,
     subs_date_join,
-    subs_date_join_important_only,
-    train_test_split,
 )
 from cvm.preprocessing.nodes import pipeline_fit
 from cvm.sample_inputs.pipeline import create_users_from_active, sample_inputs
@@ -74,9 +71,9 @@ def join_raw_features(sample_type: str) -> Pipeline:
     )
 
 
-def create_cvm_targets(sample_type: str):
-    """ Creates pipeline preparing targets. Can create data pipeline for full dataset or
-    given sample_type.
+def create_training_sample(sample_type: str):
+    """ Creates pipeline preparing training sample. Can create data pipeline for full
+    dataset or given sample_type.
 
     Args:
         sample_type: "scoring" if list created for scoring, "training" if list created
@@ -89,15 +86,15 @@ def create_cvm_targets(sample_type: str):
     return Pipeline(
         [
             node(
-                add_ard_targets,
-                [
-                    "cvm_users_list_" + sample_type,
+                func=add_ard_targets,
+                inputs=[
+                    "cvm_users_list_{}".format(sample_type),
                     "l4_revenue_prepaid_ru_f_sum_revenue_by_service_monthly",
                     "parameters",
                     "params:" + sample_type,
                 ],
-                "ard_targets_" + sample_type,
-                name="create_ard_targets_" + sample_type,
+                outputs="ard_targets_" + sample_type,
+                name="create_ard_targets",
             ),
             node(
                 add_churn_targets,
@@ -109,54 +106,6 @@ def create_cvm_targets(sample_type: str):
                 ],
                 "churn_targets_" + sample_type,
                 name="create_churn_targets_" + sample_type,
-            ),
-        ]
-    )
-
-
-def prepare_features_macrosegments(sample_type: str):
-    """ Creates pipeline preparing data with all features and macrosegments.
-
-    Args:
-        sample_type: "scoring" if list created for scoring, "training" if list created
-            for training.
-
-    Returns:
-        Kedro pipeline.
-    """
-
-    if sample_type == "training":
-        targets_datasets = [
-            "churn_targets_" + sample_type,
-            "ard_targets_" + sample_type,
-        ]
-    else:
-        targets_datasets = []
-    return Pipeline(
-        [
-            node(
-                subs_date_join_important_only,
-                [
-                    "important_columns",
-                    "parameters",
-                    "cvm_users_list_" + sample_type,
-                    "l3_customer_profile_include_1mo_non_active_" + sample_type,
-                    "l4_daily_feature_topup_and_volume_" + sample_type,
-                    "l4_usage_prepaid_postpaid_daily_features_" + sample_type,
-                    "l4_revenue_prepaid_ru_f_sum_revenue_by_service_monthly_"
-                    + sample_type,
-                    "l4_usage_postpaid_prepaid_weekly_features_sum_" + sample_type,
-                    "l4_touchpoints_to_call_center_features_" + sample_type,
-                ]
-                + targets_datasets,
-                "features_targets_" + sample_type,
-                name="create_features_targets_" + sample_type,
-            ),
-            node(
-                add_macrosegments,
-                ["features_targets_" + sample_type, "parameters"],
-                "features_macrosegments_" + sample_type,
-                name="create_features_macrosegments_" + sample_type,
             ),
         ]
     )
@@ -255,30 +204,6 @@ def create_prediction_sample(sample_type: str) -> Pipeline:
     )
 
 
-def create_cvm_training_data(sample_type: str):
-    """ Creates pipeline preparing data. Can create data pipeline for full dataset or
-    given sample_type.
-
-    Args:
-        sample_type: "scoring" if list created for scoring, "training" if list created
-            for training.
-
-    Returns:
-        Kedro pipeline.
-    """
-
-    return prepare_features_macrosegments(sample_type) + Pipeline(
-        [
-            node(
-                train_test_split,
-                ["features_macrosegments_" + sample_type, "parameters"],
-                ["train_sample_" + sample_type, "test_sample_" + sample_type],
-                name="create_train_test_split_" + sample_type,
-            ),
-        ]
-    )
-
-
 def create_cvm_important_columns():
     """ Uses training samples as inputs, runs feature extraction procedure.
 
@@ -312,7 +237,7 @@ def create_cvm_important_columns():
                 name="create_features_targets_fe",
             ),
             node(
-                add_macrosegments,
+                get_macrosegments,
                 ["features_targets_fe", "parameters"],
                 "features_macrosegments_fe",
                 name="create_features_macrosegments_fe",
@@ -364,17 +289,12 @@ def training_data_prepare(sample_type: str) -> Pipeline:
     Returns:
         Kedro pipeline.
     """
-    return (
-        join_raw_features(sample_type)
-        + create_cvm_microsegments(sample_type)
-        + create_prediction_sample(sample_type)
-    )
+    return join_raw_features(sample_type) + create_cvm_macrosegments(sample_type)
 
 
 extract_features = (
     create_users_from_active("fe")
     + sample_inputs("fe")
-    + create_cvm_targets("fe")
     + create_cvm_important_columns()
 )
 rfe_only = create_cvm_important_columns()
