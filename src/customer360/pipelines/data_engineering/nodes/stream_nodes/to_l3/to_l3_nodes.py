59f8f5c650d8fc8acc5100b3925522c0782d7633
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-07-03 11:23:13 +0530

    streaming fixes

--- src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l3/to_l3_nodes.py
@@ -1,12 +1,16 @@
-from customer360.utilities.spark_util import get_spark_session, get_spark_empty_df
-from pathlib import Path
-from customer360.utilities.re_usable_functions import union_dataframes_with_missing_cols, check_empty_dfs, \
-    data_non_availability_and_missing_check
+import os
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
-import os
+from customer360.utilities.config_parser import node_from_config, expansion
+from kedro.context.context import load_context
+from pathlib import Path
+import logging, os
+from customer360.utilities.re_usable_functions import check_empty_dfs, data_non_availability_and_missing_check
+from src.customer360.utilities.spark_util import get_spark_empty_df, get_spark_session
 
-conf = os.getenv("CONF", "local")
+conf = os.getenv("CONF", None)
+
+# Defaulted date in DAC is  exception_partitions=["2020-03-30"] as we are reading from April Starting
 
 
 def generate_l3_fav_streaming_day(input_df, app_list):
@@ -82,3 +86,217 @@ def dac_for_l3_streaming_fav_tv_show_by_episode_watched(input_df: DataFrame, cus
     ################################# End Implementing Data availability checks ###############################
 
     return [input_df, cust_df]
+
+
+def streaming_to_l3_content_type_features(input_df: DataFrame,
+                                          int_l3_streaming_content_type_features_dict: dict,
+                                          l3_streaming_fav_content_group_by_volume_dict: dict,
+                                          l3_streaming_fav_content_group_by_duration_dict: dict) -> [DataFrame,
+                                                                                                     DataFrame,
+                                                                                                     DataFrame]:
+    """
+    :param input_df:
+    :param int_l3_streaming_content_type_features_dict:
+    :param l3_streaming_fav_content_group_by_volume_dict:
+    :param l3_streaming_fav_content_group_by_duration_dict:
+    :return:
+    """
+    ################################# Start Implementing Data availability checks #############################
+    if check_empty_dfs([input_df]):
+        return [get_spark_empty_df(), get_spark_empty_df()]
+
+    input_df = data_non_availability_and_missing_check(
+        df=input_df, grouping="monthly", par_col="event_partition_date",
+        missing_data_check_flg='Y',
+        target_table_name=l3_streaming_fav_content_group_by_duration_dict["output_catalog"],
+        exception_partitions=["2020-03-30"])
+
+    if check_empty_dfs([input_df]):
+        return [get_spark_empty_df(), get_spark_empty_df()]
+
+    ################################# End Implementing Data availability checks ###############################
+
+    def divide_chunks(l, n):
+        # looping till length l
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
+
+    CNTX = load_context(Path.cwd(), env=conf)
+    data_frame = input_df
+    dates_list = data_frame.select('start_of_month').distinct().collect()
+    mvv_array = [row[0] for row in dates_list if row[0] != "SAMPLING"]
+    mvv_array = sorted(mvv_array)
+    logging.info("Dates to run for {0}".format(str(mvv_array)))
+
+    mvv_new = list(divide_chunks(mvv_array, 2))
+    add_list = mvv_new
+
+    first_item = add_list[-1]
+
+    add_list.remove(first_item)
+    for curr_item in add_list:
+        logging.info("running for dates {0}".format(str(curr_item)))
+        small_df = data_frame.filter(F.col("start_of_month").isin(*[curr_item]))
+        int_l3_streaming_content_type_features = node_from_config(small_df, int_l3_streaming_content_type_features_dict)
+
+        l3_streaming_fav_content_group_by_volume = node_from_config(int_l3_streaming_content_type_features,
+                                                                    l3_streaming_fav_content_group_by_volume_dict)
+        CNTX.catalog.save(l3_streaming_fav_content_group_by_volume_dict["output_catalog"],
+                          l3_streaming_fav_content_group_by_volume)
+
+        l3_streaming_fav_content_group_by_duration = node_from_config(int_l3_streaming_content_type_features,
+                                                                      l3_streaming_fav_content_group_by_duration_dict)
+        CNTX.catalog.save(l3_streaming_fav_content_group_by_duration_dict["output_catalog"],
+                          l3_streaming_fav_content_group_by_duration)
+
+    logging.info("Final date to run for {0}".format(str(first_item)))
+    small_df = data_frame.filter(F.col("start_of_month").isin(*[first_item]))
+    int_l3_streaming_content_type_features = node_from_config(small_df, int_l3_streaming_content_type_features_dict)
+
+    l3_streaming_fav_content_group_by_volume = node_from_config(int_l3_streaming_content_type_features,
+                                                                l3_streaming_fav_content_group_by_volume_dict)
+
+    l3_streaming_fav_content_group_by_duration = node_from_config(int_l3_streaming_content_type_features,
+                                                                  l3_streaming_fav_content_group_by_duration_dict)
+
+    return [l3_streaming_fav_content_group_by_volume, l3_streaming_fav_content_group_by_duration]
+
+
+def streaming_to_l3_tv_channel_type_features(input_df: DataFrame,
+                                             int_l3_streaming_tv_channel_features_dict: dict,
+                                             l3_streaming_fav_tv_channel_by_volume_dict: dict,
+                                             l3_streaming_fav_tv_channel_by_duration_dict: dict,
+                                             ) -> [DataFrame, DataFrame]:
+    """
+    :param input_df:
+    :param int_l3_streaming_tv_channel_features_dict:
+    :param l3_streaming_fav_tv_channel_by_volume_dict:
+    :param l3_streaming_fav_tv_channel_by_duration_dict:
+    :return:
+    """
+    ################################# Start Implementing Data availability checks #############################
+    if check_empty_dfs([input_df]):
+        return [get_spark_empty_df(), get_spark_empty_df()]
+
+    input_df = data_non_availability_and_missing_check(
+        df=input_df, grouping="monthly", par_col="event_partition_date",
+        missing_data_check_flg='Y',
+        target_table_name=l3_streaming_fav_tv_channel_by_duration_dict["output_catalog"],
+        exception_partitions=["2020-03-30"])
+
+    if check_empty_dfs([input_df]):
+        return [get_spark_empty_df(), get_spark_empty_df()]
+
+    ################################# End Implementing Data availability checks ###############################
+
+    def divide_chunks(l, n):
+        # looping till length l
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
+
+    CNTX = load_context(Path.cwd(), env=conf)
+    data_frame = input_df
+    dates_list = data_frame.select('start_of_month').distinct().collect()
+    mvv_array = [row[0] for row in dates_list if row[0] != "SAMPLING"]
+    mvv_array = sorted(mvv_array)
+    logging.info("Dates to run for {0}".format(str(mvv_array)))
+
+    mvv_new = list(divide_chunks(mvv_array, 2))
+    add_list = mvv_new
+
+    first_item = add_list[-1]
+
+    add_list.remove(first_item)
+    for curr_item in add_list:
+        logging.info("running for dates {0}".format(str(curr_item)))
+        small_df = data_frame.filter(F.col("start_of_month").isin(*[curr_item]))
+        int_l3_streaming_tv_channel_features = node_from_config(small_df, int_l3_streaming_tv_channel_features_dict)
+        CNTX.catalog.save(int_l3_streaming_tv_channel_features_dict["output_catalog"],
+                          int_l3_streaming_tv_channel_features)
+
+        l3_streaming_fav_tv_channel_by_volume = node_from_config(int_l3_streaming_tv_channel_features,
+                                                                 l3_streaming_fav_tv_channel_by_volume_dict)
+        CNTX.catalog.save(l3_streaming_fav_tv_channel_by_volume_dict["output_catalog"],
+                          l3_streaming_fav_tv_channel_by_volume)
+
+        l3_streaming_fav_tv_channel_by_duration = node_from_config(int_l3_streaming_tv_channel_features,
+                                                                   l3_streaming_fav_tv_channel_by_duration_dict)
+        CNTX.catalog.save(l3_streaming_fav_tv_channel_by_duration_dict["output_catalog"],
+                          l3_streaming_fav_tv_channel_by_duration)
+
+    logging.info("Final date to run for {0}".format(str(first_item)))
+    small_df = data_frame.filter(F.col("start_of_week").isin(*[first_item]))
+    int_l3_streaming_tv_channel_features = node_from_config(small_df, int_l3_streaming_tv_channel_features_dict)
+
+    l3_streaming_fav_tv_channel_by_volume = node_from_config(int_l3_streaming_tv_channel_features,
+                                                             l3_streaming_fav_tv_channel_by_volume_dict)
+
+    l3_streaming_fav_tv_channel_by_duration = node_from_config(int_l3_streaming_tv_channel_features,
+                                                               l3_streaming_fav_tv_channel_by_duration_dict)
+
+    return [l3_streaming_fav_tv_channel_by_volume, l3_streaming_fav_tv_channel_by_duration]
+
+
+def streaming_streaming_fav_tv_show_by_episode_watched_features(
+        input_df: DataFrame,
+        int_l3_streaming_tv_show_features_dict,
+        l3_streaming_fav_tv_show_by_episode_watched_dict) -> DataFrame:
+    """
+    :param input_df:
+    :param int_l3_streaming_tv_show_features_dict:
+    :param l3_streaming_fav_tv_show_by_episode_watched_dict:
+    :return:
+    """
+    ################################# Start Implementing Data availability checks #############################
+    if check_empty_dfs([input_df]):
+        return get_spark_empty_df()
+
+    input_df = data_non_availability_and_missing_check(
+        df=input_df, grouping="monthly", par_col="event_partition_date",
+        missing_data_check_flg='Y',
+        target_table_name=l3_streaming_fav_tv_show_by_episode_watched_dict["output_catalog"],
+        exception_partitions=["2020-03-30"])
+
+    if check_empty_dfs([input_df]):
+        return  get_spark_empty_df()
+
+    ################################# End Implementing Data availability checks ###############################
+
+    def divide_chunks(l, n):
+        # looping till length l
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
+
+    CNTX = load_context(Path.cwd(), env=conf)
+    data_frame = input_df
+    dates_list = data_frame.select('start_of_month').distinct().collect()
+    mvv_array = [row[0] for row in dates_list if row[0] != "SAMPLING"]
+    mvv_array = sorted(mvv_array)
+    logging.info("Dates to run for {0}".format(str(mvv_array)))
+
+    mvv_new = list(divide_chunks(mvv_array, 2))
+    add_list = mvv_new
+
+    first_item = add_list[-1]
+
+    add_list.remove(first_item)
+    for curr_item in add_list:
+        logging.info("running for dates {0}".format(str(curr_item)))
+        small_df = data_frame.filter(F.col("start_of_month").isin(*[curr_item]))
+        int_l3_streaming_tv_show_features = node_from_config(small_df, int_l3_streaming_tv_show_features_dict)
+
+        l3_streaming_fav_tv_show_by_episode_watched = node_from_config(int_l3_streaming_tv_show_features,
+                                                                 l3_streaming_fav_tv_show_by_episode_watched_dict)
+        CNTX.catalog.save(l3_streaming_fav_tv_show_by_episode_watched_dict["output_catalog"],
+                          l3_streaming_fav_tv_show_by_episode_watched)
+
+    logging.info("Final date to run for {0}".format(str(first_item)))
+    small_df = data_frame.filter(F.col("start_of_week").isin(*[first_item]))
+    int_l3_streaming_tv_show_features = node_from_config(small_df, int_l3_streaming_tv_show_features_dict)
+
+    l3_streaming_fav_tv_show_by_episode_watched = node_from_config(int_l3_streaming_tv_show_features,
+                                                                   l3_streaming_fav_tv_show_by_episode_watched_dict)
+
+    return l3_streaming_fav_tv_show_by_episode_watched
+
+
