8d202cd64b3db975a5ebaef8a2306ad1c05da807
jtyrek <jakub_tyrek@mckinsey.com>
2020-07-08 17:26:25 +0200

    [clustering] Drop dictionaries inputs

--- src/nba/personnas_clustering/personnas_clustering_pipeline.py
+++ src/nba/personnas_clustering/personnas_clustering_pipeline.py
@@ -11,28 +11,26 @@ def create_nba_personnas_clustering_pipeline() -> Pipeline:
         [
             node(
                 l5_all_subscribers_master_table_customer_level,
-                inputs={
-                    "df_master": "l5_all_subscribers_master_table",
-                    "propensities": "clustering_propensities",
-                    "l5_customer_ids": "l5_customer_ids",
-                    "l4_streaming_visit_count_and_down_"
-                    "traffic_feature_full_load_data_blob": "l4_streaming_visit_count_"
-                    "and_download_traffic_feature_full_load_data_blob",
-                    "subset_features": "params:nba_model_input_features",
-                },
+                inputs=[
+                    "l5_all_subscribers_master_table",
+                    "clustering_propensities",
+                    "l5_customer_ids",
+                    "l4_streaming_visit_count_and_download_"
+                    "traffic_feature_full_load_data_blob",
+                    "params:nba_model_input_features",
+                ],
                 outputs="l5_all_subscribers_master_table_customer_level",
                 name="l5_all_subscribers_master_table_customer_level",
                 tags=["l5_all_subscribers_master_table_customer_level"],
             ),
             node(
                 personnas_clustering,
-                inputs={
-                    "df_master": "l5_all_subscribers_master_table_customer_level",
-                    "clustering_features": "params:nba_personnas_clustering_features",
-                    "n_pca_components": "params:nba_personnas_"
-                    "clustering_n_pca_components",
-                    "n_clusters": "params:nba_personnas_clustering_n_clusters",
-                },
+                inputs=[
+                    "l5_all_subscribers_master_table_customer_level",
+                    "params:nba_personnas_clustering_features",
+                    "params:nba_personnas_clustering_n_pca_components",
+                    "params:nba_personnas_clustering_n_clusters",
+                ],
                 outputs=[
                     "l5_personnas_clustering_master_customer_level",
                     "personnas_clustering_model",
@@ -42,13 +40,11 @@ def create_nba_personnas_clustering_pipeline() -> Pipeline:
             ),
             node(
                 l5_personnas_clustering_summary,
-                inputs={
-                    "l5_personnas_clustering_master": "l5_personnas_clustering_"
-                    "master_customer_level",
-                    "clustering_features": "params:nba_personnas_clustering_features",
-                    "features_to_summarize": "params:nba_personnas_clustering_"
-                    "reporting_features",
-                },
+                inputs=[
+                    "l5_personnas_clustering_master_customer_level",
+                    "params:nba_personnas_clustering_features",
+                    "params:nba_personnas_clustering_reporting_features",
+                ],
                 outputs="l5_personnas_clustering_summary",
                 name="l5_personnas_clustering_summary",
                 tags=["l5_personnas_clustering_summary"],
