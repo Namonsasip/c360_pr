d9b977558f3fd0cafdff4f49de3137b28b335376
amuro71 <amuro71@gmail.com>
2020-07-08 15:31:56 +0700

    Tanachok 2020-07-08 15:31

--- src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l1/to_l1_pipeline.py
@@ -2,7 +2,6 @@ from kedro.pipeline import Pipeline, node
 
 from customer360.pipelines.data_engineering.nodes.geolocation_nodes.to_l1.to_l1_nodes import *
 
-
 def geo_to_l1_intermediate_pipeline(**kwargs):
     return Pipeline(
         [
@@ -11,7 +10,6 @@ def geo_to_l1_intermediate_pipeline(**kwargs):
         ],
         name="geo_to_l1_intermediate_pipeline"
     )
-
 
 def geo_to_l1_pipeline(**kwargs):
     return Pipeline(
