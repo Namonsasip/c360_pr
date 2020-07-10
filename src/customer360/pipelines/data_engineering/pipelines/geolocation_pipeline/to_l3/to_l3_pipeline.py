dd4b1fd19edd4a983177016e9860adeda1be73b4
KorawichAnuttra <64677410+KorawichAnuttra@users.noreply.github.com>
2020-07-10 14:16:59 +0700

    Rename catalog

--- src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l3/to_l3_pipeline.py
@@ -128,7 +128,7 @@ def geo_to_l3_pipeline(**kwargs):
             ### Home weekday city citizens
             node(
                 l3_geo_home_weekday_city_citizens_monthly,
-                ["l4_geo_home_work_location_id_for_l3_geo_home_weekday_city_citizens_monthly",
+                ["l3_geo_home_work_location_id_monthly_for_l3_geo_home_weekday_city_citizens_monthly",
                  "l0_mst_cell_masterplan_for_l3_geo_home_weekday_city_citizens_monthly",
                  "params:l3_geo_home_weekday_city_citizens_monthly"
                  ],
@@ -139,7 +139,7 @@ def geo_to_l3_pipeline(**kwargs):
             node(
                 l3_geo_work_area_center_average_monthly,
                 ["l0_geo_cust_location_visit_hr_for_l3_geo_work_area_center_average_monthly",
-                 "l3_geo_home_work_location_monthly_id_for_l3_geo_work_area_center_average_monthly"
+                 "l3_geo_home_work_location_id_monthly_for_l3_geo_work_area_center_average_monthly"
                  ],
                 "l3_geo_work_area_center_average_monthly"
             )
