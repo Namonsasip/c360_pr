aa6830326281f20f3d81623d8970b785711bccc0
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-07-13 13:19:15 +0530

    digital changes

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l2/to_l2_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l2/to_l2_nodes.py
@@ -26,40 +26,22 @@ def build_digital_l2_weekly_features(cxense_site_traffic: DataFrame,
     """
 
     ################################# Start Implementing Data availability checks #############################
-    if check_empty_dfs([cxense_site_traffic, cust_df]):
+    if check_empty_dfs([cxense_site_traffic]):
         return [get_spark_empty_df(), get_spark_empty_df(), get_spark_empty_df(), get_spark_empty_df()
             , get_spark_empty_df()]
 
     cxense_site_traffic = data_non_availability_and_missing_check(
         df=cxense_site_traffic, grouping="weekly",
-        par_col="partition_date",
-        target_table_name="l2_digital_cxenxse_site_traffic_weekly")
-
-    cust_df = data_non_availability_and_missing_check(
-        df=cust_df, grouping="weekly",
         par_col="start_of_week",
-        target_table_name="l2_digital_cxenxse_site_traffic_weekly")
+        target_table_name="l2_digital_cxenxse_site_traffic_weekly",
+        missing_data_check_flg='Y'
+        )
 
-    if check_empty_dfs([cxense_site_traffic, cust_df]):
+    if check_empty_dfs([cxense_site_traffic]):
         return [get_spark_empty_df(), get_spark_empty_df(), get_spark_empty_df(), get_spark_empty_df()
             , get_spark_empty_df()]
 
     ################################# End Implementing Data availability checks ###############################
-
-    cust_df_cols = ['access_method_num', 'start_of_week', 'subscription_identifier']
-    join_cols = ['access_method_num', 'start_of_week']
-    cxense_site_traffic = cxense_site_traffic.withColumnRenamed("mobile_no", "access_method_num") \
-        .withColumn("partition_date", f.col("partition_date").cast(StringType())) \
-        .withColumn("start_of_week", f.to_date(f.date_trunc('week', f.to_date(f.col("partition_date"), 'yyyyMMdd'))))
-
-    cust_df = cust_df.select(cust_df_cols)
-    cust_df = cust_df.withColumn("rn", expr(
-        "row_number() over(partition by start_of_week,access_method_num order by start_of_week desc)"))
-    cust_df = cust_df.where("rn = 1")
-    cust_df = cust_df.drop("rn")
-
-    cxense_site_traffic = cxense_site_traffic.join(cust_df, join_cols)
-
     weekly_features = node_from_config(cxense_site_traffic, weekly_dict)
     popular_url = node_from_config(cxense_site_traffic, popular_url_dict)
     popular_postal_code = node_from_config(cxense_site_traffic, popular_postal_code_dict)
