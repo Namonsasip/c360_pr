7b809ef7985a86ab335a52191a53a73e03cce597
KorawichAnuttra <64677410+KorawichAnuttra@users.noreply.github.com>
2020-07-17 17:16:01 +0700

    Fix code

--- src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l3/to_l3_pipeline.py
@@ -114,23 +114,22 @@ def geo_to_l3_pipeline(**kwargs):
                  "l0_usage_sum_data_location_daily_for_l3_use_non_homework_features",
                  "l3_geo_time_spent_by_location_monthly_for_l3_data_traffic_home_work_top1_top2"
                  ],
-                None
-                # "l3_geo_use_traffic_home_work"
+                "l3_geo_use_traffic_home_work"
 
             ),
 
-            # ### runnig flag == 9.0
-            # ###feature_sum_voice_location###
-            # node(
-            #     l3_call_location_home_work_monthly,
-            #     ["l0_geo_mst_cell_masterplan_current_for_l3_call_location_home_work_monthly",
-            #      "l3_geo_home_work_location_id_monthly_for_l3_call_location_home_work_monthly",
-            #      "l0_profile_customer_profile_ma_for_l3_call_location_home_work_monthly",
-            #      "l0_usage_sum_voice_location_daily_for_l3_call_location_home_work_monthly",
-            #      "l3_geo_top_visit_exclude_homework_for_l3_call_location_home_work_monthly"
-            #      ],
-            #     "l3_geo_call_location_home_work_monthly"
-            # ),
+            ### runnig flag == 9.0
+            ###feature_sum_voice_location###
+            node(
+                l3_call_location_home_work_monthly,
+                ["l0_geo_mst_cell_masterplan_current_for_l3_call_location_home_work_monthly",
+                 "l3_geo_home_work_location_id_monthly_for_l3_call_location_home_work_monthly",
+                 "l0_profile_customer_profile_ma_for_l3_call_location_home_work_monthly",
+                 "l0_usage_sum_voice_location_daily_for_l3_call_location_home_work_monthly",
+                 "l3_geo_top_visit_exclude_homework_for_l3_call_location_home_work_monthly"
+                 ],
+                "l3_geo_call_location_home_work_monthly"
+            ),
 
             # ### runnig flag == 1
             # ##Top_3_cells_on_voice_usage###
