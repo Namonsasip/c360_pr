ebf79c9c6aa418430bd7123b04cb73bf056e3c76
sboonchusanong <litty_135@hotmail.com>
2020-07-17 13:14:41 +0700

    remove empty pipeline

--- src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l1/to_l1_pipeline.py
@@ -2,14 +2,7 @@ from kedro.pipeline import Pipeline, node
 
 from customer360.pipelines.data_engineering.nodes.geolocation_nodes.to_l1.to_l1_nodes import *
 
-def geo_to_l1_intermediate_pipeline(**kwargs):
-    return Pipeline(
-        [
 
-
-        ],
-        name="geo_to_l1_intermediate_pipeline"
-    )
 
 def geo_to_l1_pipeline(**kwargs):
     return Pipeline(
@@ -131,12 +124,4 @@ def geo_to_l1_pipeline(**kwargs):
         ], name="geo_to_l1_pipeline"
     )
 
-
-def geo_to_l1_union_pipeline(**kwargs):
-    return Pipeline(
-        [
-
-
-        ], name="geo_to_l1_union_pipeline"
-    )
 
