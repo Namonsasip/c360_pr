e90a1af80cf568b1fc54c98e18491ce74112f082
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-07-21 22:58:13 +0530

    changes in campaign

--- src/customer360/pipelines/data_engineering/pipelines/campaign_pipeline/to_l4/to_l4_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/campaign_pipeline/to_l4/to_l4_pipeline.py
@@ -34,16 +34,21 @@ PLEASE DELETE THIS FILE ONCE YOU START WORKING ON YOUR OWN PROJECT!
 from kedro.pipeline import Pipeline, node
 
 from customer360.utilities.config_parser import l4_rolling_window, l4_rolling_ranked_window
-from customer360.pipelines.data_engineering.nodes.campaign_nodes.to_l4 import add_relative_time_features
+from customer360.pipelines.data_engineering.nodes.campaign_nodes.to_l4 import add_relative_time_features, \
+    build_campaign_weekly_features
 
 
 def campaign_to_l4_pipeline(**kwargs):
     return Pipeline(
         [
             node(
-                l4_rolling_window,
+                build_campaign_weekly_features,
                 ["l2_campaign_postpaid_prepaid_weekly",
-                 "params:l4_campaign_postpaid_prepaid_features"],
+                 "params:l4_campaign_postpaid_prepaid_features_min",
+                 "params:l4_campaign_postpaid_prepaid_features_max",
+                 "params:l4_campaign_postpaid_prepaid_features_sum",
+                 "params:l4_campaign_postpaid_prepaid_features_avg",
+                 ],
                 "l4_campaign_postpaid_prepaid_int"
 
             ),
