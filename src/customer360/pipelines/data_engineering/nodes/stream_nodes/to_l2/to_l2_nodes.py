aca4b26f28935873a73e9ae964d42ada8260bbe4
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-07-30 14:20:20 +0530

    changes in streaming L2

--- src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l2/to_l2_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l2/to_l2_nodes.py
@@ -25,7 +25,6 @@ def generate_l2_fav_streaming_day(input_df: DataFrame, app_list: list):
     for each_app in app_list:
         df = spark.sql("""
             select
-                access_method_num, 
                 subscription_identifier,
                 start_of_week,
                 day_of_week as fav_{each_app}_streaming_day_of_week,
