d5fb150f7e1459ed8aacac26f0372b0a35758525
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-07-31 10:46:48 +0530

    streaming L2 run

--- src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l2/to_l2_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l2/to_l2_nodes.py
@@ -45,10 +45,10 @@ def dac_for_streaming_to_l2_pipeline_from_l1(input_df: DataFrame, target_table_n
     if check_empty_dfs([input_df]):
         return get_spark_empty_df()
 
-    input_df = data_non_availability_and_missing_check(df=input_df, grouping="weekly", par_col="event_partition_date",
-                                                       target_table_name=target_table_name,
-                                                       missing_data_check_flg='Y',
-                                                       exception_partitions=["2020-04-27"])
+    # input_df = data_non_availability_and_missing_check(df=input_df, grouping="weekly", par_col="event_partition_date",
+    #                                                    target_table_name=target_table_name,
+    #                                                    missing_data_check_flg='Y',
+    #                                                    exception_partitions=["2020-04-27"])
 
     if check_empty_dfs([input_df]):
         return get_spark_empty_df()
