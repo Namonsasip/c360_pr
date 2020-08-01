c9222ba49e0675b6c624d2ebe91fa5ca5d4f9635
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-08-01 09:24:20 +0530

    streaming L2 run

--- src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l3/to_l3_nodes.py
@@ -621,13 +621,7 @@ def streaming_favourite_start_hour_of_day_func(
     application_list = ["youtube", "youtube_go", "youtubebyclick", "trueid", "truevisions", "monomaxx",
                         "qqlive", "facebook", "linetv", "ais_play", "netflix", "viu", "viutv", "iflix",
                         "spotify", "jooxmusic", "twitchtv", "bigo", "valve_steam"]
-    master_application = master_application.filter(F.lower(F.col("application")).isin(application_list))
-
-    cust_df = cust_profile_df.withColumn("rn", F.expr(
-        "row_number() over(partition by start_of_month,access_method_num order by "
-        "start_of_month desc, mobile_status_date desc)")) \
-        .where("rn = 1") \
-        .select("subscription_identifier", "access_method_num", "start_of_month")
+    master_application = master_application.filter(F.lower(F.col("application_name")).isin(application_list))
 
     input_with_application = input_df.join(master_application, ["application"])
     input_with_application = add_event_week_and_month_from_yyyymmdd(input_with_application, "partition_date")\
@@ -641,8 +635,16 @@ def streaming_favourite_start_hour_of_day_func(
     CNTX = load_context(Path.cwd(), env=conf)
     for curr_item in mvv_array:
         logging.info("Running for {0}".format(curr_item))
-        return_df = process_massive_processing(input_with_application.filter(F.col("start_of_month") == curr_item)
-                                               , cust_df)
+
+        cust_df = cust_profile_df.filter(F.col("start_of_month") == curr_item)\
+            .withColumn("rn", F.expr(
+            "row_number() over(partition by start_of_month,access_method_num order by "
+            "start_of_month desc, mobile_status_date desc)")) \
+            .where("rn = 1") \
+            .select("subscription_identifier", "access_method_num", "start_of_month")
+
+        input_with_application_curr = input_with_application.filter(F.col("start_of_month") == curr_item)
+        return_df = process_massive_processing(input_with_application_curr, cust_df)
         CNTX.catalog.save("l3_streaming_favourite_start_time_hour_of_day", return_df)
 
     return None
