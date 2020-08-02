84ece84b870362fe155a047d8d33502ba717336e
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-08-02 20:15:32 +0530

    streaming L2 run

--- src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l3/to_l3_nodes.py
@@ -629,10 +629,10 @@ def streaming_favourite_start_hour_of_day_func(
                 final_col = "share_of_{}_streaming_usage_{}_by_total".format(v_time_type, app_group_type)
                 filtered = input_with_application.filter(F.col("application_group") == v_app_group)
                 filtered_agg = filtered.groupBy(["subscription_identifier", "start_of_month"])\
-                    .agg(F.sum("download").alias("main_download"))
+                    .agg(F.sum("dw_kbyte").alias("main_download"))
 
                 curr_time_type_agg = filtered.filter(F.col("time_of_day") == v_time_type) \
-                    .groupBy(["subscription_identifier", "start_of_month"]).agg(F.sum("download").alias("download"))
+                    .groupBy(["subscription_identifier", "start_of_month"]).agg(F.sum("dw_kbyte").alias("download"))
 
                 final_df = filtered_agg.join(curr_time_type_agg, ["subscription_identifier", "start_of_month"])\
                     .withColumn(final_col, F.col("download") / F.col("main_download"))\
@@ -649,7 +649,7 @@ def streaming_favourite_start_hour_of_day_func(
                     .agg(F.sum("download").alias("main_download"))
 
                 curr_time_type_agg = filtered.filter(F.col("day_type") == v_time_type)\
-                    .groupBy(["subscription_identifier", "start_of_month"]).agg(F.sum("download").alias("download"))
+                    .groupBy(["subscription_identifier", "start_of_month"]).agg(F.sum("dw_kbyte").alias("download"))
 
                 final_df = filtered_agg.join(curr_time_type_agg, ["subscription_identifier", "start_of_month"])\
                     .withColumn(final_col, F.col("download") / F.col("main_download"))\
