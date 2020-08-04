ca8d2b3a4b641201785ffea3bfce14caea7b0729
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-08-04 10:55:29 +0530

    new changes

--- src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l3/to_l3_nodes.py
@@ -812,16 +812,16 @@ def streaming_favourite_quality_features_func(
     :return:
     """
     ################################# Start Implementing Data availability checks #############################
-    if check_empty_dfs([input_df]):
-        return get_spark_empty_df()
-
-    input_df = data_non_availability_and_missing_check(
-        df=input_df, grouping="monthly", par_col="event_partition_date",
-        missing_data_check_flg='Y',
-        target_table_name="l3_streaming_app_quality_features")
-
-    if check_empty_dfs([input_df]):
-        return get_spark_empty_df()
+    # if check_empty_dfs([input_df]):
+    #     return get_spark_empty_df()
+    #
+    # input_df = data_non_availability_and_missing_check(
+    #     df=input_df, grouping="monthly", par_col="event_partition_date",
+    #     missing_data_check_flg='Y',
+    #     target_table_name="l3_streaming_app_quality_features")
+    #
+    # if check_empty_dfs([input_df]):
+    #     return get_spark_empty_df()
     ################################# End Implementing Data availability checks ###############################
 
     dictionary = [{'filter_condition': "youtube,youtube_go,youtubebyclick",
