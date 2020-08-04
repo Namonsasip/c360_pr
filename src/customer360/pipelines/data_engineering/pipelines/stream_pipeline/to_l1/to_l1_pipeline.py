a285352fd4954fcbd8385bafdd13e8a8aa4e3245
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-08-04 12:29:43 +0530

    streaming L2 run

--- src/customer360/pipelines/data_engineering/pipelines/stream_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/stream_pipeline/to_l1/to_l1_pipeline.py
@@ -40,15 +40,15 @@ from customer360.pipelines.data_engineering.nodes.stream_nodes.to_l1.to_l1_nodes
 def streaming_sdr_sub_app_hourly_daily_for_l3_monthly(**kwargs):
     return Pipeline(
         [
-            node(
-                build_streaming_sdr_sub_app_hourly_for_l3_monthly,
-                [
-                    "l0_streaming_sdr_sub_app_hourly_for_l1_streaming_sdr_sub_app_hourly",
-                    "l0_mobile_app_master",
-                    "l1_customer_profile_union_daily_feature_for_l1_streaming_sdr_sub_app_hourly"
-                    ],
-                "l1_streaming_sdr_sub_app_hourly"
-            ),
+            # node(
+            #     build_streaming_sdr_sub_app_hourly_for_l3_monthly,
+            #     [
+            #         "l0_streaming_sdr_sub_app_hourly_for_l1_streaming_sdr_sub_app_hourly",
+            #         "l0_mobile_app_master",
+            #         "l1_customer_profile_union_daily_feature_for_l1_streaming_sdr_sub_app_hourly"
+            #         ],
+            #     "l1_streaming_sdr_sub_app_hourly"
+            # ),
             node(
                 build_streaming_ufdr_streaming_quality_for_l3_monthly,
                 [
