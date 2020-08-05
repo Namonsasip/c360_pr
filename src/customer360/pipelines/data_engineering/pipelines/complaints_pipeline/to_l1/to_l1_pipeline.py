b444d4ea6e6ace086f89099bf04a293a0f2f56c6
kansalankit <a6c3a4c63e50e25a1858bd51e690732657cc2160>
2020-08-05 16:17:26 +0530

    compaints removing commented out items

--- src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
@@ -70,23 +70,6 @@ def complaints_to_l1_pipeline(**kwargs):
                 "l1_complaints_nps_after_call"
             ),
 
-            # node(
-            #     dac_for_complaints_to_l1_pipeline,
-            #     ["l0_complaints_acc_ai_chatbot_survey_daily",
-            #      "l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_chatbot",
-            #      "params:l1_complaints_nps_after_chatbot_tbl",
-            #      "params:exception_partition_list_for_l0_complaints_acc_ai_chatbot_survey_daily"],
-            #     ["int_l0_complaints_acc_ai_chatbot_survey_daily",
-            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_chatbot"]
-            # ),
-            # node(
-            #     l1_massive_processing,
-            #     ["int_l0_complaints_acc_ai_chatbot_survey_daily",
-            #      "params:l1_complaints_nps_after_chatbot",
-            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_chatbot"],
-            #     "l1_complaints_nps_after_chatbot"
-            # ),
-
             node(
                 dac_for_complaints_to_l1_pipeline,
                 ["l0_complaints_acc_qmt_csi_daily",
