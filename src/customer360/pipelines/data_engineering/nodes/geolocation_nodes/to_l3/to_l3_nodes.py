93972bdde97a41bc466ee55abb5ce232e6fc611b
sarit boonchusanong <saritboo@ais.co.th>
2020-08-05 15:21:56 +0700

    change left to left_anti join (exclude homework)

--- src/customer360/pipelines/data_engineering/nodes/geolocation_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/geolocation_nodes/to_l3/to_l3_nodes.py
@@ -863,16 +863,16 @@ def _geo_top_visit_exclude_homework(sum_duration, homework):
     result = sum_duration_3mo.join(homework, [sum_duration_3mo.imsi == homework.imsi,
                                               sum_duration_3mo.location_id == homework.home_weekday_location_id,
                                               sum_duration_3mo.start_of_month == homework.start_of_month],
-                                   'left').select(sum_duration_3mo.imsi, 'location_id', 'sum_duration',
+                                   'left_anti').select(sum_duration_3mo.imsi, 'location_id', 'sum_duration',
                                                   sum_duration_3mo.start_of_month)
     result = result.join(homework,
                          [result.imsi == homework.imsi, result.location_id == homework.home_weekend_location_id,
                           result.start_of_month == homework.start_of_month],
-                         'left').select(result.imsi, 'location_id', 'sum_duration', result.start_of_month)
+                         'left_anti').select(result.imsi, 'location_id', 'sum_duration', result.start_of_month)
     result = result.join(homework,
                          [result.imsi == homework.imsi, result.location_id == homework.work_location_id,
                           result.start_of_month == homework.start_of_month],
-                         'left').select(result.imsi, 'location_id', 'sum_duration', result.start_of_month)
+                         'left_anti').select(result.imsi, 'location_id', 'sum_duration', result.start_of_month)
     win = Window.partitionBy("start_of_month", "imsi").orderBy(F.col("sum_duration").desc(), F.col("location_id"))
     result = result.withColumn("rank", F.row_number().over(win))
 
