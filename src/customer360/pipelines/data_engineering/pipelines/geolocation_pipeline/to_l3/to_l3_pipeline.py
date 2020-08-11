734c6b6c3816a5749aa9a8df48d6c028be6ea962
KorawichAnuttra <64677410+KorawichAnuttra@users.noreply.github.com>
2020-08-11 22:12:47 +0700

    Fix

--- src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l3/to_l3_pipeline.py
@@ -66,15 +66,15 @@ def geo_to_l3_pipeline(**kwargs):
     return Pipeline(
         [
 
-            ### WAIT
-            node(
-                int_l3_geo_top3_visit_exclude_hw_monthly,
-                ["l0_geo_cust_location_monthly_hr_for_l3_geo_top3_visit_exclude_hw_monthly",
-                 "l3_geo_home_work_location_id_monthly_for_l3_geo_top3_visit_exclude_hw_monthly",
-                 "params:int_l3_geo_top3_visit_exclude_hw_monthly"
-                 ],
-                "int_l3_geo_top3_visit_exclude_hw_monthly"
-            ),
+            # ### WAIT
+            # node(
+            #     int_l3_geo_top3_visit_exclude_hw_monthly,
+            #     ["l0_geo_cust_location_monthly_hr_for_l3_geo_top3_visit_exclude_hw_monthly",
+            #      "l3_geo_home_work_location_id_monthly_for_l3_geo_top3_visit_exclude_hw_monthly",
+            #      "params:int_l3_geo_top3_visit_exclude_hw_monthly"
+            #      ],
+            #     "int_l3_geo_top3_visit_exclude_hw_monthly"
+            # ),
             node(
                 l3_geo_top3_visit_exclude_hw_monthly,
                 ["int_l3_geo_top3_visit_exclude_hw_monthly",
