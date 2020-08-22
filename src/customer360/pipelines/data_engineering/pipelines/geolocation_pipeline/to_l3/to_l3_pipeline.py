c7496500c93c2b114527257243f66640f63fb2f8
KorawichAnuttra <64677410+KorawichAnuttra@users.noreply.github.com>
2020-08-22 12:46:05 +0700

    Fix

--- src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l3/to_l3_pipeline.py
@@ -90,7 +90,7 @@ def geo_to_l3_pipeline(**kwargs):
             ),
             node(
                 int_l3_geo_visit_ais_store_location_monthly,
-                ["int_l3_geo_visit_ais_store_location_monthly",
+                ["int_l3_geo_visit_ais_store_location_filter_monthly",
                  "l3_geo_home_work_location_id_monthly_for_l3_geo_visit_ais_store_location_monthly",
                  "l3_geo_top3_visit_exclude_hw_monthly_for_l3_geo_visit_ais_store_location_monthly"
                  ],
