7f19f1a8a426682c33082996b7f9a6b0b6e158cb
KorawichAnuttra <64677410+KorawichAnuttra@users.noreply.github.com>
2020-08-24 16:03:45 +0700

    Update to_l4_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/geolocation_nodes/to_l4/to_l4_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/geolocation_nodes/to_l4/to_l4_nodes.py
@@ -177,11 +177,15 @@ def l4_geo_top3_voice_location(input_df: DataFrame, params_config: str) -> DataF
     column_result_df = result_df.columns
     for _ in ['subscription_identifier', 'mobile_no', 'imsi', 'start_of_week']:
         column_result_df.remove(_)
+
     output_df = input_df.join(result_df, ['subscription_identifier', 'mobile_no', 'imsi', 'start_of_week'],
                               'inner').select(
-        input_df.subscription_identifier, input_df.mobile_no, input_df.imsi,
+        input_df.subscription_identifier,
+        input_df.mobile_no,
+        input_df.imsi,
         input_df.start_of_week,
-        input_df.total_call, input_df.total_call_minute,
+        input_df.total_call,
+        input_df.total_call_minute,
         input_df.top_voice_location_id_1st,
         input_df.top_voice_latitude_1st,
         input_df.top_voice_longitude_1st,
