e303025fabac3904a878d77add45a53bd765cc96
KorawichAnuttra <64677410+KorawichAnuttra@users.noreply.github.com>
2020-09-10 10:11:50 +0700

    Change parameter on pipeline

--- src/customer360/pipelines/data_engineering/pipelines/util_pipeline/lineage_dependency_pipeline/lineage_dependency_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/util_pipeline/lineage_dependency_pipeline/lineage_dependency_pipeline.py
@@ -9,7 +9,7 @@ def lineage_dependency_pipeline(**kwargs):
             # Top up count and top up volume feature pre-paid
             node(
                 generate_dependency_dataset,
-                ["params:running_environment_parameter"],
+                None,
                 ["util_dependency_report", "util_feature_report"]
             ),
         ]
