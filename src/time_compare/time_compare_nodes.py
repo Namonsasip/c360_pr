1bd5c2f53d3dbc3cf2f0f524aec3a4918ae9506e
ElementalSpark <non8956@hotmail.com>
2020-09-10 14:05:43 +0700

    date debug

--- src/time_compare/time_compare_nodes.py
+++ src/time_compare/time_compare_nodes.py
@@ -6,7 +6,7 @@ from customer360.utilities.spark_util import get_spark_session
 
 def direct_load_node(l3_customer_profile_union_monthly_feature: DataFrame,
                      l4_usage_prepaid_postpaid_daily_features: DataFrame):
-    l3_customer_profile_union_monthly_feature = l3_customer_profile_union_monthly_feature.where("start_of_month = 2020-06-01")
+    l3_customer_profile_union_monthly_feature = l3_customer_profile_union_monthly_feature.where("start_of_month = '2020-06-01'")
     l4_usage_prepaid_postpaid_daily_features = l4_usage_prepaid_postpaid_daily_features.where("event_partition_date >= '2020-06-01'"
                                                                                               "and event_partition_date <= '2020-06-30'")
     joined_df = l3_customer_profile_union_monthly_feature.join(l4_usage_prepaid_postpaid_daily_features,
@@ -19,7 +19,7 @@ def direct_load_node(l3_customer_profile_union_monthly_feature: DataFrame,
 def hive_load_node(l3_customer_profile_union_monthly_feature: DataFrame,
                      l4_usage_prepaid_postpaid_daily_features: DataFrame):
     l3_customer_profile_union_monthly_feature = l3_customer_profile_union_monthly_feature.where(
-        "start_of_month = 2020-06-01")
+        "start_of_month = '2020-06-01'")
     l4_usage_prepaid_postpaid_daily_features = l4_usage_prepaid_postpaid_daily_features.where(
         "event_partition_date >= '2020-06-01'"
         "and event_partition_date <= '2020-06-30'")
