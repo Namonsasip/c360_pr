c3911b72b25b1e87f473c427580014423db30e03
str1k <y.thanasit@gmail.com>
2020-09-11 16:45:51 +0700

    update model version

--- src/du/scoring/scoring_pipeline.py
+++ src/du/scoring/scoring_pipeline.py
@@ -129,6 +129,7 @@ def create_du_scoring_pipeline() -> Pipeline:
                 join_c360_features_latest_date,
                 inputs={
                     "df_spine": "l5_du_eligible_sub_to_score",
+                    "unused_memory_fix_id":"unused_memory_fix_id",
                     "subset_features": "params:du_model_input_features",
                     "l5_nba_customer_profile": "l5_du_customer_profile",
                     "l4_billing_rolling_window_topup_and_volume": "l4_billing_rolling_window_topup_and_volume",
@@ -137,7 +138,6 @@ def create_du_scoring_pipeline() -> Pipeline:
                     "l4_billing_rolling_window_before_top_up_balance": "l4_billing_rolling_window_before_top_up_balance",
                     "l4_billing_rolling_window_top_up_channels": "l4_billing_rolling_window_top_up_channels",
                     "l4_daily_feature_topup_and_volume": "l4_daily_feature_topup_and_volume",
-                    "l4_campaign_postpaid_prepaid_features": "l4_campaign_postpaid_prepaid_features",
                     "l4_device_summary_features": "l4_device_summary_features",
                     "l4_revenue_prepaid_ru_f_sum_revenue_by_service_monthly": "l4_revenue_prepaid_ru_f_sum_revenue_by_service_monthly",
                     # "l4_streaming_visit_count_and_download_traffic_feature": "l4_streaming_visit_count_and_download_traffic_feature",
