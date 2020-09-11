34a31779c2ba29fe7f85ce5ed63afc258fa11c34
ElementalSpark <non8956@hotmail.com>
2020-09-11 09:58:58 +0700

    compare catalog.load with spark.sql

--- src/time_compare/time_compare_nodes.py
+++ src/time_compare/time_compare_nodes.py
@@ -16,13 +16,18 @@ def direct_load_node(l3_customer_profile_union_monthly_feature: DataFrame,
     return None
 
 
-def hive_load_node(l3_customer_profile_union_monthly_feature: DataFrame,
-                     l4_usage_prepaid_postpaid_daily_features: DataFrame):
-    l3_customer_profile_union_monthly_feature = l3_customer_profile_union_monthly_feature.where(
-        "start_of_month = '2020-06-01'")
-    l4_usage_prepaid_postpaid_daily_features = l4_usage_prepaid_postpaid_daily_features.where(
-        "event_partition_date >= '2020-06-01'"
-        "and event_partition_date <= '2020-06-30'")
+def hive_load_node():
+    spark = get_spark_session()
+    # l3_customer_profile_union_monthly_feature = l3_customer_profile_union_monthly_feature.where(
+    #     "start_of_month = '2020-06-01'")
+    # l4_usage_prepaid_postpaid_daily_features = l4_usage_prepaid_postpaid_daily_features.where(
+    #     "event_partition_date >= '2020-06-01'"
+    #     "and event_partition_date <= '2020-06-30'")
+    l3_customer_profile_union_monthly_feature = spark.sql("select * from c360_external.l3_customer_profile_union_monthly_feature"
+                                                          "where start_of_month = '2020-06-01'")
+    l4_usage_prepaid_postpaid_daily_features = spark.sql(
+        "select * from c360_external.l4_usage_prepaid_postpaid_daily_features"
+        " where event_partition_date >= '2020-06-01' and event_partition_date <= '2020-06-30'")
     joined_df = l3_customer_profile_union_monthly_feature.join(l4_usage_prepaid_postpaid_daily_features,
                                                                'subscription_identifier',
                                                                'left')
--- src/time_compare/time_compare_pipeline.py
+++ src/time_compare/time_compare_pipeline.py
@@ -26,8 +26,6 @@ def hive_load_pipeline() -> Pipeline:
             node(
                 hive_load_node,
                 inputs=[
-                    "l3_customer_profile_union_monthly_feature_hive",
-                    "l4_usage_prepaid_postpaid_daily_features_hive"
                 ],
                 outputs=None,
                 name="hive_load_node",
