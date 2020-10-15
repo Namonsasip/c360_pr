3c4513e0bd22c4d49bed47407e370fc7df239144
KorawichAnuttra <64677410+KorawichAnuttra@users.noreply.github.com>
2020-10-15 10:09:53 +0700

    Clear old comment filter from all node

--- src/customer360/pipelines/data_engineering/nodes/geolocation_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/geolocation_nodes/to_l1/to_l1_nodes.py
@@ -323,9 +323,6 @@ def massive_processing_with_l1_geo_visit_ais_store_location_daily(timespent_df: 
 def massive_processing_with_l1_geo_time_spent_by_location_daily(cust_visit_df: DataFrame,
                                                                 config_param: str
                                                                 ) -> DataFrame:
-    # cust_visit_df = cust_visit_df.filter('partition_date >= 20200701 and partition_date <= 20200719')
-    # NEXT
-    # cust_visit_df = cust_visit_df.filter('partition_date <= 20200517')
     if check_empty_dfs([cust_visit_df]):
         return get_spark_empty_df()
 
@@ -374,7 +371,6 @@ def massive_processing_with_l1_geo_time_spent_by_store_daily(timespent_df: DataF
 def massive_processing_with_l1_geo_count_visit_by_location_daily(cust_visit_df: DataFrame,
                                                                  config_param: str
                                                                  ) -> DataFrame:
-    # cust_visit_df = cust_visit_df.filter('partition_date >= 20200701 and partition_date <= 20200719')
     if check_empty_dfs([cust_visit_df]):
         return get_spark_empty_df()
 
@@ -397,7 +393,6 @@ def massive_processing_with_l1_geo_count_visit_by_location_daily(cust_visit_df: 
 def massive_processing_with_l1_geo_total_distance_km_daily(cust_visit_df: DataFrame,
                                                            config_param: str
                                                            ) -> DataFrame:
-    # cust_visit_df = cust_visit_df.filter('partition_date >= 20200701 and partition_date <= 20200719')
     if check_empty_dfs([cust_visit_df]):
         return get_spark_empty_df()
 
@@ -446,8 +441,6 @@ def massive_processing_with_l1_geo_top3_voice_location_daily(usagevoice_df: Data
                                                              cust_df: DataFrame,
                                                              config_param: str
                                                              ) -> DataFrame:
-    # usagevoice_df = usagevoice_df.filter('partition_date >= 20200701 and partition_date <= 20200719')
-    # cust_df = cust_df.filter('event_partition_date >= "2020-07-01" and event_partition_date <= "2020-07-19"')
     if check_empty_dfs([usagevoice_df, master_df, cust_df]):
         return get_spark_empty_df()
 
@@ -494,8 +487,6 @@ def massive_processing_with_l1_geo_data_session_location_daily(usagedata_df: Dat
                                                                cust_df: DataFrame,
                                                                config_param: str
                                                                ) -> DataFrame:
-    # usagedata_df = usagedata_df.filter('partition_date >= 20200701 and partition_date <= 20200719')
-    # cust_df = cust_df.filter('event_partition_date >= "2020-07-01" and event_partition_date <= "2020-07-19"')
     if check_empty_dfs([usagedata_df, master_df, cust_df]):
         return get_spark_empty_df()
 
@@ -541,7 +532,6 @@ def massive_processing_with_l1_geo_data_session_location_daily(usagedata_df: Dat
 def massive_processing_with_l1_customer_profile_imsi_daily_feature(input_df: DataFrame,
                                                                    config_param: str
                                                                    ) -> DataFrame:
-    # input_df = input_df.filter('partition_date >= 20200701 and partition_date <= 20200719')
     if check_empty_dfs([input_df]):
         return get_spark_empty_df()
 
