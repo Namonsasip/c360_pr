ade391b4a02fffde9c3a35179390a4f00b72a1f9
str1k <y.thanasit@gmail.com>
2020-11-25 14:27:24 +0700

    modified codes

--- src/du/reporting/du_report_pipeline.py
+++ src/du/reporting/du_report_pipeline.py
@@ -25,20 +25,5 @@ def create_du_weekly_revenue_uplift_report_pipeline() -> Pipeline:
                 name="create_du_weekly_revenue_uplift_report",
                 tags=["create_du_weekly_revenue_uplift_report",],
             ),
-            node(
-                partial(
-                    l5_du_weekly_revenue_uplift_report_contacted_only_old,
-                    control_group_initialize_profile_date="2020-08-01",
-                ),
-                inputs={
-                    "l4_revenue_prepaid_daily_features": "l4_revenue_prepaid_daily_features",
-                    "l0_du_pre_experiment3_groups": "l0_du_pre_experiment3_groups",
-                    "l3_customer_profile_union_monthly_feature_full_load": "l3_customer_profile_union_monthly_feature_full_load",
-                    "l0_campaign_tracking_contact_list_pre_full_load": "l0_campaign_tracking_contact_list_pre_full_load",
-                },
-                outputs="l5_du_weekly_revenue_uplift_report_contacted_only_old",
-                name="create_du_weekly_revenue_uplift_report_old",
-                tags=["create_du_weekly_revenue_uplift_report",],
-            ),
         ]
     )
