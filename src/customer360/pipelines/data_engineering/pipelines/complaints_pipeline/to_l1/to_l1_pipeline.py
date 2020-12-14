711ddbf67cca00cd10ba35dd46d05e33b35d16b2
petch <S5701012620195@email.kmutnb.ac.th>
2020-12-14 09:50:58 +0700

    select dataset to run

--- src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
@@ -36,39 +36,39 @@ from src.customer360.pipelines.data_engineering.nodes.complaints_nodes.to_l1.to_
 def complaints_to_l1_pipeline(**kwargs):
     return Pipeline(
         [
-            node(
-                dac_for_complaints_to_l1_pipeline,
-                ["l0_usage_call_relation_sum_daily_for_l1_complaints_call_to_competitor_features",
-                 "l1_customer_profile_union_daily_feature_for_l1_complaints_call_to_competitor_features",
-                 "params:l1_complaints_call_to_competitor_features_tbl",
-                 "params:exception_partition_list_for_l0_usage_call_relation_sum_daily"],
-                ["int_l0_usage_call_relation_sum_daily_for_l1_complaints_call_to_competitor_features",
-                 "int_l1_customer_profile_union_daily_feature_for_l1_complaints_call_to_competitor_features"]
-            ),
-            node(
-                l1_massive_processing,
-                ["int_l0_usage_call_relation_sum_daily_for_l1_complaints_call_to_competitor_features",
-                 "params:l1_complaints_call_to_competitor_features",
-                 "int_l1_customer_profile_union_daily_feature_for_l1_complaints_call_to_competitor_features"],
-                "l1_complaints_call_to_competitor_features"
-            ),
+            # node(
+            #     dac_for_complaints_to_l1_pipeline,
+            #     ["l0_usage_call_relation_sum_daily_for_l1_complaints_call_to_competitor_features",
+            #      "l1_customer_profile_union_daily_feature_for_l1_complaints_call_to_competitor_features",
+            #      "params:l1_complaints_call_to_competitor_features_tbl",
+            #      "params:exception_partition_list_for_l0_usage_call_relation_sum_daily"],
+            #     ["int_l0_usage_call_relation_sum_daily_for_l1_complaints_call_to_competitor_features",
+            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_call_to_competitor_features"]
+            # ),
+            # node(
+            #     l1_massive_processing,
+            #     ["int_l0_usage_call_relation_sum_daily_for_l1_complaints_call_to_competitor_features",
+            #      "params:l1_complaints_call_to_competitor_features",
+            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_call_to_competitor_features"],
+            #     "l1_complaints_call_to_competitor_features"
+            # ),
 
-            node(
-                dac_for_complaints_to_l1_pipeline,
-                ["l0_complaints_acc_atsr_outbound_daily",
-                 "l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_call",
-                 "params:l1_complaints_nps_after_call_tbl",
-                 "params:exception_partition_list_for_l0_complaints_acc_atsr_outbound_daily"],
-                ["int_l0_complaints_acc_atsr_outbound_daily",
-                 "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_call"]
-            ),
-            node(
-                l1_massive_processing,
-                ["int_l0_complaints_acc_atsr_outbound_daily",
-                 "params:l1_complaints_nps_after_call",
-                 "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_call"],
-                "l1_complaints_nps_after_call"
-            ),
+            # node(
+            #     dac_for_complaints_to_l1_pipeline,
+            #     ["l0_complaints_acc_atsr_outbound_daily",
+            #      "l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_call",
+            #      "params:l1_complaints_nps_after_call_tbl",
+            #      "params:exception_partition_list_for_l0_complaints_acc_atsr_outbound_daily"],
+            #     ["int_l0_complaints_acc_atsr_outbound_daily",
+            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_call"]
+            # ),
+            # node(
+            #     l1_massive_processing,
+            #     ["int_l0_complaints_acc_atsr_outbound_daily",
+            #      "params:l1_complaints_nps_after_call",
+            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_call"],
+            #     "l1_complaints_nps_after_call"
+            # ),
 
             node(
                 dac_for_complaints_to_l1_pipeline,
@@ -86,28 +86,28 @@ def complaints_to_l1_pipeline(**kwargs):
                  "int_l1_customer_profile_union_daily_feature_for_l1_complaints_nps_after_store_visit"],
                 "l1_complaints_nps_after_store_visit"
             ),
-            node(
-                dac_for_complaints_to_l1_pipeline,
-                [
-                    "l0_streamig_ida_mobile_domain_summary_daily_for_customer_satisfaction",
-                    "l1_customer_profile_union_daily_feature_for_l1_complaints_traffic_to_dtac_web_resources",
-                    "params:l1_complaints_traffic_to_dtac_web_resources_tbl",
-                    "params:exception_partition_list_for_l0_streamig_ida_mobile_domain_summary_daily_for_customer_satisfaction",
-                ],
-                [
-                    "int_l0_streamig_ida_mobile_domain_summary_daily_for_customer_satisfaction",
-                    "int_l1_customer_profile_union_daily_feature_for_l1_complaints_traffic_to_dtac_web_resources"
-                ]
-            ),
-            node(
-                l1_massive_processing,
-                [
-                    "int_l0_streamig_ida_mobile_domain_summary_daily_for_customer_satisfaction",
-                    "params:l1_complaints_traffic_to_dtac_web_resources",
-                    "int_l1_customer_profile_union_daily_feature_for_l1_complaints_traffic_to_dtac_web_resources"
-                ],
-                "l1_complaints_traffic_to_dtac_web_resources"
-            ),
+            # node(
+            #     dac_for_complaints_to_l1_pipeline,
+            #     [
+            #         "l0_streamig_ida_mobile_domain_summary_daily_for_customer_satisfaction",
+            #         "l1_customer_profile_union_daily_feature_for_l1_complaints_traffic_to_dtac_web_resources",
+            #         "params:l1_complaints_traffic_to_dtac_web_resources_tbl",
+            #         "params:exception_partition_list_for_l0_streamig_ida_mobile_domain_summary_daily_for_customer_satisfaction",
+            #     ],
+            #     [
+            #         "int_l0_streamig_ida_mobile_domain_summary_daily_for_customer_satisfaction",
+            #         "int_l1_customer_profile_union_daily_feature_for_l1_complaints_traffic_to_dtac_web_resources"
+            #     ]
+            # ),
+            # node(
+            #     l1_massive_processing,
+            #     [
+            #         "int_l0_streamig_ida_mobile_domain_summary_daily_for_customer_satisfaction",
+            #         "params:l1_complaints_traffic_to_dtac_web_resources",
+            #         "int_l1_customer_profile_union_daily_feature_for_l1_complaints_traffic_to_dtac_web_resources"
+            #     ],
+            #     "l1_complaints_traffic_to_dtac_web_resources"
+            # ),
 
         ]
     )
