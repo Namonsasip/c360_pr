ae1fc17e9a1a0fb3a467886727da212e712417ac
ANEK KONGPEAM <anekkong@ais.co.th>
2021-02-18 17:30:51 +0700

    test show

--- src/customer360/pipelines/data_engineering/nodes/revenue_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/revenue_nodes/to_l3/to_l3_nodes.py
@@ -140,5 +140,6 @@ def dac_for_revenue_l0_to_l3_intermediate_non_profile_pipeline(source_df: DataFr
         return get_spark_empty_df()
     source_df = source_df.withColumn("start_of_month",
                                       F.to_date(F.col('partition_month').cast('string'), "yyyyMM"))
+    source_df.limit(10).show()
     return source_df
 
