f89329278592debf1decfb060aec028148008004
sarit boonchusanong <saritboo@ais.co.th>
2021-03-04 17:05:21 +0700

    l1_device_summary_customer_handset_daily

--- src/customer360/pipelines/data_engineering/nodes/device_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/device_nodes/to_l1/to_l1_nodes.py
@@ -50,3 +50,40 @@ def generate_l1_layer(device_df: DataFrame
     final_df = final_df.filter(f.col("subscription_identifier").isNotNull()).distinct()
 
     return final_df
+
+def l1_device_summary_customer_handset_daily(device_df: DataFrame
+                      , customer_df: DataFrame) -> DataFrame:
+
+    ################################# Start Implementing Data availability checks #############################
+    if check_empty_dfs([device_df, customer_df]):
+        return get_spark_empty_df()
+
+    device_df = data_non_availability_and_missing_check(df=device_df, grouping="daily", par_col="partition_date",
+                                                       target_table_name="l1_devices_summary_customer_handset_daily")
+
+    customer_df = data_non_availability_and_missing_check(df=customer_df, grouping="daily", par_col="event_partition_date",
+                                                        target_table_name="l1_devices_summary_customer_handset_daily")
+
+    if check_empty_dfs([device_df, customer_df]):
+        return get_spark_empty_df()
+
+    ################################# End Implementing Data availability checks ###############################
+
+
+    device_df = device_df.filter(f.col("mobile_no").isNotNull())
+    device_df = device_df.withColumn("event_partition_date", f.to_date('date_id')) \
+        .withColumn("start_of_week", f.to_date(f.date_trunc('week', "date_id"))) \
+        .withColumnRenamed("mobile_no", "access_method_num")
+
+    join_cols = ['access_method_num',
+                 'event_partition_date']
+
+    sel_cols = ['access_method_num',
+                'event_partition_date',
+                "subscription_identifier",
+                "start_of_week"]
+
+    final_df = device_df.join(customer_df.select(sel_cols), join_cols)
+    final_df = final_df.filter(f.col("subscription_identifier").isNotNull()).distinct()
+
+    return final_df
--- src/customer360/pipelines/data_engineering/pipelines/device_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/device_pipeline/to_l1/to_l1_pipeline.py
@@ -1,15 +1,23 @@
 from kedro.pipeline import Pipeline, node
 
-from customer360.pipelines.data_engineering.nodes.device_nodes.to_l1.to_l1_nodes import generate_l1_layer
+from customer360.pipelines.data_engineering.nodes.device_nodes.to_l1.to_l1_nodes import *
 
 
 def device_to_l1_pipeline(**kwargs):
     return Pipeline(
         [
+            # node(
+            #     generate_l1_layer,
+            #     ["l0_devices_summary_customer_handset", "l1_customer_profile_union_daily_feature_for_device"],
+            #     "l1_devices_summary_customer_handset_daily"
+            # ),
+
             node(
-                generate_l1_layer,
-                ["l0_devices_summary_customer_handset", "l1_customer_profile_union_daily_feature_for_device"],
-                "l1_devices_summary_customer_handset_daily"
+                l1_device_summary_customer_handset_daily,
+                ["l0_device_summary_customer_handset_for_l1_device_summary_customer_handset_daily",
+                 "l1_customer_profile_union_daily_feature_for_device_for_l1_device_summary_customer_handset_daily",
+                 ],
+                "l1_device_summary_customer_handset_daily"
             ),
         ], name="device_to_l1_pipeline"
     )
