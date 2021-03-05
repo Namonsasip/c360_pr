88952317abf0603e75fb871a85ce36da744e4c91
str1k <y.thanasit@gmail.com>
2021-03-05 12:51:10 +0700

    add function to update dataupsell control group

--- src/du/reporting/du_report_pipeline.py
+++ src/du/reporting/du_report_pipeline.py
@@ -27,22 +27,22 @@ def create_du_weekly_revenue_uplift_report_pipeline() -> Pipeline:
                 name="l5_du_weekly_revenue_uplift_report_overall_contacted",
                 tags=["l5_du_weekly_revenue_uplift_report_overall_contacted",],
             ),
-            node(
-                partial(
-                    l5_du_weekly_revenue_uplift_report_contacted_only,
-                    control_group_initialize_profile_date="2020-08-01",
-                ),
-                inputs={
-                    "l4_revenue_prepaid_daily_features": "l4_revenue_prepaid_daily_features",
-                    "l0_du_pre_experiment3_groups": "l0_du_pre_experiment5_groups",
-                    "l3_customer_profile_union_monthly_feature_full_load": "l3_customer_profile_union_monthly_feature_full_load",
-                    "l0_campaign_tracking_contact_list_pre_full_load": "l0_campaign_tracking_contact_list_pre_full_load",
-                    "l0_product_pru_m_ontop_master_for_weekly_full_load":"l0_product_pru_m_ontop_master_for_weekly_full_load",
-                    "dm42_promotion_prepaid":"dm42_promotion_prepaid",
-                },
-                outputs="l5_du_weekly_revenue_uplift_report_contacted_only",
-                name="create_du_weekly_revenue_uplift_report",
-                tags=["create_du_weekly_revenue_uplift_report",],
-            ),
+            # node(
+            #     partial(
+            #         l5_du_weekly_revenue_uplift_report_contacted_only,
+            #         control_group_initialize_profile_date="2020-08-01",
+            #     ),
+            #     inputs={
+            #         "l4_revenue_prepaid_daily_features": "l4_revenue_prepaid_daily_features",
+            #         "l0_du_pre_experiment3_groups": "l0_du_pre_experiment5_groups",
+            #         "l3_customer_profile_union_monthly_feature_full_load": "l3_customer_profile_union_monthly_feature_full_load",
+            #         "l0_campaign_tracking_contact_list_pre_full_load": "l0_campaign_tracking_contact_list_pre_full_load",
+            #         "l0_product_pru_m_ontop_master_for_weekly_full_load":"l0_product_pru_m_ontop_master_for_weekly_full_load",
+            #         "dm42_promotion_prepaid":"dm42_promotion_prepaid",
+            #     },
+            #     outputs="l5_du_weekly_revenue_uplift_report_contacted_only",
+            #     name="create_du_weekly_revenue_uplift_report",
+            #     tags=["create_du_weekly_revenue_uplift_report",],
+            # ),
         ]
     )
