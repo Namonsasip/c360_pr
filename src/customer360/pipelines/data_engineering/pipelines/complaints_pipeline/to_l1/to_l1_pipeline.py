993081a0c2bbcbd1eac4727ff4b0b9c443388bde
kulmanec <kulmanec@ais.co.th>
2021-03-16 16:20:39 +0700

    l1_complaints_call_to_competitor_features

--- src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
@@ -30,7 +30,19 @@ from kedro.pipeline import Pipeline, node
 from customer360.utilities.re_usable_functions import l1_massive_processing
 
 from src.customer360.pipelines.data_engineering.nodes.complaints_nodes.to_l1.to_l1_nodes import \
-    dac_for_complaints_to_l1_pipeline
+    *
+
+def complaints_to_l1_pipeline_training(**kwargs):
+    return Pipeline(
+        [
+            node(
+                l1_complaints_ai_chatbot_training_survey_trianing,
+                ["l0_complaints_ai_chatbot_training_survey_trianing",
+                 "params:l1_complaints_call_to_competitor_features_tbl"],
+                ["l1_complaints_ai_chatbot_training_survey_trianing"]
+            ),
+        ]
+    )
 
 
 def complaints_to_l1_pipeline(**kwargs):
