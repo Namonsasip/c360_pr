26a2c8c873a640bd1a13fc653cc791ee93f20601
paitn571@epicc.postbox.in.th <az@sXX23>
2021-03-16 18:02:44 +0700

    eidt

--- src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
@@ -38,7 +38,7 @@ def complaints_to_l1_pipeline_tanining(**kwargs):
             node(
                 l1_complaints_ai_chatbot_survey_training,
                 ["l0_complaints_acc_ai_chatbot_survey_training",
-                 "params:l1_complaints_acc_ai_chatbot_survey_training"]
+                 "params:l1_complaints_ai_chatbot_survey_training"]
                 ,"l1_complaints_acc_ai_chatbot_survey_training"
             ),
 
