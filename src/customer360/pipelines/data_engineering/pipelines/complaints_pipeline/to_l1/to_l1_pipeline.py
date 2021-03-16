03a0a12a83eb40920e7b4d06e08ddcda427bebcf
Detchanin Thongarsa <detcht96@wintl.postbox.in.th>
2021-03-16 16:22:44 +0700

    2021-03-16

--- src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
@@ -30,8 +30,19 @@ from kedro.pipeline import Pipeline, node
 from customer360.utilities.re_usable_functions import l1_massive_processing
 
 from src.customer360.pipelines.data_engineering.nodes.complaints_nodes.to_l1.to_l1_nodes import \
-    dac_for_complaints_to_l1_pipeline
+    *
 
+def complaints_to_l1_pipeline_training(**kwargs):
+    return Pipeline(
+        [
+        node(
+            l1_complaints_ai_chatbot_survey_training,
+            ["l0_complaints_ai_chatbot_survey_training",
+             "params:l1_complaints_ai_chatbot_survey_training"],
+            ["l1_complaints_ai_chatbot_survey_training"]
+            )
+        ]
+    )
 
 def complaints_to_l1_pipeline(**kwargs):
     return Pipeline(
