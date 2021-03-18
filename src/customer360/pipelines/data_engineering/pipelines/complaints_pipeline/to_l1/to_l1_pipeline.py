88f632cd29a5c448199761059f9452b40ac75ccb
sarit boonchusanong <saritboo@ais.co.th>
2021-03-18 15:43:55 +0700

    dev l1_complaints_shop_training

--- src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
@@ -36,6 +36,16 @@ def complaints_to_l1_pipeline_training(**kwargs):
     return Pipeline(
         [
             node(
+                l1_complaints_shop_training,
+                ["l0_complaints_shop_training",  #input1
+                 "l1_customer_profile_training", #input2
+                 ],
+                "l1_complaints_shop_training"  #output1
+            ),
+
+
+
+            node(
                 l1_complaints_ai_chatbot_survey_training,
                 ["l0_complaints_ai_chatbot_survey_training",
                  "params:l1_complaints_ai_chatbot_survey_training"],
