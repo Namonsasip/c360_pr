2d73d0b3a1f057e2b8274e9cc8f532e37bf7a21d
Thatchapong Tunkam <64589464+BenzThatchapong@users.noreply.github.com>
2021-03-18 17:59:26 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/complaints_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/complaints_nodes/to_l1/to_l1_nodes.py
@@ -69,10 +69,10 @@ group by partition_date,access_method_num
     df = df.withColumn("event_partition_date", f.to_date((f.col("partition_date")).cast(StringType()), 'yyyyMMdd'))\
         .withColumn("start_of_week", f.to_date(f.date_trunc('week', 'event_partition_date')))\
         .withColumn("start_of_month", f.to_date(f.date_trunc('month', 'event_partition_date')))
-
-    cond = [df.access_method_num == input_cust.access_method_num,
-            df.event_partition_date == input_cust.event_partition_date]
-    df_output = df.join(input_cust, cond, 'left').drop(input_cust.access_method_num, input_cust.event_partition_date)
+    #
+    # cond = [df.access_method_num == input_cust.access_method_num,
+    #         df.event_partition_date == input_cust.event_partition_date]
+    df_output = df.join(input_cust, ['access_method_num','event_partition_date'], 'left')
 
     return df_output
 
