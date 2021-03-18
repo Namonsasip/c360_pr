9ff13b7622706e1db76cb17f99919107617c7e69
wijitrpn <wijitrpn@ais.co.th>
2021-03-18 12:21:00 +0700

    update error

--- src/customer360/pipelines/data_engineering/nodes/complaints_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/complaints_nodes/to_l1/to_l1_nodes.py
@@ -5,7 +5,7 @@ from pyspark.sql import functions as f, DataFrame
 from src.customer360.utilities.spark_util import get_spark_empty_df
 from pyspark.sql.types import *
 
-def complaints_to_l1_pipeline_training(input,output):
+def l1_complaints_ai_chatbot_survey_training(input,output):
     df = node_from_config(input_df, config)
     return df
 
