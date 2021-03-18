f407d3ea319c38737daf38556c091adbc3a514c3
wiroatsa <wiroatsa@ais.co.th>
2021-03-18 16:17:26 +0700

    Training C360 Day3 -- add l0_complaints_shop_survey_training -- add l1_complaints_survey_after_store_visit -- add l1_customer_profile_training -- add getSurveyScoreNumber -- add l1_complaints_survey_after_store_visit -- add node l1_complaints_survey_after_store_visit

--- src/customer360/pipelines/data_engineering/nodes/complaints_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/complaints_nodes/to_l1/to_l1_nodes.py
@@ -2,7 +2,7 @@ from customer360.utilities.config_parser import node_from_config
 from customer360.utilities.re_usable_functions import union_dataframes_with_missing_cols, check_empty_dfs, \
     data_non_availability_and_missing_check, add_start_of_week_and_month
 from pyspark.sql import functions as f, DataFrame
-from src.customer360.utilities.spark_util import get_spark_empty_df, get_spark_session
+from src.customer360.utilities.spark_util import *
 from pyspark.sql.types import *
 
 
