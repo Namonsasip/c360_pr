970215ac028b1f05cb7245557773c9ad5b64a764
urachap8 <65007976+urachap8@users.noreply.github.com>
2021-03-18 17:29:46 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/complaints_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/complaints_nodes/to_l1/to_l1_nodes.py
@@ -35,11 +35,11 @@ def add_start_of_week_and_month_from_yyyymmdd(df, param):
     pass
 
 
-def l1_complaints_shop_training(input_complaints, input_cust, stmt_full=None):
+def l1_complaints_shop_training(input_complaints, input_cust):
     input_cust = input_cust.select('subscription_status', 'access_method_num', 'subscription_identifier',
                                    'event_partition_date')
     spark = get_spark_session()
-    spark.udf.register("getSurveyScoreNumber", getSurveyScoreNumber)
+    # spark.udf.register("getSurveyScoreNumber", getSurveyScoreNumber)
     input_complaints.registerTempTable("complaints_acc_qmt_csi")
     stmt = """
     select partition_date,access_method_num
@@ -74,7 +74,7 @@ and access_method_num is not null
 )
 group by partition_date,access_method_num
     """
-    df = spark.sql(stmt_full)
+    df = spark.sql(stmt)
     df = add_start_of_week_and_month_from_yyyymmdd(df, 'partition_date')
     
     df_output = df.join(input_cust, ['access_method_num','event_partition_date'], 'left')
