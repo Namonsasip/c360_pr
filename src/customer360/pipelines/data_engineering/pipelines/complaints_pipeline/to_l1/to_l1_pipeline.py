cd0ba77b4224bdbb70e636c12e53a049e78193d1
Thatchapong Tunkam <64589464+BenzThatchapong@users.noreply.github.com>
2021-03-18 16:56:11 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
@@ -36,12 +36,12 @@ from src.customer360.pipelines.data_engineering.nodes.complaints_nodes.to_l1.to_
 def complaints_to_l1_pipeline_training(**kwargs):
     return Pipeline(
         [
-            node(
-                l1_complaints_ai_chatbot_survey_training,
-                ["l0_complaints_ai_chatbot_survey_training",
-                 "params:l1_complaints_ai_chatbot_survey_training"],
-                 "l1_complaints_ai_chatbot_survey_training"
-            ),
+            # node(
+            #     l1_complaints_ai_chatbot_survey_training,
+            #     ["l0_complaints_ai_chatbot_survey_training",
+            #      "params:l1_complaints_ai_chatbot_survey_training"],
+            #      "l1_complaints_ai_chatbot_survey_training"
+            # ),
             node(
                 l1_complaints_shop_training,
                 ["l0_complaints_shop_training",  #input1
