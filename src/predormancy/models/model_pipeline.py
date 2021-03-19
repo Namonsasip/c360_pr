8408da41fb32d6c5633b68d6a8d533b210045529
str1k <y.thanasit@gmail.com>
2021-03-19 11:07:43 +0700

    change train date

--- src/predormancy/models/model_pipeline.py
+++ src/predormancy/models/model_pipeline.py
@@ -8,7 +8,7 @@ def create_predorm_model_pipeline() -> Pipeline:
     return Pipeline(
         [
             node(
-                partial(train_predormancy_model, train_date="20200210"),
+                partial(train_predormancy_model, train_date="20200319"),
                 inputs={"l5_predorm_master_table": "l5_predorm_master_table",},
                 outputs="l5_predormancy_imporatant_variable",
                 name="train_predormancy_model",
