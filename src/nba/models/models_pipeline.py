3cce00b8eec54de6fd94c294807262a6fbc47422
Thanasit Yiamwinya <thanasiy@ais.co.th>
2021-03-22 14:26:32 +0700

    Revert "update pipeline"
    This reverts commit bff1febabc5983646ddb57710b718160b263d2d5

--- src/nba/models/models_pipeline.py
+++ src/nba/models/models_pipeline.py
@@ -142,34 +142,34 @@ def create_nba_models_pipeline() -> Pipeline:
                 name="nba_arpu_30d_models_training",
                 tags=["nba_arpu_30d_models_training", "nba_models"],
             ),
-            # node(
-            #     partial(
-            #         train_multiple_models,
-            #         model_type="regression",
-            #         pai_run_prefix=(
-            #             f"{get_local_datetime().strftime('%Y%m%d_%H%M%S')}_"
-            #             f"arpu_7d_"
-            #             f"{getpass.getuser()[0:6]}_"
-            #         ),
-            #     ),
-            #     inputs={
-            #         "df_master": "l5_nba_master_table_only_accepted",
-            #         "group_column": "params:nba_model_group_column",
-            #         "explanatory_features": "params:nba_model_explanatory_features",
-            #         "target_column": "params:nba_arpu_7d_model_target_column",
-            #         "train_sampling_ratio": "params:nba_model_train_sampling_ratio",
-            #         "model_params": "params:nba_model_model_params",
-            #         "max_rows_per_group": "params:nba_model_max_rows_per_group",
-            #         "min_obs_per_class_for_model": "params:nba_model_min_obs_per_class_for_model",
-            #         "extra_keep_columns": "params:nba_extra_tag_columns_pai",
-            #         "pai_runs_uri": "params:nba_pai_runs_uri",
-            #         "pai_artifacts_uri": "params:nba_pai_artifacts_uri",
-            #         "regression_clip_target_quantiles": "params:regression_clip_target_quantiles_arpu",
-            #     },
-            #     outputs="nba_arpu_7d_models_train_set",
-            #     name="nba_arpu_7d_models_training",
-            #     tags=["nba_arpu_7d_models_training", "nba_models"],
-            # ),
+            node(
+                partial(
+                    train_multiple_models,
+                    model_type="regression",
+                    pai_run_prefix=(
+                        f"{get_local_datetime().strftime('%Y%m%d_%H%M%S')}_"
+                        f"arpu_7d_"
+                        f"{getpass.getuser()[0:6]}_"
+                    ),
+                ),
+                inputs={
+                    "df_master": "l5_nba_master_table_only_accepted",
+                    "group_column": "params:nba_model_group_column",
+                    "explanatory_features": "params:nba_model_explanatory_features",
+                    "target_column": "params:nba_arpu_7d_model_target_column",
+                    "train_sampling_ratio": "params:nba_model_train_sampling_ratio",
+                    "model_params": "params:nba_model_model_params",
+                    "max_rows_per_group": "params:nba_model_max_rows_per_group",
+                    "min_obs_per_class_for_model": "params:nba_model_min_obs_per_class_for_model",
+                    "extra_keep_columns": "params:nba_extra_tag_columns_pai",
+                    "pai_runs_uri": "params:nba_pai_runs_uri",
+                    "pai_artifacts_uri": "params:nba_pai_artifacts_uri",
+                    "regression_clip_target_quantiles": "params:regression_clip_target_quantiles_arpu",
+                },
+                outputs="nba_arpu_7d_models_train_set",
+                name="nba_arpu_7d_models_training",
+                tags=["nba_arpu_7d_models_training", "nba_models"],
+            ),
         ],
         tags="nba_models_pipeline",
     )
