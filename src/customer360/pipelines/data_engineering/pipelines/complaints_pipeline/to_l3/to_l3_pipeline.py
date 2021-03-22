abdbb4de969e1e3bb2624922a4bd8f6ba02d3f6d
urachap8 <65007976+urachap8@users.noreply.github.com>
2021-03-22 15:45:58 +0700

    2021/03/2021

--- src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l3/to_l3_pipeline.py
@@ -79,3 +79,25 @@ def complaints_to_l3_pipeline(**kwargs):
 
         ]
     )
+
+def complaints_to_l3_pipeline_training(**kwargs):
+    return Pipeline(
+        [
+            # Option: 02 create 2 nodes
+            node(
+                l3_massive_processing,
+                ["l1_complaints_training_for_l3_complaints_training",
+                 "params:l3_complaints_training"
+                 ],
+                "int_l3_complaints_training"
+            ),
+            node(
+                l3_complaints_training,
+                ["int_l3_complaints_training",
+                 "l3_customer_profile_union_monthly_for_l3_complaints_training"
+                 ],
+                "l3_complaints_training"
+            ),
+        ]
+    )
+
