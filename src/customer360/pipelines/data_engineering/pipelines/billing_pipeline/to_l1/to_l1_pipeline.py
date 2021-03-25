90c84e54692d5187e6297321e18eca73a58a994c
Thatchapong Tunkam <64589464+BenzThatchapong@users.noreply.github.com>
2021-03-25 21:07:59 +0700

    v1.2

--- src/customer360/pipelines/data_engineering/pipelines/billing_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/billing_pipeline/to_l1/to_l1_pipeline.py
@@ -14,53 +14,53 @@ def billing_to_l1_pipeline(**kwargs):
                  "params:l1_billing_and_payment_feature_top_up_and_count"],
                 "l1_billing_and_payments_daily_topup_and_volume"
             ),
-            #
-            # # Average revenue for roaming feature post-paid
-            # node(
-            #     billing_daily_rpu_roaming,
-            #     ["l0_billing_ir_a_usg_daily", "l1_customer_profile_union_daily_feature_for_l1_billing_and_payments_daily_rpu_roaming",
-            #      "params:l1_billing_and_payment_rpu_roaming"],
-            #     "l1_billing_and_payments_daily_rpu_roaming"
-            # ),
-            #
-            # # Balance before top up feature pre-paid
-            # node(
-            #     billing_before_topup_balance,
-            #     ["l0_billing_sa_t_account_recharge_daily", "l1_customer_profile_union_daily_feature_for_l1_billing_and_payments_daily_before_top_up_balance",
-            #      "params:l1_billing_and_payment_before_top_up_balance"],
-            #     "l1_billing_and_payments_daily_before_top_up_balance"
-            # ),
-            #
-            # # Top up channels feature pre-paid
-            # node(
-            #     billing_topup_channels,
-            #     ["l0_billing_and_payments_rt_t_recharge_daily_for_l1_billing_and_payments_daily_top_up_channels", "l1_customer_profile_union_daily_feature_for_l1_billing_and_payments_daily_top_up_channels",
-            #      "params:l1_billing_and_payment_top_up_channels"],
-            #     "l1_billing_and_payments_daily_top_up_channels"
-            # ),
-            #
-            # # Most popular top up channel feature pre-paid
-            # node(
-            #     billing_most_popular_topup_channel,
-            #     ["l0_billing_and_payments_rt_t_recharge_daily_for_l1_billing_and_payments_daily_most_popular_top_up_channel", "l1_customer_profile_union_daily_feature_for_l1_billing_and_payments_daily_most_popular_top_up_channel",
-            #      "params:l1_billing_and_payment_most_popular_topup_channel"],
-            #     "l1_billing_and_payments_daily_most_popular_top_up_channel"
-            # ),
-            #
-            # # Popular top up day and hour feature pre-paid
-            # node(
-            #     billing_popular_topup_day_hour,
-            #     ["l0_billing_and_payments_rt_t_recharge_daily_for_l1_billing_and_payments_daily_popular_topup_day", "l1_customer_profile_union_daily_feature_for_l1_billing_and_payments_daily_popular_topup_day",
-            #      "params:l1_billing_and_payment_popular_topup_day"],
-            #     "l1_billing_and_payments_daily_popular_topup_day"
-            # ),
-            #
-            # # Time since last top up feature pre-paid
-            # node(
-            #     billing_time_since_last_topup,
-            #     ["l0_billing_and_payments_rt_t_recharge_daily_for_l1_billing_and_payments_daily_time_since_last_top_up", "l1_customer_profile_union_daily_feature_for_l1_billing_and_payments_daily_time_since_last_top_up",
-            #      "params:l1_time_since_last_top_up"],
-            #     "l1_billing_and_payments_daily_time_since_last_top_up"
-            # ),
+
+            # Average revenue for roaming feature post-paid
+            node(
+                billing_daily_rpu_roaming,
+                ["l0_billing_ir_a_usg_daily", "l1_customer_profile_union_daily_feature_for_l1_billing_and_payments_daily_rpu_roaming",
+                 "params:l1_billing_and_payment_rpu_roaming"],
+                "l1_billing_and_payments_daily_rpu_roaming"
+            ),
+
+            # Balance before top up feature pre-paid
+            node(
+                billing_before_topup_balance,
+                ["l0_billing_sa_t_account_recharge_daily", "l1_customer_profile_union_daily_feature_for_l1_billing_and_payments_daily_before_top_up_balance",
+                 "params:l1_billing_and_payment_before_top_up_balance"],
+                "l1_billing_and_payments_daily_before_top_up_balance"
+            ),
+
+            # Top up channels feature pre-paid
+            node(
+                billing_topup_channels,
+                ["l0_billing_and_payments_rt_t_recharge_daily_for_l1_billing_and_payments_daily_top_up_channels", "l1_customer_profile_union_daily_feature_for_l1_billing_and_payments_daily_top_up_channels",
+                 "params:l1_billing_and_payment_top_up_channels"],
+                "l1_billing_and_payments_daily_top_up_channels"
+            ),
+
+            # Most popular top up channel feature pre-paid
+            node(
+                billing_most_popular_topup_channel,
+                ["l0_billing_and_payments_rt_t_recharge_daily_for_l1_billing_and_payments_daily_most_popular_top_up_channel", "l1_customer_profile_union_daily_feature_for_l1_billing_and_payments_daily_most_popular_top_up_channel",
+                 "params:l1_billing_and_payment_most_popular_topup_channel"],
+                "l1_billing_and_payments_daily_most_popular_top_up_channel"
+            ),
+
+            # Popular top up day and hour feature pre-paid
+            node(
+                billing_popular_topup_day_hour,
+                ["l0_billing_and_payments_rt_t_recharge_daily_for_l1_billing_and_payments_daily_popular_topup_day", "l1_customer_profile_union_daily_feature_for_l1_billing_and_payments_daily_popular_topup_day",
+                 "params:l1_billing_and_payment_popular_topup_day"],
+                "l1_billing_and_payments_daily_popular_topup_day"
+            ),
+
+            # Time since last top up feature pre-paid
+            node(
+                billing_time_since_last_topup,
+                ["l0_billing_and_payments_rt_t_recharge_daily_for_l1_billing_and_payments_daily_time_since_last_top_up", "l1_customer_profile_union_daily_feature_for_l1_billing_and_payments_daily_time_since_last_top_up",
+                 "params:l1_time_since_last_top_up"],
+                "l1_billing_and_payments_daily_time_since_last_top_up"
+            ),
         ]
     )
