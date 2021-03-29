648bf357ad0681c5d963c70a51b1999cd9f21d61
Thatchapong Tunkam <64589464+BenzThatchapong@users.noreply.github.com>
2021-03-29 14:52:15 +0700

    v2

--- src/customer360/pipelines/data_engineering/nodes/customer_profile_nodes/to_l2/to_l2_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/customer_profile_nodes/to_l2/to_l2_nodes.py
@@ -14,17 +14,17 @@ def union_weekly_cust_profile(
 
 ):
     ################################# Start Implementing Data availability checks #############################
-    # if check_empty_dfs([cust_prof_daily_df]):
-    #     return get_spark_empty_df()
-    #
-    # cust_prof_daily_df = data_non_availability_and_missing_check(df=cust_prof_daily_df, grouping="weekly",
-    #                                                              par_col="event_partition_date",
-    #                                                              missing_data_check_flg='Y',
-    #                                                              target_table_name="l2_customer_profile_union_weekly_feature",
-    #                                                              exception_partitions=exception_partition)
-    #
-    # if check_empty_dfs([cust_prof_daily_df]):
-    #     return get_spark_empty_df()
+    if check_empty_dfs([cust_prof_daily_df]):
+        return get_spark_empty_df()
+
+    cust_prof_daily_df = data_non_availability_and_missing_check(df=cust_prof_daily_df, grouping="weekly",
+                                                                 par_col="event_partition_date",
+                                                                 missing_data_check_flg='Y',
+                                                                 target_table_name="l2_customer_profile_union_weekly_feature",
+                                                                 exception_partitions=exception_partition)
+
+    if check_empty_dfs([cust_prof_daily_df]):
+        return get_spark_empty_df()
 
     ################################# End Implementing Data availability checks ###############################
 
