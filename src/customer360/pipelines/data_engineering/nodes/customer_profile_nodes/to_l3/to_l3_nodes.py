47c1452242784473e53c504f7df46f9ccd92c9ec
ChaninChaisatianQB <66584647+ChaninChaisatianQB@users.noreply.github.com>
2021-03-29 11:35:10 +0700

    Update to_l3_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/customer_profile_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/customer_profile_nodes/to_l3/to_l3_nodes.py
@@ -282,8 +282,9 @@ def df_smp_for_l3_customer_profile_include_1mo_non_active(journy_df: DataFrame, 
     #                                                  target_table_name="l3_customer_profile_include_1mo_non_active",
     #                                                  missing_data_check_flg='N')
 
-    journy_df.createOrReplaceTempView('journey')
-    smp_df.createOrReplaceTempView('smp')
+
+    journy_df.createOrReplaceTempView("journey")
+    smp_df.createOrReplaceTempView("smp")
 
     # amendment_reason_code_previous
     df1 = spark.sql("""
