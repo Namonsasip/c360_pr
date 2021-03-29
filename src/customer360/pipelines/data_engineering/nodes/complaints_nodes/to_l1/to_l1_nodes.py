d70e7700e95851a37dae19b4dd191aaa93ab0127
wiroatsa <wiroatsa@ais.co.th>
2021-03-29 14:52:27 +0700

    - Create l1_complaints_survey_after_chatbot

--- src/customer360/pipelines/data_engineering/nodes/complaints_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/complaints_nodes/to_l1/to_l1_nodes.py
@@ -206,19 +206,6 @@ def l1_complaints_survey_after_chatbot(input_df, config, cust_df):
         return get_spark_empty_df()
 
     output_df = node_from_config(input_df, config)
-    list_result_columns = output_df.columns
-    list_result_columns.remove('event_partition_date')
-    list_result_columns.remove('start_of_month')
-    list_result_columns.remove('start_of_week')
-    list_result_columns.remove('partition_date')
-    result_df = output_df.join(cust_df, [output_df.mobile_no == cust_df.access_method_num,
-                                         output_df.event_partition_date == cust_df.event_partition_date], 'left').select(
-        cust_df.access_method_num,
-        cust_df.subscription_identifier,
-        *list_result_columns,
-        output_df.event_partition_date,
-        output_df.start_of_week,
-        output_df.start_of_month
-    )
+    result_df = output_df
 
     return result_df
