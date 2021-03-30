62603bfae49c49b835be283030b631906d7ec402
Thatchapong Tunkam <64589464+BenzThatchapong@users.noreply.github.com>
2021-03-30 09:55:25 +0700

    Update kedro_run.py

--- kedro_run.py
+++ kedro_run.py
@@ -12,32 +12,6 @@ kedro_run_cmd = "kedro run --pipeline={} --env={}".format(pipeline_to_run, env_t
 
 print(kedro_run_cmd)
 
-
-# def run_command(command):
-#    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, cwd='/home/cdsw/customer360/', encoding='utf8')
-#    with open("logfile.txt", "w") as log_file:
-#        while process.poll() is None:
-#            output = process.stdout.readline()
-#            if output == '' and process.poll() is not None:
-#                break
-#            if output:
-#                print(output.strip())
-#                log_file.write(output)
-# 
-# run_command(kedro_run_cmd)
-
-# import subprocess
-# import shlex
-# import os
-# 
-# pipeline_to_run = os.environ["pipeline_to_run"]
-# env_to_use = os.environ["CONF"]
-# 
-# kedro_run_cmd = "kedro run --pipeline={} --env={}".format(pipeline_to_run, env_to_use)
-# 
-# print(kedro_run_cmd)
-# 
-# 
 def run_command(command):
     process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE,
                                cwd='/home/cdsw/customer360/', encoding='utf8', bufsize=-1)
