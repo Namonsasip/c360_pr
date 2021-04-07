e3120da940827852bbcb82a2edb0ddb3d0bd037a
paitn571@epicc.postbox.in.th <az@sXX23>
2021-04-07 14:58:28 +0700

    update pipeline

--- src/customer360/pipeline.py
+++ src/customer360/pipeline.py
@@ -102,6 +102,7 @@ from .pipelines.data_engineering.pipelines.campaign_pipeline import (
 from .pipelines.data_engineering.pipelines.complaints_pipeline.to_l1.to_l1_pipeline import (
     complaints_to_l1_pipeline,
     complaints_to_l1_pipeline_survey,
+
 )
 from .pipelines.data_engineering.pipelines.complaints_pipeline.to_l2.to_l2_pipeline import (
     complaints_to_l2_pipeline,
@@ -178,7 +179,7 @@ from .pipelines.data_engineering.pipelines.stream_pipeline.to_l4.to_l4_pipeline 
     streaming_l1_to_l4_pipeline,
 )
 from .pipelines.data_engineering.pipelines.touchpoints_pipeline.to_l1.to_l1_pipeline import (
-    touchpoints_to_l1_pipeline,
+    touchpoints_to_l1_pipeline,touchpoints_to_l1_pipeline_chatbot,
 )
 from .pipelines.data_engineering.pipelines.touchpoints_pipeline.to_l2.to_l2_pipeline import (
     touchpoints_to_l2_pipeline,
@@ -284,6 +285,7 @@ def create_c360_pipeline(**kwargs) -> Dict[str, Pipeline]:
         "complaints_to_l3_pipeline_survey": complaints_to_l3_pipeline_survey(),
         "complaints_to_l4_pipeline": complaints_to_l4_pipeline(),
         "touchpoints_to_l1_pipeline": touchpoints_to_l1_pipeline(),
+        "touchpoints_to_l1_pipeline_chatbot": touchpoints_to_l1_pipeline_chatbot(),
         "touchpoints_to_l2_pipeline": touchpoints_to_l2_pipeline(),
         "touchpoints_to_l3_pipeline": touchpoints_to_l3_pipeline(),
         "touchpoints_to_l4_pipeline": touchpoints_to_l4_pipeline(),
