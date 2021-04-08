9bce7bc6e8b91660ee9a7065d1b0077fbb995662
wiroatsa <wiroatsa@ais.co.th>
2021-04-08 11:31:13 +0700

    - Create l3_complaints_survey_after_chatbot

--- src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l3/to_l3_pipeline.py
@@ -33,23 +33,6 @@ from customer360.pipelines.data_engineering.nodes.complaints_nodes.to_l3.to_l3_n
 from src.customer360.pipelines.data_engineering.nodes.complaints_nodes.to_l3.to_l3_nodes import \
     run_for_complaints_to_l3_pipeline_from_l1
 
-
-def complaints_to_l3_pipeline_survey_test(**kwargs):
-    return Pipeline(
-        [
-            node(
-                run_for_complaints_to_l3_pipeline_from_l1,
-                ["l1_complaints_survey_after_chatbot_for_l3_complaints_survey_after_chatbot",
-                 "params:l3_complaints_survey_after_chatbot_tbl",
-                 "params:l3_complaints_survey_after_chatbot",
-                 "params:exception_partition_list_for_monthly_l3_complaints_survey_after_chatbot"
-                 ],
-                "l3_complaints_survey_after_chatbot"
-            ),
-        ]
-    )
-
-
 def complaints_to_l3_pipeline_survey(**kwargs):
     return Pipeline(
         [
@@ -68,7 +51,7 @@ def complaints_to_l3_pipeline_survey(**kwargs):
                 ["l1_complaints_survey_after_store_visit_for_l3_complaints_survey_after_store_visit",
                  "params:l3_complaints_survey_after_store_visit_tbl",
                  "params:l3_complaints_survey_after_store_visit",
-                 "exception_partition_list_for_monthly_l3_complaints_survey_after_store_visit"
+                 "params:exception_partition_list_for_monthly_l3_complaints_survey_after_store_visit"
                  ],
                 "l3_complaints_survey_after_store_visit"
             ),
@@ -78,7 +61,7 @@ def complaints_to_l3_pipeline_survey(**kwargs):
                 ["l1_complaints_survey_after_myais_for_l3_complaints_survey_after_myais",
                  "params:l3_complaints_survey_after_myais_tbl",
                  "params:l3_complaints_survey_after_myais",
-                 "exception_partition_list_for_monthly_l3_complaints_survey_after_myais"
+                 "params:exception_partition_list_for_monthly_l3_complaints_survey_after_myais"
                  ],
                 "l3_complaints_survey_after_myais"
             ),
@@ -88,7 +71,7 @@ def complaints_to_l3_pipeline_survey(**kwargs):
                 ["l1_complaints_survey_after_chatbot_for_l3_complaints_survey_after_chatbot",
                  "params:l3_complaints_survey_after_chatbot_tbl",
                  "params:l3_complaints_survey_after_chatbot",
-                 "exception_partition_list_for_monthly_l3_complaints_survey_after_chatbot"
+                 "params:exception_partition_list_for_monthly_l3_complaints_survey_after_chatbot"
                  ],
                 "l3_complaints_survey_after_chatbot"
             ),
