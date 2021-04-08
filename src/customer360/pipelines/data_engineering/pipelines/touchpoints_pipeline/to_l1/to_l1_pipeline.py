e13aa0b7566ebd2ecf53db9391539ee49b281810
paitn571@epicc.postbox.in.th <az@sXX23>
2021-04-08 11:21:02 +0700

    edit

--- src/customer360/pipelines/data_engineering/pipelines/touchpoints_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/touchpoints_pipeline/to_l1/to_l1_pipeline.py
@@ -36,15 +36,28 @@ from src.customer360.pipelines.data_engineering.nodes.touchpoints_nodes.to_l1.to
 def touchpoints_to_l1_pipeline_contact(**kwargs):
     return Pipeline(
         [
+
+            node(
+                dac_for_touchpoints_to_l1_intermediate_pipeline,
+                ["l0_online_acc_ai_chatbot_summary",
+                 "l1_customer_profile_union_daily_feature_for_l1_touchpoints_aunjai_chatbot_features",
+                 "params:l1_touchpoints_aunjai_chatbot_features_tbl",
+                 "params:exception_partition_list_for_l0_online_acc_ai_chatbot_summary_for_l1_touchpoints_aunjai_chatbot_features"],
+                ["int_l0_online_acc_ai_chatbot_summary",
+                 "int_l1_customer_profile_union_daily_feature_for_l1_touchpoints_aunjai_chatbot_features"]
+            ),
+
             node(
                 l1_touchpoints_aunjai_chatbot_features,
-                ["l0_online_acc_ai_chatbot_summary",
-                 "l1_customer_profile_union_daily_feature_for_l1_touchpoints_aunjai_chatbot_features"],
+                ["int_l0_online_acc_ai_chatbot_summary",
+                 "int_l1_customer_profile_union_daily_feature_for_l1_touchpoints_aunjai_chatbot_features"],
                  "l1_touchpoints_aunjai_chatbot_features"
             ),
 
         ]
     )
+
+
 
 
 def touchpoints_to_l1_pipeline(**kwargs):
