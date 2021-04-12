7844b859342c7381cb106e683e15c0b8a8c7b5d9
paitn571@epicc.postbox.in.th <az@sXX23>
2021-04-12 10:49:25 +0700

    edit pipeline

--- src/customer360/pipelines/data_engineering/pipelines/touchpoints_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/touchpoints_pipeline/to_l1/to_l1_pipeline.py
@@ -32,7 +32,7 @@ from customer360.utilities.config_parser import node_from_config
 from customer360.utilities.re_usable_functions import l1_massive_processing
 from src.customer360.pipelines.data_engineering.nodes.touchpoints_nodes.to_l1.to_l1_nodes import *
 
-def touchpoints_to_l1_pipeline_contact_v1(**kwargs):
+def touchpoints_to_l1_pipeline_contact_v2(**kwargs):
     return Pipeline(
         [
             node(
