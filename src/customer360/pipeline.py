0106d06db15f18e674e05df0c11e3aad2c7e2a9c
paitn571@epicc.postbox.in.th <az@sXX23>
2021-04-19 15:07:28 +0700

    sss

--- src/customer360/pipeline.py
+++ src/customer360/pipeline.py
@@ -180,7 +180,7 @@ from .pipelines.data_engineering.pipelines.stream_pipeline.to_l4.to_l4_pipeline 
     streaming_l1_to_l4_pipeline,
 )
 from .pipelines.data_engineering.pipelines.touchpoints_pipeline.to_l1.to_l1_pipeline import (
-    touchpoints_to_l1_pipeline,touchpoints_to_l1_pipeline_contact,touchpoints_to_l1_pipeline_contact_v2,
+    touchpoints_to_l1_pipeline,touchpoints_to_l1_pipeline_contact,touchpoints_to_l1_pipeline_contact_test,
 
 )
 from .pipelines.data_engineering.pipelines.touchpoints_pipeline.to_l2.to_l2_pipeline import (
@@ -290,7 +290,7 @@ def create_c360_pipeline(**kwargs) -> Dict[str, Pipeline]:
         "complaints_to_l4_pipeline": complaints_to_l4_pipeline(),
         "touchpoints_to_l1_pipeline": touchpoints_to_l1_pipeline(),
         "touchpoints_to_l1_pipeline_contact": touchpoints_to_l1_pipeline_contact(),
-        "touchpoints_to_l1_pipeline_contact_v2": touchpoints_to_l1_pipeline_contact_v2(),
+        "touchpoints_to_l1_pipeline_contact_test": touchpoints_to_l1_pipeline_contact_test(),
         "touchpoints_to_l2_pipeline": touchpoints_to_l2_pipeline(),
         "touchpoints_to_l3_pipeline": touchpoints_to_l3_pipeline(),
         "touchpoints_to_l3_pipeline_contact": touchpoints_to_l3_pipeline_contact(),
