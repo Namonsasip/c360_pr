b56f77e22b2a63d2f3e713042a7eded5cb0b49fd
paitn571@epicc.postbox.in.th <az@sXX23>
2021-04-19 14:32:04 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/touchpoints_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/touchpoints_pipeline/to_l1/to_l1_pipeline.py
@@ -47,15 +47,7 @@ def touchpoints_to_l1_pipeline_contact_v2(**kwargs):
             node(
                 l1_touchpoints_contact_myais_features,
                 ["l0_touchpoints_myais_distinct_sub_daily",
-                 "l1_touchpoints_contact_myais_features"
-                 ],
-                "l1_touchpoints_contact_myais_features"
-            ),
-
-            node(
-                l1_touchpoints_contact_myais_features,
-                ["l0_touchpoints_myais_distinct_sub_daily",
-                 "params:l1_customer_profile_union_daily_feature_for_l1_touchpoints_contact_myais_features"
+                 "l1_customer_profile_union_daily_feature_for_l1_touchpoints_contact_myais_features"
                  ],
                 "l1_touchpoints_contact_myais_features"
             ),
