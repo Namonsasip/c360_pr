68440dc3a5b234cc39b19b2a0f0665fe23538e62
PoomtepR <64820619+PoomtepR@users.noreply.github.com>
2021-04-20 10:04:15 +0700

    Update to_l3_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/customer_profile_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/customer_profile_pipeline/to_l3/to_l3_pipeline.py
@@ -32,7 +32,6 @@ from customer360.utilities.config_parser import node_from_config
 from customer360.pipelines.data_engineering.nodes.customer_profile_nodes.to_l3.to_l3_nodes import *
 from customer360.pipelines.data_engineering.nodes.customer_profile_nodes.to_l1.to_l1_nodes import *
 
-
 def customer_profile_to_l3_pipeline(**kwargs):
     return Pipeline(
         [
