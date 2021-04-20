ea74d91d96668f799a2cf5f87adc4d5e2a153ba4
paitn571@epicc.postbox.in.th <az@sXX23>
2021-04-20 14:03:54 +0700

    Update to_l3_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/touchpoints_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/touchpoints_nodes/to_l3/to_l3_nodes.py
@@ -7,6 +7,23 @@ from customer360.utilities.re_usable_functions import union_dataframes_with_miss
     data_non_availability_and_missing_check
 
 
+def l3_pipeline_from_l1(input_df: DataFrame, target_table_name: str, exception_partition=None):
+    ################################# Start Implementing Data availability checks #############################
+    if check_empty_dfs([input_df]):
+        return get_spark_empty_df()
+
+    input_df = data_non_availability_and_missing_check(df=input_df, grouping="monthly", par_col="event_partition_date",
+                                                       target_table_name=target_table_name,
+                                                       missing_data_check_flg='Y',
+                                                       exception_partitions=exception_partition)
+
+    if check_empty_dfs([input_df]):
+        return get_spark_empty_df()
+
+    ################################# End Implementing Data availability checks ###############################
+
+    return input_df
+
 def dac_for_touchpoints_to_l3_pipeline_from_l1(input_df: DataFrame, target_table_name: str, exception_partition=None):
     ################################# Start Implementing Data availability checks #############################
     if check_empty_dfs([input_df]):
