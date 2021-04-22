095d5669c6723a9ad7aeb2bcd615d9e72caaefd9
PoomtepR <64820619+PoomtepR@users.noreply.github.com>
2021-04-22 10:16:21 +0700

    merged

--- src/customer360/pipelines/data_engineering/pipelines/customer_profile_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/customer_profile_pipeline/to_l3/to_l3_pipeline.py
@@ -56,7 +56,6 @@ def customer_profile_to_l3_pipeline(**kwargs):
                  "l0_customer_profile_lm_address_master_for_l3_profile_include_1mo_non_active"],
                 "int_l3_customer_profile_basic_features_3"
             ),
-
             #lot6
             node(
                 df_customer_profile_drm_t_newsub_prepaid_history_for_l3_profile_include_1mo_non_active,
@@ -64,6 +63,7 @@ def customer_profile_to_l3_pipeline(**kwargs):
                  "l0_customer_profile_drm_t_newsub_prepaid_history_for_l3_profile_include_1mo_non_active"],
                 "l3_customer_profile_include_1mo_non_active_test_lot6"
              ),
+            
             # node(
             #     node_from_config,
             #     ["int_l3_customer_profile_basic_features_4",
