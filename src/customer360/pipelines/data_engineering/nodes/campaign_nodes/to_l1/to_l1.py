c5d599b25e950e1a7faca80504c40789ae074aa4
Anurak Homkaew <anurakho@ais.co.th>
2021-04-29 11:00:03 +0700

    add group by on l1_campaign_top_channel_daily

--- src/customer360/pipelines/data_engineering/nodes/campaign_nodes/to_l1/to_l1.py
+++ src/customer360/pipelines/data_engineering/nodes/campaign_nodes/to_l1/to_l1.py
@@ -178,10 +178,10 @@ def pre_process_df(data_frame: DataFrame) -> [DataFrame, DataFrame]:
       subscription_identifier
     , access_method_num
     , contact_channel as campaign_channel
-    , (campaign_total_success/campaign_total_eligible)  as campaign_channel_success_ratio
     , contact_date
+    , (campaign_total_success/campaign_total_eligible)  as campaign_channel_success_ratio
     from int_l1_campaign_summary_daily
-
+    group by subscription_identifier, access_method_num, campaign_channel, contact_date
     ''')
     print('---------pre_process_df final_df------------')
     final_df.limit(10).show()
