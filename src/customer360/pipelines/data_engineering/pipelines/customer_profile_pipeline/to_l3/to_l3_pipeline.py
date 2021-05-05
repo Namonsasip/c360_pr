08cc99042da6c0f660675d2307bb0af4b49b3848
PoomtepR <64820619+PoomtepR@users.noreply.github.com>
2021-05-05 15:50:11 +0700

    Update to_l3_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/customer_profile_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/customer_profile_pipeline/to_l3/to_l3_pipeline.py
@@ -55,17 +55,17 @@ def customer_profile_to_l3_pipeline(**kwargs):
                  "l0_customer_profile_lm_address_master_for_l3_profile_include_1mo_non_active"],
                 "int_l3_customer_profile_basic_features_3"
             ),
+           node(
+               df_customer_profile_drm_t_newsub_prepaid_history_for_l3_profile_include_1mo_non_active,
+               ["int_l3_customer_profile_basic_features_3",
+                "l0_customer_profile_drm_t_newsub_prepaid_history_for_l3_profile_include_1mo_non_active"],
+               "l3_customer_profile_include_1mo_non_active_test_lot6"
+           ),
             node(
                 node_from_config,
-                ["int_l3_customer_profile_basic_features_3",
+                ["l3_customer_profile_include_1mo_non_active_test_lot6",
                  "params:int_l3_customer_profile_basic_features"],
                 "int_l3_customer_profile_basic_features_4"
-            ),
-            node(
-                df_customer_profile_drm_t_newsub_prepaid_history_for_l3_profile_include_1mo_non_active,
-                ["int_l3_customer_profile_basic_features_3",
-                 "l0_customer_profile_drm_t_newsub_prepaid_history_for_l3_profile_include_1mo_non_active"],
-                "l3_customer_profile_include_1mo_non_active_test_lot6"
             ),
 
             node(
