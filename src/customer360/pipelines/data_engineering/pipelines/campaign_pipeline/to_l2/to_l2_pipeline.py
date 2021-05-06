507520ea7a08a0911c605295618daca572e426bc
chawc175 <chawc175@adirc.postbox.in.th>
2021-05-06 09:59:34 +0700

    sum

--- src/customer360/pipelines/data_engineering/pipelines/campaign_pipeline/to_l2/to_l2_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/campaign_pipeline/to_l2/to_l2_pipeline.py
@@ -40,9 +40,9 @@ def campaign_to_l2_pipeline(**kwargs):
     return Pipeline(
         [
             node(build_campaign_l2_layer,
-                 ["l1_campaign_post_pre_daily", "l1_campaign_top_channel_daily"
-                     , "params:l2_campaign_post_pre_weekly", "params:l2_campaign_top_channel_weekly"],
-                 ["l2_campaign_postpaid_prepaid_weekly_new", "l2_campaign_top_channel_weekly_new"]
+                 ["l1_campaign_post_pre_daily", "l1_campaign_top_channel_daily",
+                  "params:l2_campaign_post_pre_weekly", "params:l2_campaign_top_channel_weekly"],
+                 ["l2_campaign_postpaid_prepaid_weekly", "l2_campaign_top_channel_weekly"]
                  ),
 
         ], name="campaign_to_l2_pipeline"
