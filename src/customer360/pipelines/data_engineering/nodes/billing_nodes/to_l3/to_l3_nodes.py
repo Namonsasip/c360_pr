6ea8cbb143f4a5ba7ff6d6c4067a2d2c60e770bb
spysert <61383598+spysert@users.noreply.github.com>
2021-05-10 22:32:26 +0700

    Update to_l3_nodes.py
    edit start_of_week_and_month

--- src/customer360/pipelines/data_engineering/nodes/billing_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/billing_nodes/to_l3/to_l3_nodes.py
@@ -11,7 +11,7 @@ import os
 from src.customer360.utilities.spark_util import get_spark_empty_df
 from pyspark.sql.types import *
 from customer360.utilities.re_usable_functions import union_dataframes_with_missing_cols, check_empty_dfs, \
-    data_non_availability_and_missing_check
+    data_non_availability_and_missing_check, add_start_of_week_and_month
 from customer360.pipelines.data_engineering.nodes.geolocation_nodes.to_l1.to_l1_nodes import get_max_date_from_master_data
 
 conf = os.getenv("CONF", None)
@@ -823,16 +823,16 @@ def int_l3_billing_and_payments_monthly_roaming_bill_volume(billing_df, ir_packa
     billing_ir_package = billing_df.join(ir_package_df,['offering_identifier'])
     billing_ir_ppu = billing_df.where("charge_classification_id = 'USAGE' and charge_class_catgry_identifier = 'IR_MARKUP'")
 
-    billing_ir_package.add_start_of_week_and_month(billing_ir_package, "bill_stmt_charge_chrg_end_date")
-    billing_ir_ppu.add_start_of_week_and_month(billing_ir_ppu, "bill_stmt_charge_chrg_end_date")
+    billing_ir_package = add_start_of_week_and_month(billing_ir_package, "bill_stmt_charge_chrg_end_date")
+    billing_ir_ppu = add_start_of_week_and_month(billing_ir_ppu, "bill_stmt_charge_chrg_end_date")
 
     output_df = billing_ir_package.union(billing_ir_ppu)
 
     return output_df
 
 def l3_billing_and_payments_monthly_roaming_bill_volume(billing_ir_package, billing_ir_ppu):
-    billing_ir_package = billing_ir_package.drop("start_of_week", "event_partition_date")
-    billing_ir_ppu = billing_ir_ppu.drop("start_of_week", "event_partition_date")
+    #billing_ir_package = billing_ir_package.drop("start_of_week")
+    #billing_ir_ppu = billing_ir_ppu.drop("start_of_week")
     output_df = union_dataframes_with_missing_cols([
         billing_ir_package, billing_ir_ppu
     ])
