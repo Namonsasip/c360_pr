750417e0864454d102933d8aae40c7c1222bbcc5
Navenavey39 <36034923+Navenavey39@users.noreply.github.com>
2021-05-12 17:46:42 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/stream_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/stream_pipeline/to_l1/to_l1_pipeline.py
@@ -446,7 +446,7 @@ def soc_web_daily_agg_pipeline(**kwargs):
             # ),
             node(
                 func=node_join_soc_web_hourly_with_with_aib_agg_catlv2,
-                inputs=["l0_soc_web_hourly_raw_catlv2", "l1_aib_categories_clean_catlv2"],
+                inputs=["l0_soc_web_hourly_raw_catlv2", "l1_aib_categories_clean"],
                 outputs="l1_soc_web_hourly_with_iab_catlv2",
                 tags=["node_join_soc_web_hourly_with_with_aib_agg_catlv2"],
             ),
