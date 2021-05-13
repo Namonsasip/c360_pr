de0c14820efc1b0bd3a6394cfc7d9819217e8d9c
thanads8 <thanadet.work@gmail.com>
2021-05-13 14:58:43 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/usage_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/usage_pipeline/to_l1/to_l1_pipeline.py
@@ -78,7 +78,7 @@ def usage_to_l1_pipeline(**kwargs):
             node(
                 node_from_config,
                 ["l0_usage_call_relation_sum_daily_for_l1_usage_most_idd_features",
-                 "l1_customer_profile_union_daily_feature_for_l1_usage_most_idd_features"],
+                 "params:l1_customer_profile_union_daily_feature_for_l1_usage_most_idd_features"],
                 "l1_usage_most_idd_features"
             ),
 
