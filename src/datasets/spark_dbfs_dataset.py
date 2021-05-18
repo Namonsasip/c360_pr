de733951fc3e8e7adcbca4155545f8fea1db6061
Thatchapong Tunkam <64589464+BenzThatchapong@users.noreply.github.com>
2021-05-18 15:53:52 +0700

    Update spark_dbfs_dataset.py

--- src/datasets/spark_dbfs_dataset.py
+++ src/datasets/spark_dbfs_dataset.py
@@ -782,6 +782,7 @@ class SparkDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
             load_path = _strip_dbfs_prefix(self._fs_prefix + str(self._get_load_path()))
             logging.info("p_partition: {}".format(p_partition))
             logging.info("p_features: {}".format(p_features))
+            p_no = "run"
             if (running_environment == "on_cloud"):
                 if ("/" == load_path[-1:]):
                     load_path = load_path
@@ -1070,16 +1071,18 @@ class SparkDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
                     base_filepath = str(load_path)
                     p_partition_type = ""
                     p_month1 = ""
+                    p_no = "no"
 
                 load_path1 = str(load_path) + p_partition_type + str(p_month1)
                 if (p_features == "feature_l4" or p_features == "feature_l2" or p_features == "feature_l3"):
                     logging.info("basePath: {}".format(base_filepath))
                     logging.info("load_path: {}".format(load_path))
                     logging.info("file_format: {}".format(self._file_format))
-                    logging.info("partition_type: {}".format(p_partition_type.split('=')[0]))
-                    logging.info("read_start: {}".format(p_month2))
-                    logging.info("read_end: {}".format(p_month1))
-                    logging.info("Fetching source data")
+                    if (p_no == "run"):
+                        logging.info("partition_type: {}".format(p_partition_type.split('=')[0]))
+                        logging.info("read_start: {}".format(p_month2))
+                        logging.info("read_end: {}".format(p_month1))
+                        logging.info("Fetching source data")
                 else:
                     logging.info("basePath: {}".format(base_filepath))
                     logging.info("load_path: {}".format(load_path1))
@@ -1410,16 +1413,18 @@ class SparkDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
                     base_filepath = str(load_path)
                     p_partition_type = ""
                     p_month1 = ""
+                    p_no = "no"
 
                 load_path1 = str(load_path) + p_partition_type + str(p_month1)
                 if (p_features == "feature_l4" or p_features == "feature_l2" or p_features == "feature_l3"):
                     logging.info("basePath: {}".format(base_filepath))
                     logging.info("load_path: {}".format(load_path))
                     logging.info("file_format: {}".format(self._file_format))
-                    logging.info("partition_type: {}".format(p_partition_type.split('=')[0]))
-                    logging.info("read_start: {}".format(p_month2))
-                    logging.info("read_end: {}".format(p_month1))
-                    logging.info("Fetching source data")
+                    if (p_no == "run"):
+                        logging.info("partition_type: {}".format(p_partition_type.split('=')[0]))
+                        logging.info("read_start: {}".format(p_month2))
+                        logging.info("read_end: {}".format(p_month1))
+                        logging.info("Fetching source data")
                 else:
                     logging.info("basePath: {}".format(base_filepath))
                     logging.info("load_path: {}".format(load_path1))
