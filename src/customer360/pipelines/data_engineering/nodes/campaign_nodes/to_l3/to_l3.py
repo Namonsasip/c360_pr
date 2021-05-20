31b2421ba48003592c96937a15343c16e98ec773
chawc175 <chawc175@adirc.postbox.in.th>
2021-05-20 16:28:06 +0700

    Update to_l3.py

--- src/customer360/pipelines/data_engineering/nodes/campaign_nodes/to_l3/to_l3.py
+++ src/customer360/pipelines/data_engineering/nodes/campaign_nodes/to_l3/to_l3.py
@@ -116,34 +116,42 @@ def build_campaign_l3_layer(l1_campaign_post_pre_fbb_daily: DataFrame,
 
     first_item = add_list[-1]
 
+    l1_campaign_top_channel_daily = l1_campaign_top_channel_daily.drop('run_date')
+    # top_campaign_df['run_date'] = top_campaign_df.to_datetime('today')
+    l1_campaign_top_channel_daily = l1_campaign_top_channel_daily.withColumn("run_date", F.current_date())
+    print(l1_campaign_top_channel_daily)
+    print('** ** ** ** ** ** ** ** ** ** ** ** ** **  print(l1_campaign_top_channel_daily)** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** * ')
+    l1_campaign_top_channel_daily.limit(1).show()
+    print('** ** ** ** ** ** ** ** ** ** ** ** ** ** ** *l1_campaign_top_channel_daily1111111111111111111111.show()* ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** *')
+
     add_list.remove(first_item)
     for curr_item in add_list:
         logging.info("running for dates {0}".format(str(curr_item)))
         small_df = data_frame.filter(F.col("start_of_month").isin(*[curr_item]))
         top_campaign_df = l1_campaign_top_channel_daily.filter(F.col("start_of_month").isin(*[curr_item]))
 
-        top_campaign_df = top_campaign_df.drop('run_date')
-        # top_campaign_df['run_date'] = top_campaign_df.to_datetime('today')
-        top_campaign_df = top_campaign_df.withColumn("run_date", F.current_date())
-        print(top_campaign_df)
-        print('** ** ** ** ** ** ** ** ** ** ** ** ** **  print(top_campaign_df)** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** * ')
-        top_campaign_df.limit(1).show()
-        print('** ** ** ** ** ** ** ** ** ** ** ** ** ** ** *top_campaign_df.show()* ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** *')
+        # top_campaign_df = top_campaign_df.drop('run_date')
+        # # top_campaign_df['run_date'] = top_campaign_df.to_datetime('today')
+        # top_campaign_df = top_campaign_df.withColumn("run_date", F.current_date())
+        # print(top_campaign_df)
+        # print('** ** ** ** ** ** ** ** ** ** ** ** ** **  print(top_campaign_df)** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** * ')
+        # top_campaign_df.limit(1).show()
+        # print('** ** ** ** ** ** ** ** ** ** ** ** ** ** ** *top_campaign_df1111111111111111111111.show()* ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** *')
 
         output_df_1 = expansion(small_df, dictObj_1)
         output_df_2 = expansion(top_campaign_df, dictObj_2)
         CNTX.catalog.save("l3_campaign_postpaid_prepaid_monthly", output_df_1)
         CNTX.catalog.save("l3_campaign_top_channel_monthly", output_df_2)
 
-    small_df = data_frame.filter(F.col("start_of_month").isin(*[first_item]))
 
-    print('** ** ** ** ** ** ** ** ** ** ** ** ** **small_df.show() ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** * ')
+    small_df = data_frame.filter(F.col("start_of_month").isin(*[first_item]))
+    print('** ** ** ** ** ** ** ** ** ** ** ** ** **small_df.showwwwwwwwwwwwwwwwwwwwwwwww() ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** * ')
     small_df.limit(1).show()
     print('** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** *')
 
     top_campaign_df = l1_campaign_top_channel_daily.filter(F.col("start_of_month").isin(*[first_item]))
 
-    print('** ** ** ** ** ** ** ** ** ** ** ** ** ** top_campaign_df.show()** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** * ')
+    print('** ** ** ** ** ** ** ** ** ** ** ** ** ** top_campaign_df222222222222222.show()** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** * ')
     top_campaign_df.limit(1).show()
     print('** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** *')
 
