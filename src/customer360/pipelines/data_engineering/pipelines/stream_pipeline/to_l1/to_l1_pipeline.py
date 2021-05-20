bbd6662916db4ba530c8f3522eed357b88108bd1
sukanyapatra <sukanya_patra@mckinsey.com>
2021-05-21 01:30:57 +0530

    wip

--- src/customer360/pipelines/data_engineering/pipelines/stream_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/stream_pipeline/to_l1/to_l1_pipeline.py
@@ -272,13 +272,13 @@ def soc_app_daily_agg_pipeline(**kwargs):
                     "l0_soc_app_hourly_raw",
                     "l1_stream_mobile_app_categories_master_clean",
                 ],
-                outputs="l1_soc_app_hourly_with_iab", #4
+                outputs="l1_soc_app_hourly_with_iab@output", #4
                 tags=["node_join_soc_hourly_with_aib_agg"],
             ),
             node(
                 func=node_join_soc_daily_with_aib_agg,
                 inputs=["l0_soc_app_daily_raw", "l1_aib_categories_clean"],
-                outputs="l1_soc_app_daily_with_iab", #1
+                outputs="l1_soc_app_daily_with_iab@output", #1
                 tags=["node_join_soc_daily_with_aib_agg"],
             ),
             node(
