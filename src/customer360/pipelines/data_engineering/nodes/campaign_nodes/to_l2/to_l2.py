a4e102df6d3f8dcdd9e9933a449f8ba02aad58d8
chawc175 <chawc175@adirc.postbox.in.th>
2021-05-21 09:32:20 +0700

    Update to_l2.py

--- src/customer360/pipelines/data_engineering/nodes/campaign_nodes/to_l2/to_l2.py
+++ src/customer360/pipelines/data_engineering/nodes/campaign_nodes/to_l2/to_l2.py
@@ -85,12 +85,14 @@ def build_campaign_l2_layer(l1_campaign_post_pre_fbb_daily: DataFrame,
 
     small_df = data_frame.filter(F.col("start_of_week").isin(*[first_item]))
     top_campaign_df = l1_campaign_top_channel_daily.filter(F.col("start_of_week").isin(*[first_item]))
+    first_return_df = first_return_df.drop('run_date')
+    second_return_df = second_return_df.withColumn("run_date", F.current_date())
     first_return_df = expansion(small_df, dictObj_1)
     second_return_df = expansion(top_campaign_df, dictObj_2)
 
     # data_frame = data_frame.drop('run_date')
     # data_frame = data_frame.withColumn("run_date", F.current_date())
-    first_return_df = first_return_df.drop('run_date')
-    second_return_df = second_return_df.withColumn("run_date", F.current_date())
+    # first_return_df = first_return_df.drop('run_date')
+    # second_return_df = second_return_df.withColumn("run_date", F.current_date())
 
     return [first_return_df, second_return_df]
