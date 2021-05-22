8683e61d361011c6aafb804a9c2a94226b841cf9
sukanyapatra <sukanya_patra@mckinsey.com>
2021-05-23 01:56:41 +0530

    test web

--- src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l1/to_l1_nodes.py
@@ -1907,7 +1907,7 @@ def node_soc_web_daily_features_massive_processing(
 
     mvv_array = sorted(mvv_array)
 
-    partition_num_per_job = 3
+    partition_num_per_job = 2
     mvv_new = list(divide_chunks(mvv_array, partition_num_per_job))
     add_list = mvv_new
     first_item = add_list[-1]
