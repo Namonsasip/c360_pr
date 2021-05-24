1003210104e82b2dbdc96af5874c7430339a61f6
sukanyapatra <sukanya_patra@mckinsey.com>
2021-05-25 04:58:55 +0530

    test: subs_id pk

--- src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l3/to_l3_nodes.py
@@ -1160,7 +1160,7 @@ def node_compute_chunk_soc_app_monthly_features(
     logging.info("2.completed: config_soc_app_monthly_stats")
 
     df_final_sum = df_soc_app_monthly_sum_features.join(
-        df_soc_app_monthly_stats, on=["mobile_no", "start_of_month"], how="left"
+        df_soc_app_monthly_stats, on=["mobile_no", "start_of_month", "subscription_identifier"], how="left"
     )
     logging.info("3.completed: join sum features and daily stats")
 
