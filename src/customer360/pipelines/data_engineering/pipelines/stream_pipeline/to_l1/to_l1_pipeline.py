8d6111c9bfc96afa0b4604069f3c4d2002f0668a
sukanyapatra <sukanya_patra@mckinsey.com>
2021-05-25 23:21:58 +0530

    wip

--- src/customer360/pipelines/data_engineering/pipelines/stream_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/stream_pipeline/to_l1/to_l1_pipeline.py
@@ -527,8 +527,8 @@ def comb_soc_app_web_features_pipeline(**kwargs):
             node(
                 func=node_combine_soc_app_and_web_massive_processing,
                 inputs=[
-                    "l1_combined_soc_app_daily_and_hourly_agg@l1_comb_soc_web_and_app",
-                    "l1_combined_soc_web_daily_and_hourly_agg@l1_comb_soc_web_and_app",
+                    "l1_combined_soc_app_daily_and_hourly_agg_for_l1_comb_soc_web_and_app",
+                    "l1_combined_soc_web_daily_and_hourly_agg_for_l1_comb_soc_web_and_app",
                 ],
                 outputs="l1_comb_soc_web_and_app",
                 tags=["node_combine_soc_app_and_web_massive_processing"],
