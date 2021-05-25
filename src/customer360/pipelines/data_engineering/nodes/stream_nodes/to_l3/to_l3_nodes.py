493f557a3fdd32ff59d3f3fc27f2b897a1acaa3e
sukanyapatra <sukanya_patra@mckinsey.com>
2021-05-25 21:17:28 +0530

    wip

--- src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/stream_nodes/to_l3/to_l3_nodes.py
@@ -1439,11 +1439,14 @@ def node_compute_chunk_soc_web_monthly_features(
     )
     logging.info("1.completed: config_soc_web_monthly_agg")
 
+    df_soc_web_daily.show(10, False)
+    print(config_soc_web_monthly_stats)
     df_soc_web_monthly_stats = node_from_config(
         df_soc_web_daily, config_soc_web_monthly_stats
     )
+    df_soc_web_monthly_stats.show(10, False)
     logging.info("2.completed: config_soc_web_monthly_stats")
-
+    df_soc_web_monthly_stats.show(10, False)
     df_final_sum = df_soc_web_monthly_sum_features.join(
         df_soc_web_monthly_stats, on=["mobile_no", "start_of_month","subscription_identifier"], how="left"
     )
