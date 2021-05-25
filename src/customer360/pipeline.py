d6f47775dc22ec0388aa5b973d087aef62a298b2
Navenavey39 <36034923+Navenavey39@users.noreply.github.com>
2021-05-25 17:40:39 +0700

    Update pipeline.py

--- src/customer360/pipeline.py
+++ src/customer360/pipeline.py
@@ -132,7 +132,8 @@ from .pipelines.data_engineering.pipelines.digital_pipeline import (
 # add new pipeline import 
 from .pipelines.data_engineering.pipelines.digital_pipeline.to_l1.to_l1_pipeline import (
     digital_to_l1_app_agg_daily_pipeline,
-    l1_digital_aib_categoy_clean_master
+    l1_digital_aib_categoy_clean_master,
+    l1_digital_mobile_web_agg_daily
 )
 
 from .pipelines.data_engineering.pipelines.loyalty_pipeline import (
@@ -272,6 +273,7 @@ def create_c360_pipeline(**kwargs) -> Dict[str, Pipeline]:
         # add
         "digital_to_l1_app_agg_daily_pipeline": digital_to_l1_app_agg_daily_pipeline(),
         "l1_digital_aib_categoy_clean_master" : l1_digital_aib_categoy_clean_master(),
+        "l1_digital_mobile_web_agg_daily" : l1_digital_mobile_web_agg_daily(),
 
         "digital_to_l2_pipeline": digital_to_l2_pipeline(),
         "digital_to_l3_pipeline": digital_to_l3_pipeline(),
