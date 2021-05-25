2829f5c7f583bd6c54b5e1fe3576cfef23784c73
thanads8 <thanadet.work@gmail.com>
2021-05-25 17:35:55 +0700

    update to_l3_nodes

--- src/customer360/pipelines/data_engineering/nodes/usage_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/usage_nodes/to_l3/to_l3_nodes.py
@@ -54,11 +54,11 @@ def l3_usage_most_idd_features(input_df: DataFrame,config,exception_partition=No
     if check_empty_dfs([input_df]):
         return get_spark_empty_df()
 
-    input_df = data_non_availability_and_missing_check(df=input_df, grouping="monthly",
-                                                         par_col="event_partition_date",
-                                                         # target_table_name="l3_usage_most_idd_features",
-                                                         missing_data_check_flg='Y',
-                                                         exception_partitions=exception_partition)
+    # input_df = data_non_availability_and_missing_check(df=input_df, grouping="monthly",
+    #                                                      par_col="event_partition_date",
+    #                                                      target_table_name="l3_usage_most_idd_features",
+    #                                                      missing_data_check_flg='Y',
+    #                                                      exception_partitions=exception_partition)
 
     if check_empty_dfs([input_df]):
         return get_spark_empty_df()
