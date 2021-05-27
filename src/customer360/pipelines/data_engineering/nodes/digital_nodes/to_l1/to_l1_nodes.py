c02dd1d1ca9326cc869f6e10fb1e71df166c948c
Paipayon10 <Paipayon10@gmail.com>
2021-05-27 17:39:08 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -187,7 +187,7 @@ def digital_mobile_app_category_agg_timeband(Mobile_app_timeband: DataFrame,app_
         max_date = key_c360.select(f.max(f.to_date((f.col("event_partition_date")).cast(StringType()), 'yyyy-MM-dd')).alias("max_date"))
     max_date.show()
 
-    #key_c360 = key_c360.filter(key_c360["event_partition_date"]=max_date)
+    key_c360 = key_c360.filter(key_c360["event_partition_date"]=max_date)
 
     #join key
     # Mobile_app_timeband = Mobile_app_timeband.join(f.broadcast(key_c360),
