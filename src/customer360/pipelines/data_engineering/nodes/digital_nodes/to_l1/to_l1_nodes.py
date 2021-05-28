673ea783ebf312672377ea8773d7d376d8d9748b
Paipayon10 <Paipayon10@gmail.com>
2021-05-28 23:13:53 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -211,10 +211,6 @@ def digital_mobile_app_category_agg_timeband(Mobile_app_timeband: DataFrame,app_
     ################### timeband join sub ################################
 
 def digital_mobile_app_category_agg_timeband_feature(Mobile_app_timeband: DataFrame,customer_profile_key: DataFrame):
-
-    spark = get_spark_session()
-    spark.conf.set("spark.sql.autoBroadcastJoinThreshold", '2g')
-
     customer_profile_key.select(customer_profile_key["access_method_num"],customer_profile_key["subscription_identifier"]).groupBy(customer_profile_key["access_method_num"],customer_profile_key["subscription_identifier"])
 
     Mobile_app_timeband = Mobile_app_timeband.withColumnRenamed("msisdn", "mobile_no").join(f.broadcast(customer_profile_key),
