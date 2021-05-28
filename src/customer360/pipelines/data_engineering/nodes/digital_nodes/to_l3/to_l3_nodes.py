7505e3d0cc44908114974e3b60242a790f499b0b
pakawat <pakap571@epicc.postbox.in.th>
2021-05-28 16:35:52 +0700

    fix run

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
@@ -85,10 +85,10 @@ def l3_digital_mobile_web_category_agg_monthly (mobile_web_daily_agg: DataFrame)
     df_mobile_web_monthly_category_agg = df_mobile_web_monthly.groupBy("mobile_no","subscription_identifier","category_name","priority"
                                                                        ,"start_of_month").agg(
         f.sum("total_visit_counts").alias("total_visit_counts"),
-        f.sum("total_visit_duration").alias("total_visit_counts"),
-        f.sum("total_download_byte").alias("total_visit_counts"),
-        f.sum("total_upload_byte").alias("total_visit_counts"),
-        f.sum("total_volume_byte").alias("total_visit_counts")
+        f.sum("total_visit_duration").alias("total_visit_duration"),
+        f.sum("total_download_byte").alias("total_download_byte"),
+        f.sum("total_upload_byte").alias("total_upload_byte"),
+        f.sum("total_volume_byte").alias("total_volume_byte")
         )
 
     return df_mobile_web_monthly_category_agg
