b3334f0f6918587c40e1b315c3f2bca2ec4db3dc
Paipayon10 <Paipayon10@gmail.com>
2021-05-28 15:51:18 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -190,11 +190,10 @@ def digital_mobile_app_category_agg_timeband(Mobile_app_timeband: DataFrame,app_
     else:
         Mobile_app_timeband = Mobile_app_timeband.filter(Mobile_app_timeband["ld_hour"] >= 0 ).filter(Mobile_app_timeband["ld_hour"] <= 5 )
     
-    Mobile_app_timeband.show(5)  
     # where this column more than 0
     Mobile_app_timeband = Mobile_app_timeband.where(f.col("dw_byte") > 0)
     Mobile_app_timeband = Mobile_app_timeband.where(f.col("ul_kbyte") > 0)
-    Mobile_app_timeband0show(5)
+    Mobile_app_timeband.show(5)
     #join master
     Mobile_app_timeband = Mobile_app_timeband.withColumnRenamed("msisdn", "mobile_no").join(f.broadcast(app_categories_master),
         on=[app_categories_master.application_id == Mobile_app_timeband.application],
