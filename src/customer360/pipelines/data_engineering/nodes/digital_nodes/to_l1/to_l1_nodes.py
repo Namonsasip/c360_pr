60f8f10b4662afc736fa48036c3fb044dd0e7ef8
Navenavy39 <navenavy39@hotmail.com>
2021-05-28 23:01:03 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -108,8 +108,8 @@ def build_l1_digital_iab_category_table(aib_raw: DataFrame, aib_priority_mapping
         "category", "level_4"
     ).withColumn("level_4", f.trim(f.lower(f.col("level_4"))))
     iab_category_table = aib_clean.join(
-        aib_priority_mapping_clean, on=[aib_priority_mapping_clean.level_4 == aib_clean.level_4], how="inner"
-    ).select("argument", "level_1", "level_2", "level_3", aib_priority_mapping_clean.level_4 , "priority").withColumnRenamed("level_4" , "category_name")
+        aib_priority_mapping_clean, on=[aib_clean.level_4 == aib_priority_mapping_clean.level_4], how="left"
+    ).select("argument", "level_1", "level_2", "level_3", aib_clean.level_4 , "priority").withColumnRenamed("level_4" , "category_name")
 
     return iab_category_table
 
