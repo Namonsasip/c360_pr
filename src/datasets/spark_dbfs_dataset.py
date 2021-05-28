a8fb6b0950067d70d60f5065dd229bf7b296289f
Thatchapong Tunkam <64589464+BenzThatchapong@users.noreply.github.com>
2021-05-28 15:04:21 +0700

    Update spark_dbfs_dataset.py

--- src/datasets/spark_dbfs_dataset.py
+++ src/datasets/spark_dbfs_dataset.py
@@ -1009,10 +1009,6 @@ class SparkDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
                     else:
                         for read_path in list_temp:
                             list_path.append(str(read_path)[2:-1].split('dbfs')[1])
-
-                    # logging.info("Partition: {}".format(list_path[0].split('/')[-1]))
-                    # logging.info("Partition: {}".format(list_path[0].split('/')[-2]))
-                    # logging.info("Partition: {}".format(list_path[0].split('/')[-3]))
                     if ("/partition_month=" in list_path[0]):
                         p_partition_type = "partition_month="
                         if (p_features == "feature_l2" or p_features == "feature_l1" or p_features == "feature_l3"):
