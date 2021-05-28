44d69ef3c25736d9ca37e4340efcd233ad2d8c59
Navenavey39 <36034923+Navenavey39@users.noreply.github.com>
2021-05-28 16:24:30 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -152,12 +152,12 @@ def digital_to_l1_app_agg_timeband_pipeline(**kwargs):
 def digital_to_l1_digital_mobile_web_agg_daily(**kwargs):
     return Pipeline(
         [
-            node(
-                func=l1_digital_customer_web_category_agg_daily,
-                inputs=["l0_digital_mobile_web_daily", "l1_digital_aib_categories_clean"],
-                outputs="l1_digital_customer_web_category_agg_daily",
-                tags="l1_digital_customer_web_category_agg_daily",
-            ),
+            # node(
+            #     func=l1_digital_customer_web_category_agg_daily,
+            #     inputs=["l0_digital_mobile_web_daily", "l1_digital_aib_categories_clean"],
+            #     outputs="l1_digital_customer_web_category_agg_daily",
+            #     tags="l1_digital_customer_web_category_agg_daily",
+            # ),
             node(
                 func=l1_digital_mobile_web_category_agg_timebrand,
                 inputs=
@@ -171,16 +171,16 @@ def digital_to_l1_digital_mobile_web_agg_daily(**kwargs):
                 outputs="l1_digital_mobile_web_category_agg_timebrand",
                 tags=["l1_digital_mobile_web_category_agg_timebrand"],
             ),
-            # node(
-            #     func=l1_digital_mobile_web_category_agg_timebrand_subscription,
-            #     inputs=
-            #     [
-            #         "l0_digital_cutomer_profile_union_daily",
-            #         "l1_digital_mobile_web_category_agg_timebrand",
-            #     ],
-            #     outputs="l1_digital_mobile_web_category_agg_timebrand_subscription",
-            #     tags=["l1_digital_mobile_web_category_agg_timebrand_subscription"],
-            # ),
+            node(
+                func=l1_digital_mobile_web_category_agg_timebrand_subscription,
+                inputs=
+                [
+                    "l0_digital_cutomer_profile_union_daily",
+                    "l1_digital_mobile_web_category_agg_timebrand",
+                ],
+                outputs="l1_digital_mobile_web_category_agg_timebrand_subscription",
+                tags=["l1_digital_mobile_web_category_agg_timebrand_subscription"],
+            ),
             # node(
             #     func=l1_digital_mobile_web_level_category,
             #     inputs="l1_digital_customer_web_category_agg_daily",
