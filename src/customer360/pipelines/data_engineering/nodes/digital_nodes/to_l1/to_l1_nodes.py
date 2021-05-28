63cab814c675e47356dd8caee03b98f6369e7584
Navenavy39 <navenavy39@hotmail.com>
2021-05-28 22:33:54 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -99,17 +99,17 @@ def build_l1_digital_iab_category_table(aib_raw: DataFrame, aib_priority_mapping
         return get_spark_empty_df()
 
     aib_clean = (
-        aib_raw.withColumn("level_2", f.trim(f.lower(f.col("level_2"))))
+        aib_raw.withColumn("level_1", f.trim(f.lower(f.col("level_1"))))
             .filter(f.col("argument").isNotNull())
             .filter(f.col("argument") != "")
     )
 
     aib_priority_mapping_clean = aib_priority_mapping.withColumnRenamed(
-        "category", "level_2"
-    ).withColumn("level_2", f.trim(f.lower(f.col("level_2"))))
+        "category", "level_1"
+    ).withColumn("level_1", f.trim(f.lower(f.col("level_1"))))
     iab_category_table = aib_clean.join(
         aib_priority_mapping_clean, on=[aib_priority_mapping_clean.level_2 == aib_clean.level_2], how="inner"
-    ).select("argument", "level_1", aib_clean.level_2, "level_3", "level_4" , "priority").withColumnRenamed("level_2" , "category_name")
+    ).select("argument", "level_1", aib_clean.level_2, "level_3", "level_4" , "priority").withColumnRenamed("level_1" , "category_name")
 
     return iab_category_table
 
