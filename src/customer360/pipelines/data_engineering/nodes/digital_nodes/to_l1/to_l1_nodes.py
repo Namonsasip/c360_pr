8294f5ed6a80bec7f144bfe5c8dc23ea540fcc1f
Navenavy39 <navenavy39@hotmail.com>
2021-05-28 19:57:26 +0700

    update iab
    update iab

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -97,17 +97,17 @@ def build_l1_digital_iab_category_table(aib_raw: DataFrame, aib_priority_mapping
         return get_spark_empty_df()
 
     aib_clean = (
-        aib_raw.withColumn("level_3", f.trim(f.lower(f.col("level_3"))))
+        aib_raw.withColumn("level_2", f.trim(f.lower(f.col("level_2"))))
             .filter(f.col("argument").isNotNull())
             .filter(f.col("argument") != "")
     ).drop_duplicates()
 
     aib_priority_mapping = aib_priority_mapping.withColumnRenamed(
-        "category", "level_3"
-    ).withColumn("level_3", f.trim(f.lower(f.col("level_3"))))
+        "category", "level_2"
+    ).withColumn("level_2", f.trim(f.lower(f.col("level_2"))))
     iab_category_table = aib_clean.join(
-        aib_priority_mapping, on=["level_3"], how="inner"
-    ).withColumnRenamed("level_3", "category_name").drop("level_1", "level_2", "level_3", "level_4")
+        aib_priority_mapping, on=["level_2"], how="inner"
+    ).withColumnRenamed("level_2", "category_name").drop("level_1", "level_2", "level_3", "level_4")
 
     return iab_category_table
 
