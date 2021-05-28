1fea256806d60577246f5dd5d5502e13001697a0
Paipayon10 <Paipayon10@gmail.com>
2021-05-28 23:21:27 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -211,6 +211,9 @@ def digital_mobile_app_category_agg_timeband(Mobile_app_timeband: DataFrame,app_
     ################### timeband join sub ################################
 
 def digital_mobile_app_category_agg_timeband_feature(Mobile_app_timeband: DataFrame,customer_profile_key: DataFrame):
+    Mobile_app_timeband = Mobile_app_timeband.repartition(1000)
+    Mobile_app_timeband.cache()
+    
     customer_profile_key.select(customer_profile_key["access_method_num"],customer_profile_key["subscription_identifier"]).groupBy(customer_profile_key["access_method_num"],customer_profile_key["subscription_identifier"])
 
     Mobile_app_timeband = Mobile_app_timeband.withColumnRenamed("msisdn", "mobile_no").join(f.broadcast(customer_profile_key),
