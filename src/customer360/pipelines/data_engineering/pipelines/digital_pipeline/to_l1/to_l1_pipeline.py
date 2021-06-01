14d8dde27bdb4893f98be042884bdb9dcd04493a
Navenavy39 <navenavy39@hotmail.com>
2021-06-01 10:43:20 +0700

    timeband l1
    timeband l1

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -186,55 +186,55 @@ def digital_to_l1_digital_mobile_web_agg_timeband(**kwargs):
                 outputs="l1_digital_customer_web_category_agg_timeband_morning_catlv_1",
                 tags="l1_digital_mobile_web_category_agg_timeband_Morning",
             ),
-            node(
-                func=l1_digital_mobile_web_category_agg_timeband,
-                inputs=
-                [
-                    "l0_digital_mobile_web_hourly",
-                    "l0_digital_customer_profile_union_daily",
-                    "l1_digital_aib_categories_clean",
-                    "params:l1_digital_mobile_web_agg_category_timeband_afternoon_catlv1",
-                    "params:timeband_web_afternoon"
-                ],
-                outputs="l1_digital_customer_web_category_agg_timeband_afternoon_catlv_1",
-                tags="l1_digital_mobile_web_category_agg_timeband_Afternoon",
-            ),
-            node(
-                func=l1_digital_mobile_web_category_agg_timeband,
-                inputs=
-                [
-                    "l0_digital_mobile_web_hourly",
-                    "l0_digital_customer_profile_union_daily",
-                    "l1_digital_aib_categories_clean",
-                    "params:l1_digital_mobile_web_agg_category_timeband_evening_catlv1",
-                    "params:timeband_web_evening"
-                ],
-                outputs="l1_digital_customer_web_category_agg_timeband_evening_catlv_1",
-                tags="l1_digital_mobile_web_category_agg_timeband_Evening",
-            ),
-            node(
-                func=l1_digital_mobile_web_category_agg_timeband,
-                inputs=
-                [
-                    "l0_digital_mobile_web_hourly",
-                    "l0_digital_customer_profile_union_daily",
-                    "l1_digital_aib_categories_clean",
-                    "params:l1_digital_mobile_web_agg_category_timeband_night_catlv1",
-                    "params:timeband_web_night"
-                ],
-                outputs="l1_digital_customer_web_category_agg_timeband_night_catlv_1",
-                tags="l1_digital_mobile_web_category_agg_timeband_Night",
-            ),
-            node(
-                func=l1_digital_mobile_web_category_agg_timeband_features,
-                inputs=
-                [
-                    "l1_digital_union_daily_feature_for_mobile_web_catefory_timeband",
-                    "l1_digital_mobile_web_category_agg_morning_timeband",
-                ],
-                outputs="l1_digital_customer_web_category_agg_timeband_Morning_catlv_1",
-                tags=["l1_digital_mobile_web_category_agg_timeband_features"],
-            ),
+            # node(
+            #     func=l1_digital_mobile_web_category_agg_timeband,
+            #     inputs=
+            #     [
+            #         "l0_digital_mobile_web_hourly",
+            #         "l0_digital_customer_profile_union_daily",
+            #         "l1_digital_aib_categories_clean",
+            #         "params:l1_digital_mobile_web_agg_category_timeband_afternoon_catlv1",
+            #         "params:timeband_web_afternoon"
+            #     ],
+            #     outputs="l1_digital_customer_web_category_agg_timeband_afternoon_catlv_1",
+            #     tags="l1_digital_mobile_web_category_agg_timeband_Afternoon",
+            # ),
+            # node(
+            #     func=l1_digital_mobile_web_category_agg_timeband,
+            #     inputs=
+            #     [
+            #         "l0_digital_mobile_web_hourly",
+            #         "l0_digital_customer_profile_union_daily",
+            #         "l1_digital_aib_categories_clean",
+            #         "params:l1_digital_mobile_web_agg_category_timeband_evening_catlv1",
+            #         "params:timeband_web_evening"
+            #     ],
+            #     outputs="l1_digital_customer_web_category_agg_timeband_evening_catlv_1",
+            #     tags="l1_digital_mobile_web_category_agg_timeband_Evening",
+            # ),
+            # node(
+            #     func=l1_digital_mobile_web_category_agg_timeband,
+            #     inputs=
+            #     [
+            #         "l0_digital_mobile_web_hourly",
+            #         "l0_digital_customer_profile_union_daily",
+            #         "l1_digital_aib_categories_clean",
+            #         "params:l1_digital_mobile_web_agg_category_timeband_night_catlv1",
+            #         "params:timeband_web_night"
+            #     ],
+            #     outputs="l1_digital_customer_web_category_agg_timeband_night_catlv_1",
+            #     tags="l1_digital_mobile_web_category_agg_timeband_Night",
+            # ),
+            # node(
+            #     func=l1_digital_mobile_web_category_agg_timeband_features,
+            #     inputs=
+            #     [
+            #         "l1_digital_union_daily_feature_for_mobile_web_catefory_timeband",
+            #         "l1_digital_mobile_web_category_agg_morning_timeband",
+            #     ],
+            #     outputs="l1_digital_mobile_web_category_agg_timeband_morning_catlv_1",
+            #     tags=["l1_digital_mobile_web_category_agg_timeband_features"],
+            # ),
         ],tags="digital_to_l1_digital_mobile_web_agg_timeband",
     )
 
