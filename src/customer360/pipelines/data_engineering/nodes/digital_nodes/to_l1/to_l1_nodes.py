57b251fd58715536a201afbb0d7d0b965973ff96
paipayon <paipayon2@gmail.com>
2021-06-03 22:38:47 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -219,9 +219,10 @@ def digital_mobile_app_category_agg_timeband(Mobile_app_timeband: DataFrame,Mobi
     Mobile_app_daily = Mobile_app_daily.withColumnRenamed("priority", 'priority_daily')
     logging.info("Dates to run for join time band and daily")
     Mobile_app_timeband = Mobile_app_timeband.join(Mobile_app_daily,
-        on=[Mobile_app_timeband.mobile_no == Mobile_app_daily.mobile_no ,Mobile_app_timeband.category_name == Mobile_app_daily.category_name ],
+        on=[Mobile_app_timeband.mobile_no == Mobile_app_daily.mobile_no ,Mobile_app_timeband.category_name == Mobile_app_daily.category_name,Mobile_app_timeband.event_partition_date == Mobile_app_daily.event_partition_date ],
         how="inner",
     )
+    logging.info("select column")
     Mobile_app_timeband = Mobile_app_timeband.select(Mobile_app_daily["subscription_identifier"],Mobile_app_daily["mobile_no"],Mobile_app_daily["category_name"],Mobile_app_timeband["priority"],"total_visit_count","total_visit_duration","total_volume_byte","total_upload_byte","total_visit_count_daily","total_visit_duration_daily","total_volume_byte_daily","total_download_byte_daily","total_upload_byte_daily",Mobile_app_timeband["event_partition_date"])
     logging.info("Dates to run for share")
     df_return = node_from_config(Mobile_app_timeband, mobile_app_timeband_sql_share)
