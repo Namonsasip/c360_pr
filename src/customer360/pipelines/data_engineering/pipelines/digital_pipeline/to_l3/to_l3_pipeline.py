9d9354bb039d085d83ac751d527e8824a21a4453
Veesuda <veesudr8@ascgp.postbox.in.th>
2021-06-07 16:04:44 +0700

    add digital_to_l3_app_agg_timeband_pipeline to pipeline

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l3/to_l3_pipeline.py
@@ -34,8 +34,6 @@ PLEASE DELETE THIS FILE ONCE YOU START WORKING ON YOUR OWN PROJECT!
 from kedro.pipeline import Pipeline, node
 from customer360.pipelines.data_engineering.nodes.digital_nodes.to_l3.to_l3_nodes import *
 from customer360.pipelines.data_engineering.nodes.digital_nodes.to_l3 import build_digital_l3_monthly_features
-from customer360.pipelines.data_engineering.nodes.digital_nodes.to_l3.to_l3_nodes import *
-
 
 def digital_to_l3_pipeline(**kwargs):
     return Pipeline(
