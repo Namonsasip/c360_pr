236932c9f9886a9eb0dab02b4ac9b5f24e53164d
Thatchapong Tunkam <64589464+BenzThatchapong@users.noreply.github.com>
2021-06-07 15:00:48 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/customer_profile_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/customer_profile_pipeline/to_l1/to_l1_pipeline.py
@@ -116,18 +116,19 @@ def customer_profile_to_l1_pipeline(**kwargs):
             #      "l1_touchpoints_service_order_profile_flag"
             #      ]
             # ),
-            node(
-                row_number_func1,
-                ["l1_customer_profile_union_daily_temp3",
-                 "l0_touchpoints_service_order_profile_for_l1_customer_profile_union_daily_feature",
-                 ],
-                ["int_l1_customer_profile_union_daily_temp3",
-                 "l1_touchpoints_service_order_profile_flag_temp"
-                 ]
-            ),
+            # node(
+            #     row_number_func1,
+            #     ["l1_customer_profile_union_daily_temp3",
+            #      "l0_touchpoints_service_order_profile_for_l1_customer_profile_union_daily_feature",
+            #      ],
+            #     ["int_l1_customer_profile_union_daily_temp3",
+            #      "l1_touchpoints_service_order_profile_flag_temp"
+            #      ]
+            # ),
             node(
                 def_feature_lot7_func,
-                ["int_l1_customer_profile_union_daily_temp3",
+                ["l1_customer_profile_union_daily_temp3",
+                 # "int_l1_customer_profile_union_daily_temp3",
                  "l1_touchpoints_service_order_profile_row_num",
                  "l1_profile_service_order_profile_pps_row_num"
                  ],
