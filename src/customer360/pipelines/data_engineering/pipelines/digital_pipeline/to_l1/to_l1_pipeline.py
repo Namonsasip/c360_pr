f03ed6f7ec5011c610b1cab8df3a451cf70084d4
Paipayon Sukcharoen <paipayon10@gmail.com>
2021-06-07 22:59:29 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -146,15 +146,15 @@ def digital_to_l1_cxense_traffic_daily_agg_pipeline(**kwargs):
         [
             node(
                 func=digital_cxense_traffic_clean,
-                inputs=["l0_digital_cxense_traffic_raw"],
-                outputs=["l1_digital_cxense_traffic"],
+                inputs="l0_digital_cxense_traffic_raw",
+                outputs="l1_digital_cxense_traffic",
                 tags=["digital_cxense_traffic_clean"],
             ),
             node(
                 func=digital_cxense_content_profile_clean,
-                inputs=["l0_digital_cxense_content_profile_raw",],
-                outputs=["l1_digital_cxense_content_profile"],
-                ags=["digital_cxense_content_profile_clean"],
+                inputs="l0_digital_cxense_content_profile_raw",
+                outputs="l1_digital_cxense_content_profile",
+                tags=["digital_cxense_content_profile_clean"],
             ),
             # node(
             #     func=digital_cxense_content_profile_mapping,
