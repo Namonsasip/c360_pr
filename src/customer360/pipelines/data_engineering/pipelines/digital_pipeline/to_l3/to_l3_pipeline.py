e8493365f87e930e51822f937284601c4fb45342
Paipayon10 <Paipayon10@gmail.com>
2021-06-07 09:45:37 +0700

    Update to_l3_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l3/to_l3_pipeline.py
@@ -144,16 +144,18 @@ def digital_to_l3_customer_relay_agg_monthly(**kwargs):
             # ),
         ]
     )
-
+    
 def digital_to_l3_digital_combine_agg_monthly(**kwargs):
     return Pipeline(
         [
             node(
                 func=digital_to_l3_digital_combine_agg_monthly,
-                inputs=["l1_digital_customer_combine_category_agg_daily_catlv_1","params:l3_digital_combine_monthly_feature_pipeline"],
+                inputs=[
+                    "l1_digital_customer_combine_category_agg_daily_catlv_1",
+                    "params:l3_digital_combine_monthly_feature_pipeline",
+                ],
                 outputs="l3_digital_customer_combine_category_agg_monthly_catlv_1",
-                tags="l3_digital_customer_combine_category_agg_monthly_catlv_1"
+                tags=["l3_digital_customer_combine_category_agg_monthly_catlv_1"],
             ),
         ], name="digital_to_l3_digital_mobile_combine_agg_monthly"
     )
-
