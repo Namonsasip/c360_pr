5e1b09ab08afaad9f64cbd234df1ac9d52a1a5e8
Thatchapong Tunkam <64589464+BenzThatchapong@users.noreply.github.com>
2021-06-07 15:19:05 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/customer_profile_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/customer_profile_pipeline/to_l1/to_l1_pipeline.py
@@ -117,15 +117,6 @@ def customer_profile_to_l1_pipeline(**kwargs):
                  ]
             ),
             node(
-                row_number_func1,
-                ["l1_customer_profile_union_daily_temp3",
-                 "l0_touchpoints_service_order_profile_for_l1_customer_profile_union_daily_feature",
-                 ],
-                ["int_l1_customer_profile_union_daily_temp3",
-                 "l1_touchpoints_service_order_profile_flag_temp"
-                 ]
-            ),
-            node(
                 def_feature_lot7_func,
                 ["int_l1_customer_profile_union_daily_temp3",
                  "l1_touchpoints_service_order_profile_row_num",
