e55d55e5300ebc883c0e2edce808c4c4a45ca2cd
paipayon <paipayon2@gmail.com>
2021-06-09 00:35:27 +0700

    Update to_l3_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
@@ -390,11 +390,25 @@ def digital_mobile_app_category_favorite_monthly(app_category_agg_daily: pyspark
         ],
         how="inner",
     )
+    app_category_agg_daily = app_category_agg_daily.select(
+        app_category_agg_daily["subscription_identifier"] ,
+        app_category_agg_daily["mobile_no"],
+        app_category_agg_daily["priority"],
+        app_category_agg_daily["total_visit_count"],
+        app_category_agg_daily["total_visit_duration"],
+        app_category_agg_daily["total_volume_byte"],
+        app_category_agg_daily_sql_total["sum_total_visit_count"],
+        app_category_agg_daily_sql_total["sum_total_visit_duration"],
+        app_category_agg_daily_sql_total["sum_total_volume_byte"],
+        app_category_agg_daily["start_of_month"])
     #---------------  sum cal fav ------------------
     logging.info("favorite ------- > cal")
     app_category_agg_daily_transection = node_from_config(app_category_agg_daily,sql_transection)
+    logging.info("favorite ------- > transection complete")
     app_category_agg_daily_duration = node_from_config(app_category_agg_daily,sql_duration)
+    logging.info("favorite ------- > duration complete")
     app_category_agg_daily_volume = node_from_config(app_category_agg_daily,sql_volume)
+    logging.info("favorite ------- > volume complete")
     #---------------  union ------------------
     logging.info("favorite ------- > union")
     df_return = app_category_agg_daily_transection.union(app_category_agg_daily_duration)
