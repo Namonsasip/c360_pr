12f898d21821ee9907784dacfeb6cf2ca0e420a6
PakawatPalanandMck <pakap571@epicc.postbox.in.th>
2021-06-08 17:48:03 +0700

    fix

--- src/customer360/utilities/config_parser.py
+++ src/customer360/utilities/config_parser.py
@@ -319,24 +319,21 @@ def l4_rolling_window(input_df: DataFrame, config: dict):
                     function=agg_function,
                     feature_column=each_feature_column,
                     window=create_monthly_lookback_window(1, config["partition_by"]),
-                    column_name="{}_{}_monthly_last_month".format(agg_function,
-                                                                  each_feature_column)
+                    column_name="{}_{}_monthly_last_month".format(agg_function, each_feature_column)
                 ))
 
                 features.append("{function}({feature_column}) over ({window}) as {column_name}".format(
                     function=agg_function,
                     feature_column=each_feature_column,
                     window=create_monthly_lookback_window(3, config["partition_by"]),
-                    column_name="{}_{}_monthly_last_two_month".format(agg_function,
-                                                                        each_feature_column)
+                    column_name="{}_{}_monthly_last_two_month".format(agg_function, each_feature_column)
                 ))
 
                 features.append("{function}({feature_column}) over ({window}) as {column_name}".format(
                     function=agg_function,
                     feature_column=each_feature_column,
                     window=create_monthly_lookback_window(3, config["partition_by"]),
-                    column_name="{}_{}_monthly_last_three_month".format(agg_function,
-                                                                        each_feature_column)
+                    column_name="{}_{}_monthly_last_three_month".format(agg_function, each_feature_column)
                 ))
 
     sql_stmt = sql_stmt.format(',\n'.join(features),
