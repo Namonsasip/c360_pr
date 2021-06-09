541c00b3719da0f7e219c5b40bff6d296be098f2
worrawut jitnumsab <j.worrawut@hotmail.com>
2021-06-09 16:48:28 +0700

    Update to_l3_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/revenue_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/revenue_pipeline/to_l3/to_l3_pipeline.py
@@ -145,28 +145,28 @@ def revenue_feature_to_l3_pipeline(**kwargs):
                  'params:l3_revenue_features_postpaid_ru_f_sum_revenue_by_service_monthly'],
                 'l3_revenue_features_postpaid_ru_f_sum_revenue_by_service_monthly_stg'
             ),
-            node(
-                l3_rename_sub_id_to_subscription_identifier,
-                ['l3_revenue_features_postpaid_ru_f_sum_revenue_by_service_monthly_stg',
-                 'params:l3_revenue_features_postpaid_ru_f_sum_revenue_by_service_monthly'],
-                'l3_revenue_features_postpaid_ru_f_sum_revenue_by_service_monthly_stg1'
-            ),
+            # node(
+            #     l3_rename_sub_id_to_subscription_identifier,
+            #     ['l3_revenue_features_postpaid_ru_f_sum_revenue_by_service_monthly_stg',
+            #      'params:l3_revenue_features_postpaid_ru_f_sum_revenue_by_service_monthly'],
+            #     'l3_revenue_features_postpaid_ru_f_sum_revenue_by_service_monthly_stg1'
+            # ),
             node(
                 node_from_config,
                 ['l0_revenue_prepaid_pru_f_revenue_allocate_usage',
                  'params:l3_revenue_features_prepaid_pru_f_revenue_allocate_usage_monthly'],
                 'l3_revenue_features_prepaid_pru_f_revenue_allocate_usage_monthly_stg'
-            ),
-            node(
-                l3_rename_c360_subscription_identifier_to_subscription_identifier,
-                ['l3_revenue_features_prepaid_pru_f_revenue_allocate_usage_monthly_stg',
-                 'params:l3_revenue_features_prepaid_pru_f_revenue_allocate_usage_monthly'],
-                'l3_revenue_features_prepaid_pru_f_revenue_allocate_usage_monthly_stg1'
-            ),
+            # ),
+            # node(
+            #     l3_rename_c360_subscription_identifier_to_subscription_identifier,
+            #     ['l3_revenue_features_prepaid_pru_f_revenue_allocate_usage_monthly_stg',
+            #      'params:l3_revenue_features_prepaid_pru_f_revenue_allocate_usage_monthly'],
+            #     'l3_revenue_features_prepaid_pru_f_revenue_allocate_usage_monthly_stg1'
+            # ),
             node(
                 l3_merge_postpaid_ru_f_sum_revenue_by_service_with_prepaid_pru_f_revenue_allocate_usage,
-                ['l3_revenue_features_prepaid_pru_f_revenue_allocate_usage_monthly_stg1',
-                 'l3_revenue_features_postpaid_ru_f_sum_revenue_by_service_monthly_stg1',
+                ['l3_revenue_features_prepaid_pru_f_revenue_allocate_usage_monthly_stg',
+                 'l3_revenue_features_postpaid_ru_f_sum_revenue_by_service_monthly_stg',
                  'params:l3_revenue_features_for_prepaid_postpaid_revenue'],
                 'l3_revenue_features_for_prepaid_postpaid_revenue'
             ),
