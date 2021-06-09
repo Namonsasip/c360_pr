8545489bfc3b7b7e3c6ab7e243ed8963da874b5a
Arin Sangthong <sangthongarin@gmail.com>
2021-06-09 21:14:06 +0700

    avg_set3

--- src/customer360/pipelines/data_engineering/pipelines/billing_pipeline/to_l4/to_l4_pipeline_weekly.py
+++ src/customer360/pipelines/data_engineering/pipelines/billing_pipeline/to_l4/to_l4_pipeline_weekly.py
@@ -85,18 +85,18 @@ def billing_to_l4_pipeline_weekly(**kwargs):
             #      "params:l4_billing_top_up_channels_avg_set1"],
             #     "l4_billing_rolling_window_top_up_channels_avg_set1"
             # ),
-            node(
-                l4_rolling_window,
-                ["l2_billing_and_payments_weekly_top_up_channels_for_l4_billing_rolling_window_top_up_channels",
-                 "params:l4_billing_top_up_channels_avg_set2"],
-                "l4_billing_rolling_window_top_up_channels_avg_set2"
-            ),
             # node(
             #     l4_rolling_window,
             #     ["l2_billing_and_payments_weekly_top_up_channels_for_l4_billing_rolling_window_top_up_channels",
-            #      "params:l4_billing_top_up_channels_avg_set3"],
-            #     "l4_billing_rolling_window_top_up_channels_avg_set3"
+            #      "params:l4_billing_top_up_channels_avg_set2"],
+            #     "l4_billing_rolling_window_top_up_channels_avg_set2"
             # ),
+            node(
+                l4_rolling_window,
+                ["l2_billing_and_payments_weekly_top_up_channels_for_l4_billing_rolling_window_top_up_channels",
+                 "params:l4_billing_top_up_channels_avg_set3"],
+                "l4_billing_rolling_window_top_up_channels_avg_set3"
+            ),
             # node(
             #     l4_rolling_window,
             #     ["l2_billing_and_payments_weekly_top_up_channels_for_l4_billing_rolling_window_top_up_channels",
