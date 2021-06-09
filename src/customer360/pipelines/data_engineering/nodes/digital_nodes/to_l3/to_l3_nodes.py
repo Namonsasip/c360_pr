d4da62d450e4910c2a3727fe2c749664349b3abe
Paipayon10 <Paipayon10@gmail.com>
2021-06-09 09:32:04 +0700

    Update to_l3_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
@@ -391,13 +391,13 @@ def digital_mobile_app_category_favorite_monthly(app_category_agg_daily: pyspark
         how="inner",
     )
     app_category_agg_daily = app_category_agg_daily.select(
-        "subscription_identifier",
-        "mobile_no",
-        "priority",
-        "start_of_month",
-        "total_visit_count",
-        "total_visit_duration",
-        "total_volume_byte",
+        app_category_agg_daily["subscription_identifier"],
+        app_category_agg_daily["mobile_no"],
+        app_category_agg_daily["priority"],
+        app_category_agg_daily["start_of_month"],
+        app_category_agg_daily["total_visit_count"],
+        app_category_agg_daily["total_visit_duration"],
+        app_category_agg_daily["total_volume_byte"],
         app_category_agg_daily_sql_total["sum_total_visit_count"],
         app_category_agg_daily_sql_total["sum_total_visit_duration"],
         app_category_agg_daily_sql_total["sum_total_volume_byte"]
