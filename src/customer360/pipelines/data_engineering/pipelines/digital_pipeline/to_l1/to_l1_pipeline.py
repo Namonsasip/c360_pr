d536a297d97f0276e4ffbd79da4b5cc09ce7d821
PakawatPalanandMck <pakap571@epicc.postbox.in.th>
2021-06-10 17:02:41 +0700

    update l3 timeband web

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -258,16 +258,16 @@ def digital_to_l1_digital_mobile_web_agg_timeband(**kwargs):
                 outputs="l1_digital_customer_web_category_agg_timeband_night_catlv_1",
                 tags="l1_digital_mobile_web_category_agg_timeband_Night",
             ),
-            # node(
-            #     func=l1_digital_mobile_web_category_agg_timeband_features,
-            #     inputs=
-            #     [
-            #         "l1_digital_union_daily_feature_for_mobile_web_category_timeband",
-            #         "l1_digital_customer_web_category_agg_timeband_night_catlv_1",
-            #     ],
-            #     outputs="l1_digital_mobile_web_category_agg_timeband_night_catlv_1",
-            #     tags=["l1_digital_mobile_web_category_agg_timeband_features_Night"],
-            # ),
+            node(
+                func=l1_digital_mobile_web_category_agg_timeband_features,
+                inputs=
+                [
+                    "l1_digital_union_daily_feature_for_mobile_web_category_timeband",
+                    "l1_digital_customer_web_category_agg_timeband_night_catlv_1",
+                ],
+                outputs="l1_digital_mobile_web_category_agg_timeband_night_catlv_1",
+                tags=["l1_digital_mobile_web_category_agg_timeband_features_Night"],
+            ),
         ], tags="digital_to_l1_digital_mobile_web_agg_timeband_morning",
     )
 
