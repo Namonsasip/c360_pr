32ffe7ce5f26185b24cae8a1d19a49398301eb7a
str1k <Ni4810123>
2021-06-10 07:30:45 +0700

    add new report to run compare between TG and CG

--- src/du/reporting/du_report_pipeline.py
+++ src/du/reporting/du_report_pipeline.py
@@ -197,9 +197,9 @@ def create_du_weekly_revenue_uplift_report_pipeline() -> Pipeline:
                     "control_group_tbl": "data_upsell_usecase_control_group_2021",
                     "l5_du_offer_weekly_low_score_list": "l5_du_offer_weekly_low_score_list",
                 },
-                outputs="l5_data_upsell_ontop_revenue_weekly_report_group_combine_hs",
-                name="l5_data_upsell_ontop_revenue_weekly_report_group_combine_hs",
-                tags=["l5_data_upsell_ontop_revenue_weekly_report_group_combine_hs", ],
+                outputs="l5_data_upsell_ontop_revenue_weekly_report_group_sandbox_tg_cg_combine_hs",
+                name="l5_data_upsell_ontop_revenue_weekly_report_group_sandbox_tg_cg_combine_hs",
+                tags=["l5_data_upsell_ontop_revenue_weekly_report_group_sandbox_tg_cg_combine_hs", ],
             ),
             # node(
             #     partial(
