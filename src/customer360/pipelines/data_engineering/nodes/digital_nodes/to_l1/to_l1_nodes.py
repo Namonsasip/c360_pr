5843396309cc94162df38b3433d793e98b8bb7a5
Navenavy39 <navenavy39@hotmail.com>
2021-06-10 11:16:50 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -358,10 +358,7 @@ def l1_digital_customer_web_category_agg_timeband(mobile_web_hourly_raw: DataFra
     mobile_web_daily = mobile_web_daily.withColumnRenamed("total_upload_byte", 'total_upload_byte_daily')
     mobile_web_daily = mobile_web_daily.withColumnRenamed("priority", 'priority_daily')
 
-    mobile_web_hourly_raw = mobile_web_hourly_raw.join(mobile_web_daily, on=[mobile_web_hourly_raw.mobile_no == mobile_web_daily.mobile_no,
-                                                       mobile_web_hourly_raw.category_name == mobile_web_daily.category_name],
-                                                   how="inner",
-                                                   )
+    mobile_web_hourly_raw = mobile_web_hourly_raw.join(mobile_web_daily, on=[mobile_web_hourly_raw.mobile_no == mobile_web_daily.mobile_no],how="inner",)
 
     mobile_web_hourly_raw = mobile_web_hourly_raw.select(mobile_web_daily.subscription_identifier,
                                                mobile_web_daily.mobile_no,
