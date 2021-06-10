3a9c17622b73f47c6b6c30e58cd5cdee51fe4f2e
Navenavy39 <navenavy39@hotmail.com>
2021-06-10 16:31:14 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -373,7 +373,6 @@ def l1_digital_customer_web_category_agg_timeband(mobile_web_hourly_raw: DataFra
                                                                                                                                                                     "total_download_byte_daily",
                                                                                                                                                                     "total_upload_byte_daily",
                                                                                                                                                                     "event_partition_date")
-
     df_return = node_from_config(mobile_web_hourly_raw, mobile_web_timeband_sql_share)
     return df_return
 
