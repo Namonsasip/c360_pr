d6c01a6bbdb0c35ac717fcc634a1fd1b775585ed
Thatchapong Tunkam <64589464+BenzThatchapong@users.noreply.github.com>
2021-06-11 12:27:38 +0700

    Update spark_dbfs_dataset.py

--- src/datasets/spark_dbfs_dataset.py
+++ src/datasets/spark_dbfs_dataset.py
@@ -822,8 +822,7 @@ class SparkDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
             logging.info("Skipping incremental load mode because incremental_flag is 'master'")
             load_path = _strip_dbfs_prefix(self._fs_prefix + str(self._get_load_path()))
             p_increment_flag_load = self._increment_flag_load
-            spark = self._get_spark()
-            # logging.info("increment_flag: {}".format(p_increment_flag_load))
+            logging.info("increment_flag: {}".format(p_increment_flag_load))
             if (running_environment == "on_cloud"):
                 if ("/" == load_path[-1:]):
                     load_path = load_path
@@ -859,7 +858,6 @@ class SparkDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
                     df = self._get_spark().read.option("multiline", "true").option("mode", "PERMISSIVE").option(
                         "inferSchema", "true").option(
                         "basePath", base_filepath).load(list_path, self._file_format)
-
             else:
                 if ("/" == load_path[-1:]):
                     load_path = load_path
@@ -882,14 +880,14 @@ class SparkDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
                             list_temp = subprocess.check_output(
                                 "hadoop fs -ls -d " + load_path + "*/ |grep C360 |awk -F' ' '{print $NF}' |grep Benz",
                                 shell=True).splitlines()
+
                 except:
                     list_temp = ""
-                list_path = ""
                 if (list_temp == ""):
-                    list_path = "no_partition"
+                    list_path = ("no_partition")
                 else:
                     for read_path in list_temp:
-                        list_path == (str(read_path)[2:-1])
+                        list_path = (str(read_path)[2:-1])
 
                 base_filepath = load_path
                 logging.info("basePath: {}".format(base_filepath))
@@ -897,11 +895,12 @@ class SparkDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
                 logging.info("file_format: {}".format(self._file_format))
                 logging.info("Fetching source data")
                 if ("no_partition" == list_path):
-                    df = spark.read.option("multiline", "true").option("mode", "PERMISSIVE").option(
-                            "basePath", base_filepath).load(load_path)
+                    df = self._get_spark().read.option("multiline", "true").option("mode", "PERMISSIVE").option(
+                        "inferSchema", "true").load(load_path, self._file_format)
                 else:
-                    df = spark.read.option("multiline", "true").option("mode", "PERMISSIVE").option(
-                            "basePath", base_filepath).load(list_path)
+                    df = self._get_spark().read.option("multiline", "true").option("mode", "PERMISSIVE").option(
+                        "inferSchema", "true").option(
+                        "basePath", base_filepath).load(list_path, self._file_format)
             return df
 
 
