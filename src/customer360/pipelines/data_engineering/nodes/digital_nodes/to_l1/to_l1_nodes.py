8afc0cae4cfdb2005e565fea0d48604490ed632e
Navenavy39 <navenavy39@hotmail.com>
2021-06-11 17:02:27 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -374,6 +374,7 @@ def l1_digital_customer_web_category_agg_timeband(mobile_web_hourly_raw: DataFra
                                                                                                                                                                         "total_upload_byte_daily",
                                                                                                                                                                         mobile_web_hourly_raw.event_partition_date)
     df_return = node_from_config(mobile_web_hourly_raw, mobile_web_timeband_sql_share)
+    df_return.count()
     return df_return
 
 ################## Timebrand join subscription identifier ###########################
