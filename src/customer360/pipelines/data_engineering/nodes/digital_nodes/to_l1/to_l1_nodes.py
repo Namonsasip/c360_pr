0c36c2262ae9d3f43c4d6a2a3fbf4201d4b9416a
Navenavy39 <navenavy39@hotmail.com>
2021-06-11 21:39:09 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -361,7 +361,7 @@ def l1_digital_customer_web_category_agg_timeband(mobile_web_hourly_raw: DataFra
     mobile_web_hourly_raw = mobile_web_hourly_raw.join(mobile_web_daily_raw,
                                                        on=[mobile_web_hourly_raw.mobile_no == mobile_web_daily_raw.mobile_no,
                                                            mobile_web_hourly_raw.category_name == mobile_web_daily_raw.category_name],
-                                                       how="inner").select(mobile_web_daily_raw.mobile_web_daily_raw,
+                                                       how="inner").select(mobile_web_daily_raw.subscription_identifier,
                                                        mobile_web_daily_raw.mobile_no,
                                                        mobile_web_daily_raw.category_name,
                                                        mobile_web_hourly_raw.priority,
