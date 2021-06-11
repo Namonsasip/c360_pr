df581b9ba173b0e5b2d8de9984f7c3b4e939b98e
PoomtepR <64820619+PoomtepR@users.noreply.github.com>
2021-06-11 11:30:45 +0700

    Update to_l3_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l3/to_l3_pipeline.py
@@ -124,8 +124,8 @@ def geo_to_l3_pipeline(**kwargs):
             ),
             node(
                 l3_geo_visit_ais_store_location_monthly,
-                ["int_l3_geo_ais_store_near_home_work_monthly_2",
-                 "int_l3_geo_ais_store_near_top3_visit_monthly_2",
+                ["int_l3_geo_ais_store_near_home_work_monthly",
+                 "int_l3_geo_ais_store_near_top3_visit_monthly",
                  "params:l3_geo_visit_ais_store_location_monthly"
                  ],
                 "l3_geo_visit_ais_store_location_monthly"
