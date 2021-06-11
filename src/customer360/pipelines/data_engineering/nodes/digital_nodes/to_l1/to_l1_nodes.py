df1163e380551d7f911353eef1cbe993d4a01f6a
Navenavy39 <navenavy39@hotmail.com>
2021-06-11 20:50:42 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -363,11 +363,11 @@ def l1_digital_customer_web_category_agg_timeband(mobile_web_hourly_raw: DataFra
                                                        mobile_web_hourly_raw.mobile_no,
                                                        mobile_web_hourly_raw.category_name,
                                                        mobile_web_hourly_raw.priority,
-                                                       mobile_web_daily_raw.total_visit_count,
-                                                       mobile_web_daily_raw.total_visit_duration,
-                                                       mobile_web_daily_raw.total_volume_byte,
-                                                       mobile_web_daily_raw.total_download_byte,
-                                                       mobile_web_daily_raw.total_upload_byte,
+                                                       mobile_web_hourly_raw.total_visit_count,
+                                                       mobile_web_hourly_raw.total_visit_duration,
+                                                       mobile_web_hourly_raw.total_volume_byte,
+                                                       mobile_web_hourly_raw.total_download_byte,
+                                                       mobile_web_hourly_raw.total_upload_byte,
                                                        mobile_web_hourly_raw.event_partition_date)
 
     df_return = node_from_config(mobile_web_hourly_raw)
