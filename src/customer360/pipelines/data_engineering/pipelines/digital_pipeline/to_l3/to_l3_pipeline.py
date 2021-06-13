470bd80f06cce27bcfe90bd8cb1b9af7f8853883
Navenavy39 <navenavy39@hotmail.com>
2021-06-13 15:10:33 +0700

    Update to_l3_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l3/to_l3_pipeline.py
@@ -278,14 +278,14 @@ def digital_to_l3_digital_mobile_web_agg_timeband(**kwargs):
                 outputs="l3_digital_customer_web_category_agg_timeband_evening_catlv_1",
                 tags="l3_digital_customer_web_category_agg_timeband"
             ),
-            # node(
-            #     func=l3_digital_mobile_web_category_agg_timeband,
-            #     inputs=["l1_digital_customer_web_category_agg_timeband_night_catlv_1",
-            #             "l3_digital_customer_web_agg_monthly",
-            #             "params:l3_digital_customer_web_agg_category_timeband_sql"],
-            #     outputs="l3_digital_customer_web_category_agg_timeband_night_catlv_1",
-            #     tags="l3_digital_customer_web_category_agg_timeband"
-            # ),
+            node(
+                func=l3_digital_mobile_web_category_agg_timeband,
+                inputs=["l1_digital_customer_web_category_agg_timeband_night_catlv_1",
+                        "l3_digital_customer_web_agg_monthly",
+                        "params:l3_digital_customer_web_agg_category_timeband_sql"],
+                outputs="l3_digital_customer_web_category_agg_timeband_night_catlv_1",
+                tags="l3_digital_customer_web_category_agg_timeband"
+            ),
         ],
     )
 
