865c885ef6db10be9391f7fd6eba00a694fe7b03
paipayon <paipayon2@gmail.com>
2021-06-14 10:15:52 +0700

    Update to_l3_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
@@ -873,7 +873,18 @@ def digital_mobile_app_category_agg_timeband_monthly(Mobile_app_timeband_monthly
     ################################## combine_score_monthly ################################
 def l3_digital_mobile_combine_category_score_monthly(app_category_fav_monthly: pyspark.sql.DataFrame,web_category_fav_monthly: pyspark.sql.DataFrame,sql_total: Dict[str, Any],sql_sum: Dict[str, Any]):
     #join app & web 
-    app_category_fav_monthly = app_category_fav_monthly.join(web_category_fav_monthly,on=["subscription_identifier","category_name","start_of_month",],how="inner")
+    app_category_fav_monthly = app_category_fav_monthly.alias('app_category_fav_monthly').join(web_category_fav_monthly.alias('web_category_fav_monthly'),on=["subscription_identifier","category_name","start_of_month",],how="inner")
+    app_category_fav_monthly = app_category_fav_monthly.select(
+        "subscription_identifier",
+        "category_name",
+        "start_of_month",
+        "total_visit_count",
+        "total_visit_duration",
+        "total_volume_byte",
+        "sum_total_visit_count",
+        "sum_total_visit_duration",
+        "sum_total_volume_byte"
+        )
     #sprit by favorite by
     app_category_fav_monthly_transaction = app_category_fav_monthly.filter(app_category_fav_monthly["favorite_by"] == 'Transaction')
     app_category_fav_monthly_duration = app_category_fav_monthly.filter(app_category_fav_monthly["favorite_by"] == 'Duration')
