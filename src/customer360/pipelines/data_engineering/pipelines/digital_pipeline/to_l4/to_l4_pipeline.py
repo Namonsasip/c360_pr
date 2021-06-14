ee9d334b2fcee1e1540d19ae28b803a9241c3341
paipayon <paipayon2@gmail.com>
2021-06-14 22:08:11 +0700

    Update to_l4_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l4/to_l4_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l4/to_l4_pipeline.py
@@ -34,7 +34,7 @@ PLEASE DELETE THIS FILE ONCE YOU START WORKING ON YOUR OWN PROJECT!
 from kedro.pipeline import Pipeline, node
 
 from customer360.utilities.config_parser import l4_rolling_window, l4_rolling_ranked_window
-from customer360.pipelines.data_engineering.nodes.digital_nodes.to_l4.to_l4_nodes import customer_app_category_windows
+from customer360.pipelines.data_engineering.nodes.digital_nodes.to_l4.to_l4_nodes import *
 
 
 def digital_to_l4_monthly_pipeline(**kwargs):
