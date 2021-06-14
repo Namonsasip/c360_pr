a4aaeddcc057ab4c599080c0a45fa4a63c89bef3
paipayon <paipayon2@gmail.com>
2021-06-14 21:54:19 +0700

    Update

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l4/to_l4_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l4/to_l4_pipeline.py
@@ -137,11 +137,13 @@ def digital_to_l4_digital_customer_app_category_monthly(**kwargs):
         [
             node(
                 func=customer_app_category_windows,
-                inputs=["l3_digital_customer_app_category_agg_monthly_for_l4_windows_app_category_agg_monthly",
-                    "params:customer_app_category_monthly_groupby"
-                    "params:customer_app_category_monthly_feature"],
-                outputs="l4_digital_mobile_web_agg_monthly_features",
+                inputs=[
+                    "l3_digital_customer_app_category_agg_monthly_for_l4_windows_app_category_agg_monthly",
+                    "params:customer_app_category_monthly_groupby",
+                    "params:customer_app_category_monthly_feature"
+                ],
+                outputs="l4_digital_customer_app_category_agg_monthly",
                 tags=["l4_windows_app_category_agg_monthly"],
             ),
-        ],name="l4_digital_to_l4_digital_customer_app_category_monthly,
+        ], name="l4_digital_to_l4_digital_customer_app_category_monthly"
     )
