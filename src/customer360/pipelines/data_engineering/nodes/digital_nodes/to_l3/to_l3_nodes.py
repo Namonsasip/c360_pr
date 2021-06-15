f6646fca6297d28a96c9fd104881d9181dacdbbb
Navenavy39 <navenavy39@hotmail.com>
2021-06-15 23:06:18 +0700

    Update to_l3_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
@@ -104,6 +104,7 @@ def l3_digital_mobile_web_category_agg_timeband (mobile_web_daily_agg_timeband: 
         return get_spark_empty_df()
     if check_empty_dfs([mobile_web_agg_monthly]):
         return get_spark_empty_df()
+
     mobile_web_daily_agg_timeband = mobile_web_daily_agg_timeband.withColumn("start_of_month", f.to_date(f.date_trunc('month', "event_partition_date")))
     mobile_web_timeband_monthly = mobile_web_daily_agg_timeband.groupBy("subscription_identifier","mobile_no","category_name","priority"
                                                                        ,"start_of_month").agg(
@@ -113,6 +114,7 @@ def l3_digital_mobile_web_category_agg_timeband (mobile_web_daily_agg_timeband: 
         f.sum("total_download_byte").alias("total_download_byte"),
         f.sum("total_upload_byte").alias("total_upload_byte")
         )
+
     mobile_web_agg_monthly = mobile_web_agg_monthly.withColumnRenamed("total_visit_count", 'total_visit_count_monthly')
     mobile_web_agg_monthly = mobile_web_agg_monthly.withColumnRenamed("total_visit_duration", 'total_visit_duration_monthly')
     mobile_web_agg_monthly = mobile_web_agg_monthly.withColumnRenamed("total_volume_byte", 'total_volume_byte_monthly')
@@ -193,9 +195,9 @@ def digital_customer_app_category_agg_timeband_monthly(customer_app_agg_timeband
 
 
         customer_app_agg_timeband = customer_app_agg_timeband.withColumn("start_of_month", f.to_date(f.date_trunc('month', "event_partition_date")))
-        customer_app_agg_timeband = customer_app_agg_timeband.groupBy("subscription_identifier", "mobile_no",
+        customer_app_agg_timeband_monthly = customer_app_agg_timeband.groupBy("subscription_identifier", "mobile_no",
                                                                         "category_name", "priority"
-                                                                        , "start_of_month").agg(
+                                                                        ,"start_of_month").agg(
         f.sum("total_visit_count").alias("total_visit_count"),
         f.sum("total_visit_duration").alias("total_visit_duration"),
         f.sum("total_volume_byte").alias("total_volume_byte"),
@@ -212,18 +214,19 @@ def digital_customer_app_category_agg_timeband_monthly(customer_app_agg_timeband
     customer_app_agg = customer_app_agg.withColumnRenamed("total_upload_byte", 'total_upload_byte_monthly')
     customer_app_agg = customer_app_agg.withColumnRenamed("priority", 'priorityt_monthly')
 
-    customer_app_agg_timeband = customer_app_agg_timeband.join(customer_app_agg,
+    customer_app_agg_timeband_monthly = customer_app_agg_timeband_monthly.join(customer_app_agg,
                                                                    on=[
-                                                                       customer_app_agg_timeband.subscription_identifier == customer_app_agg.subscription_identifier,
-                                                                       customer_app_agg_timeband.category_name == customer_app_agg.category_name,
-                                                                       customer_app_agg_timeband.start_of_month == customer_app_agg.start_of_month],
+                                                                       customer_app_agg_timeband_monthly.subscription_identifier == customer_app_agg.subscription_identifier,
+                                                                       customer_app_agg_timeband_monthly.category_name == customer_app_agg.category_name,
+                                                                       customer_app_agg_timeband_monthly.start_of_month == customer_app_agg.start_of_month
+                                                                   ],
                                                                    how="inner",
                                                                    )
 
-    customer_app_agg_timeband = customer_app_agg_timeband.select(customer_app_agg["subscription_identifier"],
+    customer_app_agg_timeband_monthly = customer_app_agg_timeband_monthly.select(customer_app_agg["subscription_identifier"],
                                                                      customer_app_agg["mobile_no"],
                                                                      customer_app_agg["category_name"],
-                                                                     customer_app_agg_timeband["priority"],
+                                                                     customer_app_agg_timeband_monthly["priority"],
                                                                      "total_visit_count",
                                                                      "total_visit_duration",
                                                                      "total_volume_byte",
@@ -234,9 +237,9 @@ def digital_customer_app_category_agg_timeband_monthly(customer_app_agg_timeband
                                                                      "total_volume_byte_monthly",
                                                                      "total_download_byte_monthly",
                                                                      "total_upload_byte_monthly",
-                                                                     customer_app_agg_timeband["start_of_month"])
+                                                                     customer_app_agg_timeband_monthly["start_of_month"])
 
-    df_return = node_from_config(customer_app_agg_timeband, customer_app_timeband_monthly_share_sql)
+    df_return = node_from_config(customer_app_agg_timeband_monthly, customer_app_timeband_monthly_share_sql)
     return df_return
 
 
