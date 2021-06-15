b8ed4f7e45047e0764c7246602da56af00fe98a0
Navenavey39 <36034923+Navenavey39@users.noreply.github.com>
2021-06-15 15:14:17 +0700

    update web category daily
    update web category daily

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -253,7 +253,7 @@ def l1_digital_customer_web_category_agg_daily(
         mobile_web_daily_raw: DataFrame,
         aib_categories_clean: DataFrame,
         cxense_daily: DataFrame,
-        web_sql_sum: dict
+        # web_sql_sum: dict
 ) -> DataFrame:
     ##check missing data##
     if check_empty_dfs([mobile_web_daily_raw]):
@@ -286,8 +286,8 @@ def l1_digital_customer_web_category_agg_daily(
 
     df_mobile_web_daily_category_agg_partition = df_mobile_web_daily_category_agg.withColumnRenamed("partition_date", "event_partition_date")
 
-    cxense_daily = cxense_daily.withColumn("total_volume_byte", f.lit(None).cast("decimal(35,4)")).withColumn(
-        "total_download_byte", f.lit(None).cast("decimal(35,4)")).withColumn("total_upload_byte",f.lit(None).cast("decimal(35,4)"))
+    cxense_daily = cxense_daily.withColumn("total_volume_byte", f.lit(0).cast("decimal(35,4)")).withColumn(
+        "total_download_byte", f.lit(0).cast("decimal(35,4)")).withColumn("total_upload_byte",f.lit(0).cast("decimal(35,4)")).withColumn("total_visit_count",f.lit(0).cast("decimal(35,4)")).withColumn("total_visit_duration",f.lit(0).cast("decimal(35,4)"))
     cxense_daily = cxense_daily.select("subscription_identifier",
                                        "mobile_no",
                                        "category_name",
@@ -300,7 +300,7 @@ def l1_digital_customer_web_category_agg_daily(
                                        "event_partition_date")
 
     df_return = df_mobile_web_daily_category_agg_partition.union(cxense_daily)
-    df_return = node_from_config(df_return, web_sql_sum)
+    # df_return = node_from_config(df_return, web_sql_sum)
 
     return df_return
 
