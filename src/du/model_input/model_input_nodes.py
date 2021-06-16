ea66b75474f50c76ffe26d7e523b471800064d34
Chayaphat <chayaphn@ais.co.th>
2021-06-16 12:00:30 +0700

    - Bug fix: Add single quote to date()

--- src/du/model_input/model_input_nodes.py
+++ src/du/model_input/model_input_nodes.py
@@ -27,7 +27,7 @@ def node_l5_du_target_variable_table_new(
     #     "SELECT * FROM c360_l0.campaign_tracking_contact_list_pre WHERE date(contact_date) >= date('2020-10-01') AND date(contact_date) <= date('2021-01-20')"
     # )
 
-    l0_campaign_tracking_contact_list_pre_full_load = l0_campaign_tracking_contact_list_pre_full_load.where(f"date(contact_date) >= date({starting_date})")
+    l0_campaign_tracking_contact_list_pre_full_load = l0_campaign_tracking_contact_list_pre_full_load.where(f"date(contact_date) >= date('{starting_date}')")
     latest_campaign_update = l0_campaign_tracking_contact_list_pre_full_load.groupby(
         "subscription_identifier", "campaign_child_code", "contact_date"
     ).agg(F.max("update_date").alias("update_date"))
