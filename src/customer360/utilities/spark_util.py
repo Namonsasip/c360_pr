36c3c7c0c64a1108f524f164cd936b8a21316a59
Thatchapong Tunkam <64589464+BenzThatchapong@users.noreply.github.com>
2021-06-16 09:45:00 +0700

    Update spark_util.py

--- src/customer360/utilities/spark_util.py
+++ src/customer360/utilities/spark_util.py
@@ -20,18 +20,26 @@ def get_spark_session() -> SparkSession:
     :return:
     """
     if running_environment.lower() == 'on_premise':
-        CNTX = load_context(Path.cwd(), env=conf)
-        parameter = CNTX.catalog.load("params:spark_conf")
-        spark_conf = SparkConf().setAll(parameter.items())
-
-        spark_session_conf = (
-            SparkSession.builder.appName(
-                "{}_{}".format(PROJECT_NAME, getpass.getuser())
-            )
-                .enableHiveSupport()
-                .config(conf=spark_conf)
-        )
-        spark = spark_session_conf.master(running_user).getOrCreate()
+        # CNTX = load_context(Path.cwd(), env=conf)
+        # parameter = CNTX.catalog.load("params:spark_conf")
+        # spark_conf = SparkConf().setAll(parameter.items())
+        #
+        # spark_session_conf = (
+        #     SparkSession.builder.appName(
+        #         "{}_{}".format(PROJECT_NAME, getpass.getuser())
+        #     )
+        #         .enableHiveSupport()
+        #         .config(conf=spark_conf)
+        # )
+        # spark = spark_session_conf.master(running_user).getOrCreate()
+        spark = SparkSession.builder.getOrCreate()
+        spark.conf.set("spark.sql.parquet.binaryAsString", "true")
+        # pyarrow is not working so disable it for now
+        spark.conf.set("spark.sql.execution.arrow.enabled", "false")
+        # Dont delete this line. This allow spark to only overwrite the partition
+        # saved to parquet instead of entire table folder
+        spark.conf.set("spark.sql.sources.partitionOverwriteMode", "DYNAMIC")
+        spark.conf.set("spark.sql.autoBroadcastJoinThreshold", -1)
 
     else:
         spark = SparkSession.builder.getOrCreate()
