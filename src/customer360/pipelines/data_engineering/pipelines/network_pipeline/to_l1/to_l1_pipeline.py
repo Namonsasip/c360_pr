7e37cc919b8cfebf646b778ceff89c003f604819
Thatchapong Tunkam <64589464+BenzThatchapong@users.noreply.github.com>
2021-06-16 13:57:33 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
@@ -146,28 +146,28 @@ def network_to_l1_pipeline(**kwargs):
                  "params:exception_partitions_list_for_l1_network_file_transfer_cqi"],
                 "l1_network_file_transfer_cqi"
             ),
-            node(
-                build_network_cei_voice_qoe_incoming,
-                [
-                    "l0_network_sdr_dyn_cea_cei_qoe_cell_usr_voice_1day_for_l1_network_cei_voice_qoe_incoming",
-                    "l0_network_sdr_dyn_cea_cei_qoe_cell_usr_volte_1day_for_l1_network_cei_voice_qoe_incoming",
-                    "l0_network_xdr_cs_cdr_ims_mt_call_leg_sip_for_l1_network_cei_voice_qoe_incoming",
-                    "l1_customer_profile_union_daily_feature_for_l1_network_cei_voice_qoe_incoming",
-                    "params:l1_network_cei_voice_qoe_incoming",
-                    "params:exception_partition_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_voice_1day",
-                    "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_volte_1day"],
-                "l1_network_cei_voice_qoe_incoming"
-            ),
-            node(
-                build_network_cei_voice_qoe_outgoing,
-                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_voice_1day_for_l1_network_cei_voice_qoe_outgoing",
-                 "l0_network_sdr_dyn_cea_cei_qoe_cell_usr_volte_1day_for_l1_network_cei_voice_qoe_outgoing",
-                 "l1_customer_profile_union_daily_feature_for_l1_network_cei_voice_qoe_outgoing",
-                 "params:l1_network_cei_voice_qoe_outgoing",
-                 "params:exception_partition_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_voice_1day",
-                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_volte_1day"],
-                "l1_network_cei_voice_qoe_outgoing"
-            ),
+            # node(
+            #     build_network_cei_voice_qoe_incoming,
+            #     [
+            #         "l0_network_sdr_dyn_cea_cei_qoe_cell_usr_voice_1day_for_l1_network_cei_voice_qoe_incoming",
+            #         "l0_network_sdr_dyn_cea_cei_qoe_cell_usr_volte_1day_for_l1_network_cei_voice_qoe_incoming",
+            #         "l0_network_xdr_cs_cdr_ims_mt_call_leg_sip_for_l1_network_cei_voice_qoe_incoming",
+            #         "l1_customer_profile_union_daily_feature_for_l1_network_cei_voice_qoe_incoming",
+            #         "params:l1_network_cei_voice_qoe_incoming",
+            #         "params:exception_partition_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_voice_1day",
+            #         "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_volte_1day"],
+            #     "l1_network_cei_voice_qoe_incoming"
+            # ),
+            # node(
+            #     build_network_cei_voice_qoe_outgoing,
+            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_voice_1day_for_l1_network_cei_voice_qoe_outgoing",
+            #      "l0_network_sdr_dyn_cea_cei_qoe_cell_usr_volte_1day_for_l1_network_cei_voice_qoe_outgoing",
+            #      "l1_customer_profile_union_daily_feature_for_l1_network_cei_voice_qoe_outgoing",
+            #      "params:l1_network_cei_voice_qoe_outgoing",
+            #      "params:exception_partition_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_voice_1day",
+            #      "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_volte_1day"],
+            #     "l1_network_cei_voice_qoe_outgoing"
+            # ),
             node(
                 build_network_voice_data_features,
                 ["l0_network_sdr_dyn_cea_cei_voiceqoe_usr_1day_for_l1_network_voice_cei",
