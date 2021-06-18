f87bd2520ad2e8d8d7c172fa5d1b32efe9f8226b
ChaninChaisatianQB <66584647+ChaninChaisatianQB@users.noreply.github.com>
2021-06-18 11:01:34 +0700

    1

--- src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/geolocation_pipeline/to_l3/to_l3_pipeline.py
@@ -17,28 +17,28 @@ def geo_to_l3_pipeline_1st(**kwargs):
                  "int_l3_geo_work_location_id_filter_monthly"
                  ]
             ),
-            node(
-                int_geo_home_location_id_monthly,
-                ["int_l3_geo_home_location_id_filter_monthly"
-                 ],
-                "int_l3_geo_home_location_id_monthly"
-            ),
-            node(
-                int_geo_work_location_id_monthly,
-                ["int_l3_geo_work_location_id_filter_monthly"
-                 ],
-                ["int_l3_geo_work_location_id_monthly",
-                 "int_l3_geo_work_location_id_last3_monthly"
-                 ]
-            ),
-            ### FINISH
-            node(
-                l3_massive_processing,
-                ["l1_geo_data_session_location_daily_for_l3_geo_data_session_location_monthly",
-                 "params:l3_geo_data_session_location_monthly"
-                 ],
-                "l3_geo_data_session_location_monthly"
-            ),
+            # node(
+            #     int_geo_home_location_id_monthly,
+            #     ["int_l3_geo_home_location_id_filter_monthly"
+            #      ],
+            #     "int_l3_geo_home_location_id_monthly"
+            # ),
+            # node(
+            #     int_geo_work_location_id_monthly,
+            #     ["int_l3_geo_work_location_id_filter_monthly"
+            #      ],
+            #     ["int_l3_geo_work_location_id_monthly",
+            #      "int_l3_geo_work_location_id_last3_monthly"
+            #      ]
+            # ),
+            # ### FINISH
+            # node(
+            #     l3_massive_processing,
+            #     ["l1_geo_data_session_location_daily_for_l3_geo_data_session_location_monthly",
+            #      "params:l3_geo_data_session_location_monthly"
+            #      ],
+            #     "l3_geo_data_session_location_monthly"
+            # ),
 
         ], name="geo_to_l3_pipeline_1st"
     )
