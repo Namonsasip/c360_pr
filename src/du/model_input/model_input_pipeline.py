dc5a8c7a24ac60acb7b35db1a9977d5ee22a77ca
Chayaphat <chayaphn@ais.co.th>
2021-06-22 11:10:30 +0700

    - Edit du_mlflow_model_version_training \ - Uncomment l4_campaign_postpaid_prepaid_features in model_input_pipeline node

--- src/du/model_input/model_input_pipeline.py
+++ src/du/model_input/model_input_pipeline.py
@@ -75,7 +75,7 @@ def create_du_model_input_pipeline() -> Pipeline:
                     "l4_billing_rolling_window_before_top_up_balance": "l4_billing_rolling_window_before_top_up_balance",
                     "l4_billing_rolling_window_top_up_channels": "l4_billing_rolling_window_top_up_channels",
                     "l4_daily_feature_topup_and_volume": "l4_daily_feature_topup_and_volume",
-                    # "l4_campaign_postpaid_prepaid_features": "l4_campaign_postpaid_prepaid_features",
+                    "l4_campaign_postpaid_prepaid_features": "l4_campaign_postpaid_prepaid_features",
                     "l4_device_summary_features": "l4_device_summary_features",
                     "l4_revenue_prepaid_ru_f_sum_revenue_by_service_monthly": "l4_revenue_prepaid_ru_f_sum_revenue_by_service_monthly",
                     "l4_usage_prepaid_postpaid_daily_features": "l4_usage_prepaid_postpaid_daily_features",
