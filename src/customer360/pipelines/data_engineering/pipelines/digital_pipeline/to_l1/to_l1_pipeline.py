8a5f0df831dbff3d1cb60661b93527657c51a28d
Navenavey39 <36034923+Navenavey39@users.noreply.github.com>
2021-06-22 17:26:08 +0700

    update category level 2
    update category level 2

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -190,7 +190,7 @@ def digital_to_l1_digital_mobile_web_agg_daily(**kwargs):
             #     [
             #         "l0_digital_mobile_web_daily",
             #         "l1_digital_aib_categories_clean",
-            #         "l1_digital_cxense_traffic_complete_agg_daily_for_l1_dital_customer_web_agg_daily",
+            #         "l1_digital_cxense_traffic_complete_agg_daily_for_l1_dital_customer_web_agg_daily_catlv_1",
             #         "params:l1_digital_mobile_web_agg_sql"
             #     ],
             #     outputs="l1_digital_customer_web_category_agg_daily_catlv_1",
@@ -202,7 +202,7 @@ def digital_to_l1_digital_mobile_web_agg_daily(**kwargs):
                 [
                     "l0_digital_mobile_web_daily",
                     "l1_digital_aib_categories_clean",
-                    "l1_digital_cxense_traffic_complete_agg_daily_for_l1_dital_customer_web_agg_daily",
+                    "l1_digital_cxense_traffic_complete_agg_daily_for_l1_dital_customer_web_agg_daily_catlv_2",
                     "params:l1_digital_mobile_web_agg_sql",
                     "params:level_2",
                 ],
