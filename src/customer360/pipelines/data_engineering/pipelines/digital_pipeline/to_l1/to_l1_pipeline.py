da82b3b1f3bc0fe39b68200d58649c5adf5a3de1
Navenavey39 <36034923+Navenavey39@users.noreply.github.com>
2021-06-22 16:03:23 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -373,6 +373,12 @@ def digital_to_l1_cxense_traffic_daily_agg_pipeline(**kwargs):
                 # "l1_digital_cxense_content_profile_int",
                 tags="l1_digital_cxense_traffic_mapping_night",
             ),
+            # node(
+            #     func=node_create_content_profile_mapping,
+            #     inputs=["l1_cxense_content_profile_int", "l1_aib_categories_clean"],
+            #     outputs="l1_cxense_content_profile_mapping",
+            #     tags=["node_create_content_profile_mapping"],
+            # ),
             node(
                 func=l1_digital_agg_cxense_traffic,
                 inputs="l1_digital_cxense_traffic_int_morning",
