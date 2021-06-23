ef2c5c4ac0285e424c2280c8ee86ed02210cfadf
paipayon <paipayon2@gmail.com>
2021-06-23 11:17:09 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -585,7 +585,7 @@ def digital_to_l1_combine_app_web_daily(**kwargs):
             node(
                 func=digital_to_l1_combine_app_web_agg_daily,
                 inputs=["l1_digital_customer_app_category_agg_daily_catlv_1_for_combine_daily",
-                        "l1_digital_customer_web_category_agg_daily",
+                        "l1_digital_customer_app_category_agg_daily_catlv_1",
                         "params:l1_digital_customer_combine_app_web_agg_daily",
                         ],
                 outputs="l1_digital_customer_combine_category_agg_daily_catlv_1",
