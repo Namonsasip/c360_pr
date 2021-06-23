bb0c8e6b26526f6df62321dc1d20f52b2c2c9136
PoomtepR <64820619+PoomtepR@users.noreply.github.com>
2021-06-23 14:08:53 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/product_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/product_pipeline/to_l1/to_l1_pipeline.py
@@ -8,51 +8,51 @@ def product_to_l1_pipeline(**kwargs):
     return Pipeline(
         [
 
-            node(
-                dac_product_customer_promotion_for_daily,
-                ["l0_product_customer_promotion_for_daily",
-                 "l0_revenue_sa_t_package_trans_for_l0_revenue_prepaid_main_product_customer_promotion_daily",
-                 "l0_prepaid_ontop_product_customer_promotion_for_daily",
-                 "l1_customer_profile_union_daily_feature_for_int_l1_product_active_customer_promotion_features",
-                 "l0_product_pru_m_package_master_group_for_l1_prepaid_postpaid_processing",
-                 "l0_product_pru_m_ontop_master_for_l1_prepaid_postpaid_processing"
-                 ],
-                ["int_l1_product_active_customer_promotion_features",
-                 "int_l1_prepaid_main_product_active_customer_promotion_features",
-                 "int_l1_prepaid_ontop_product_active_customer_promotion_features",
-                 "int_l1_customer_profile_union_daily_feature_for_int_l1_product_active_customer_promotion_features",
-                 "int_l0_product_pru_m_package_master_group_for_l1_prepaid_postpaid_processing",
-                 "int_l0_product_pru_m_ontop_master_for_l1_prepaid_postpaid_processing",
-                 ]
-            ),
-
-            node(
-                l1_prepaid_postpaid_processing,
-                ["int_l1_prepaid_main_product_active_customer_promotion_features",
-                 "int_l1_prepaid_ontop_product_active_customer_promotion_features",
-                 "int_l1_product_active_customer_promotion_features",
-                 "int_l1_customer_profile_union_daily_feature_for_int_l1_product_active_customer_promotion_features",
-                 "int_l0_product_pru_m_package_master_group_for_l1_prepaid_postpaid_processing",
-                 "int_l0_product_pru_m_ontop_master_for_l1_prepaid_postpaid_processing"],
-                "l1_product_active_customer_promotion_features_prepaid_postpaid"
-            ),
-
-            node(
-                l1_build_product,
-                ["l1_product_active_customer_promotion_features_prepaid_postpaid",
-                 "params:int_l1_product_active_customer_promotion_features"],
-                "int_l1_product_active_customer_promotion_features_temp"
-            ),
-
-            node(
-                join_with_master_package,
-                ["int_l1_product_active_customer_promotion_features_temp",
-                 "l0_product_pru_m_package_master_group_for_l1_product_active_customer_promotion_features_daily",
-                 "l0_product_pru_m_ontop_master_for_l1_product_active_customer_promotion_features_daily",
-                 "l0_product_ru_m_main_promotion_cvm_proj_for_daily",
-                 "l0_product_ru_m_ontop_promotion_cvm_proj_for_daily"],
-                "l1_product_active_customer_promotion_features_daily"
-            ),
+            # node(
+            #     dac_product_customer_promotion_for_daily,
+            #     ["l0_product_customer_promotion_for_daily",
+            #      "l0_revenue_sa_t_package_trans_for_l0_revenue_prepaid_main_product_customer_promotion_daily",
+            #      "l0_prepaid_ontop_product_customer_promotion_for_daily",
+            #      "l1_customer_profile_union_daily_feature_for_int_l1_product_active_customer_promotion_features",
+            #      "l0_product_pru_m_package_master_group_for_l1_prepaid_postpaid_processing",
+            #      "l0_product_pru_m_ontop_master_for_l1_prepaid_postpaid_processing"
+            #      ],
+            #     ["int_l1_product_active_customer_promotion_features",
+            #      "int_l1_prepaid_main_product_active_customer_promotion_features",
+            #      "int_l1_prepaid_ontop_product_active_customer_promotion_features",
+            #      "int_l1_customer_profile_union_daily_feature_for_int_l1_product_active_customer_promotion_features",
+            #      "int_l0_product_pru_m_package_master_group_for_l1_prepaid_postpaid_processing",
+            #      "int_l0_product_pru_m_ontop_master_for_l1_prepaid_postpaid_processing",
+            #      ]
+            # ),
+            #
+            # node(
+            #     l1_prepaid_postpaid_processing,
+            #     ["int_l1_prepaid_main_product_active_customer_promotion_features",
+            #      "int_l1_prepaid_ontop_product_active_customer_promotion_features",
+            #      "int_l1_product_active_customer_promotion_features",
+            #      "int_l1_customer_profile_union_daily_feature_for_int_l1_product_active_customer_promotion_features",
+            #      "int_l0_product_pru_m_package_master_group_for_l1_prepaid_postpaid_processing",
+            #      "int_l0_product_pru_m_ontop_master_for_l1_prepaid_postpaid_processing"],
+            #     "l1_product_active_customer_promotion_features_prepaid_postpaid"
+            # ),
+            #
+            # node(
+            #     l1_build_product,
+            #     ["l1_product_active_customer_promotion_features_prepaid_postpaid",
+            #      "params:int_l1_product_active_customer_promotion_features"],
+            #     "int_l1_product_active_customer_promotion_features_temp"
+            # ),
+            #
+            # node(
+            #     join_with_master_package,
+            #     ["int_l1_product_active_customer_promotion_features_temp",
+            #      "l0_product_pru_m_package_master_group_for_l1_product_active_customer_promotion_features_daily",
+            #      "l0_product_pru_m_ontop_master_for_l1_product_active_customer_promotion_features_daily",
+            #      "l0_product_ru_m_main_promotion_cvm_proj_for_daily",
+            #      "l0_product_ru_m_ontop_promotion_cvm_proj_for_daily"],
+            #     "l1_product_active_customer_promotion_features_daily"
+            # ),
             node(
                 dac_product_fbb_a_customer_promotion_current_for_daily,
                 [
