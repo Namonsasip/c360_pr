574567549e5ecc7a93b9095f22b2fe9034d9805c
Thatchapong Tunkam <64589464+BenzThatchapong@users.noreply.github.com>
2021-06-24 11:39:05 +0700

    Update re_usable_functions.py

--- src/customer360/utilities/re_usable_functions.py
+++ src/customer360/utilities/re_usable_functions.py
@@ -272,8 +272,6 @@ def _join_with_filtered_customer_profile(
 
     col_to_select = []
 
-    result_df.show(3)
-
     # Select all columns for right table except those used for joins
     # and exist in filtered_cust_profile_df columns
     for col in input_df.columns:
@@ -287,8 +285,6 @@ def _join_with_filtered_customer_profile(
         col_to_select.append(F.col("left.{}".format(col)).alias(col))
 
     result_df = result_df.select(col_to_select)
-
-    result_df.show(3)
 
     return result_df
 
