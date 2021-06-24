dbe286c4d8e438162ffc84781dd80636a3a136c0
PoomtepR <64820619+PoomtepR@users.noreply.github.com>
2021-06-24 09:55:29 +0700

    Update to_l2_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/product_pipeline/to_l2/to_l2_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/product_pipeline/to_l2/to_l2_pipeline.py
@@ -15,7 +15,7 @@ def product_to_l2_pipeline(**kwargs):
                  "l0_product_ru_m_main_promotion_cvm_proj_for_weekly",
                  "l0_product_ru_m_ontop_promotion_cvm_proj_for_weekly"],
                 "l2_product_activated_deactivated_features_weekly_1"
-            # ),
+            ),
             # node(get_product_package_promotion_group_tariff_weekly,
             #      'l0_product_pru_m_package_master_group_for_l2_product_package_promotion_group_tariff',
             #      'l2_product_package_promotion_group_tariff_weekly')
