815664952e1cf083117d733f472a9afb2e0bf267
pakawat <pakap571@epicc.postbox.in.th>
2021-06-25 11:03:38 +0700

    feat : L4_FEATURES add category_level_2 - 4
    **combine_score
    - catalog add /category_level=level_2-4
    - pipeline add _catlv_2-4

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l4/to_l4_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l4/to_l4_pipeline.py
@@ -199,6 +199,36 @@ def digital_to_l4_digital_customer_combine_agg_monthly(**kwargs):
                 outputs="l4_digital_customer_combine_category_score_monthly",
                 tags=["l4_windows_app_combine_monthly_feature_score"],
             ),
+            node(
+                func=customer_category_windows,
+                inputs=[
+                    "l3_digital_customer_combine_category_score_monthly_catlv_2",
+                    "params:customer_combine_monthly_feature_score_groupby",
+                    "params:customer_combine_monthly_feature_score_feature"
+                ],
+                outputs="l4_digital_customer_combine_category_score_monthly_catlv_2",
+                tags=["l4_windows_app_combine_monthly_feature_score_catlv_2"],
+            ),
+            node(
+                func=customer_category_windows,
+                inputs=[
+                    "l3_digital_customer_combine_category_score_monthly_catlv_3",
+                    "params:customer_combine_monthly_feature_score_groupby",
+                    "params:customer_combine_monthly_feature_score_feature"
+                ],
+                outputs="l4_digital_customer_combine_category_score_monthly_catlv_3",
+                tags=["l4_windows_app_combine_monthly_feature_score_catlv_3"],
+            ),
+            node(
+                func=customer_category_windows,
+                inputs=[
+                    "l3_digital_customer_combine_category_score_monthly_catlv_4",
+                    "params:customer_combine_monthly_feature_score_groupby",
+                    "params:customer_combine_monthly_feature_score_feature"
+                ],
+                outputs="l4_digital_customer_combine_category_score_monthly_catlv_4",
+                tags=["l4_windows_app_combine_monthly_feature_score_catlv_4"],
+            ),
         ], name="l4_digital_to_l4_digital_customer_app_combine_agg_monthly"
     )
 
