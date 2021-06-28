cbba1eb5c6e7248b45d8d380d304bda987820245
Navenavy39 <navenavy39@hotmail.com>
2021-06-28 15:21:07 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -725,7 +725,7 @@ def digital_to_l1_cxense_traffic_daily_agg_pipeline(**kwargs):
             # ), # l1 cxense traffic clean
             node(
                 func=create_content_profile_mapping,
-                inputs=["l1_digital_cxense_content_profile_int", "l1_aib_categories_clean"],
+                inputs=["l1_digital_cxense_content_profile_int", "l1_digital_aib_categories_clean"],
                 outputs="l1_digital_cxense_content_profile_mapping",
                 tags=["create_content_profile_mapping"],
             ),
