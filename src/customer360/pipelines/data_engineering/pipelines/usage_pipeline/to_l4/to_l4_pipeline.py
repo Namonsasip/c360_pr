7f8171388a91a1f6503b280ae061cc8b42d2b463
NitipongChaikitiphornQB <77653671+NitipongChaikitiphornQB@users.noreply.github.com>
2021-07-01 11:04:18 +0700

    mod pipe

--- src/customer360/pipelines/data_engineering/pipelines/usage_pipeline/to_l4/to_l4_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/usage_pipeline/to_l4/to_l4_pipeline.py
@@ -406,8 +406,8 @@ def usage_to_l4_pipeline_sum1(**kwargs):
             node(
                 l4_usage_filter_date_rolling_window_weekly,
                 ["l2_usage_postpaid_prepaid_weekly_for_l4_postpaid_prepaid_weekly_features_sum",
-                 "params:l4_usage_postpaid_prepaid_weekly_features_sum_second"],
-                "l4_usage_postpaid_prepaid_weekly_features_sum_second"
+                 "params:l4_usage_postpaid_prepaid_weekly_features_sum_twelfth"],
+                "l4_usage_postpaid_prepaid_weekly_features_sum_twelfth"
             ),
         ]
     )
