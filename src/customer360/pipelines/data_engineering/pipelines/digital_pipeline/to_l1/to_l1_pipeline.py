7fd4b62023a10727f63c2d4d75c91b245e4b1405
Kanon <kanonsu8@ascgp.postbox.in.th>
2021-07-04 16:29:51 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -1045,15 +1045,15 @@ def digital_to_l1_cxense_traffic_agg_complete_daily(**kwargs):
 def digital_to_l1_combine_app_web_daily(**kwargs):
     return Pipeline(
         [
-            node(
-                func=digital_to_l1_combine_app_web_agg_daily,
-                inputs=["l1_digital_customer_app_category_agg_daily_catlv_1_for_combine_daily_catlv_1",
-                        "l1_digital_customer_web_category_agg_daily_catlv_1",
-                        "params:l1_digital_customer_combine_app_web_agg_daily",
-                        ],
-                outputs="l1_digital_customer_combine_category_agg_daily_catlv_1",
-                tags=["l1_digital_customer_combine_category_agg_daily_catlv_1"],
-            ),
+            # node(
+            #     func=digital_to_l1_combine_app_web_agg_daily,
+            #     inputs=["l1_digital_customer_app_category_agg_daily_catlv_1_for_combine_daily_catlv_1",
+            #             "l1_digital_customer_web_category_agg_daily_catlv_1",
+            #             "params:l1_digital_customer_combine_app_web_agg_daily",
+            #             ],
+            #     outputs="l1_digital_customer_combine_category_agg_daily_catlv_1",
+            #     tags=["l1_digital_customer_combine_category_agg_daily_catlv_1"],
+            # ),
             node(
                 func=digital_to_l1_combine_app_web_agg_daily,
                 inputs=["l1_digital_customer_app_category_agg_daily_catlv_2_for_combine_daily_catlv_2",
