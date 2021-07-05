ec2f7ee8ac667fa58ba2793ee15ae91e8bd59daf
Navenavey39 <36034923+Navenavey39@users.noreply.github.com>
2021-07-05 14:50:42 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -336,8 +336,7 @@ def digital_to_l1_digital_mobile_web_agg_daily(**kwargs):
                 inputs=
                 [
                     "l0_digital_mobile_web_daily",
-                    "l1_digital_aib_categories_clean",
-                    "l1_digital_cxense_traffic_complete_agg_daily_for_l1_dital_customer_web_agg_daily_catlv_1"
+                    "l1_digital_aib_categories_clean"
                 ],
                 outputs="int_l1_digital_customer_web_category_agg_daily_catlv_1",
                 tags="l1_digital_customer_web_category_agg_daily_catlv_1"
