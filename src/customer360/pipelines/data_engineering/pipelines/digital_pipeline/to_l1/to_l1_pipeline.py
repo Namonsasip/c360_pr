73d68cd229e54695279ca13a470db9b46b2f5760
Kanon <kanonsu8@ascgp.postbox.in.th>
2021-07-06 17:21:44 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -86,16 +86,16 @@ def digital_to_l1_aib_categoy_clean_master(**kwargs):
 def digital_to_l1_app_agg_daily_pipeline(**kwargs):
     return Pipeline(
         [
-            # node(
-            #     func=digital_mobile_app_category_agg_daily,
-            #     inputs=[
-            #      "l0_digital_mobile_app_daily",
-            #      "params:l1_digital_mobile_app_agg_category_daily",
-            #      "params:category_level_1",
-            #      ],
-            #     outputs="l1_digital_customer_app_category_agg_daily_catlv_1",
-            #     tags=["digital_mobile_app_category_agg_daily_catlv_1"],
-            # ),
+            node(
+                func=digital_mobile_app_category_agg_daily,
+                inputs=[
+                 "l0_digital_mobile_app_daily",
+                 "params:l1_digital_mobile_app_agg_category_daily",
+                 "params:category_level_1",
+                 ],
+                outputs="l1_digital_customer_app_category_agg_daily_catlv_1",
+                tags=["digital_mobile_app_category_agg_daily_catlv_1"],
+            ),
             node(
                 func=digital_mobile_app_category_agg_daily,
                 inputs=[
@@ -116,16 +116,16 @@ def digital_to_l1_app_agg_daily_pipeline(**kwargs):
                 outputs="l1_digital_customer_app_category_agg_daily_catlv_3",
                 tags=["digital_mobile_app_category_agg_daily_catlv_3"],
             ),
-            # node(
-            #     func=digital_mobile_app_category_agg_daily,
-            #     inputs=[
-            #      "l0_digital_mobile_app_daily",
-            #      "params:l1_digital_mobile_app_agg_category_daily",
-            #      "params:category_level_4",
-            #      ],
-            #     outputs="l1_digital_customer_app_category_agg_daily_catlv_4",
-            #     tags=["digital_mobile_app_category_agg_daily_catlv_4"],
-            # ),
+            node(
+                func=digital_mobile_app_category_agg_daily,
+                inputs=[
+                 "l0_digital_mobile_app_daily",
+                 "params:l1_digital_mobile_app_agg_category_daily",
+                 "params:category_level_4",
+                 ],
+                outputs="l1_digital_customer_app_category_agg_daily_catlv_4",
+                tags=["digital_mobile_app_category_agg_daily_catlv_4"],
+            ),
         ], name="digital_to_l1_app_agg_daily_pipeline"
     )
 ######################### App category agg category timeband daily ############################################
