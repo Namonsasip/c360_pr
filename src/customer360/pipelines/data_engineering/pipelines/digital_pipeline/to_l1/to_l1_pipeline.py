51eb92789de214b592ca34dad8c627a25345fdf3
Navenavey39 <36034923+Navenavey39@users.noreply.github.com>
2021-07-14 16:55:58 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -733,17 +733,17 @@ def digital_to_l1_cxense_traffic_daily_agg_pipeline(**kwargs):
                 outputs="l1_digital_best_match_for_unmatched_urls",
                 tags=["l1_digital_get_best_match_for_unmatched_urls"],
             ),
-            node(
-                func=l1_digital_union_matched_and_unmatched_urls,
-                inputs=
-                [
-                    "l0_digital_customer_profile_union_daily_for_cxense_category_catlv_1",
-                    "l1_digital_matched_urls_for_union_matched_and_unmatched_urls_catlv_1",
-                    "l1_digital_best_match_for_unmatched_urls_for_union_matched_and_unmatched_urls_catlv_1"
-                ],
-                outputs="l1_digital_cxense_traffic_complete_agg_daily_catlv_1",
-                tags=["l1_digital_union_matched_and_unmatched_urls_catlv_1"],
-            ),
+            # node(
+            #     func=l1_digital_union_matched_and_unmatched_urls,
+            #     inputs=
+            #     [
+            #         "l0_digital_customer_profile_union_daily_for_cxense_category_catlv_1",
+            #         "l1_digital_matched_urls_for_union_matched_and_unmatched_urls_catlv_1",
+            #         "l1_digital_best_match_for_unmatched_urls_for_union_matched_and_unmatched_urls_catlv_1"
+            #     ],
+            #     outputs="l1_digital_cxense_traffic_complete_agg_daily_catlv_1",
+            #     tags=["l1_digital_union_matched_and_unmatched_urls_catlv_1"],
+            # ),
             # node(
             #     func=l1_digital_union_matched_and_unmatched_urls_cat_level,
             #     inputs=
