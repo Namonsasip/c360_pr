e02e2e84b25d2759517353262d8bb89445ccc5e9
Navenavey39 <36034923+Navenavey39@users.noreply.github.com>
2021-07-15 10:58:24 +0700

    upadate cxense daily V3
    upadate cxense daily V3

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -738,8 +738,8 @@ def digital_to_l1_cxense_traffic_daily_agg_pipeline(**kwargs):
                 inputs=
                 [
                     "l0_digital_customer_profile_union_daily_for_cxense_category_catlv_1",
-                    "l1_digital_matched_urls@l1_digital_cxense_traffic_complete_agg_daily_catlv_1",
-                    "l1_digital_best_match_for_unmatched_urls@l1_digital_cxense_traffic_complete_agg_daily_catlv_1"
+                    "l1_digital_matched_urls_for_l1_digital_cxense_traffic_complete_agg_daily_catlv_1",
+                    "l1_digital_best_match_for_unmatched_urls_for_l1_digital_cxense_traffic_complete_agg_daily_catlv_1"
                 ],
                 outputs="l1_digital_cxense_traffic_complete_agg_daily_catlv_1",
                 tags=["l1_digital_union_matched_and_unmatched_urls_catlv_1"],
@@ -749,8 +749,8 @@ def digital_to_l1_cxense_traffic_daily_agg_pipeline(**kwargs):
                 inputs=
                 [
                     "l0_digital_customer_profile_union_daily_for_cxense_category_catlv_2",
-                    "l1_digital_matched_urls@l1_digital_cxense_traffic_complete_agg_daily_catlv_2",
-                    "l1_digital_best_match_for_unmatched_urls@l1_digital_cxense_traffic_complete_agg_daily_catlv_2",
+                    "l1_digital_matched_urls_for_l1_digital_cxense_traffic_complete_agg_daily_catlv_2",
+                    "l1_digital_best_match_for_unmatched_urls_for_l1_digital_cxense_traffic_complete_agg_daily_catlv_2",
                     "params:level_2",
                 ],
                 outputs="l1_digital_cxense_traffic_complete_agg_daily_catlv_2",
@@ -761,8 +761,8 @@ def digital_to_l1_cxense_traffic_daily_agg_pipeline(**kwargs):
                 inputs=
                 [
                     "l0_digital_customer_profile_union_daily_for_cxense_category_catlv_3",
-                    "l1_digital_matched_urls@l1_digital_cxense_traffic_complete_agg_daily_catlv_3",
-                    "l1_digital_best_match_for_unmatched_urls@l1_digital_cxense_traffic_complete_agg_daily_catlv_3",
+                    "l1_digital_matched_urls_for_l1_digital_cxense_traffic_complete_agg_daily_catlv_3",
+                    "l1_digital_best_match_for_unmatched_urls_for_l1_digital_cxense_traffic_complete_agg_daily_catlv_3",
                     "params:level_3",
                 ],
                 outputs="l1_digital_cxense_traffic_complete_agg_daily_catlv_3",
@@ -773,8 +773,8 @@ def digital_to_l1_cxense_traffic_daily_agg_pipeline(**kwargs):
                 inputs=
                 [
                     "l0_digital_customer_profile_union_daily_for_cxense_category_catlv_4",
-                    "l1_digital_matched_urls@l1_digital_cxense_traffic_complete_agg_daily_catlv_4",
-                    "l1_digital_best_match_for_unmatched_urls@l1_digital_cxense_traffic_complete_agg_daily_catlv_4",
+                    "l1_digital_matched_urls_for_l1_digital_cxense_traffic_complete_agg_daily_catlv_4",
+                    "l1_digital_best_match_for_unmatched_urls_for_l1_digital_cxense_traffic_complete_agg_daily_catlv_4",
                     "params:level_4",
                 ],
                 outputs="l1_digital_cxense_traffic_complete_agg_daily_catlv_4",
