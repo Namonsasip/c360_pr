a240f5e9336f42de74e1557e266e74dfbf555e60
Navenavy39 <navenavy39@hotmail.com>
2021-07-16 14:19:08 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -716,11 +716,8 @@ def digital_to_l1_cxense_traffic_daily_agg_pipeline(**kwargs):
                     "l1_digital_cxense_traffic_agg_daily",
                     "l1_digital_cxense_content_profile_mapping",
                 ],
-                outputs=
-                [
-                    "l1_digital_matched_urls",
+                outputs="l1_digital_matched_urls",
                     # "l1_digital_best_match_for_unmatched_urls"
-                ],
                 tags=["l1_digital_get_matched_and_unmatched_urls"],
             ),
             # node(
