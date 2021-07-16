69610191435a8fe50e70c30b5046f0b4eb412739
Navenavy39 <navenavy39@hotmail.com>
2021-07-16 15:21:33 +0700

    update cxense
    update cxense

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -1053,18 +1053,18 @@ def l1_digital_get_matched_and_unmatched_urls(
              "level_3",
              "level_4")
 
-    df_cp_join_iab_join_ais_priority = get_cp_category_ais_priorities(df_cp_join_iab)
-    df_traffic_get_missing_urls = (
-        unmatched_urls.join(df_cp_join_iab_join_ais_priority,
-            on=
-            [
-                unmatched_urls.siteid == df_cp_join_iab_join_ais_priority.siteid
-            ],
-            how="left",
-        )
-    )
+    # df_cp_join_iab_join_ais_priority = get_cp_category_ais_priorities(df_cp_join_iab)
+    # df_traffic_get_missing_urls = (
+    #     unmatched_urls.join(df_cp_join_iab_join_ais_priority,
+    #         on=
+    #         [
+    #             unmatched_urls.siteid == df_cp_join_iab_join_ais_priority.siteid
+    #         ],
+    #         how="left",
+    #     )
+    # )
 
-    return df_traffic_get_missing_urls
+    return [matched_urls,unmatched_urls]
 
 def l1_digital_get_best_match_for_unmatched_urls(
         df_traffic_join_cp_missing: pyspark.sql.DataFrame,
