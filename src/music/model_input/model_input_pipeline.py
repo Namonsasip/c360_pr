a7c9cb30f58c5017e1298fa8cd4d6d78d584c4f5
Manapat Weeramongkolkul <hungkeng99@gmail.com>
2021-07-20 14:26:09 +0700

    change output node

--- src/music/model_input/model_input_pipeline.py
+++ src/music/model_input/model_input_pipeline.py
@@ -38,7 +38,7 @@ def create_calling_melody_propensity_model_input_pipeline() -> Pipeline:
                     "l0_product_ru_a_callingmelody_daily":"l0_product_ru_a_callingmelody_daily" ,
                     "l3_customer_profile_include_1mo_non_active": "l3_customer_profile_include_1mo_non_active",
                 },
-                outputs="campaign_tracking_contact_list_pre",
+                outputs="l0_calling_melody_campaign_lift_table",
                 name="node_l0_calling_melody_target_variable",
                 tags=["node_l0_calling_melody_target_variable"],
             ),
