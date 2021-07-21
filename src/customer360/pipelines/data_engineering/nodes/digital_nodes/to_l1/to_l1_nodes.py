be539910cbbd7814c8531b2369aba3f2eeaf87ee
Navenavy39 <navenavy39@hotmail.com>
2021-07-21 13:34:34 +0700

    update mobile web daily
    update mobile web daily

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -301,23 +301,6 @@ def l1_digital_customer_web_category_agg_daily(
                                                                                "total_download_byte",
                                                                                "total_upload_byte",
                                                                                "event_partition_date")
-    #
-    # cxense_daily = cxense_daily.withColumn("total_volume_byte", f.lit(0).cast(IntegerType()))\
-    #     .withColumn("total_download_byte", f.lit(0).cast(IntegerType()))\
-    #     .withColumn("total_upload_byte",f.lit(0).cast(IntegerType()))
-    #
-    # cxense_daily = cxense_daily.select("subscription_identifier",
-    #                                    "mobile_no",
-    #                                    "category_name",
-    #                                    "priority",
-    #                                    "total_visit_count",
-    #                                    "total_visit_duration",
-    #                                    "total_volume_byte",
-    #                                    "total_download_byte",
-    #                                    "total_upload_byte",
-    #                                    cxense_daily.event_partition_date)
-    #
-    # df_return = df_mobile_web_daily_category_agg.unionAll(cxense_daily).distinct()
 
     return df_mobile_web_daily_category_agg
 
