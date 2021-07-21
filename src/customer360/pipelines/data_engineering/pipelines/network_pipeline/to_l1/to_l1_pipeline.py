9bea78674505336d532b3347aac8e3028cc7d044
pakawat <pakawat.pln@gmail.com>
2021-07-21 10:53:51 +0700

    fix : add #

--- src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
@@ -305,14 +305,14 @@ def network_to_l1_pipeline_test(**kwargs):
 def network_cqi_to_l1_pipeline_fix_test(**kwargs):
         return Pipeline(
         [
-            node(
-                build_network_im_cqi,
-                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day_for_l1_network_im_cqi",
-                 "params:l1_network_im_cqi",
-                 "l1_customer_profile_union_daily_feature_for_l1_network_im_cqi",
-                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day"],
-                "l1_network_im_cqi"
-            ),
+            # node(
+            #     build_network_im_cqi,
+            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day_for_l1_network_im_cqi",
+            #      "params:l1_network_im_cqi",
+            #      "l1_customer_profile_union_daily_feature_for_l1_network_im_cqi",
+            #      "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day"],
+            #     "l1_network_im_cqi"
+            # ),
 
             node(
                 build_network_streaming_cqi,
