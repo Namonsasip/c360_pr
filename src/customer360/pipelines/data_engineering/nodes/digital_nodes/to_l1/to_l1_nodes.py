ecd503d10ec5539dfb9e76d98f1aeb65fddbe020
sirin571 <sirin571@epicc.postbox.in.th>
2021-07-22 20:39:12 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -1516,7 +1516,7 @@ def digital_customer_web_network_company_usage_hourly(
     df_traffic = df_traffic.withColumnRenamed("company", "network_company")
     df_traffic = df_traffic.withColumnRenamed("connectionspeed", "network_type")
     df_traffic = df_traffic.withColumn("hour", f.hour("time"))
-    df_traffic = df_traffic.withColumn("ais_sim_flag",f.when(f.col("network_type") == "mobile" , "1").otherwise("0"))
+    customer_web_network_company_usage_hourly = df_traffic.withColumn("ais_sim_flag",f.when(f.col("network_type") == "mobile" & f.col("network_company") == "ais 3g4g" , "1").otherwise("0"))
     # customer_web_network_company_usage_hourly = df_traffic.withColumn("ais_sim_flag", f.when(f.col("network_type") == "mobile" & f.col("network_company") == "ais 3g4g" | f.col("network_company") == "ais mobile", "1").otherwise("0"))
     # customer_web_network_company_usage_hourly = df_traffic.withColumn("ais_sim_flag", f.when(
     #     f.col("network_type") == "mobile" & (
@@ -1527,7 +1527,7 @@ def digital_customer_web_network_company_usage_hourly(
 
 
 
-    customer_web_network_company_usage_hourly = df_traffic.withColumn("timeband",f.when(f.col("hour").between(6, 11), "Morning").f.when(f.col("hour").between(12, 17), "Afternoon").f.when(f.col("hour").between(18, 23), "Evening").otherwise("Other"))
+    # df_traffic = df_traffic.withColumn("timeband",f.when(f.col("hour").between(6, 11), "Morning")f.when(f.col("hour").between(12, 17), "Afternoon").f.when(f.col("hour").between(18, 23), "Evening").otherwise("Other"))
 
     # customer_web_network_company_usage_hourly = node_from_config(df_traffic, customer_web_network_company)
 
