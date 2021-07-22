0f42731e41b62a006d38438c7eafa6e96ff9af92
sirin571 <sirin571@epicc.postbox.in.th>
2021-07-22 17:09:24 +0700

    digital_customer_web_network_company_usage_hourly

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -1198,9 +1198,12 @@ def digital_to_l1_customer_web_network_company(**kwargs):
         [
             node(
                 func=digital_customer_web_network_company_usage_hourly,
-                inputs="l0_digital_cxense_traffic",
+                inputs=[
+                    "l0_digital_cxense_traffic",
+                    "params:l1_customer_web_network_company",
+                    ],
                 outputs="l1_digital_customer_web_network_company_usage_hourly",
-                tags=["digital_cxense_traffic_json"],
+                tags=["digital_customer_web_network_company_usage_hourly"],
             ),
         ]
     )
