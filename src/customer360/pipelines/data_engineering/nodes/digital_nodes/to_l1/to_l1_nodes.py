76121bbaf4f3b40269a714fb594dee5bf46fcd1f
sirin571 <sirin571@epicc.postbox.in.th>
2021-07-22 17:46:31 +0700

    l1_customer_web_network_company

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -1516,16 +1516,16 @@ def digital_customer_web_network_company_usage_hourly(
     df_traffic = df_traffic.withColumnRenamed("company", "network_company")
     df_traffic = df_traffic.withColumnRenamed("connectionspeed", "network_type")
     df_traffic = df_traffic.withColumn("hour", f.hour("time"))
-    df_traffic = df_traffic.withColumn("ais_sim_flag", "CASE WHEN f.col("network_type") == "mobile" and (f.col("company") == "ais 3g4g" or f.col("company") == "ais mobile") THEN 1 ELSE 0 END")
+    # df_traffic = df_traffic.withColumn("ais_sim_flag", "CASE WHEN f.col("network_type") == "mobile" and (f.col("company") == "ais 3g4g" or f.col("company") == "ais mobile") THEN 1 ELSE 0 END")
 
 
 
 
-    # customer_web_network_company_usage_hourly = df_traffic.withColumn("timeband",
-    #                                    f.when(f.col("hour").between(6, 11), "Morning"),
-    #                                    f.when(f.col("hour").between(12, 17), "Afternoon"),
-    #                                    f.when(f.col("hour").between(18, 23), "Evening"),
-    #                                    f.when(f.col("hour").between(0, 5), "Night"))
+    df_traffic = df_traffic.withColumn("timeband",
+                                       f.when(f.col("hour").between(6, 11), "Morning"),
+                                       f.when(f.col("hour").between(12, 17), "Afternoon"),
+                                       f.when(f.col("hour").between(18, 23), "Evening"),
+                                       f.when(f.col("hour").between(0, 5), "Night"))
 
     customer_web_network_company_usage_hourly = node_from_config(df_traffic, customer_web_network_company)
 
