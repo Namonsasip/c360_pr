bc09745071a844977c83e2e05817354537d53a89
sirin571 <sirin571@epicc.postbox.in.th>
2021-07-22 18:17:52 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -1516,7 +1516,7 @@ def digital_customer_web_network_company_usage_hourly(
     df_traffic = df_traffic.withColumnRenamed("company", "network_company")
     df_traffic = df_traffic.withColumnRenamed("connectionspeed", "network_type")
     df_traffic = df_traffic.withColumn("hour", f.hour("time"))
-    df_traffic = df_traffic.withColumn("ais_sim_flag", f.when(f.col("network_type") == "mobile" & (f.col("company") == "ais 3g4g" | f.col("company") == "ais mobile"), '1').otherwise('0'))
+    df_traffic = df_traffic.withColumn("ais_sim_flag", f.when(f.col("network_type") == "mobile" & (f.col("company") == "ais 3g4g" | f.col("company") == "ais mobile"), "1").otherwise("0"))
     customer_web_network_company_usage_hourly = df_traffic.withColumn("timeband",f.when(f.col("hour").between(6, 11), "Morning").otherwise("Night"))
 
 
