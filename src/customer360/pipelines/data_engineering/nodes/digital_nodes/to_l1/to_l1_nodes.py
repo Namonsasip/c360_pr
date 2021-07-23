41fdaf5f340e4faa350a1550e73c8d0fd315b778
sirin571 <sirin571@epicc.postbox.in.th>
2021-07-23 14:22:58 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -1505,7 +1505,12 @@ def digital_customer_web_network_company_usage_hourly(
                                    "time",
                                    "company",
                                    "connectionspeed",
-                                   "partition_date").distinct()
+                                   "partition_date")
+    df_traffic = df_traffic.groupby("mobile_no",
+                                   "time",
+                                   "company",
+                                   "connectionspeed",
+                                   "partition_date")
 
     df_traffic = df_traffic.where("connectionspeed IN ('mobile','broadband')")
     df_traffic = df_traffic.filter(f.col("mobile_no").isNotNull())
