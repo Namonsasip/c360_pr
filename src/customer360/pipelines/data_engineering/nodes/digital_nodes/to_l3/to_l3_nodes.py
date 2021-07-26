260bf2422ed389c1902fd8c71bf6a765eb353dab
sirin571 <sirin571@epicc.postbox.in.th>
2021-07-26 23:30:12 +0700

    Update to_l3_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
@@ -1072,6 +1072,6 @@ def digital_customer_multi_company_sim_monthly(
     customer_multi_company_sim = customer_multi_company_sim.withColumn("multi_company_sim_flag", f.when((f.col("competitor_sim_flag") != 0), "Y").otherwise("N"))
     customer_multi_company_sim = customer_multi_company_sim.withColumn("multi_company_broadband_flag", f.when((f.col("competitor_broadband_flag") != 0), "Y").otherwise("N"))
 
-    customer_multi_company_sim = customer_multi_company_sim.select("mobile_no", "multi_company_sim_flag", "multi_company_broadband_flag", "multi_company_sim_days", "multi_company_broadband_days","start_of_month")
+    customer_multi_company_sim = customer_multi_company_sim.select("subscription_identifier","mobile_no", "multi_company_sim_flag", "multi_company_broadband_flag", "multi_company_sim_days", "multi_company_broadband_days","start_of_month")
 
     return customer_multi_company_sim
