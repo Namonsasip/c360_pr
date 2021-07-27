61d6a8fdc1c26e096f7deab502f1d56b2ae65b0d
pakawat <pakap571@epicc.postbox.in.th>
2021-07-27 14:33:18 +0700

    fix run

--- src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
@@ -305,31 +305,22 @@ def network_to_l1_pipeline_test(**kwargs):
 def network_cqi_to_l1_pipeline_fix_test(**kwargs):
         return Pipeline(
         [
-            # node(
-            #     build_network_im_cqi,
-            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day_for_l1_network_im_cqi",
-            #      "params:l1_network_im_cqi",
-            #      "l1_customer_profile_union_daily_feature_for_l1_network_im_cqi",
-            #      "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day"],
-            #     "l1_network_im_cqi"
-            # ),#error เรื่อง partition
+            node(
+                build_network_im_cqi,
+                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day_for_l1_network_im_cqi",
+                 "params:l1_network_im_cqi",
+                 "l1_customer_profile_union_daily_feature_for_l1_network_im_cqi",
+                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day"],
+                "l1_network_im_cqi"
+            ),#error เรื่อง partition
 
             node(
-                build_network_streaming_cqi,
-                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_stream_1day_for_l1_network_streaming_cqi",
-                 "params:l1_network_streaming_cqi",
-                 "l1_customer_profile_union_daily_feature_for_l1_network_streaming_cqi",
-                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_stream_1day"],
-                "l1_network_streaming_cqi"
-            ),#error เรื่อง blacklist
-
-            # node(
-            #     build_network_file_transfer_cqi,
-            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_fileaccess_1day_for_l1_network_file_transfer_cqi",
-            #      "params:l1_network_file_transfer_cqi",
-            #      "l1_customer_profile_union_daily_feature_for_l1_network_file_transfer_cqi",
-            #      "params:exception_partitions_list_for_l1_network_file_transfer_cqi"],
-            #     "l1_network_file_transfer_cqi"
-            # )#error เรื่อง blacklist
+                build_network_web_cqi,
+                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_web_1day_for_l1_network_web_cqi",
+                 "params:l1_network_web_cqi",
+                 "l1_customer_profile_union_daily_feature_for_l1_network_web_cqi",
+                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_web_1day"],
+                "l1_network_web_cqi"
+            ),            
         ]
     )
