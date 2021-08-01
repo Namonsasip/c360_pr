8d4d85cb089630cdfdad345a21dacbb0b0a0d5fd
pakawat <pakawat.pln@gmail.com>
2021-08-01 20:17:34 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
@@ -328,14 +328,14 @@ def network_to_l1_pipeline_test(**kwargs):
                 "l1_network_file_transfer_cqi"
             ),
   
-            node(
-                build_network_streaming_cqi,
-                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_stream_1day_for_l1_network_streaming_cqi",
-                 "params:l1_network_streaming_cqi",
-                 "l1_customer_profile_union_daily_feature_for_l1_network_streaming_cqi",
-                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_stream_1day"],
-                "l1_network_streaming_cqi"
-            )                                             
+            # node(
+            #     build_network_streaming_cqi,
+            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_stream_1day_for_l1_network_streaming_cqi",
+            #      "params:l1_network_streaming_cqi",
+            #      "l1_customer_profile_union_daily_feature_for_l1_network_streaming_cqi",
+            #      "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_stream_1day"],
+            #     "l1_network_streaming_cqi"
+            # )                                             
         ]
     )
 #rerun 2 table wrong msisdn
