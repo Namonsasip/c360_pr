3db18ac3090ff6ad5f4924e0ff73a33a4c4c0106
chayaphn <chayaphn@ais.co.th>
2021-08-02 18:33:27 +0700

    Edit: Uncomment input pipeline Edit: Uncomment the rest, run only binary model

--- src/du/models/models_pipeline.py
+++ src/du/models/models_pipeline.py
@@ -11,36 +11,36 @@ from kedro.pipeline import Pipeline, node
 def create_du_models_pipeline() -> Pipeline:
     return Pipeline(
         [
-            node(
-                calculate_feature_importance,
-                inputs={
-                    "df_master": "l5_du_master_tbl",
-                    "model_params": "params:du_model_model_params",
-                    "binary_target_column": "params:du_acceptance_model_target_column",
-                    "regression_target_column": "params:du_arpu_30d_model_target_column",
-                    "train_sampling_ratio": "params:du_model_train_sampling_ratio",
-                    "model_type": "params:du_acceptance_model_tag",
-                    "min_obs_per_class_for_model": "params:du_model_min_obs_per_class_for_model",
-                },
-                outputs="feature_importance_binary_model",
-                name="du_acceptance_models_feature_importance",
-                tags=["du_acceptance_models_feature_importance", "du_models"]
-            ),
-            node(
-                calculate_feature_importance,
-                inputs={
-                    "df_master": "l5_du_master_tbl",
-                    "model_params": "params:du_model_model_params",
-                    "binary_target_column": "params:du_acceptance_model_target_column",
-                    "regression_target_column": "params:du_arpu_30d_model_target_column",
-                    "train_sampling_ratio": "params:du_model_train_sampling_ratio",
-                    "model_type": "params:du_arpu_model_tag",
-                    "min_obs_per_class_for_model": "params:du_model_min_obs_per_class_for_model",
-                },
-                outputs="feature_importance_regression_model",
-                name="du_arpu_30d_models_feature_importance",
-                tags=["du_arpu_30d_models_feature_importance", "du_models"]
-            ),
+            # node(
+            #     calculate_feature_importance,
+            #     inputs={
+            #         "df_master": "l5_du_master_tbl",
+            #         "model_params": "params:du_model_model_params",
+            #         "binary_target_column": "params:du_acceptance_model_target_column",
+            #         "regression_target_column": "params:du_arpu_30d_model_target_column",
+            #         "train_sampling_ratio": "params:du_model_train_sampling_ratio",
+            #         "model_type": "params:du_acceptance_model_tag",
+            #         "min_obs_per_class_for_model": "params:du_model_min_obs_per_class_for_model",
+            #     },
+            #     outputs="feature_importance_binary_model",
+            #     name="du_acceptance_models_feature_importance",
+            #     tags=["du_acceptance_models_feature_importance", "du_models"]
+            # ),
+            # node(
+            #     calculate_feature_importance,
+            #     inputs={
+            #         "df_master": "l5_du_master_tbl",
+            #         "model_params": "params:du_model_model_params",
+            #         "binary_target_column": "params:du_acceptance_model_target_column",
+            #         "regression_target_column": "params:du_arpu_30d_model_target_column",
+            #         "train_sampling_ratio": "params:du_model_train_sampling_ratio",
+            #         "model_type": "params:du_arpu_model_tag",
+            #         "min_obs_per_class_for_model": "params:du_model_min_obs_per_class_for_model",
+            #     },
+            #     outputs="feature_importance_regression_model",
+            #     name="du_arpu_30d_models_feature_importance",
+            #     tags=["du_arpu_30d_models_feature_importance", "du_models"]
+            # ),
             node(
                 partial(
                     train_multiple_models,
@@ -66,32 +66,32 @@ def create_du_models_pipeline() -> Pipeline:
                 name="du_acceptance_models_training",
                 tags=["du_acceptance_models_training", "du_models"],
             ),
-            node(
-                partial(
-                    train_multiple_models,
-                    model_type="regression",
-                    pai_run_prefix="dummy_regression_",
-                    undersampling=False
-                ),
-                inputs={
-                    "df_master": "l5_du_master_table_only_accepted",
-                    "group_column": "params:du_model_group_column",
-                    "target_column": "params:du_arpu_30d_model_target_column",
-                    "train_sampling_ratio": "params:du_model_train_sampling_ratio",
-                    "model_params": "params:du_model_model_params",
-                    "max_rows_per_group": "params:du_model_max_rows_per_group",
-                    "min_obs_per_class_for_model": "params:du_model_min_obs_per_class_for_model",
-                    "mlflow_model_version": "params:du_mlflow_model_version_training",
-                    "extra_keep_columns": "params:du_extra_tag_columns_pai",
-                    "pai_runs_uri": "params:du_pai_runs_uri",
-                    "pai_artifacts_uri": "params:du_pai_artifacts_uri",
-                    "regression_clip_target_quantiles": "params:du_regression_clip_target_quantiles_arpuu",
-                    "du_top_features": "feature_importance_regression_model"
-                },
-                outputs="du_arpu_30d_models_train_set",
-                name="du_arpu_30d_models_training",
-                tags=["du_arpu_30d_models_training", "du_models"],
-            ),
+            # node(
+            #     partial(
+            #         train_multiple_models,
+            #         model_type="regression",
+            #         pai_run_prefix="dummy_regression_",
+            #         undersampling=False
+            #     ),
+            #     inputs={
+            #         "df_master": "l5_du_master_table_only_accepted",
+            #         "group_column": "params:du_model_group_column",
+            #         "target_column": "params:du_arpu_30d_model_target_column",
+            #         "train_sampling_ratio": "params:du_model_train_sampling_ratio",
+            #         "model_params": "params:du_model_model_params",
+            #         "max_rows_per_group": "params:du_model_max_rows_per_group",
+            #         "min_obs_per_class_for_model": "params:du_model_min_obs_per_class_for_model",
+            #         "mlflow_model_version": "params:du_mlflow_model_version_training",
+            #         "extra_keep_columns": "params:du_extra_tag_columns_pai",
+            #         "pai_runs_uri": "params:du_pai_runs_uri",
+            #         "pai_artifacts_uri": "params:du_pai_artifacts_uri",
+            #         "regression_clip_target_quantiles": "params:du_regression_clip_target_quantiles_arpuu",
+            #         "du_top_features": "feature_importance_regression_model"
+            #     },
+            #     outputs="du_arpu_30d_models_train_set",
+            #     name="du_arpu_30d_models_training",
+            #     tags=["du_arpu_30d_models_training", "du_models"],
+            # ),
             # node(
             #     lambda pdf_master_chunk, pdf_extra_pai_metrics: create_model_function(
             #         as_pandas_udf=False,
