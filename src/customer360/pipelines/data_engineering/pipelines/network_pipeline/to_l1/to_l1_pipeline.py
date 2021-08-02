90343b6f5f6cdf527f09bfd273352cd63b91bd64
pakawat <pakap571@epicc.postbox.in.th>
2021-08-02 10:32:23 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
@@ -274,41 +274,41 @@ def network_cei_to_l1_pipeline(**kwargs):
 def network_to_l1_pipeline_test(**kwargs):
     return Pipeline(
         [
-            # node(
-            #     build_network_user_cqi,
-            #     ["l0_network_sdr_dyn_cea_cei_cei_usr_1day_for_l1_network_user_cqi",
-            #      "params:l1_network_user_cqi",
-            #      "l1_customer_profile_union_daily_feature_for_l1_network_user_cqi",
-            #      "params:exception_partition_for_l1_network_user_cqi"],
-            #     "l1_network_user_cqi"
-            # ),            
+            node(
+                build_network_user_cqi,
+                ["l0_network_sdr_dyn_cea_cei_cei_usr_1day_for_l1_network_user_cqi",
+                 "params:l1_network_user_cqi",
+                 "l1_customer_profile_union_daily_feature_for_l1_network_user_cqi",
+                 "params:exception_partition_for_l1_network_user_cqi"],
+                "l1_network_user_cqi"
+            ),            
 
-            # node(
-            #     build_network_im_cqi,
-            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day_for_l1_network_im_cqi",
-            #      "params:l1_network_im_cqi",
-            #      "l1_customer_profile_union_daily_feature_for_l1_network_im_cqi",
-            #      "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day"],
-            #     "l1_network_im_cqi"
-            # ),#error เรื่อง partition
+            node(
+                build_network_im_cqi,
+                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day_for_l1_network_im_cqi",
+                 "params:l1_network_im_cqi",
+                 "l1_customer_profile_union_daily_feature_for_l1_network_im_cqi",
+                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day"],
+                "l1_network_im_cqi"
+            ),#error เรื่อง partition
 
-            # node(
-            #     build_network_voip_cqi,
-            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_voip_1day_for_l1_network_voip_cqi",
-            #      "params:l1_network_voip_cqi",
-            #      "l1_customer_profile_union_daily_feature_for_l1_network_voip_cqi",
-            #      "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_voip_1day"],
-            #     "l1_network_voip_cqi"
-            # ),
+            node(
+                build_network_voip_cqi,
+                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_voip_1day_for_l1_network_voip_cqi",
+                 "params:l1_network_voip_cqi",
+                 "l1_customer_profile_union_daily_feature_for_l1_network_voip_cqi",
+                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_voip_1day"],
+                "l1_network_voip_cqi"
+            ),
 
-            # node(
-            #     build_network_volte_cqi,
-            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_volte_1day_for_l1_network_volte_cqi",
-            #      "params:l1_network_volte_cqi",
-            #      "l1_customer_profile_union_daily_feature_for_l1_network_volte_cqi",
-            #      "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_volte_1day"],
-            #     "l1_network_volte_cqi"
-            # ),
+            node(
+                build_network_volte_cqi,
+                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_volte_1day_for_l1_network_volte_cqi",
+                 "params:l1_network_volte_cqi",
+                 "l1_customer_profile_union_daily_feature_for_l1_network_volte_cqi",
+                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_volte_1day"],
+                "l1_network_volte_cqi"
+            ),
 
             node(
                 build_network_web_cqi,
@@ -319,23 +319,23 @@ def network_to_l1_pipeline_test(**kwargs):
                 "l1_network_web_cqi"
             ),  
 
-            # node(
-            #     build_network_file_transfer_cqi,
-            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_fileaccess_1day_for_l1_network_file_transfer_cqi",
-            #      "params:l1_network_file_transfer_cqi",
-            #      "l1_customer_profile_union_daily_feature_for_l1_network_file_transfer_cqi",
-            #      "params:exception_partitions_list_for_l1_network_file_transfer_cqi"],
-            #     "l1_network_file_transfer_cqi"
-            # ),
+            node(
+                build_network_file_transfer_cqi,
+                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_fileaccess_1day_for_l1_network_file_transfer_cqi",
+                 "params:l1_network_file_transfer_cqi",
+                 "l1_customer_profile_union_daily_feature_for_l1_network_file_transfer_cqi",
+                 "params:exception_partitions_list_for_l1_network_file_transfer_cqi"],
+                "l1_network_file_transfer_cqi"
+            ),
   
-            # node(
-            #     build_network_streaming_cqi,
-            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_stream_1day_for_l1_network_streaming_cqi",
-            #      "params:l1_network_streaming_cqi",
-            #      "l1_customer_profile_union_daily_feature_for_l1_network_streaming_cqi",
-            #      "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_stream_1day"],
-            #     "l1_network_streaming_cqi"
-            # )                                             
+            node(
+                build_network_streaming_cqi,
+                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_stream_1day_for_l1_network_streaming_cqi",
+                 "params:l1_network_streaming_cqi",
+                 "l1_customer_profile_union_daily_feature_for_l1_network_streaming_cqi",
+                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_stream_1day"],
+                "l1_network_streaming_cqi"
+            )                                             
         ]
     )
 #rerun 2 table wrong msisdn
