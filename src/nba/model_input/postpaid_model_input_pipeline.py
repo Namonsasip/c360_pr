b2302078547caf1f0cad7f7d8672ce79074e4561
thanakse <thanakse@ais.co.th>
2021-08-02 13:06:55 +0700

    Comment out to test model input pipeline

--- src/nba/model_input/postpaid_model_input_pipeline.py
+++ src/nba/model_input/postpaid_model_input_pipeline.py
@@ -20,34 +20,34 @@ def create_nba_postpaid_model_input_pipeline() -> Pipeline:
     return Pipeline(
         [
             # OK for postpaid
-            # node(
-            #     node_l5_nba_postpaid_customer_profile,
-            #     inputs={
-            #         "l3_customer_profile_include_1mo_non_active": "l3_customer_profile_include_1mo_non_active",
-            #     },
-            #     outputs="l5_nba_postpaid_customer_profile",
-            #     name="l5_nba_postpaid_customer_profile",
-            #     tags=["l5_nba_postpaid_customer_profile"],
-            # ),
-            #
-            # node(
-            #     node_l4_revenue_billcycle_postpaid_aggregation,
-            #     inputs={
-            #         "l0_revenue_nbo_postpaid_input_data": "l0_revenue_nbo_postpaid_input_data"
-            #     },
-            #     outputs="l4_revenue_postpaid_average_by_bill_cycle",
-            #     tags=["l4_revenue_postpaid_average_by_bill_cycle"]
-            # ),
-            #
-            # node(
-            #     node_l5_nba_postpaid_campaign_master,
-            #     inputs={
-            #         "campaign_history_master_active": "campaign_history_master_active",
-            #     },
-            #     outputs="l5_nba_postpaid_campaign_master",
-            #     name="l5_nba_postpaid_campaign_master",
-            #     tags=["l5_nba_postpaid_campaign_master"],
-            # ),
+            node(
+                node_l5_nba_postpaid_customer_profile,
+                inputs={
+                    "l3_customer_profile_include_1mo_non_active": "l3_customer_profile_include_1mo_non_active",
+                },
+                outputs="l5_nba_postpaid_customer_profile",
+                name="l5_nba_postpaid_customer_profile",
+                tags=["l5_nba_postpaid_customer_profile"],
+            ),
+
+            node(
+                node_l4_revenue_billcycle_postpaid_aggregation,
+                inputs={
+                    "l0_revenue_nbo_postpaid_input_data": "l0_revenue_nbo_postpaid_input_data"
+                },
+                outputs="l4_revenue_postpaid_average_by_bill_cycle",
+                tags=["l4_revenue_postpaid_average_by_bill_cycle"]
+            ),
+
+            node(
+                node_l5_nba_postpaid_campaign_master,
+                inputs={
+                    "campaign_history_master_active": "campaign_history_master_active",
+                },
+                outputs="l5_nba_postpaid_campaign_master",
+                name="l5_nba_postpaid_campaign_master",
+                tags=["l5_nba_postpaid_campaign_master"],
+            ),
             # TODO l0_campaign_tracking_contact_list_pre_full_load --> Post-paid, full_load ? Done
             #     l4_revenue_prepaid_daily_features --> Post-paid
             #     nba_prioritized_campaigns_child_codes --> Post-paid
@@ -55,24 +55,24 @@ def create_nba_postpaid_model_input_pipeline() -> Pipeline:
             #     nba_model_use_cases_child_codes --> Post-paid
             #     nba_master_table_date_min, nba_master_table_date_max --> Post-paid, Maybe history features must be longer than Pre-paid
             #     nba_min_feature_days_lag --> Post-paid
-            # node(
-            #     node_l5_nba_postpaid_master_table_spine,
-            #     inputs={
-            #         "l0_campaign_tracking_contact_list_post": "l0_campaign_tracking_contact_list_post_full_load",
-            #         "l1_customer_profile_union_daily_feature_full_load": "l1_customer_profile_union_daily_feature_full_load",
-            #         #"l4_revenue_postpaid_ru_f_sum_revenue_by_service_monthly": "l4_revenue_postpaid_ru_f_sum_revenue_by_service_monthly",
-            #         "l4_revenue_postpaid_average_by_bill_cycle": "l4_revenue_postpaid_average_by_bill_cycle",
-            #         "l5_nba_campaign_master": "l5_nba_postpaid_campaign_master",
-            #         "nba_model_group_column_push_campaign": "params:nba_postpaid_model_group_column_push_campaign",
-            #         "nba_model_group_column_pull_campaign": "params:nba_postpaid_model_group_column_pull_campaign",
-            #         "date_min": "params:nba_postpaid_master_table_date_min",
-            #         "date_max": "params:nba_postpaid_master_table_date_max",
-            #         "postpaid_min_feature_days_lag": "params:nba_postpaid_min_feature_days_lag"
-            #     },
-            #     outputs="l5_nba_postpaid_master_table_spine",
-            #     name="l5_nba_postpaid_master_table_spine",
-            #     tags=["l5_nba_postpaid_master_table_spine", "nba_postpaid_masters"],
-            # ),
+            node(
+                node_l5_nba_postpaid_master_table_spine,
+                inputs={
+                    "l0_campaign_tracking_contact_list_post": "l0_campaign_tracking_contact_list_post_full_load",
+                    "l1_customer_profile_union_daily_feature_full_load": "l1_customer_profile_union_daily_feature_full_load",
+                    #"l4_revenue_postpaid_ru_f_sum_revenue_by_service_monthly": "l4_revenue_postpaid_ru_f_sum_revenue_by_service_monthly",
+                    "l4_revenue_postpaid_average_by_bill_cycle": "l4_revenue_postpaid_average_by_bill_cycle",
+                    "l5_nba_campaign_master": "l5_nba_postpaid_campaign_master",
+                    "nba_model_group_column_push_campaign": "params:nba_postpaid_model_group_column_push_campaign",
+                    "nba_model_group_column_pull_campaign": "params:nba_postpaid_model_group_column_pull_campaign",
+                    "date_min": "params:nba_postpaid_master_table_date_min",
+                    "date_max": "params:nba_postpaid_master_table_date_max",
+                    "postpaid_min_feature_days_lag": "params:nba_postpaid_min_feature_days_lag"
+                },
+                outputs="l5_nba_postpaid_master_table_spine",
+                name="l5_nba_postpaid_master_table_spine",
+                tags=["l5_nba_postpaid_master_table_spine", "nba_postpaid_masters"],
+            ),
             # TODO All features check time range, sla, calculate time point, can predict by BC?
             #     nba_model_input_features --> Post-paid
             #     l4_billing_rolling_window_topup_and_volume --> Post-paid
