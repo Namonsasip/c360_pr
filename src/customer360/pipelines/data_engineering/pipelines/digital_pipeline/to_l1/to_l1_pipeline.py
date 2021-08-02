da06e4beb27c110594a3c89b792bcb9aa2c6226b
paipayon <paipayon2@gmail.com>
2021-08-02 10:12:00 +0700

    a

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -356,7 +356,7 @@ def digital_to_l1_digital_mobile_web_agg_daily(**kwargs):
                 func=l1_digital_customer_web_category_agg_cat_level_union_daily,
                 inputs=
                 [
-                    "int_l1_digital_customer_web_category_agg_daily_catlv_2",
+                    "int_l1_digital_customer_web_category_agg_daily_catlv_1@l1_digital_customer_web_category_agg_daily_catlv_2",
                     "l1_digital_cxense_traffic_complete_agg_daily_for_l1_dital_customer_web_agg_daily_catlv_2",
                     "params:level_2"
                 ],
@@ -367,7 +367,7 @@ def digital_to_l1_digital_mobile_web_agg_daily(**kwargs):
                 func=l1_digital_customer_web_category_agg_cat_level_union_daily,
                 inputs=
                 [
-                    "int_l1_digital_customer_web_category_agg_daily_catlv_3",
+                    "int_l1_digital_customer_web_category_agg_daily_catlv_1@l1_digital_customer_web_category_agg_daily_catlv_3",
                     "l1_digital_cxense_traffic_complete_agg_daily_for_l1_dital_customer_web_agg_daily_catlv_3",
                     "params:level_3"
                 ],
@@ -378,7 +378,7 @@ def digital_to_l1_digital_mobile_web_agg_daily(**kwargs):
                 func=l1_digital_customer_web_category_agg_cat_level_union_daily,
                 inputs=
                 [
-                    "int_l1_digital_customer_web_category_agg_daily_catlv_4",
+                    "int_l1_digital_customer_web_category_agg_daily_catlv_1@ll1_digital_customer_web_category_agg_daily_catlv_3",
                     "l1_digital_cxense_traffic_complete_agg_daily_for_l1_dital_customer_web_agg_daily_catlv_4",
                     "params:level_4"
                 ],
