841591833640e391c531db0d0edfdba63f907833
pakawat <pakawat.pln@gmail.com>
2021-08-04 22:53:12 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
@@ -361,22 +361,22 @@ def network_to_l1_pipeline_rerun(**kwargs):
             #     "l1_network_im_cqi"
             # ),#error เรื่อง partition
 
-            # node(
-            #     build_network_web_cqi,
-            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_web_1day_for_l1_network_web_cqi",
-            #      "params:l1_network_web_cqi",
-            #      "l1_customer_profile_union_daily_feature_for_l1_network_web_cqi",
-            #      "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_web_1day"],
-            #     "l1_network_web_cqi"
-            # ),
             node(
-                build_network_voip_cqi,
-                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_voip_1day_for_l1_network_voip_cqi",
-                 "params:l1_network_voip_cqi",
-                 "l1_customer_profile_union_daily_feature_for_l1_network_voip_cqi",
-                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_voip_1day"],
-                "l1_network_voip_cqi"
+                build_network_web_cqi,
+                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_web_1day_for_l1_network_web_cqi",
+                 "params:l1_network_web_cqi",
+                 "l1_customer_profile_union_daily_feature_for_l1_network_web_cqi",
+                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_web_1day"],
+                "l1_network_web_cqi"
             ),
+            # node(
+            #     build_network_voip_cqi,
+            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_voip_1day_for_l1_network_voip_cqi",
+            #      "params:l1_network_voip_cqi",
+            #      "l1_customer_profile_union_daily_feature_for_l1_network_voip_cqi",
+            #      "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_voip_1day"],
+            #     "l1_network_voip_cqi"
+            # ),
             node(
                 build_network_volte_cqi,
                 ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_volte_1day_for_l1_network_volte_cqi",
