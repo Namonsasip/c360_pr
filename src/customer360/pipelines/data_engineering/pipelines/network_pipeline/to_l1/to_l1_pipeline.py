591f6062d8a1b7a0366fec324e2757af7226a869
pakawat <pakawat.pln@gmail.com>
2021-08-06 13:44:18 +0700

    fix

--- src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/network_pipeline/to_l1/to_l1_pipeline.py
@@ -352,23 +352,23 @@ def network_to_l1_pipeline_test(**kwargs):
 def network_to_l1_pipeline_rerun(**kwargs):
         return Pipeline(
         [
-            node(
-                build_network_im_cqi,
-                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day_for_l1_network_im_cqi",
-                 "params:l1_network_im_cqi",
-                 "l1_customer_profile_union_daily_feature_for_l1_network_im_cqi",
-                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day"],
-                "l1_network_im_cqi"
-            ),#error เรื่อง partition
-
             # node(
-            #     build_network_web_cqi,
-            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_web_1day_for_l1_network_web_cqi",
-            #      "params:l1_network_web_cqi",
-            #      "l1_customer_profile_union_daily_feature_for_l1_network_web_cqi",
-            #      "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_web_1day"],
-            #     "l1_network_web_cqi"
-            # ),
+            #     build_network_im_cqi,
+            #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day_for_l1_network_im_cqi",
+            #      "params:l1_network_im_cqi",
+            #      "l1_customer_profile_union_daily_feature_for_l1_network_im_cqi",
+            #      "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_im_1day"],
+            #     "l1_network_im_cqi"
+            # ),#error เรื่อง partition
+
+            node(
+                build_network_web_cqi,
+                ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_web_1day_for_l1_network_web_cqi",
+                 "params:l1_network_web_cqi",
+                 "l1_customer_profile_union_daily_feature_for_l1_network_web_cqi",
+                 "params:exception_partitions_list_for_network_sdr_dyn_cea_cei_qoe_cell_usr_web_1day"],
+                "l1_network_web_cqi"
+            ),
             # node(
             #     build_network_voip_cqi,
             #     ["l0_network_sdr_dyn_cea_cei_qoe_cell_usr_voip_1day_for_l1_network_voip_cqi",
