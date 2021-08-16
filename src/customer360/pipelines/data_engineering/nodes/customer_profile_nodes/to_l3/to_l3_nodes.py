326f5ad386db80832ecfee8629534689aa9dcbdf
ChaninChaisatianQB <66584647+ChaninChaisatianQB@users.noreply.github.com>
2021-08-16 13:33:32 +0700

    Update to_l3_nodes.py
    get_spark_empty_df

--- src/customer360/pipelines/data_engineering/nodes/customer_profile_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/customer_profile_nodes/to_l3/to_l3_nodes.py
@@ -11,7 +11,7 @@ conf = os.getenv("CONF", None)
 def df_copy_for_l3_customer_profile_include_1mo_non_active(input_df, segment_df, multisum_df):
     ################################ Start Implementing Data availability checks #############################
     if check_empty_dfs([input_df, segment_df, multisum_df]):
-        return get_spark_empty_df()
+        return get_spark_empty_df(3)
 
     input_df = data_non_availability_and_missing_check(df=input_df, grouping="monthly",
                                                        par_col="partition_month",
