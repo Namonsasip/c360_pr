11662f3da589bb9bfe52615154b784279b1df9ff
sitticsr <sitticsr@ais.co.th>
2021-08-18 23:14:03 +0700

    debug bi model :mlflow_model_version to v 1

--- src/nba/models/models_pipeline.py
+++ src/nba/models/models_pipeline.py
@@ -58,7 +58,7 @@ def create_nba_models_pipeline() -> Pipeline:
                         f"acceptance_"
                         f"sitticsr"
                     ),
-                    undersampling=True
+                    undersampling=False
                 ),
                 inputs={
                     "df_master": "l5_nba_master_table",
@@ -80,38 +80,38 @@ def create_nba_models_pipeline() -> Pipeline:
                 name="nba_acceptance_models_training",
                 tags=["nba_acceptance_models_training", "nba_models"],
             ),
-            node(
-                partial(
-                    train_multiple_models,
-                    model_type="regression",
-                    pai_run_prefix=(
-                        f"{get_local_datetime().strftime('%Y%m%d_%H%M%S')}_"
-                        f"arpu_30d_"
-                        f"sitticsr_"
-                    ),
-                    undersampling=False
-                ),
-                inputs={
-                    "df_master": "l5_nba_master_table_only_accepted",
-                    "group_column": "params:nba_model_group_column",
-                    "target_column": "params:nba_arpu_30d_model_target_column",
-                    "train_sampling_ratio": "params:nba_model_train_sampling_ratio",
-                    "model_params": "params:nba_model_model_params",
-                    "max_rows_per_group": "params:nba_model_max_rows_per_group",
-                    "minimun_row": "params:nba_model_min_obs_per_class",
-                    "min_obs_per_class_for_model": "params:nba_model_min_obs_per_class_for_model",
-                    "mlflow_model_version": "params:nba_mlflow_model_version_training",
-                    "extra_keep_columns": "params:nba_extra_tag_columns_pai",
-                    "pai_runs_uri": "params:nba_pai_runs_uri",
-                    "pai_artifacts_uri": "params:nba_pai_artifacts_uri",
-                    "regression_clip_target_quantiles": "params:regression_clip_target_quantiles_arpu",
-                    "campaigns_child_codes_list": "params:nba_prioritized_campaigns_child_codes",
-                    "nba_top_features": "nba_feature_importance_regression_model"
-                },
-                outputs="nba_arpu_30d_models_train_set",
-                name="nba_arpu_30d_models_training",
-                tags=["nba_arpu_30d_models_training", "nba_models"],
-            ),
+            # node(
+            #     partial(
+            #         train_multiple_models,
+            #         model_type="regression",
+            #         pai_run_prefix=(
+            #             f"{get_local_datetime().strftime('%Y%m%d_%H%M%S')}_"
+            #             f"arpu_30d_"
+            #             f"sitticsr_"
+            #         ),
+            #         undersampling=False
+            #     ),
+            #     inputs={
+            #         "df_master": "l5_nba_master_table_only_accepted",
+            #         "group_column": "params:nba_model_group_column",
+            #         "target_column": "params:nba_arpu_30d_model_target_column",
+            #         "train_sampling_ratio": "params:nba_model_train_sampling_ratio",
+            #         "model_params": "params:nba_model_model_params",
+            #         "max_rows_per_group": "params:nba_model_max_rows_per_group",
+            #         "minimun_row": "params:nba_model_min_obs_per_class",
+            #         "min_obs_per_class_for_model": "params:nba_model_min_obs_per_class_for_model",
+            #         "mlflow_model_version": "params:nba_mlflow_model_version_training",
+            #         "extra_keep_columns": "params:nba_extra_tag_columns_pai",
+            #         "pai_runs_uri": "params:nba_pai_runs_uri",
+            #         "pai_artifacts_uri": "params:nba_pai_artifacts_uri",
+            #         "regression_clip_target_quantiles": "params:regression_clip_target_quantiles_arpu",
+            #         "campaigns_child_codes_list": "params:nba_prioritized_campaigns_child_codes",
+            #         "nba_top_features": "nba_feature_importance_regression_model"
+            #     },
+            #     outputs="nba_arpu_30d_models_train_set",
+            #     name="nba_arpu_30d_models_training",
+            #     tags=["nba_arpu_30d_models_training", "nba_models"],
+            # ),
             # node(
             #     partial(
             #         train_multiple_models,
