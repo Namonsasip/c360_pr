a183555592b2454eea9416287aba11feb47845be
sitticsr <sitticsr@ais.co.th>
2021-08-20 12:18:34 +0700

    debug: Re train model and Retrain feature imp fn ver3.3 new param: regression and Binary

--- src/nba/models/postpaid_models_nodes.py
+++ src/nba/models/postpaid_models_nodes.py
@@ -240,7 +240,7 @@ def filter_valid_campaign_child_code(l5_nba_master: pyspark.sql.DataFrame,
 
     # Store the model_group that agree to the first condition
     # Recall that MODELLING_N_OBS_THRESHOLD = 10000
-    MODELLING_N_OBS_THRESHOLD = 5000
+    MODELLING_N_OBS_THRESHOLD = 2000
     agree_with_the_condition_1 = count_in_each_model_group.filter(
         count_in_each_model_group['count'] >= MODELLING_N_OBS_THRESHOLD).select(group_column).toPandas()
     ccc_agree_with_the_condition_1 = agree_with_the_condition_1[group_column].to_list()
@@ -350,7 +350,7 @@ def calculate_feature_importance(
     ###########
 
     # Use Window function to random maximum of 10K records for each model
-    n = 20000
+    n = 30000
     w = Window.partitionBy(F.col(group_column)).orderBy(F.col("rnd_"))
 
     sampled_master_table = (l5_nba_master_with_valid_campaign_child_code
@@ -408,7 +408,7 @@ def calculate_feature_importance(
             count_target_all = train_single_model_pdf[target_column].count()
             pct_target_1 = (count_target_1 / count_target_all) * 100
 
-            if pct_target_1 >= 1.5:
+            if pct_target_1 >= 1:
                 print('Condition pass: pct_target is', pct_target_1)
 
                 # try:
