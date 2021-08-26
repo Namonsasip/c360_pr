a109f2d12fc224f4b7af1c3bfacc80a163d4e35c
paipayon <paipayon2@gmail.com>
2021-08-26 10:59:27 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -339,7 +339,7 @@ def digital_to_l1_digital_mobile_web_agg_daily(**kwargs):
                 [
                     "l0_digital_mobile_web_daily_for_l1_digital_customer_web_category_agg_daily_catlv_1",
                     "l1_digital_cxense_traffic_complete_agg_daily_for_l1_dital_customer_web_agg_daily_catlv_1",
-                    "params:level_1",
+                    "params:category_level_1",
                     "params:l1_dital_customer_web_agg_daily_sql"
                 ],
                 outputs="l1_digital_customer_web_category_agg_daily_catlv_1",
@@ -351,7 +351,7 @@ def digital_to_l1_digital_mobile_web_agg_daily(**kwargs):
                 [
                     "l0_digital_mobile_web_daily_for_l1_digital_customer_web_category_agg_daily_catlv_2",
                     "l1_digital_cxense_traffic_complete_agg_daily_for_l1_dital_customer_web_agg_daily_catlv_2",
-                    "params:level_2",
+                    "params:category_level_2",
                     "params:l1_dital_customer_web_agg_daily_sql"
                 ],
                 outputs="l1_digital_customer_web_category_agg_daily_catlv_2",
@@ -363,7 +363,7 @@ def digital_to_l1_digital_mobile_web_agg_daily(**kwargs):
                 [
                     "l0_digital_mobile_web_daily_for_l1_digital_customer_web_category_agg_daily_catlv_3",
                     "l1_digital_cxense_traffic_complete_agg_daily_for_l1_dital_customer_web_agg_daily_catlv_3",
-                    "params:level_3",
+                    "params:category_level_3",
                     "params:l1_dital_customer_web_agg_daily_sql"
                 ],
                 outputs="l1_digital_customer_web_category_agg_daily_catlv_3",
@@ -375,7 +375,7 @@ def digital_to_l1_digital_mobile_web_agg_daily(**kwargs):
                 [
                     "l0_digital_mobile_web_daily_for_l1_digital_customer_web_category_agg_daily_catlv_4",
                     "l1_digital_cxense_traffic_complete_agg_daily_for_l1_dital_customer_web_agg_daily_catlv_4",
-                    "params:level_4",
+                    "params:category_level_4",
                     "params:l1_dital_customer_web_agg_daily_sql"
                 ],
                 outputs="l1_digital_customer_web_category_agg_daily_catlv_4",
