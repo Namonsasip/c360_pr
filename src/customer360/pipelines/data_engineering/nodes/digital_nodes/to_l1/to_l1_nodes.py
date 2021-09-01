35d1d7130382cc16b6626782f77f73bb30824db2
paipayon <paipayon2@gmail.com>
2021-09-01 22:10:57 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -1503,12 +1503,13 @@ def digital_customer_cxense_agg_daily( cxen_traffic:pyspark.sql.DataFrame,cxen_m
             f.lit("-"),
             f.substring(f.col("partition_date").cast("string"), 7, 2)
             ))
-    #-------- Join Master ---------#
-    cxen_traffic = cxen_traffic.join(cxen_master,on=[cxen_traffic.url == cxen_master.site_url],how="left")
     #-------- Sum ---------#
     cxen_traffic = cxen_traffic.groupBy("mobile_no", "url", "partition_date").agg(f.sum("activetime").alias("total_visit_duration"),f.count("*").alias("total_visit_count"))
+    #-------- Join Master ---------#
+    cxen_traffic = cxen_traffic.join(cxen_master,on=[cxen_traffic.url == cxen_master.site_url],how="left")
+    cxen_traffic = cxen_traffic.withColumnRenamed(cat_level, 'category_name')
+    cxen_traffic = cxen_traffic.select("mobile_no", "category_name", "total_visit_count","total_visit_duration","event_partition_date")
     #-------- Join Profile ---------#
     cxen_traffic = cxen_traffic.join(customer_profile,on=[cxen_traffic.mobile_no == customer_profile.access_method_num],how="left")
-    cxen_traffic = cxen_traffic.withColumnRenamed(cat_level, 'category_name')
-    cxen_traffic = cxen_traffic.select("access_method_num","mobile_no", "category_name","priority", "total_visit_count","total_visit_duration","event_partition_date")
+    cxen_traffic = cxen_traffic.select("subscription_identifier","mobile_no", "category_name", "total_visit_count","total_visit_duration","event_partition_date")
     return cxen_traffic
