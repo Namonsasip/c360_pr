d4e954adb64940018fa90d2bf528d928ca671077
paipayon <paipayon2@gmail.com>
2021-09-01 23:33:52 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -1512,4 +1512,6 @@ def digital_customer_cxense_agg_daily( cxen_traffic:pyspark.sql.DataFrame,cxen_m
     #-------- Join Profile ---------#
     cxen_traffic = cxen_traffic.join(customer_profile,on=[cxen_traffic.mobile_no == customer_profile.access_method_num,cxen_traffic.event_partition_date == customer_profile.event_partition_date],how="left")
     cxen_traffic = cxen_traffic.select("subscription_identifier","mobile_no", "url", "category_name", "total_visit_count","total_visit_duration",customer_profile.event_partition_date)
+    cxen_traffic = cxen_traffic.filter(f.col("mobile_no").isNotNull())
+    cxen_traffic = cxen_traffic.filter(f.col("url").isNotNull())
     return cxen_traffic
