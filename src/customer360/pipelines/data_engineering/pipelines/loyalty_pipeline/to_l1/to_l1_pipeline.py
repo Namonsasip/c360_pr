e691dfc8d4c63506919f16e9f307f51be23c7fb7
KraiwitSuwanmekQB <66598279+KraiwitSuwanmekQB@users.noreply.github.com>
2021-09-07 15:08:22 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/loyalty_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/loyalty_pipeline/to_l1/to_l1_pipeline.py
@@ -17,22 +17,22 @@ def loyalty_to_l1_pipeline(**kwargs):
                  "l0_loyalty_drm_t_privilege_success"],
                 "l1_loyalty_number_of_services_daily"
             ),
-
-            # Number of rewards spent for each category
-            node(
-                loyalty_number_of_rewards_redeemed_for_each_category,
-                ["l1_customer_profile_union_daily_feature_for_l1_loyalty_number_of_rewards",
-                 "l0_loyalty_drm_t_aunjai_point_collection_daily_for_l1_loyalty_number_of_rewards"],
-                "l1_loyalty_number_of_rewards_daily"
-            ),
-
-            # Number of points spend for each category
-            node(
-                loyalty_number_of_points_spend_for_each_category,
-                ["l1_customer_profile_union_daily_feature_for_l1_loyalty_number_of_points_spend",
-                 "l0_loyalty_priv_point_transaction"],
-                "l1_loyalty_number_of_points_spend_daily"
-            ),
+            #
+            # # Number of rewards spent for each category
+            # node(
+            #     loyalty_number_of_rewards_redeemed_for_each_category,
+            #     ["l1_customer_profile_union_daily_feature_for_l1_loyalty_number_of_rewards",
+            #      "l0_loyalty_drm_t_aunjai_point_collection_daily_for_l1_loyalty_number_of_rewards"],
+            #     "l1_loyalty_number_of_rewards_daily"
+            # ),
+            #
+            # # Number of points spend for each category
+            # node(
+            #     loyalty_number_of_points_spend_for_each_category,
+            #     ["l1_customer_profile_union_daily_feature_for_l1_loyalty_number_of_points_spend",
+            #      "l0_loyalty_priv_point_transaction"],
+            #     "l1_loyalty_number_of_points_spend_daily"
+            # ),
 
         ]
     )
