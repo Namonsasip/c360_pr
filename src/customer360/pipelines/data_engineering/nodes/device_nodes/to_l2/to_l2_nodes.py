67acf58ac73df753b0bd397f4d8006083b38381e
NitipongChaikitiphornQB <77653671+NitipongChaikitiphornQB@users.noreply.github.com>
2021-09-08 13:59:45 +0700

    mod node

--- src/customer360/pipelines/data_engineering/nodes/device_nodes/to_l2/to_l2_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/device_nodes/to_l2/to_l2_nodes.py
@@ -46,9 +46,9 @@ def device_summary_with_configuration(hs_summary: DataFrame,
     hs_configs = hs_configs.withColumn("start_of_week",
                                        f.to_date(f.date_trunc('week', f.to_date(f.col("partition_date"), 'yyyyMMdd'))))
 
-    hs_summary = hs_summary.filter(F.col('start_of_week').between('2021-07-05', '2021-08-09'))
-    hs_configs = hs_configs.filter(F.col('start_of_week').between('2021-07-05', '2021-08-09'))
-    logging.info("---------------- Filter 2021-07-05 to 2021-08-09 Completed ----------------")
+    hs_summary = hs_summary.filter(F.col('start_of_week').between('2021-08-02', '2021-08-09'))
+    hs_configs = hs_configs.filter(F.col('start_of_week').between('2021-08-02', '2021-08-09'))
+    logging.info("---------------- Filter 2021-08-02 to 2021-08-09 Completed ----------------")
 
     hs_config_sel = ["start_of_week", "hs_brand_code", "hs_model_code", "month_id", "os", "launchprice", "saleprice",
                      "gprs_handset_support", "hsdpa", "google_map", "video_call"]
