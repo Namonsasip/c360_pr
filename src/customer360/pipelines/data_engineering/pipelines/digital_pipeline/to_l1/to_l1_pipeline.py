e209bc86a9e8aef59dc57b479b655fa50f7d5268
paipayon <paipayon2@gmail.com>
2021-09-08 09:02:30 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l1/to_l1_pipeline.py
@@ -344,7 +344,7 @@ def digital_to_l1_digital_mobile_web_agg_daily(**kwargs):
                 inputs=
                 [
                     "l0_digital_mobile_web_daily_for_l1_digital_customer_web_category_agg_daily_catlv_1",
-                    "cxense",
+                    "l1_digital_cxense_traffic_complete_agg_daily_for_l1_dital_customer_web_agg_daily_catlv_1",
                     "params:category_level_1",
                     "params:l1_dital_customer_web_agg_daily_sql"
                 ],
