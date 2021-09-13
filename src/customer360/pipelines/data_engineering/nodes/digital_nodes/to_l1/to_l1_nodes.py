30c5dac1ba1a815b992aca8da51e3bcf0292cd0b
paipayon <paipayon2@gmail.com>
2021-09-13 16:41:22 +0700

    Update to_l1_nodes.py

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l1/to_l1_nodes.py
@@ -1436,14 +1436,14 @@ def digital_customer_cxense_agg_daily( cxen_traffic:pyspark.sql.DataFrame,cxen_m
         return get_spark_empty_df()
     #-------- Sum ---------#
     cxen_traffic = cxen_traffic.groupBy("subscription_identifier","mobile_no", "url", "event_partition_date").agg(f.sum("activetime").alias("duration"),f.count("*").alias("count_trans"))
-    # cxen_traffic = cxen_traffic.withColumn("event_partition_date",
-    #     f.concat(
-    #         f.substring(f.col("partition_date").cast("string"), 1, 4), 
-    #         f.lit("-"),
-    #         f.substring(f.col("partition_date").cast("string"), 5, 2), 
-    #         f.lit("-"),
-    #         f.substring(f.col("partition_date").cast("string"), 7, 2)
-    #         )).drop(*["partition_date"])
+    cxen_traffic = cxen_traffic.withColumn("event_partition_date",
+        f.concat(
+            f.substring(f.col("partition_date").cast("string"), 1, 4), 
+            f.lit("-"),
+            f.substring(f.col("partition_date").cast("string"), 5, 2), 
+            f.lit("-"),
+            f.substring(f.col("partition_date").cast("string"), 7, 2)
+            )).drop(*["partition_date"])
     #-------- Join Master ---------#
     logging.info("0")
     cxen_traffic = cxen_traffic.join(cxen_master,on=[cxen_traffic.url == cxen_master.site_url],how="left")
@@ -1456,7 +1456,7 @@ def digital_customer_cxense_agg_daily( cxen_traffic:pyspark.sql.DataFrame,cxen_m
     logging.info("2")
     cxen_traffic = cxen_traffic.select("subscription_identifier","mobile_no", "url", "category_level_1", "category_level_2", "category_level_3", "category_level_4", "count_trans","duration","event_partition_date")
     #-------- Join Profile ---------#
-    # cxen_traffic = cxen_traffic.join(customer_profile,on=[cxen_traffic.mobile_no == customer_profile.access_method_num,cxen_traffic.event_partition_date == customer_profile.event_partition_date],how="left")
+    cxen_traffic = cxen_traffic.join(customer_profile,on=[cxen_traffic.mobile_no == customer_profile.access_method_num,cxen_traffic.event_partition_date == customer_profile.event_partition_date],how="left")
     #-------- select column ---------#
     cxen_traffic = cxen_traffic.withColumn("upload_byte", f.lit(0).cast(LongType()))
     cxen_traffic = cxen_traffic.withColumn("download_byte", f.lit(0).cast(LongType()))
