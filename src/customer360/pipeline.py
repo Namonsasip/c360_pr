cff6c0dc2897bc1af220d50e8b098242dd3c8c2f
Anurak Homkaew <anurakho@ais.co.th>
2021-09-14 08:05:45 +0700

    update l1

--- src/customer360/pipeline.py
+++ src/customer360/pipeline.py
@@ -93,6 +93,7 @@ create_du_weekly_low_score_pipeline,
 )
 from .pipelines.data_engineering.pipelines.campaign_pipeline import (
     campaign_to_l1_pipeline,
+    campaign_to_l1_pipeline_hdfs,
     campaign_to_l2_pipeline,
     campaign_to_l3_pipeline,
     campaign_to_l4_pipeline,
@@ -403,6 +404,7 @@ def create_c360_pipeline(**kwargs) -> Dict[str, Pipeline]:
         "touchpoints_to_l3_pipeline_contact": touchpoints_to_l3_pipeline_contact(),
         "touchpoints_to_l4_pipeline": touchpoints_to_l4_pipeline(),
         "campaign_to_l1_pipeline": campaign_to_l1_pipeline(),
+        "campaign_to_l1_pipeline_hdfs": campaign_to_l1_pipeline_hdfs(),
         "campaign_to_l2_pipeline": campaign_to_l2_pipeline(),
         "campaign_to_l3_pipeline": campaign_to_l3_pipeline(),
         "campaign_to_l4_pipeline": campaign_to_l4_pipeline(),
