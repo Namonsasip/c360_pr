80763079d8d91352e3b1ebc8b114a38cef92d763
paipayon <paipayon2@gmail.com>
2021-09-16 17:11:44 +0700

    Update spark_dbfs_dataset.py

--- src/datasets/spark_dbfs_dataset.py
+++ src/datasets/spark_dbfs_dataset.py
@@ -1173,37 +1173,26 @@ class SparkDataSet(DefaultArgumentsMixIn, AbstractVersionedDataSet):
                 str_list = []
                 
                 p_domain_c360 = filepath.split('data/')[1].split('/')[0]
+                p_table_c360 = filepath.split(p_domain_c360 + '/')[1].split('/')[1]
+
                 if p_domain_c360 == 'DIGITAL':
                     p_table_name = self._target_table
-                    p_table_c360 = filepath.split(p_domain_c360 + '/')[1].split('/')[1]
-                    partition_date = str(line[partitionBy])
-                    row_count = str(line['count'])
-                    str_list.append("""P_C360_DOMAIN|'""" + p_domain_c360 + """'""")
-                    str_list.append("""P_C360_LEVEL|'""" + p_table_c360.split('_')[0] + """'""")
-                    str_list.append("""P_C360_DATE|'""" + partition_date + """'""")
-                    str_list.append(
-                        """P_C360_SOURCE_FULL|'""" + filepath + partitionBy + """=""" + partition_date + """/'""")
-                    str_list.append("""P_C360_SOURCE_PATH|'""" + filepath.split(p_table_c360)[0] + p_table_c360 + """/'""")
-                    str_list.append("""P_C360_PARTITION_NAME|'""" + partitionBy + """'""")
-                    str_list.append("""P_C360_COUNT|'""" + row_count + """'""")
-                    str_list_terget = '\n'.join([str(elem) for elem in str_list])
-                    file_name_c360 = "C360-" + p_domain_c360 + "-" + p_table_name + "-" + partition_date.replace('-',
-                                                                                                                '') + ".complete"
                 else:
-                    p_table_c360 = filepath.split(p_domain_c360 + '/')[1].split('/')[1]
-                    partition_date = str(line[partitionBy])
-                    row_count = str(line['count'])
-                    str_list.append("""P_C360_DOMAIN|'""" + p_domain_c360 + """'""")
-                    str_list.append("""P_C360_LEVEL|'""" + p_table_c360.split('_')[0] + """'""")
-                    str_list.append("""P_C360_DATE|'""" + partition_date + """'""")
-                    str_list.append(
-                        """P_C360_SOURCE_FULL|'""" + filepath + partitionBy + """=""" + partition_date + """/'""")
-                    str_list.append("""P_C360_SOURCE_PATH|'""" + filepath.split(p_table_c360)[0] + p_table_c360 + """/'""")
-                    str_list.append("""P_C360_PARTITION_NAME|'""" + partitionBy + """'""")
-                    str_list.append("""P_C360_COUNT|'""" + row_count + """'""")
-                    str_list_terget = '\n'.join([str(elem) for elem in str_list])
-                    file_name_c360 = "C360-" + p_domain_c360 + "-" + p_table_c360 + "-" + partition_date.replace('-',
-                                                                                                                '') + ".complete"
+                    p_table_name = p_table_c360
+
+                partition_date = str(line[partitionBy])
+                row_count = str(line['count'])
+                str_list.append("""P_C360_DOMAIN|'""" + p_domain_c360 + """'""")
+                str_list.append("""P_C360_LEVEL|'""" + p_table_c360.split('_')[0] + """'""")
+                str_list.append("""P_C360_DATE|'""" + partition_date + """'""")
+                str_list.append(
+                    """P_C360_SOURCE_FULL|'""" + filepath + partitionBy + """=""" + partition_date + """/'""")
+                str_list.append("""P_C360_SOURCE_PATH|'""" + filepath.split(p_table_c360)[0] + p_table_c360 + """/'""")
+                str_list.append("""P_C360_PARTITION_NAME|'""" + partitionBy + """'""")
+                str_list.append("""P_C360_COUNT|'""" + row_count + """'""")
+                str_list_terget = '\n'.join([str(elem) for elem in str_list])
+                file_name_c360 = "C360-" + p_domain_c360 + "-" + p_table_name + "-" + partition_date.replace('-',
+                                                                                                            '') + ".complete"
                 cmd = "hadoop fs -rm -skipTrash " + path_sendRun + file_name_c360
                 a = os.system(cmd)
                 cmd = "echo '" + str_list_terget + "'| hadoop fs -put - " + path_sendRun + file_name_c360
