b0f5adcd7982ac1a2cd88870794f59076fc0955b
chayaphn <chayaphn@ais.co.th>
2021-09-30 17:05:45 +0700

    disney_mlflow_model_version_training=671

--- src/du/scoring/scoring_nodes.py
+++ src/du/scoring/scoring_nodes.py
@@ -13,6 +13,7 @@ from kedro.io import CSVLocalDataSet
 import pandas as pd
 import numpy as np
 
+
 def format_time(elapsed):
     """
     Takes a time in seconds and returns a string hh:mm:ss
@@ -23,10 +24,10 @@ def format_time(elapsed):
     # Format as hh:mm:ss
     return str(datetime.timedelta(seconds=elapsed_rounded))
 
+
 def get_metrics_by_deciles(y_true,
                            y_proba
                            ) -> pd.DataFrame:
-
     report = pd.DataFrame({"y_true": y_true, "y_proba": y_proba},
                           columns=["y_true", "y_proba"])
 
@@ -50,6 +51,7 @@ def get_metrics_by_deciles(y_true,
     )
 
     return report
+
 
 # get latest available daily profile from c360 feature
 def l5_scoring_profile(
