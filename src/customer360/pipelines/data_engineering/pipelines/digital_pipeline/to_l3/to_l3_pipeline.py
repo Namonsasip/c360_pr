a1b87e43634f8331b0ed8621cbb9669729ab85d6
pakawat <pakawat.pln@gmail.com>
2021-10-06 23:31:54 +0700

    Update to_l3_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/digital_pipeline/to_l3/to_l3_pipeline.py
@@ -890,20 +890,20 @@ def digital_to_l3_web_monthly_feature_favorite(**kwargs):
                 tags=["digital_mobile_web_category_favorite_monthly_catlv_4"],
             ),
             ############# Fav by domain ###################
-            # node(
-            #     func=digital_mobile_web_favorite_by_category_monthly,
-            #     inputs=[
-            #         "l0_digital_mobile_web_monthly_for_l3_digital_customer_web_category_favorite_by_category_monthly_catlv_1",
-            #         "l1_digital_aib_categories_clean_for_l3_digital_customer_web_category_favorite_by_category_monthly_catlv_1",
-            #         "params:l3_digital_customer_web_favorite_by_category_sql",
-            #         "params:l3_digital_customer_web_favorite_by_category_sql_transaction",
-            #         "params:l3_digital_customer_web_favorite_by_category_sql_duration",
-            #         "params:l3_digital_customer_web_favorite_by_category_sql_volume",
-            #         "params:category_level_1"
-            #     ],
-            #     outputs="l3_digital_customer_web_category_favorite_by_category_monthly_catlv_1",
-            #     tags=["l3_digital_mobile_web_favorite_by_category_monthly"],
-            # ),
+            node(
+                func=digital_mobile_web_favorite_by_category_monthly,
+                inputs=[
+                    "l0_digital_mobile_web_monthly_for_l3_digital_customer_web_category_favorite_by_category_monthly_catlv_1",
+                    "l1_digital_aib_categories_clean",
+                    "params:l3_digital_customer_web_favorite_by_category_sql",
+                    "params:l3_digital_customer_web_favorite_by_category_sql_transaction",
+                    "params:l3_digital_customer_web_favorite_by_category_sql_duration",
+                    "params:l3_digital_customer_web_favorite_by_category_sql_volume",
+                    "params:category_level_1"
+                ],
+                outputs="l3_digital_customer_web_category_favorite_by_category_monthly_catlv_1",
+                tags=["l3_digital_mobile_web_favorite_by_category_monthly"],
+            ),
             # node(
             #     func=digital_mobile_web_favorite_by_category_monthly,
             #     inputs=[
