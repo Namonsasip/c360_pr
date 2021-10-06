72e96bb0b2c490a51106886f49f49cf15915be02
sirin571 <sirin571@epicc.postbox.in.th>
2021-10-06 15:20:03 +0700

    Revert "Update to_l3_nodes.py"
    This reverts commit 3d4c5fb16cca753a3eb98c493f9ad9b88bae9804.

--- src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/digital_nodes/to_l3/to_l3_nodes.py
@@ -231,7 +231,8 @@ def digital_mobile_web_category_favorite_monthly(web_category_agg_daily: pyspark
                                                  iab_mapping: Dict[str, Any],
                                                  web_sql_transaction: Dict[str, Any],
     # ---------------  sum traffic ------------------
-    web_category_agg_daily = web_category_agg_daily.join(iab_mapping, on=[web_category_agg_daily.category_name == iab_mapping.category_level_1], how="left").select(
+    web_category_agg_daily = web_category_agg_daily.join(iab_mapping, on=[web_category_agg_daily.category_name == iab_mapping.category_level_1], how="left")
+    web_category_agg_daily = web_category_agg_daily.select(
         "web_category_agg_daily.subscription_identifier",
         "web_category_agg_daily.mobile_no",
         "web_category_agg_daily.priority",
