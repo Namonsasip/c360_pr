53fb79296d1325c53406d8d3126078714594410a
NitipongChaikitiphornQB <77653671+NitipongChaikitiphornQB@users.noreply.github.com>
2021-10-21 12:21:27 +0700

    mod node

--- src/customer360/pipelines/data_engineering/nodes/usage_nodes/to_l1/to_l1_nodes.py
+++ src/customer360/pipelines/data_engineering/nodes/usage_nodes/to_l1/to_l1_nodes.py
@@ -346,7 +346,7 @@ def usage_data_postpaid_pipeline(input_df, sql) -> DataFrame:
     """
     :return:
     """
-    input_df = input_df.filter(F.col('partition_date').between('20210901', '20210919'))
+    input_df = input_df.filter(F.col('partition_date').between('20210910', '20210919'))
     ################################# Start Implementing Data availability checks #############################
     if check_empty_dfs([input_df]):
         return get_spark_empty_df()
