ec67d03701bdaef40cc5b5384a928f6dcb836533
spysert <61383598+spysert@users.noreply.github.com>
2021-11-10 16:54:06 +0700

    fixbug

--- src/customer360/pipelines/data_engineering/pipelines/billing_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/billing_pipeline/to_l3/to_l3_pipeline.py
@@ -221,15 +221,15 @@ def billing_l0_to_l3_pipeline(**kwargs):
                  "params:l3_bill_volume"],
                 "l3_billing_and_payments_monthly_bill_volume"
             ),
-
-            node(
-                billing_statement_hist_data_with_customer_profile,
-                ["l3_customer_profile_include_1mo_non_active_for_l3_billing_and_payments_monthly_missed_bills",
-                 "l0_billing_statement_history_monthly_for_l3_billing_and_payments_monthly_missed_bills", "params:l3_billing_and_payments_monthly_missed_bills_tbl"],
-                "billing_stat_hist_monthly_data_missed_bills_stg"
-            ),
+            ################################################
             # Monthly missed bills feature post-paid
             # Old version 20211110
+            # node(
+            #     billing_statement_hist_data_with_customer_profile,
+            #     ["l3_customer_profile_include_1mo_non_active_for_l3_billing_and_payments_monthly_missed_bills",
+            #      "l0_billing_statement_history_monthly_for_l3_billing_and_payments_monthly_missed_bills", "params:l3_billing_and_payments_monthly_missed_bills_tbl"],
+            #     "billing_stat_hist_monthly_data_missed_bills_stg"
+            # ),
             # node(
             #     billing_data_joined,
             #     ["billing_stat_hist_monthly_data_missed_bills_stg",
@@ -242,6 +242,7 @@ def billing_l0_to_l3_pipeline(**kwargs):
             #      "params:l3_missed_bills"],
             #     "l3_billing_and_payments_monthly_missed_bills"
             # ),
+            ################################################
             # Monthly missed bills feature post-paid
             node(
                 billing_statement_hist_data_with_customer_profile,
