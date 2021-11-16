de5e997aafae2d7bb95df345cc1ac3c99d6acddf
KraiwitSuwanmekQB <66598279+KraiwitSuwanmekQB@users.noreply.github.com>
2021-11-16 13:19:41 +0700

    Update to_l3_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/touchpoints_pipeline/to_l3/to_l3_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/touchpoints_pipeline/to_l3/to_l3_pipeline.py
@@ -37,34 +37,34 @@ from src.customer360.pipelines.data_engineering.nodes.touchpoints_nodes.to_l3.to
 def touchpoints_to_l3_pipeline_contact(**kwargs):
     return Pipeline(
         [
-            node(
-                dac_for_touchpoints_to_l3_pipeline_from_l1,
-                ["l1_touchpoints_aunjai_chatbot_features_to_l3_touchpoints_aunjai_chatbot_features",
-                 "params:l3_touchpoints_aunjai_chatbot_features_tbl",
-                 "params:exception_partition_list_for_l1_touchpoints_aunjai_chatbot_features_to_l3_touchpoints_aunjai_chatbot_features"],
-                "int_l3_touchpoints_aunjai_chatbot_features"
-            ),
-
-            node(
-                node_from_config,
-                ["int_l3_touchpoints_aunjai_chatbot_features",
-                 "params:l3_touchpoints_aunjai_chatbot_features"],
-                "l3_touchpoints_aunjai_chatbot_features"
-            ),
-
-            node(
-                dac_for_touchpoints_to_l3_pipeline_from_l1,
-                ["l1_touchpoints_contact_shop_features_for_l3_touchpoints_contact_shop_features",
-                 "params:l3_touchpoints_contact_shop_features_tbl",
-                 "params:exception_partition_list_for_l1_touchpoints_from_call_center_features_for_l3_touchpoints_contact_shop_features"],
-                "int_l3_touchpoints_contact_shop_features"
-            ),
-            node(
-                node_from_config,
-                ["int_l3_touchpoints_contact_shop_features",
-                 "params:l3_touchpoints_contact_shop_features"],
-                "l3_touchpoints_contact_shop_features"
-            ),
+            # node(
+            #     dac_for_touchpoints_to_l3_pipeline_from_l1,
+            #     ["l1_touchpoints_aunjai_chatbot_features_to_l3_touchpoints_aunjai_chatbot_features",
+            #      "params:l3_touchpoints_aunjai_chatbot_features_tbl",
+            #      "params:exception_partition_list_for_l1_touchpoints_aunjai_chatbot_features_to_l3_touchpoints_aunjai_chatbot_features"],
+            #     "int_l3_touchpoints_aunjai_chatbot_features"
+            # ),
+            #
+            # node(
+            #     node_from_config,
+            #     ["int_l3_touchpoints_aunjai_chatbot_features",
+            #      "params:l3_touchpoints_aunjai_chatbot_features"],
+            #     "l3_touchpoints_aunjai_chatbot_features"
+            # ),
+            #
+            # node(
+            #     dac_for_touchpoints_to_l3_pipeline_from_l1,
+            #     ["l1_touchpoints_contact_shop_features_for_l3_touchpoints_contact_shop_features",
+            #      "params:l3_touchpoints_contact_shop_features_tbl",
+            #      "params:exception_partition_list_for_l1_touchpoints_from_call_center_features_for_l3_touchpoints_contact_shop_features"],
+            #     "int_l3_touchpoints_contact_shop_features"
+            # ),
+            # node(
+            #     node_from_config,
+            #     ["int_l3_touchpoints_contact_shop_features",
+            #      "params:l3_touchpoints_contact_shop_features"],
+            #     "l3_touchpoints_contact_shop_features"
+            # ),
 
             node(
                 dac_for_touchpoints_to_l3_pipeline_from_l1,
@@ -81,20 +81,20 @@ def touchpoints_to_l3_pipeline_contact(**kwargs):
                 "l3_touchpoints_contact_call_center_features"
             ),
 
-            node(
-                dac_for_touchpoints_to_l3_pipeline_from_l1,
-                ["l1_touchpoints_contact_myais_features_to_l3_touchpoints_contact_myais_features",
-                 "params:l3_touchpoints_contact_myais_features_tbl",
-                 "params:exception_partition_list_for_l1_touchpoints_contact_myais_features_to_l3_touchpoints_contact_myais_features"],
-                "int_l3_touchpoints_contact_myais_features"
-            ),
-
-            node(
-                node_from_config,
-                ["int_l3_touchpoints_contact_myais_features",
-                 "params:l3_touchpoints_contact_myais_features"],
-                "l3_touchpoints_contact_myais_features"
-            ),
+            # node(
+            #     dac_for_touchpoints_to_l3_pipeline_from_l1,
+            #     ["l1_touchpoints_contact_myais_features_to_l3_touchpoints_contact_myais_features",
+            #      "params:l3_touchpoints_contact_myais_features_tbl",
+            #      "params:exception_partition_list_for_l1_touchpoints_contact_myais_features_to_l3_touchpoints_contact_myais_features"],
+            #     "int_l3_touchpoints_contact_myais_features"
+            # ),
+            #
+            # node(
+            #     node_from_config,
+            #     ["int_l3_touchpoints_contact_myais_features",
+            #      "params:l3_touchpoints_contact_myais_features"],
+            #     "l3_touchpoints_contact_myais_features"
+            # ),
         ]
     )
 
