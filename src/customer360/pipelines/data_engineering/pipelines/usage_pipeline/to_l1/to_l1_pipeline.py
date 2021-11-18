90902811fec95a3d6a88941a704f6ab0511140c7
NitipongChaikitiphornQB <77653671+NitipongChaikitiphornQB@users.noreply.github.com>
2021-11-18 08:51:48 +0700

    run gprs ins

--- src/customer360/pipelines/data_engineering/pipelines/usage_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/usage_pipeline/to_l1/to_l1_pipeline.py
@@ -152,21 +152,21 @@ def usage_to_l1_pipeline(**kwargs):
             #     "l1_usage_incoming_call_relation_sum_daily"
             # ),
 
-            node(
-                usage_outgoing_voice_sms_ins_pipeline,
-                ["l0_usage_ru_a_voice_sms_ins_usage_daily_outgoing",
-                 "l1_usage_favourite_number_master",
-                 "params:l1_usage_outgoing_call_relation_sum_daily"
-                 ],
-                "l1_usage_ru_a_voice_sms_ins_usage_daily"
-            ),
-
             # node(
-            #     usage_data_gprs_ins_pipeline,
-            #     ["l0_usage_gprs_usage_data_record",
-            #      "params:l1_usage_ru_a_gprs_cbs_usage_daily"],
-            #     "l1_usage_gprs_ins_usage_daily"
+            #     usage_outgoing_voice_sms_ins_pipeline,
+            #     ["l0_usage_ru_a_voice_sms_ins_usage_daily_outgoing",
+            #      "l1_usage_favourite_number_master",
+            #      "params:l1_usage_outgoing_call_relation_sum_daily"
+            #      ],
+            #     "l1_usage_ru_a_voice_sms_ins_usage_daily"
             # ),
+
+            node(
+                usage_data_gprs_ins_pipeline,
+                ["l0_usage_gprs_usage_data_record",
+                 "params:l1_usage_ru_a_gprs_cbs_usage_daily"],
+                "l1_usage_gprs_ins_usage_daily"
+            ),
 
             # node(merge_all_dataset_to_one_table, [
             #     'l1_usage_outgoing_call_relation_sum_daily', 'l1_usage_incoming_call_relation_sum_daily',
