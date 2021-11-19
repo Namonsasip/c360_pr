79d7d92f11c7d857790ea2be1948ae64b1322b43
KraiwitSuwanmekQB <66598279+KraiwitSuwanmekQB@users.noreply.github.com>
2021-11-19 10:35:09 +0700

    Update to_l1_pipeline.py

--- src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
+++ src/customer360/pipelines/data_engineering/pipelines/complaints_pipeline/to_l1/to_l1_pipeline.py
@@ -55,58 +55,58 @@ def complaints_to_l1_pipeline_survey(**kwargs):
                 "l1_complaints_survey_after_chatbot"
             ),
 
-            node(
-                dac_for_complaints_to_l1_pipeline,
-                ["l0_complaints_acc_atsr_outbound_survey_after_call",
-                 "l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_call",
-                 "params:l1_complaints_survey_after_call_tbl",
-                 "params:exception_partition_list_for_l0_complaints_acc_atsr_outbound_survey_after_call"],
-                ["int_l0_complaints_acc_atsr_outbound_survey_after_call",
-                 "int_l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_call"]
-            ),
-            node(
-                l1_complaints_survey_after_call,
-                [
-                    "int_l0_complaints_acc_atsr_outbound_survey_after_call",
-                    "int_l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_call"
-                ],
-                "l1_complaints_survey_after_call"
-            ),
-
-            node(
-                dac_for_complaints_to_l1_pipeline,
-                ["l0_complaints_complaints_acc_qmt_csi",
-                 "l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_store_visit",
-                 "params:l1_complaints_survey_after_store_visit_tbl",
-                 "params:exception_partition_list_for_l0_complaints_acc_qmt_csi_daily"],
-                ["int_l0_complaints_complaints_acc_qmt_csi",
-                 "int_l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_store_visit"]
-            ),
-            node(
-                l1_complaints_survey_after_store_visit,
-                ["int_l0_complaints_complaints_acc_qmt_csi",
-                 "int_l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_store_visit"
-                 ],
-                "l1_complaints_survey_after_store_visit"
-            ),
-
-            node(
-                dac_for_complaints_to_l1_pipeline,
-                ["l0_complaints_myais_es_log_survey_daily",
-                 "l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_myais",
-                 "params:l1_complaints_survey_after_myais_tbl",
-                 "params:exception_partition_list_for_l0_complaints_myais_es_log_survey_daily"],
-                ["int_l0_complaints_myais_es_log_survey_daily",
-                 "int_l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_myais"]
-            ),
-            node(
-                l1_complaints_survey_after_myais,
-                ["int_l0_complaints_myais_es_log_survey_daily",
-                 "params:l1_complaints_survey_after_myais",
-                 "int_l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_myais"
-                 ],
-                "l1_complaints_survey_after_myais"
-            ),
+            # node(
+            #     dac_for_complaints_to_l1_pipeline,
+            #     ["l0_complaints_acc_atsr_outbound_survey_after_call",
+            #      "l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_call",
+            #      "params:l1_complaints_survey_after_call_tbl",
+            #      "params:exception_partition_list_for_l0_complaints_acc_atsr_outbound_survey_after_call"],
+            #     ["int_l0_complaints_acc_atsr_outbound_survey_after_call",
+            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_call"]
+            # ),
+            # node(
+            #     l1_complaints_survey_after_call,
+            #     [
+            #         "int_l0_complaints_acc_atsr_outbound_survey_after_call",
+            #         "int_l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_call"
+            #     ],
+            #     "l1_complaints_survey_after_call"
+            # ),
+            #
+            # node(
+            #     dac_for_complaints_to_l1_pipeline,
+            #     ["l0_complaints_complaints_acc_qmt_csi",
+            #      "l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_store_visit",
+            #      "params:l1_complaints_survey_after_store_visit_tbl",
+            #      "params:exception_partition_list_for_l0_complaints_acc_qmt_csi_daily"],
+            #     ["int_l0_complaints_complaints_acc_qmt_csi",
+            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_store_visit"]
+            # ),
+            # node(
+            #     l1_complaints_survey_after_store_visit,
+            #     ["int_l0_complaints_complaints_acc_qmt_csi",
+            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_store_visit"
+            #      ],
+            #     "l1_complaints_survey_after_store_visit"
+            # ),
+            #
+            # node(
+            #     dac_for_complaints_to_l1_pipeline,
+            #     ["l0_complaints_myais_es_log_survey_daily",
+            #      "l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_myais",
+            #      "params:l1_complaints_survey_after_myais_tbl",
+            #      "params:exception_partition_list_for_l0_complaints_myais_es_log_survey_daily"],
+            #     ["int_l0_complaints_myais_es_log_survey_daily",
+            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_myais"]
+            # ),
+            # node(
+            #     l1_complaints_survey_after_myais,
+            #     ["int_l0_complaints_myais_es_log_survey_daily",
+            #      "params:l1_complaints_survey_after_myais",
+            #      "int_l1_customer_profile_union_daily_feature_for_l1_complaints_survey_after_myais"
+            #      ],
+            #     "l1_complaints_survey_after_myais"
+            # ),
 
         ]
     )
